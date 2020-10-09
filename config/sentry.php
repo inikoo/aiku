3bd754767ace189791e4c227bcef49b76fad06d8
Raul Perusquia <raul@inikoo.com>
2020-10-09 15:40:28 +0800

    fix problem sentry

--- config/sentry.php
+++ config/sentry.php
@@ -1,11 +1,15 @@
 <?php
 
+
+
 return [
 
     'dsn'     => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
 
     // capture release as git sha
-    'release' => trim(exec('git --git-dir '.base_path('.git').' log --pretty="%h" -n1 HEAD')),
+    'release' =>( is_dir(base_path('.git')) ? trim(exec('git --git-dir '.base_path('.git').' log --pretty="%h" -n1 HEAD')) : date('Ymd')),
+
+
 
     'breadcrumbs'      => [
         // Capture Laravel logs in breadcrumbs
