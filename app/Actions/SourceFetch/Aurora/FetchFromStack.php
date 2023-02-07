6b09f1f4fe8845c91b4fed92ecaf2431b4c69cb6
Raul Perusquia <raul@inikoo.com>
2023-02-08 03:17:58 +0800

    fetch from stack

---
+++ app/Actions/SourceFetch/Aurora/FetchFromStack.php
@@ -0,0 +1,103 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 07 Feb 2023 19:01:11 Malaysia Time, Ubud, Bali
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\SourceFetch\Aurora;
+
+
+use App\Actions\WithTenantsArgument;
+use App\Managers\Tenant\SourceTenantManager;
+use App\Services\Tenant\SourceTenantService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use JetBrains\PhpStorm\NoReturn;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+
+class FetchFromStack
+{
+    use AsAction;
+    use WithTenantsArgument;
+
+
+    public string $commandSignature = 'fetch:stack {tenants?*} {--M|max_run_time=0 : max fun time in seconds}';
+    protected float $startTime;
+    protected ?float $maxRunTime = 0;
+
+    #[NoReturn] public function handle(SourceTenantService $tenantSource): void
+    {
+        $query = DB::connection('aurora')
+            ->table('pika_fetch')
+            ->where('error', 'No');
+        $query->orderByDesc('created_at');
+
+        foreach ($query->get() as $jobData) {
+            switch ($jobData->model) {
+                case 'Customer':
+                    $res = FetchCustomers::run($tenantSource, $jobData->model_id);
+                    break;
+                case 'Stock':
+                    $res = FetchStocks::run($tenantSource, $jobData->model_id);
+                    break;
+                case 'Order':
+                    $res = FetchOrders::run($tenantSource, $jobData->model_id);
+                    break;
+                case 'DeliveryNote':
+                    $res = FetchDeliveryNotes::run($tenantSource, $jobData->model_id);
+                    break;
+                case 'Invoice':
+                    $res = FetchInvoices::run($tenantSource, $jobData->model_id);
+                    break;
+                case 'Product':
+                    $res = FetchProducts::run($tenantSource, $jobData->model_id);
+                    break;
+                default:
+                    continue 2;
+            }
+
+            if ($res) {
+                DB::connection('aurora')->table('pika_fetch')->where('id', $jobData->id)->delete();
+            } else {
+                DB::connection('aurora')->table('pika_fetch')->where('id', $jobData->id)->update(['error', 'Yes']);
+            }
+
+            if ($this->maxRunTime > 0 and (microtime(true) - $this->startTime) > $this->maxRunTime) {
+                return;
+            }
+        }
+    }
+
+
+    public function asCommand(Command $command): int
+    {
+        $this->startTime = microtime(true);
+
+        $tenants  = $this->getTenants($command);
+        $exitCode = 0;
+
+        foreach ($tenants as $tenant) {
+            $result = (int)$tenant->run(
+            /**
+             * @throws \Illuminate\Contracts\Container\BindingResolutionException
+             */ function () use ($command) {
+                $tenantSource = app(SourceTenantManager::class)->make(Arr::get(tenant()->source, 'type'));
+                $tenantSource->initialisation(tenant());
+
+                $this->handle($tenantSource);
+            }
+            );
+
+            if ($result !== 0) {
+                $exitCode = $result;
+            }
+        }
+
+        return $exitCode;
+    }
+
+
+}
