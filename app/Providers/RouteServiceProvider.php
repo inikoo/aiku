c74d15b73fb3dc7c457a2b680cd05110ba63302a
Raul Perusquia <raul@inikoo.com>
2023-02-21 04:00:21 +0800

    fix ide helper docs + composer + npm updates

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,7 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
