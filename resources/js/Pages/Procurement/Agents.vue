610e6bae1b6a2465b9f08523a3344844f9de231d
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-02-24 08:10:28 +0100

    PKA-40 Agents.vue

---
+++ resources/js/Pages/Procurement/Agents.vue
@@ -0,0 +1,30 @@
+<!--
+  - Author: Jonathan Lopez Sanchez <jonathan@ancientwisdom.biz>
+  - Created: Wed, 22 Feb 2023 12:20:38 Central European Standard Time, Malaga, Spain
+  - Copyright (c) 2023, Inikoo LTD
+  -->
+
+<script setup>
+import {Head, Link} from '@inertiajs/vue3';
+import PageHeading from '@/Components/Headings/PageHeading.vue';
+import Table from '@/Components/Table/Table.vue';
+
+defineProps(['agents', 'title', 'pageHead']);
+
+</script>
+
+<template layout="App">
+    <Head :title="title"/>
+    <PageHeading :data="pageHead"></PageHeading>
+    <Table :resource="agents" class="mt-5">
+
+
+        <template #cell(code)="{ item: agent }">
+            <Link :href="route('procurement.agents.index',[agent.id,agent.slug])">
+                {{ agent.code }}
+            </Link>
+        </template>
+
+    </Table>
+</template>
+
