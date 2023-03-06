c75299bab35cc7f57334a70a413f5fb0a7a4b0be
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-03-06 14:15:21 +0100

    PKA-61

---
+++ resources/js/Components/SearchBar.vue
@@ -0,0 +1,75 @@
+<!--
+  - Author: Jonathan Lopez Sanchez <jonathan@ancientwisdom.biz>
+  - Created: Mon, 06 Mar 2023 13:45:35 Central European Standard Time, Malaga, Spain
+  - Copyright (c) 2023, Inikoo LTD
+  -->
+
+<template>
+    <TransitionRoot :show="open" as="template" @after-leave="query = ''" appear>
+        <Dialog as="div" class="relative z-10" @close="open = false">
+            <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+                <div class="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
+            </TransitionChild>
+            <div class="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
+                <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 scale-95" enter-to="opacity-100 scale-100" leave="ease-in duration-200" leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
+                    <DialogPanel class="mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
+                        <Combobox @update:modelValue="onSelect">
+                            <div class="relative">
+                                <MagnifyingGlassIcon class="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                <ComboboxInput class="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm" placeholder="Search..." @change="query = $event.target.value" />
+                            </div>
+
+                            <ComboboxOptions v-if="filteredPeople.length > 0" static class="max-h-72 scroll-py-2 overflow-y-auto py-2 text-sm text-gray-800">
+                                <ComboboxOption v-for="person in filteredPeople" :key="person.id" :value="person" as="template" v-slot="{ active }">
+                                    <li :class="['cursor-default select-none px-4 py-2', active && 'bg-indigo-600 text-white']">
+                                        {{ person.name }}
+                                    </li>
+                                </ComboboxOption>
+                            </ComboboxOptions>
+
+                            <p v-if="query !== '' && filteredPeople.length === 0" class="p-4 text-sm text-gray-500">No people found.</p>
+                        </Combobox>
+                    </DialogPanel>
+                </TransitionChild>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+import { MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxOptions,
+    ComboboxOption,
+    Dialog,
+    DialogPanel,
+    TransitionChild,
+    TransitionRoot,
+} from '@headlessui/vue'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "../../private/pro-light-svg-icons";
+library.add(
+    faSearch
+)
+const people = [
+    { id: 1, name: 'Leslie Alexander', url: '#' },
+    // More people...
+]
+
+const open = ref(true)
+const query = ref('')
+const filteredPeople = computed(() =>
+    query.value === ''
+        ? []
+        : people.filter((person) => {
+            return person.name.toLowerCase().includes(query.value.toLowerCase())
+        })
+)
+
+function onSelect(person) {
+    window.location = person.url
+}
+</script>
--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -8,16 +8,16 @@
 
 
 <script setup>
-import {ref, inject} from 'vue';
+import { ref, inject } from "vue";
 import {
     Menu,
     MenuButton,
     MenuItem,
-    MenuItems,
-} from '@headlessui/vue';
-import Breadcrumbs from '@/Components/Navigation/Breadcrumbs.vue';
-import {trans} from 'laravel-vue-i18n';
-import {library} from '@fortawesome/fontawesome-svg-core';
+    MenuItems
+} from "@headlessui/vue";
+import Breadcrumbs from "@/Components/Navigation/Breadcrumbs.vue";
+import { trans } from "laravel-vue-i18n";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import {
     faHome,
     faDollyFlatbedAlt,
@@ -34,8 +34,8 @@ import {
     faParachuteBox,
     faDollyEmpty,
     faShoppingCart,
-    faAbacus, faChevronDown, faCube, faGlobe,
-} from '@/../private/pro-light-svg-icons';
+    faAbacus, faChevronDown, faCube, faGlobe
+} from "@/../private/pro-light-svg-icons";
 
 library.add(
     faHome,
@@ -60,15 +60,17 @@ library.add(
     faGlobe
 );
 
-const initialiseApp = inject('initialiseApp');
+const initialiseApp = inject("initialiseApp");
 const layout = initialiseApp();
 
 const sidebarOpen = ref(false);
 
-import {Disclosure, DisclosureButton, DisclosurePanel} from '@headlessui/vue';
-import AppLeftSideBar from '@/Layouts/AppLeftSideBar.vue';
-import AppShopNavigation from '@/Layouts/AppShopNavigation.vue';
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import AppLeftSideBar from "@/Layouts/AppLeftSideBar.vue";
+import AppShopNavigation from "@/Layouts/AppShopNavigation.vue";
+import SearchBar from "@/Components/SearchBar.vue";
 
+const showComponent = ref(false);
 
 </script>
 
@@ -95,34 +97,46 @@ import AppShopNavigation from '@/Layouts/AppShopNavigation.vue';
 
                         <div class="flex items-center">
 
-                            <button type="button" class=" p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
-                                <span class="sr-only">{{ trans('Search') }}</span>
-                                <font-awesome-icon aria-hidden="true" icon="fa-regular fa-search" size="lg"/>
-
+                            <button v-on:click="showComponent = true" type="button"
+                                    class=" p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
+                                <span class="sr-only">{{ trans("Search") }}</span>
+                                <font-awesome-icon aria-hidden="true" icon="fa-regular fa-search" size="lg">
+                                </font-awesome-icon>
+                                <SearchBar v-if="showComponent" v-on:close="showComponent">
+                                </SearchBar>
                             </button>
 
-                            <button type="button" class=" p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
-                                <span class="sr-only">{{ trans('View notifications') }}</span>
-                                <font-awesome-icon aria-hidden="true" icon="fa-regular fa-bell" size="lg"/>
+                            <button type="button"
+                                    class=" p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
+                                <span class="sr-only">{{ trans("View notifications") }}</span>
+                                <font-awesome-icon aria-hidden="true" icon="fa-regular fa-bell" size="lg" />
 
                             </button>
 
                             <Menu as="div" class="relative ml-3 mr-6 hidden lg:block ">
                                 <div>
-                                    <MenuButton class="flex max-w-xs items-center rounded-full bg-gray-100 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                        <span class="sr-only">{{ trans('Open user menu') }}</span>
+                                    <MenuButton
+                                        class="flex max-w-xs items-center rounded-full bg-gray-100 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                        <span class="sr-only">{{ trans("Open user menu") }}</span>
 
                                         <img v-if="$page.props.auth.user['avatar']" class="h-8 w-8 rounded-full"
                                              :src="$page.props.auth.user['avatar']??null"
-                                             alt=""/>
+                                             alt="" />
 
                                     </MenuButton>
                                 </div>
-                                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
-                                            leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                                    <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <transition enter-active-class="transition ease-out duration-100"
+                                            enter-from-class="transform opacity-0 scale-95"
+                                            enter-to-class="transform opacity-100 scale-100"
+                                            leave-active-class="transition ease-in duration-75"
+                                            leave-from-class="transform opacity-100 scale-100"
+                                            leave-to-class="transform opacity-0 scale-95">
+                                    <MenuItems
+                                        class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                         <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                                            <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                                            <a :href="item.href"
+                                               :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name
+                                                }}</a>
                                         </MenuItem>
                                     </MenuItems>
                                 </transition>
@@ -132,9 +146,11 @@ import AppShopNavigation from '@/Layouts/AppShopNavigation.vue';
                     </div>
 
 
-                    <button type="button" class="border-l border-gray-400 px-4 text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500 lg:hidden" @click="sidebarOpen = true">
+                    <button type="button"
+                            class="border-l border-gray-400 px-4 text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500 lg:hidden"
+                            @click="sidebarOpen = true">
                         <span class="sr-only">Open sidebar</span>
-                        <font-awesome-icon aria-hidden="true" icon="fa-regular fa-bars  " size="lg"/>
+                        <font-awesome-icon aria-hidden="true" icon="fa-regular fa-bars  " size="lg" />
 
                     </button>
 
@@ -154,7 +170,7 @@ import AppShopNavigation from '@/Layouts/AppShopNavigation.vue';
                 <div class="border-t border-gray-700 pt-4 pb-3">
                     <div class="flex items-center px-5">
                         <div class="flex-shrink-0">
-                            <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt=""/>
+                            <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
                         </div>
                         <div class="ml-3">
 
@@ -162,29 +178,32 @@ import AppShopNavigation from '@/Layouts/AppShopNavigation.vue';
                             <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
                         </div>
 
-                        <button type="button" class=" p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
-                            <span class="sr-only">{{ trans('View notifications') }}</span>
-                            <font-awesome-icon aria-hidden="true" icon="fa-regular fa-bell" size="lg"/>
+                        <button type="button"
+                                class=" p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
+                            <span class="sr-only">{{ trans("View notifications") }}</span>
+                            <font-awesome-icon aria-hidden="true" icon="fa-regular fa-bell" size="lg" />
 
                         </button>
 
                     </div>
                     <div class="mt-3 space-y-1 px-2">
                         <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href"
-                                          class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">{{ item.name }}
+                                          class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
+                            {{ item.name }}
                         </DisclosureButton>
                     </div>
                 </div>
             </DisclosurePanel>
         </Disclosure>
 
-        <div class="mt-10 hidden lg:fixed lg:inset-y-0 lg:flex lg:w-64 lg:flex-col lg:border-r lg:border-gray-200 lg:bg-gray-100 lg:pt-0 lg:pb-4">
-            <AppLeftSideBar/>
+        <div
+            class="mt-10 hidden lg:fixed lg:inset-y-0 lg:flex lg:w-64 lg:flex-col lg:border-r lg:border-gray-200 lg:bg-gray-100 lg:pt-0 lg:pb-4">
+            <AppLeftSideBar />
         </div>
         <div class="flex flex-col lg:pl-64">
             <main>
-                <Breadcrumbs :breadcrumbs="$page.props.breadcrumbs??[]"/>
-                <slot/>
+                <Breadcrumbs :breadcrumbs="$page.props.breadcrumbs??[]" />
+                <slot />
             </main>
         </div>
 
