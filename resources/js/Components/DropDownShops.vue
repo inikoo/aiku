b02cd1b67e1931be2430377a0a410d2366c23746
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-03-06 10:56:43 +0100

    PKA-61

--- resources/js/Components/DropDownShops.vue
+++ resources/js/Components/DropDownShops.vue
@@ -19,7 +19,7 @@ defineEmits(['change:shop']);
                     leave-active-class="transition ease-in duration-75"
                     leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
             <MenuItems
-                class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                class="absolute right-32 z-10 mt-2  origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div class="py-1">
                     <MenuItem v-slot="{ active }" v-for="(shop, shopIndex) in shops" :key="shopIndex">
                         <button
