954d0b74354e489d2a52c659846f202807532ea3
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-03-09 16:02:39 +0100

    PKA-73 1º Reduce code of SearchBar.vue - 2º Find error when entenring word - 3º Find if there are more search errors in the web

--- resources/js/Components/SearchBar.vue
+++ resources/js/Components/SearchBar.vue
@@ -21,33 +21,10 @@ import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
 const searchResults = computed(() => usePage().props.searchResults)
 
-const people = [
-    {
-        id: 1,
-        name: 'Leslie Alexander',
-        phone: '1-493-747-9031',
-        email: 'lesliealexander@example.com',
-        role: 'Co-Founder / CEO',
-        url: 'https://example.com',
-        profileUrl: '#',
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    // More people...
-]
-
-
-const recent = [people[0]]
 
 const open = ref(true)
 const query = ref('')
-const filteredPeople = computed(() =>
-    query.value === ''
-        ? []
-        : people.filter((person) => {
-            return person.name.toLowerCase().includes(query.value.toLowerCase())
-        })
-)
+
 
 function onSelect(person) {
     window.location = person.url
@@ -97,7 +74,7 @@ function handleKeyDown() {
                                 <input type="text" v-model="searchInput" @input="handleSearchInput" @keydown="handleKeyDown"
                                 class="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm" placeholder="Search..." @change="query = $event.target.value">
                             </div>
-                            <ComboboxOptions v-if="query === '' || filteredPeople.length > 0" class="flex divide-x divide-gray-100" as="div" static hold>
+                            <ComboboxOptions  class="flex divide-x divide-gray-100" as="div" static hold>
                                 <div :class="['max-h-96 min-w-0 flex-auto scroll-py-4 overflow-y-auto px-6 py-4', activeOption && 'sm:h-96']">
                                     <h2 v-if="query === ''" class="mt-2 mb-4 text-xs font-semibold text-gray-500">Recent searches</h2>
                                     <div hold class="-mx-2 text-sm text-gray-700">
@@ -139,12 +116,6 @@ function handleKeyDown() {
                                     </div>
                                 </div>
                             </ComboboxOptions>
-
-                            <div v-if="query !== '' && filteredPeople.length === 0" class="py-14 px-6 text-center text-sm sm:px-14">
-
-                                <p class="mt-4 font-semibold text-gray-900">No people found</p>
-                                <p class="mt-2 text-gray-500">We couldn’t find anything with that term. Please try again.</p>
-                            </div>
                         </Combobox>
                     </DialogPanel>
                 </TransitionChild>
@@ -152,71 +123,3 @@ function handleKeyDown() {
         </Dialog>
     </TransitionRoot>
 </template>
-
-<!--<template>
-    <TransitionRoot :show="open" as="template" @after-leave="query = ''" appear>
-        <Dialog as="div" class="relative z-10" @close="open = false">
-            <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
-                <div class="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
-            </TransitionChild>
-            <div class="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
-                <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 scale-95" enter-to="opacity-100 scale-100" leave="ease-in duration-200" leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
-                    <DialogPanel class="mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
-                        <Combobox @update:modelValue="onSelect">
-                            <div class="relative">
-                                <ComboboxInput class="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm" placeholder="Search..." @change="query = $event.target.value" />
-                            </div>
-
-                            <ComboboxOptions v-if="filteredPeople.length > 0" static class="max-h-72 scroll-py-2 overflow-y-auto py-2 text-sm text-gray-800">
-                                <ComboboxOption v-for="person in filteredPeople" :key="person.id" :value="person" as="template" v-slot="{ active }">
-                                    <li :class="['cursor-default select-none px-4 py-2', active && 'bg-indigo-600 text-white']">
-                                        {{ person.name }}
-                                    </li>
-                                </ComboboxOption>
-                            </ComboboxOptions>
-
-                            <p v-if="query !== '' && filteredPeople.length === 0" class="p-4 text-sm text-gray-500">No people found.</p>
-                        </Combobox>
-                    </DialogPanel>
-                </TransitionChild>
-            </div>
-        </Dialog>
-    </TransitionRoot>
-</template>
-
-<script setup>
-import { computed, ref } from 'vue'
-import {
-    Combobox,
-    ComboboxInput,
-    ComboboxOptions,
-    ComboboxOption,
-    Dialog,
-    DialogPanel,
-    TransitionChild,
-    TransitionRoot,
-} from '@headlessui/vue'
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "../../private/pro-light-svg-icons";
-library.add(
-    faSearch
-)
-const people = [
-    { id: 1, name: 'Leslie Alexander', url: '#' },
-    // More people...
-]
-
-const open = ref(true)
-const query = ref('')
-const filteredPeople = computed(() =>
-    query.value === ''
-        ? []
-        : people.filter((person) => {
-            return person.name.toLowerCase().includes(query.value.toLowerCase())
-        })
-)
-
-function onSelect(person) {
-    window.location = person.url
-}
-</script>-->
