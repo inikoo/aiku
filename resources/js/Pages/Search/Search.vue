bc5099b7e88ee3416b774399250391ab9ddf0328
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-03-09 14:55:38 +0100

    PKA-72 ShowSearch

---
+++ resources/js/Pages/Search/Search.vue
@@ -0,0 +1,18 @@
+<!--
+  - Author: Jonathan Lopez Sanchez <jonathan@ancientwisdom.biz>
+  - Created: Wed, 22 Feb 2023 10:36:47 Central European Standard Time, Malaga, Spain
+  - Copyright (c) 2023, Inikoo LTD
+  -->
+
+<script setup>
+
+import SearchBarTest from "@/Components/SearchBarTest.vue";
+
+</script>
+
+<template layout="App">
+
+    <SearchBarTest/>
+
+</template>
+
--- routes/tenant/search.php
+++ routes/tenant/search.php
@@ -7,6 +7,10 @@
 
 
 use App\Actions\Search\RunSearch;
+use App\Actions\Search\ShowSearch;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', RunSearch::class)->name('run');
+//Route::get('/', RunSearch::class)->name('run');
+
+
+Route::get('/', ShowSearch::class)->name('show');
