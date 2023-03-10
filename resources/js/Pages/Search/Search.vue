be288e2f07a22ec1ffb59bf01ae84d8168135a36
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-03-10 08:22:15 +0100

    PKA-74 Make a search bar in the Search Route
    input

--- resources/js/Pages/Search/Search.vue
+++ resources/js/Pages/Search/Search.vue
@@ -6,13 +6,28 @@
 
 <script setup>
 
-import SearchBarTest from "@/Components/SearchBarTest.vue";
 
 </script>
 
 <template layout="App">
 
-    <SearchBarTest/>
+    <!--
+
+    crear un input,
+
+    añadir una de las tablas : https://tailwindui.com/components/application-ui/lists/stacked-lists | two column with avatar
+
+    cuando busques en uno de ellos te devuelva los resultados por ejemplo del jbb
+
+
+    -->
+
+    <div class="h-screen mt-20 ml-14">
+        <label for="" class=" text-sm font-medium leading-6 text-gray-900">Search</label>
+        <div class="mt-2">
+            <input type="search" name="search" id="search" class="block w-auto  rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Type..." />
+        </div>
+    </div>
 
 </template>
 
