48a118249bf5e5c01dc91bf022d523872284d886
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-03-16 12:20:46 +0100

    PKA-124 Create/Edit/EM Buttons . Users model

---
+++ app/Actions/SysAdmin/User/UI/HasUIUser.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Author: Jonathan Lopez Sanchez <jonathan@ancientwisdom.biz>
+ * Created: Mon, 13 Mar 2023 15:06:29 Central European Standard Time, Malaga, Spain
+ * Copyright (c) 2023, Inikoo LTD
+ */
+
+namespace App\Actions\SysAdmin\User\UI;
+
+use App\Actions\UI\SysAdmin\SysAdminDashboard;
+use App\Models\SysAdmin\User;
+
+trait HasUIUser
+{
+    public function getBreadcrumbs(User $user): array
+    {
+        return array_merge(
+            (new SysAdminDashboard())->getBreadcrumbs(),
+            [
+                'sysadmin.users.show' => [
+                    'route'           => 'sysadmin.users.show',
+                    'routeParameters' => $user->id,
+                    'index'           => [
+                        'route'   => 'sysadmin.users.index',
+                        'overlay' => __("users' list")
+                    ],
+                    'modelLabel'      => [
+                        'label' => __('user')
+                    ],
+                    'name'            => '@'.$user->username,
+
+                ],
+            ]
+        );
+    }
+}
