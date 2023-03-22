06543a2a2096c9eb14fb3639d1bc0f677fc34b15
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-03-22 13:42:22 +0100

    PKA-131 V2 for indexModels

---
+++ resources/js/types/family.ts
@@ -0,0 +1,17 @@
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 21 Mar 2023 00:11:49 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+export interface Family {
+    slug:string,
+    shop_id: string,
+    state: string
+    code: string
+    name: string
+    description: string
+    created_at: string
+    updated_at: string
+    department_id: string
+}
