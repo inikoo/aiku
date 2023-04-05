8445f80e7b629f7a0fee5f808eb5dd7f4dea6a9c
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-04-05 11:05:08 +0200

    PKA-228 showcase vue

--- resources/js/Pages/Marketing/ProductShowcase.vue
+++ resources/js/Pages/Marketing/ProductShowcase.vue
@@ -25,82 +25,82 @@ const product = props.data.data;
         </div>
         <table class="lg:mr-6 lg:ml-0 md:ml-28 md:mr-28">
             <tr>
-                <td class="px-1 border border-gray-500">All sales since: Thu 30 Mar 2023</td>
+                <td class="px-1 border border-gray-200">All sales since: Thu 30 Mar 2023</td>
             </tr>
             <tr>
-                <td class="px-1 border border-gray-500">€0.00</td>
-                <td class="px-1 border border-gray-500">0</td>
-                <td class="px-1 border border-gray-500">0 (NA)</td>
+                <td class="px-1 border border-gray-200">€0.00</td>
+                <td class="px-1 border border-gray-200">0</td>
+                <td class="px-1 border border-gray-200">0 (NA)</td>
             </tr>
             <tr>
-                <td class="px-1 border border-gray-500">2023</td>
-                <td class="px-1 border border-gray-500">2022</td>
-                <td class="px-1 border border-gray-500">2021</td>
-                <td class="px-1 border border-gray-500">2020</td>
-                <td class="px-1 border border-gray-500">2019</td>
+                <td class="px-1 border border-gray-200">2023</td>
+                <td class="px-1 border border-gray-200">2022</td>
+                <td class="px-1 border border-gray-200">2021</td>
+                <td class="px-1 border border-gray-200">2020</td>
+                <td class="px-1 border border-gray-200">2019</td>
             </tr>
             <tr>
-                <td class="px-1 border border-gray-500">€0</td>
-                <td class="px-1 border border-gray-500">€0</td>
-                <td class="px-1 border border-gray-500">€0</td>
-                <td class="px-1 border border-gray-500">€0</td>
-                <td class="px-1 border border-gray-500">€0</td>
+                <td class="px-1 border border-gray-200">€0</td>
+                <td class="px-1 border border-gray-200">€0</td>
+                <td class="px-1 border border-gray-200">€0</td>
+                <td class="px-1 border border-gray-200">€0</td>
+                <td class="px-1 border border-gray-200">€0</td>
             </tr>
             <tr>
-                <td class="px-1 border border-gray-500">0</td>
-                <td class="px-1 border border-gray-500">0</td>
-                <td class="px-1 border border-gray-500">0</td>
-                <td class="px-1 border border-gray-500">0</td>
-                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-200">0</td>
+                <td class="px-1 border border-gray-200">0</td>
+                <td class="px-1 border border-gray-200">0</td>
+                <td class="px-1 border border-gray-200">0</td>
+                <td class="px-1 border border-gray-200">0</td>
             </tr>
             <tr>
-                <td class="px-1 border border-gray-500">2Q 23</td>
-                <td class="px-1 border border-gray-500">1Q 23</td>
-                <td class="px-1 border border-gray-500">4Q 22</td>
-                <td class="px-1 border border-gray-500">3Q 22</td>
-                <td class="px-1 border border-gray-500">2Q 22</td>
+                <td class="px-1 border border-gray-200">2Q 23</td>
+                <td class="px-1 border border-gray-200">1Q 23</td>
+                <td class="px-1 border border-gray-200">4Q 22</td>
+                <td class="px-1 border border-gray-200">3Q 22</td>
+                <td class="px-1 border border-gray-200">2Q 22</td>
             </tr>
             <tr>
-                <td class="px-1 border border-gray-500">€0</td>
-                <td class="px-1 border border-gray-500">€0</td>
-                <td class="px-1 border border-gray-500">€0</td>
-                <td class="px-1 border border-gray-500">€0</td>
-                <td class="px-1 border border-gray-500">€0</td>
+                <td class="px-1 border border-gray-200">€0</td>
+                <td class="px-1 border border-gray-200">€0</td>
+                <td class="px-1 border border-gray-200">€0</td>
+                <td class="px-1 border border-gray-200">€0</td>
+                <td class="px-1 border border-gray-200">€0</td>
             </tr>
             <tr>
-                <td class="px-1 border border-gray-500">0</td>
-                <td class="px-1 border border-gray-500">0</td>
-                <td class="px-1 border border-gray-500">0</td>
-                <td class="px-1 border border-gray-500">0</td>
-                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-200">0</td>
+                <td class="px-1 border border-gray-200">0</td>
+                <td class="px-1 border border-gray-200">0</td>
+                <td class="px-1 border border-gray-200">0</td>
+                <td class="px-1 border border-gray-200">0</td>
             </tr>
         </table>
 
         <table class="table-auto lg:ml-10 lg:mr-6 md:ml-28 md:mr-28">
             <tbody>
             <tr>
-                <td class="px-1 border-b border-t border-gray-500">Active</td>
-                <td class="px-1 border-b border-t border-gray-500">Online</td>
+                <td class="px-1 border-b border-t border-gray-200">Active</td>
+                <td class="px-1 border-b border-t border-gray-200">Online</td>
             </tr>
             <tr>
-                <td class="px-1 border-b border-gray-500">Stock available:</td>
-                <td class="px-1 border-b border-gray-500">50</td>
+                <td class="px-1 border-b border-gray-200">Stock available:</td>
+                <td class="px-1 border-b border-gray-200">50</td>
             </tr>
             <tr>
-                <td class="px-1 border-b border-gray-500">Cost</td>
-                <td class="px-1 border-b border-gray-500">€10.05 (€1.01/bag)</td>
+                <td class="px-1 border-b border-gray-200">Cost</td>
+                <td class="px-1 border-b border-gray-200">€10.05 (€1.01/bag)</td>
             </tr>
             <tr>
-                <td class="px-1 border-b border-gray-500">Price</td>
-                <td class="px-1 border-b border-gray-500">€30.50 (€3.05/bag) margin 67.0%</td>
+                <td class="px-1 border-b border-gray-200">Price</td>
+                <td class="px-1 border-b border-gray-200">€30.50 (€3.05/bag) margin 67.0%</td>
             </tr>
             <tr>
-                <td class="px-1 border-b border-gray-500">RRP</td>
-                <td class="px-1 border-b border-gray-500">€7.35/bag margin 58.5%</td>
+                <td class="px-1 border-b border-gray-200">RRP</td>
+                <td class="px-1 border-b border-gray-200">€7.35/bag margin 58.5%</td>
             </tr>
             <tr>
-                <td class="px-1 border-b border-gray-500">Webpage</td>
-                <td class="px-1 border-b border-gray-500">jsb-03</td>
+                <td class="px-1 border-b border-gray-200">Webpage</td>
+                <td class="px-1 border-b border-gray-200">jsb-03</td>
             </tr>
             </tbody>
         </table>
