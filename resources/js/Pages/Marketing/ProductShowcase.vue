15a8ab9f0a6af592886981b42a343dad0efe4970
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-04-05 09:51:25 +0200

    PKA-228 showcase vue

--- resources/js/Pages/Marketing/ProductShowcase.vue
+++ resources/js/Pages/Marketing/ProductShowcase.vue
@@ -7,7 +7,6 @@
 <script setup lang="ts">
 
 
-
 const props = defineProps<{
     data: object
 }>();
@@ -18,30 +17,111 @@ const product = props.data.data;
 
 
 <template>
+    <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2  xl:grid-cols-3 gap-4 ">
+        <div class="m-auto">
+            <img :src="route('media.show',product.image_id)" alt="product main image"
+                 class=" sm:max-w-xs  md:max-w-xs lg:h-64 content-center"
+            />
+        </div>
+        <table >
+            <tr>
+                <td class="px-1 border border-gray-500">All sales since: Thu 30 Mar 2023</td>
+            </tr>
+            <tr>
+                <td class="px-1 border border-gray-500">€0.00</td>
+                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-500">0 (NA)</td>
+            </tr>
+            <tr>
+                <td class="px-1 border border-gray-500">2023</td>
+                <td class="px-1 border border-gray-500">2022</td>
+                <td class="px-1 border border-gray-500">2021</td>
+                <td class="px-1 border border-gray-500">2020</td>
+                <td class="px-1 border border-gray-500">2019</td>
+            </tr>
+            <tr>
+                <td class="px-1 border border-gray-500">€0</td>
+                <td class="px-1 border border-gray-500">€0</td>
+                <td class="px-1 border border-gray-500">€0</td>
+                <td class="px-1 border border-gray-500">€0</td>
+                <td class="px-1 border border-gray-500">€0</td>
+            </tr>
+            <tr>
+                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-500">0</td>
+            </tr>
+            <tr>
+                <td class="px-1 border border-gray-500">2Q 23</td>
+                <td class="px-1 border border-gray-500">1Q 23</td>
+                <td class="px-1 border border-gray-500">4Q 22</td>
+                <td class="px-1 border border-gray-500">3Q 22</td>
+                <td class="px-1 border border-gray-500">2Q 22</td>
+            </tr>
+            <tr>
+                <td class="px-1 border border-gray-500">€0</td>
+                <td class="px-1 border border-gray-500">€0</td>
+                <td class="px-1 border border-gray-500">€0</td>
+                <td class="px-1 border border-gray-500">€0</td>
+                <td class="px-1 border border-gray-500">€0</td>
+            </tr>
+            <tr>
+                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-500">0</td>
+                <td class="px-1 border border-gray-500">0</td>
+            </tr>
+        </table>
 
-    <div class="grid grid-cols-3  ">
-        <img
-            :src="route('media.show',product.image_id)"
-            alt="product main image" class="object-cover object-center ml-10"
-        />
-            <div class="grid text-center">
-                <div>
-                    hello
-                </div>
-                <div>
-                    hello
-                </div>
-            </div>
-            <div class="grid text-center" >
-                <div>
-                    hello
-                </div>
-                <div>
-                    hello
-                </div>
-            </div>
+        <table class="table-auto lg:mr-6">
+            <tbody>
+            <tr>
+                <td class="px-1 border-b border-gray-500">Active</td>
+                <td class="px-1 border-b border-gray-500">Online</td>
+            </tr>
+            <tr>
+                <td class="px-1 border-b border-gray-500">Stock available:</td>
+                <td class="px-1 border-b border-gray-500">50</td>
+            </tr>
+            <tr>
+                <td class="px-1 border-b border-gray-500">Cost</td>
+                <td class="px-1 border-b border-gray-500">€10.05 (€1.01/bag)</td>
+            </tr>
+            <tr>
+                <td class="px-1 border-b border-gray-500">Price</td>
+                <td class="px-1 border-b border-gray-500">€30.50 (€3.05/bag) margin 67.0%</td>
+            </tr>
+            <tr>
+                <td class="px-1 border-b border-gray-500">RRP</td>
+                <td class="px-1 border-b border-gray-500">€7.35/bag margin 58.5%</td>
+            </tr>
+            <tr>
+                <td class="px-1 border-b border-gray-500">Webpage</td>
+                <td class="px-1 border-b border-gray-500">jsb-03</td>
+            </tr>
+            </tbody>
+        </table>
+
     </div>
-
-
-
+<!--
+    <table class="table-auto">
+        <tbody>
+        <tr>
+            <td class="px-1">Primera fila</td>
+        </tr>
+        <tr>
+            <td class="px-1">Segunda fila</td>
+        </tr>
+        <tr>
+            <td class="px-1">Tercera fila</td>
+        </tr>
+        <tr>
+            <td class="px-1">Cuarta fila</td>
+        </tr>
+        </tbody>
+    </table>
+-->
 </template>
