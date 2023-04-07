eb73cadc43bf317205b77a3d3e872a6e8bf707cf
Raul Perusquia <raul@inikoo.com>
2023-04-07 10:08:32 +0800

    PKA-203 Add Favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,12 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-    <link rel="icon" href="{{ url('art/favicon-new.png') }}">
+
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('favicons/aiku-favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('favicons/aiku-favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="48x48" href="{{ url('favicons/aiku-favicon-48x48.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('favicons/aiku-favicon-180x180.png') }}">
+
 
     @routes
     @vite('resources/js/app.js')
