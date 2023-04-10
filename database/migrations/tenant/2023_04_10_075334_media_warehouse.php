c45fbc969e0701ce8405d6ec1a405c9df8faa164
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-04-10 09:54:45 +0200

    PKA-233 Database Tables for Showcase

---
+++ database/migrations/tenant/2023_04_10_075334_media_warehouse.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Author: Jonathan Lopez Sanchez <jonathan@ancientwisdom.biz>
+ * Created: Mon, 10 Apr 2023 09:54:06 Central European Summer Time, Malaga, Spain
+ * Copyright (c) 2023, Inikoo LTD
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up()
+    {
+        Schema::create('media_warehouse', function (Blueprint $table) {
+            $table->unsignedInteger('warehouse_id')->index();
+            $table->string('type')->index();
+            $table->foreign('warehouse_id')->references('id')->on('warehouses');
+            $table->unsignedBigInteger('media_id')->index();
+            $table->foreign('media_id')->references('id')->on('media');
+            $table->unique(['warehouse_id', 'media_id']);
+            $table->string('owner_type')->index();
+            $table->unsignedInteger('owner_id');
+            $table->boolean('public')->default(false)->index();
+
+            $table->timestampsTz();
+            $table->index(['owner_type', 'owner_id']);
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('media_warehouse');
+    }
+};
