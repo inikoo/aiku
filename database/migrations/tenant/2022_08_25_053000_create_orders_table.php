9b4079a082369577a9e4213c147f2c5db1f38416
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-04-10 09:49:09 +0200

    PKA-233 Database Tables for Showcase

--- database/migrations/tenant/2022_08_25_053000_create_orders_table.php
+++ database/migrations/tenant/2022_08_25_053000_create_orders_table.php
@@ -18,6 +18,8 @@ return new class () extends Migration {
 
             $table->unsignedInteger('customer_id')->index();
             $table->foreign('customer_id')->references('id')->on('customers');
+            $table->unsignedBigInteger('image_id')->nullable();
+            $table->foreign('image_id')->references('id')->on('media');
 
             $table->unsignedInteger('customer_client_id')->nullable()->index();
             $table->foreign('customer_client_id')->references('id')->on('customer_clients');
