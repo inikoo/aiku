aa92e1b55a79c356034c0fc1e5ee934e72e90e48
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-04-14 10:06:59 +0200

    PKA-254 inline create dialog

--- resources/js/Components/NewItems/NewWarehouseArea.vue
+++ resources/js/Components/NewItems/NewWarehouseArea.vue
@@ -24,15 +24,18 @@ const open = ref(true)
                         <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
                             <div>
                                 <div class="mt-3 text-center sm:mt-5">
-                                    <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Payment successful</DialogTitle>
-                                    <div class="mt-2">
-                                        <p class="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.</p>
-                                    </div>
+                                    <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">New Warehouse Area</DialogTitle>
                                 </div>
                             </div>
-                            <div class="mt-5 sm:mt-6">
-                                <button type="button" class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="open = false">Go back to dashboard</button>
-                            </div>
+                            <form class="mt-5 sm:flex sm:items-center">
+                                <div class="w-full sm:max-w-xs">
+                                    <label for="email" class="sr-only">Email</label>
+                                    <input type="email" name="email" id="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Code" />
+                                    <br>
+                                    <input type="email" name="email" id="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Name" />
+                                </div>
+                                <button type="submit" class="mt-3 inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto">Save</button>
+                            </form>
                         </DialogPanel>
                     </TransitionChild>
                 </div>
