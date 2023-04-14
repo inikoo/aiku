9654cad3d36fe69e2154a168320486bc48d3c9d8
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-04-14 12:01:27 +0200

    PKA-254 inline create dialog

--- resources/js/Components/NewItem.vue
+++ resources/js/Components/NewItem.vue
@@ -40,8 +40,13 @@ const open = ref(true)
                                     <br>
                                     <input type="email" name="email" id="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Name" />
                                 </div>
-                                <button type="submit" class="capitalize mt-3 inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto">{{data.saveLabel}}</button>
                             </form>
+                            <div class="flex justify-around mt-4">
+                                <button type="submit" class="capitalize mt-3 inline-flex w-full items-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto">{{data.cancelLabel}}</button>
+                                <button type="submit" class="capitalize mt-3 inline-flex w-full items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto">{{data.saveLabel}}</button>
+                            </div>
+
+
                         </DialogPanel>
                     </TransitionChild>
                 </div>
