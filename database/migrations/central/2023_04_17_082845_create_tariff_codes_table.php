ef37f4e4d237f8bfe4951f938e68d7543e99e02b
artha <artha@aw-advantage.com>
2023-04-17 17:05:33 +0800

    PKA-278 Create barcode Model

---
+++ database/migrations/central/2023_04_17_082845_create_tariff_codes_table.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Author: Artha <artha@aw-advantage.com>
+ * Created: Mon, 17 Apr 2023 17:04:29 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up(): void
+    {
+        Schema::create('tariff_codes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('section');
+            $table->string('hs_code')->unique();
+            $table->text('description');
+            $table->unsignedInteger('parent_id')->index();
+            $table->foreign('parent_id')->references('id')->on('tariff_codes');
+            $table->unsignedSmallInteger('level')->index();
+            $table->timestampsTz();
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tariff_codes');
+    }
+};
