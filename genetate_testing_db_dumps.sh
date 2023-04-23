63e9e057b214ffaa79cab96df4d1afa26813290e
Raul Perusquia <raul@inikoo.com>
2023-04-24 05:14:06 +0800

    setup testing

---
+++ genetate_testing_db_dumps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PURPLE='\033[01;35m'
+UNDERLINE='\033[4m'
+ITALIC='\e[3m'
+NONE='\033[00m'
+
+DB=aiku_test
+
+echo -e "✨ Resetting database ${ITALIC}${DB}${NONE}"
+dropdb -f --if-exists ${DB}
+createdb ${DB}
+echo "🌱 Migrating and seeding database"
+php artisan --env=testing migrate --path=database/migrations/central  --database=central
+php artisan --env=testing db:seed
+echo -e "💾 Saving ${PURPLE}fresh_with_assets.dump${NONE}"
+pg_dump -Fc -f "tests/datasets/db_dumps/fresh_with_assets.dump" ${DB}
