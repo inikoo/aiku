924d16c36f7c67a81160d5dada7c4b97ab70d438
Raul Perusquia <raul@inikoo.com>
2023-04-24 13:12:55 +0800

    PKA-418 DevOps Tests

---
+++ tests/Feature/DevOpsTest.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 24 Apr 2023 20:04:32 Malaysia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+
+test('create first deployment', function () {
+
+})->todo();
