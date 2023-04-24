3ff041127dc0d873798e083238595516b0908ead
Raul Perusquia <raul@inikoo.com>
2023-04-24 19:34:47 +0800

    PKA-421 Assets Testing

---
+++ tests/Feature/AssetsTest.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 24 Apr 2023 20:04:32 Malaysia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+beforeAll(fn () => loadDB('fresh_with_assets.dump'));
+
+test('countries exists', function () {
+
+})->todo();
