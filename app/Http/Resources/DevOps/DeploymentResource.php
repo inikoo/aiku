67179f6aafad45ca20875a112fc300e3c99abddd
artha <artha@aw-advantage.com>
2023-04-25 11:31:47 +0800

    PKA-423 Create DeploymentResource

---
+++ app/Http/Resources/DevOps/DeploymentResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\DevOps;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/***
+ *
+ * @property $version
+ * @property $hash
+ * @property $state
+ * @property $data
+ *
+ */
+class DeploymentResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'version' => $this->version,
+            'hash' => $this->hash,
+            'state' => $this->state,
+            'data' => $this->data
+        ];
+    }
+}
