f78ecb434609fb8028d7763629cf51247e75a7b9
artha <artha@aw-advantage.com>
2023-04-25 11:13:59 +0800

    PKA-421 Assets Testing

--- tests/Feature/AssetsTest.php
+++ tests/Feature/AssetsTest.php
@@ -6,6 +6,12 @@
  */
 
 use App\Models\Assets\Country;
+use App\Models\Assets\Timezone;
+use App\Models\Assets\Language;
+use App\Models\Assets\TariffCode;
+use App\Models\Assets\Currency;
+use App\Actions\Helpers\CurrencyExchange\GetCurrencyExchange;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 beforeAll(fn () => loadDB('fresh_with_assets.dump'));
 
@@ -17,8 +23,77 @@ it('has countries', function (string $countryCode) {
 ]);
 
 it('is no countries with code', function (string $countryCode) {
-    $country= Country::where('code', $countryCode)->firstOrFail();
-    expect($country->id)->toThrow(Exception::class);
+    expect(function() use($countryCode) {
+        Country::where('code', $countryCode)->firstOrFail();
+    })->toThrow(ModelNotFoundException::class);
 })->with([
     'XX','es',''
-])->todo('Fix toThrow');
+]);
+
+it('has timezones', function (string $timezone) {
+    $timezone = Timezone::where('name', $timezone)->firstOrFail();
+    expect($timezone->id)->toBeInt();
+})->with([
+    'Asia/Jakarta','Pacific/Norfolk'
+]);
+
+it('is no timezones with code', function (string $countryCode) {
+    expect(function() use($countryCode) {
+        Timezone::where('name', $countryCode)->firstOrFail();
+    })->toThrow(ModelNotFoundException::class);
+})->with([
+    'XX', ''
+]);
+
+it('has languages', function (string $language) {
+    $language = Language::where('code', $language)->firstOrFail();
+    expect($language->id)->toBeInt();
+})->with([
+    'en-us', 'id', 'zu'
+]);
+
+it('is no languages with code', function (string $language) {
+    expect(function() use($language) {
+        Language::where('name', $language)->firstOrFail();
+    })->toThrow(ModelNotFoundException::class);
+})->with([
+    'xx', ''
+]);
+
+it('has tariff codes', function (string $tariffCode) {
+    $tariffCode = TariffCode::where('hs_code', $tariffCode)->firstOrFail();
+    expect($tariffCode->id)->toBeInt();
+})->with([
+    '0101', '520100', '999999'
+]);
+
+it('is no tariff codes with code', function (string $tariffCode) {
+    expect(function() use($tariffCode) {
+        TariffCode::where('hs_code', $tariffCode)->firstOrFail();
+    })->toThrow(ModelNotFoundException::class);
+})->with([
+    '0000', ''
+]);
+
+it('has currencies', function (string $currency) {
+    $currency = Currency::where('code', $currency)->firstOrFail();
+    expect($currency->id)->toBeInt();
+})->with([
+    'IDR', 'AFN', 'ZWL'
+]);
+
+it('is no currency with code', function (string $currency) {
+    expect(function() use($currency) {
+        Currency::where('code', $currency)->firstOrFail();
+    })->toThrow(ModelNotFoundException::class);
+})->with([
+    'XXX', ''
+]);
+
+it('currency exchange test', function () {
+    $baseCurrency   = Currency::where('code', 'IDR')->firstOrFail();
+    $targetCurrency = Currency::where('code', 'USD')->firstOrFail();
+
+    $currencyExchange = GetCurrencyExchange::run($baseCurrency, $targetCurrency);
+    expect($currencyExchange)->toBeFloat();
+});
