047ebc43b369152ed2992db4b34ee8752fbd9578
artha <artha@aw-advantage.com>
2023-04-26 15:18:13 +0800

    PKA-430 Continue procurement test

--- tests/Feature/ProcurementTest.php
+++ tests/Feature/ProcurementTest.php
@@ -5,65 +5,61 @@
  * Copyright (c) 2023, Raul A Perusquia Flores
  */
 
-namespace Tests\Feature;
-
 use App\Actions\Procurement\Agent\StoreAgent;
+use App\Actions\Procurement\PurchaseOrder\StorePurchaseOrder;
 use App\Actions\Procurement\Supplier\StoreSupplier;
+use App\Actions\Procurement\SupplierProduct\StoreSupplierProduct;
 use App\Models\Helpers\Address;
 use App\Models\Procurement\Agent;
 use App\Models\Procurement\Supplier;
+use App\Models\Procurement\SupplierProduct;
 use App\Models\Tenancy\Tenant;
 
 beforeAll(fn () => loadDB('d3_with_tenants.dump'));
 
+beforeEach(function () {
+    $this->tenant = Tenant::where('slug', 'agb')->first();
+    $this->tenant->makeCurrent();
+});
+
 test('create agents', function () {
-    $tenant = Tenant::where('slug', 'agb')->first();
-
-    $tenant->makeCurrent();
-
-    $agent = StoreAgent::make()->action($tenant, Agent::factory()->definition(), Address::factory()->definition());
+    $agent = StoreAgent::make()->action($this->tenant, Agent::factory()->definition(), Address::factory()->definition());
     $this->assertModelExists($agent);
 });
 
 test('number of agents should be one', function () {
-    $tenant = Tenant::where('slug', 'agb')->first();
-    $tenant->makeCurrent();
 
-    $this->assertEquals(1, $tenant->procurementStats->number_agents);
-    $this->assertEquals(1, $tenant->procurementStats->number_active_agents);
+
+    $this->assertEquals(1, $this->tenant->procurementStats->number_agents);
+    $this->assertEquals(1, $this->tenant->procurementStats->number_active_agents);
 });
 
 test('create another agents', function () {
-    $tenant = Tenant::where('slug', 'agb')->first();
-    $tenant->makeCurrent();
 
-    $agent = StoreAgent::make()->action($tenant, Agent::factory()->definition(), Address::factory()->definition());
+
+    $agent = StoreAgent::make()->action($this->tenant, Agent::factory()->definition(), Address::factory()->definition());
 
     $this->assertModelExists($agent);
 });
 
 test('number of agents should be two', function () {
-    $tenant = Tenant::where('slug', 'agb')->first();
-    $tenant->makeCurrent();
 
-    $this->assertEquals(2, $tenant->procurementStats->number_agents);
-    $this->assertEquals(2, $tenant->procurementStats->number_active_agents);
+    $this->assertEquals(2, $this->tenant->procurementStats->number_agents);
+    $this->assertEquals(2, $this->tenant->procurementStats->number_active_agents);
 });
 
 test('create supplier', function () {
-     $tenant = Tenant::where('slug', 'agb')->first();
-     $tenant->makeCurrent();
 
-     $supplier = StoreSupplier::make()->action($tenant, Supplier::factory()->definition());
+
+     $supplier = StoreSupplier::make()->action($this->tenant, Supplier::factory()->definition());
 
      $this->assertModelExists($supplier);
 });
 
 test('create agent with the supplier', function () {
-    $tenant = Tenant::where('slug', 'agb')->first();
-    $tenant->makeCurrent();
 
-    $agent = StoreAgent::make()->action($tenant, Agent::factory()->definition(), Address::factory()->definition());
+
+    $agent = StoreAgent::make()->action($this->tenant, Agent::factory()->definition(), Address::factory()->definition());
 
     $supplier = StoreSupplier::make()->action($agent, Supplier::factory()->definition());
 
@@ -71,18 +67,36 @@ test('create agent with the supplier', function () {
 });
 
 test('number of agents should be three', function () {
-    $tenant = Tenant::where('slug', 'agb')->first();
-    $tenant->makeCurrent();
 
-    $this->assertEquals(3, $tenant->procurementStats->number_agents);
-    $this->assertEquals(3, $tenant->procurementStats->number_active_agents);
+
+    $this->assertEquals(3, $this->tenant->procurementStats->number_agents);
+    $this->assertEquals(3, $this->tenant->procurementStats->number_active_agents);
 });
 
 test('number of supplier should be two', function () {
 
-    $tenant = Tenant::where('slug', 'agb')->first();
-    $tenant->makeCurrent();
 
-    $this->assertEquals(2, $tenant->procurementStats->number_suppliers);
-    $this->assertEquals(2, $tenant->procurementStats->number_active_suppliers);
+    $this->assertEquals(2, $this->tenant->procurementStats->number_suppliers);
+    $this->assertEquals(2, $this->tenant->procurementStats->number_active_suppliers);
+});
+
+test('create supplier product', function () {
+
+    $supplier = Supplier::latest()->first();
+
+    $purchaseOrder = StoreSupplierProduct::run($supplier, [
+        'code' => 'asdf',
+        'cost' => rand(111, 9999)
+    ]);
+
+    $this->assertModelExists($purchaseOrder);
+});
+
+test('create purchase order', function () {
+
+    $supplier = Supplier::latest()->first();
+
+    $purchaseOrder = StorePurchaseOrder::run($supplier, []);
+
+    $this->assertModelExists($purchaseOrder);
 });
