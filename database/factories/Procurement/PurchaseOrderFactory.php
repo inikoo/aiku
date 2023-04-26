1140f17c88fc6f213a4fccbcfcc780dfbf29956a
artha <artha@aw-advantage.com>
2023-04-26 16:58:12 +0800

    PKA-435 put the rules in procurements

---
+++ database/factories/Procurement/PurchaseOrderFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories\Procurement;
+
+use App\Models\Assets\Currency;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Procurement\PurchaseOrder>
+ */
+class PurchaseOrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $currency = Currency::where('code', 'USD')->firstOrFail();
+
+        return [
+            'number' => fake()->numberBetween(100, 999),
+            'provider_id' => 2,
+            'provider_type' => '?',
+            'date' => fake()->date,
+            'currency_id' => $currency->id,
+            'exchange' => 1
+        ];
+    }
+}
