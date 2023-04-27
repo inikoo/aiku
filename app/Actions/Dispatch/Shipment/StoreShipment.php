dcfb1d4f75e6c044553ed0527a5467ce15958065
artha <artha@aw-advantage.com>
2023-04-27 16:52:10 +0800

    PKA-443 dispatch test

--- app/Actions/Dispatch/Shipment/StoreShipment.php
+++ app/Actions/Dispatch/Shipment/StoreShipment.php
@@ -10,11 +10,15 @@ namespace App\Actions\Dispatch\Shipment;
 use App\Actions\Dispatch\Shipment\Hydrators\ShipmentHydrateUniversalSearch;
 use App\Models\Dispatch\DeliveryNote;
 use App\Models\Dispatch\Shipment;
+use App\Models\Marketing\OfferCampaign;
+use App\Models\Marketing\Shop;
 use Lorisleiva\Actions\Concerns\AsAction;
+use Lorisleiva\Actions\Concerns\WithAttributes;
 
 class StoreShipment
 {
     use AsAction;
+    use WithAttributes;
 
     public function handle(DeliveryNote $parent, array $modelData): Shipment
     {
@@ -24,5 +28,21 @@ class StoreShipment
         ShipmentHydrateUniversalSearch::dispatch($shipment);
 
         return $shipment;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'code' => ['required', 'unique:tenant.shippers', 'between:2,9', 'alpha'],
+            'name' => ['required', 'max:250', 'string']
+        ];
+    }
+
+    public function action(DeliveryNote $parent, array $objectData): Shipment
+    {
+        $this->setRawAttributes($objectData);
+        $validatedData = $this->validateAttributes();
+
+        return $this->handle($parent, $validatedData);
     }
 }
---
+++ database/factories/Dispatch/ShipperFactory.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Author: Artha <artha@aw-advantage.com>
+ * Created: Thu, 27 Apr 2023 16:51:52 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace Database\Factories\Dispatch;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Dispatch\Shipper>
+ */
+class ShipperFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'code' => fake()->lexify(),
+            'name' => fake()->name,
+            'contact_name' => fake()->name,
+            'company_name' => fake()->company,
+            'email' => fake()->email,
+            'phone' => fake()->phoneNumber,
+            'website' => fake()->url,
+            'tracking_url' => fake()->url,
+        ];
+    }
+}
