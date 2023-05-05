a14f8d564f778e36b128558a0687300299c19878
artha <artha@aw-advantage.com>
2023-05-05 16:55:54 +0800

    PKA-486 Sales Test Customer Order Submit, Delete, Unsubmit

---
+++ app/Actions/Procurement/PurchaseOrder/RemoveItemPurchaseOrder.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Author: Artha <artha@aw-advantage.com>
+ * Created: Fri, 05 May 2023 16:55:25 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Procurement\PurchaseOrder;
+
+use App\Models\Procurement\PurchaseOrder;
+use App\Models\Procurement\PurchaseOrderItem;
+use App\Models\Procurement\SupplierProduct;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Lorisleiva\Actions\Concerns\WithAttributes;
+
+class RemoveItemPurchaseOrder
+{
+    use AsAction;
+    use WithAttributes;
+
+    public function handle(PurchaseOrderItem $purchaseOrderItem): void
+    {
+        $purchaseOrderItem->delete();
+    }
+}
