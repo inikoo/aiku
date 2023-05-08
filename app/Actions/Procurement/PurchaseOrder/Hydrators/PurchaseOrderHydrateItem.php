b69e2636bd4b02714dcf0cf0b4ac6c03a0d4b032
artha <artha@aw-advantage.com>
2023-05-08 15:12:12 +0800

    PKA-500 Sum cost items in purchase orders

--- app/Actions/Procurement/PurchaseOrder/Hydrators/PurchaseOrderHydrateItem.php
+++ app/Actions/Procurement/PurchaseOrder/Hydrators/PurchaseOrderHydrateItem.php
@@ -10,6 +10,7 @@ namespace App\Actions\Procurement\PurchaseOrder\Hydrators;
 
 use App\Actions\WithTenantJob;
 use App\Models\Procurement\PurchaseOrder;
+use App\Models\Procurement\SupplierProduct;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -20,9 +21,22 @@ class PurchaseOrderHydrateItem  implements ShouldBeUnique {
     public function handle(PurchaseOrder $purchaseOrder): void
     {
         $stats = [
-            'number_of_items' => $purchaseOrder->items()->count()
+            'number_of_items' => $purchaseOrder->items()->count(),
+            'cost_items' => $this->getTotalCostItem($purchaseOrder)
         ];
 
         $purchaseOrder->update($stats);
+    }
+
+    public function getTotalCostItem(PurchaseOrder $purchaseOrder): float
+    {
+        $costItems = 0;
+        $supplierProduct = $purchaseOrder->provider->products->first();
+
+        foreach ($purchaseOrder->items as $item) {
+            $costItems += $item->unit_price * $supplierProduct->cost;
+        }
+
+        return $costItems;
     }
 }
