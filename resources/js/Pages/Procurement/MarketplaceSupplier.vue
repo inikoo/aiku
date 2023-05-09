89443be1abefe86f1c3f9ead4117f45466a41810
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-05-09 11:30:55 +0200

    PKA-267 IndexPurchaseOrders

---
+++ resources/js/Pages/Procurement/MarketplaceSupplier.vue
@@ -0,0 +1,72 @@
+<!--
+  -  Author: Raul Perusquia <raul@inikoo.com>
+  -  Created: Thu, 15 Sept 2022 16:07:20 Malaysia Time, Kuala Lumpur, Malaysia
+  -  Copyright (c) 2022, Raul A Perusquia Flores
+  -->
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import PageHeading from '@/Components/Headings/PageHeading.vue';
+import Tabs from "@/Components/Navigation/Tabs.vue";
+import {computed, defineAsyncComponent, ref} from "vue";
+import ModelDetails from "@/Pages/ModelDetails.vue";
+import {useTabChange} from "@/Composables/tab-change";
+import TableSuppliers from "@/Pages/Tables/TableSuppliers.vue";
+import TableSupplierProducts from "@/Pages/Tables/TableSupplierProducts.vue";
+
+const ModelChangelog = defineAsyncComponent(() => import('@/Pages/ModelChangelog.vue'))
+
+const props = defineProps<{
+    title: string,
+    pageHead: object,
+    tabs: {
+        current: string;
+        navigation: object;
+    },
+    suppliers?: object
+    supplier_products?: object,
+}>()
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {
+    faInventory,
+    faWarehouse,
+    faPersonDolly,
+    faParachuteBox,
+    faTruck,
+    faTerminal,
+    faCameraRetro
+} from "@/../private/pro-light-svg-icons";
+
+library.add(
+    faInventory,
+    faWarehouse,
+    faPersonDolly,
+    faParachuteBox,
+    faTruck,
+    faTerminal,
+    faCameraRetro
+);
+
+let currentTab = ref(props.tabs.current);
+const handleTabUpdate = (tabSlug) => useTabChange(tabSlug, currentTab);
+
+const component = computed(() => {
+
+    const components = {
+        suppliers: TableSuppliers,
+        supplier_products: TableSupplierProducts,
+        details: ModelDetails,
+        history: ModelChangelog
+    };
+    return components[currentTab.value];
+
+});
+
+</script>
+
+<template layout="App">
+    <Head :title="title" />
+    <PageHeading :data="pageHead"></PageHeading>
+    <Tabs :current="currentTab" :navigation="tabs['navigation']" @update:tab="handleTabUpdate"/>
+    <component :is="component" :data="props[currentTab]"></component>
+</template>
+
