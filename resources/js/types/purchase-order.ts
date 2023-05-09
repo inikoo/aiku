055fe486abbbe2ce2b029b12f1019931a57c1719
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-05-09 11:28:49 +0200

    PKA-267 IndexPurchaseOrders

---
+++ resources/js/types/purchase-order.ts
@@ -0,0 +1,19 @@
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 21 Mar 2023 00:11:49 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+export interface PurchaseOrder {
+    slug:string,
+    provider_id: string,
+    provider_type: string,
+    number: string
+    state: string
+    status: string
+    created_at: string
+    updated_at: string
+
+
+
+}
