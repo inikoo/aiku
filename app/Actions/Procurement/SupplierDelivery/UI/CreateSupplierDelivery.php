885ec23dc33cf48bf4265c6b4e8c8735a867b7b6
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-05-10 15:19:34 +0200

    PKA-523 Adding all the blueprint missing

--- app/Actions/Procurement/SupplierDelivery/UI/CreateSupplierDelivery.php
+++ app/Actions/Procurement/SupplierDelivery/UI/CreateSupplierDelivery.php
@@ -36,7 +36,7 @@ class CreateSupplierDelivery extends InertiaAction
                 'formData' => [
                     'blueprint' => [
                         [
-                            'title'  => __('supplier delivery'),
+                            'title'  => __(' create supplier delivery'),
                             'fields' => [
 
                                 'number' => [
