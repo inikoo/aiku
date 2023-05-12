61606637f6e482e4f206a9d25af040b599b2aa9f
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-05-12 13:56:28 +0200

    PKA-492 IndexMarketplace Suppliers

---
+++ app/Http/Resources/Procurement/MarketplaceSupplierResource.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Fri, 12 May 2023 17:20:09 Malaysia Time, Airport, Bali, Id
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Http\Resources\Procurement;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property string $code
+ * @property string $name
+ * @property string $slug
+ * @property string $created_at
+ * @property string $updated_at
+ * @property numeric $number_suppliers_deliveries
+ * @property numeric $number_supplier_products
+ * @property numeric $number_purchase_orders
+ */
+class MarketplaceSupplierResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'code'                          => $this->code,
+            'name'                          => $this->name,
+            'slug'                          => $this->slug,
+            'number_suppliers_deliveries'   => $this->number_suppliers_deliveries,
+            'number_supplier_products'      => $this->number_supplier_products,
+            'number_purchase_orders'        => $this->number_purchase_orders,
+
+        ];
+    }
+}
