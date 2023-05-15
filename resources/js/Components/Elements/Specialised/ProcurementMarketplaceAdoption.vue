4e4ed8816d872cfb6d287b0d94d67f6b885b036f
Raul Perusquia <raul@inikoo.com>
2023-05-15 18:02:38 +0800

    PKA-266 Procurement, work on agent/tenants ownership

--- resources/js/Components/Elements/Specialised/ProcurementMarketplaceAdoption.vue
+++ resources/js/Components/Elements/Specialised/ProcurementMarketplaceAdoption.vue
@@ -6,10 +6,11 @@
 
 <script setup lang="ts">
 import { library } from "@fortawesome/fontawesome-svg-core"
-import {faCheckCircle,faPlusCircle,faPauseCircle } from "@/../private/pro-solid-svg-icons"
+import {faCheckCircle,faPlusCircle,faPauseCircle,faScrubber } from "@/../private/pro-solid-svg-icons"
 
-library.add(faCheckCircle,faPlusCircle,faPauseCircle)
+library.add(faCheckCircle,faPlusCircle,faPauseCircle,faScrubber)
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {trans} from "laravel-vue-i18n";
 
 const props = defineProps<{
     value: string
@@ -18,9 +19,10 @@ const props = defineProps<{
 </script>
 
 <template>
-    <FontAwesomeIcon v-if="value==='adopted'"  icon="fas fa-check-circle" class="mt-1.5  h-5 w-5 text-green-500" aria-hidden="true"/>
-    <FontAwesomeIcon v-if="value==='available'"  icon="fas fa-plus-circle" class="mt-1.5  h-5 w-5 text-indigo-200 hover:text-indigo-500" aria-hidden="true"/>
-    <FontAwesomeIcon v-if="value==='removed'"  icon="fas fa-pause-circle" class="mt-1.5  h-5 w-5 text-gray-200" aria-hidden="true"/>
+    <FontAwesomeIcon v-if="value==='adopted'"  icon="fas fa-check-circle" class="mt-1.5  h-5 w-5 text-green-500" aria-hidden="true"  :title="trans('Adopted')" />
+    <FontAwesomeIcon v-if="value==='available'"  icon="fas fa-plus-circle" class="mt-1.5  h-5 w-5 text-indigo-200 hover:text-indigo-500" aria-hidden="true"  :title="trans('Available')"  />
+    <FontAwesomeIcon v-if="value==='removed'"  icon="fas fa-pause-circle" class="mt-1.5  h-5 w-5 text-gray-200" aria-hidden="true"  :title="trans('Removed')"  />
+    <FontAwesomeIcon v-if="value==='owner'"  icon="fas fa-scrubber" class="mt-1.5  h-5 w-5 text-indigo-500" aria-hidden="true" :title="trans('Owner')"  />
 
 </template>
 
