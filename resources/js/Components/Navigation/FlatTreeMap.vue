16ce8b72c2973c4ca9d431677cc3b874b34a35b5
aqordeon <aqordivika@yahoo.co.id>
2023-05-16 08:42:15 +0800

    fix small UI issue on FlatTreeMap

--- resources/js/Components/Navigation/FlatTreeMap.vue
+++ resources/js/Components/Navigation/FlatTreeMap.vue
@@ -20,41 +20,41 @@ const dummyLink = "procurement.supplier-deliveries.index"
 <template>
     <nav aria-label="Progress">
         <ol role="list" class="divide-y divide-gray-300 rounded-md border border-gray-300 sm:flex sm:divide-y-0">
-            <li v-for="(node, nodeIdx) in nodes" :key="node.name" class="relative sm:flex sm:flex-1">
+            <li v-for="(node, nodeIdx) in nodes" :key="node.name" class="relative sm:flex sm:flex-1 items-center">
 
                 <Link :href="route(node.href[0], node.href[1])" class="group flex-1 items-center">
-                <span class="grid grid-flow-col justify-between items-center px-6 py-4 text-lg font-medium">
-                    <div>
-                        <FontAwesomeIcon size="lg" :icon="node.icon" class="flex-shrink-0 " aria-hidden="true" />
-                        <span class="ml-4 capitalize font-medium text-gray-500 group-hover:text-gray-900">
-                            {{ node.name }}
-                            <!-- {{ node.href[0] }} -->
-                        </span>
-                        <!-- Bars and count -->
-                        <span v-if="node.index"
-                            class="ml-4 font-medium text-gray-500 group-hover:text-gray-900 whitespace-nowrap">
-                            <FontAwesomeIcon icon="fal fa-bars" class="mr-1" />
-                            <span v-if="node.index.number">{{ locale.number(node.index.number) }}</span>
-                            <FontAwesomeIcon v-else icon="fal fa-empty-set" />
-                        </span>
-                    </div>
+                    <span class="grid grid-flow-col justify-between items-center px-6 py-4 text-lg font-medium">
+                        <div>
+                            <FontAwesomeIcon size="lg" :icon="node.icon" class="flex-shrink-0 " aria-hidden="true" />
+                            <span class="ml-4 capitalize font-medium text-gray-500 group-hover:text-gray-900">
+                                {{ node.name }}
+                                <!-- {{ node.href[0] }} -->
+                            </span>
+                            <!-- Bars and count -->
+                            <span v-if="node.index"
+                                class="ml-4 font-medium text-gray-500 group-hover:text-gray-900 whitespace-nowrap">
+                                <FontAwesomeIcon icon="fal fa-bars" class="mr-1" />
+                                <span v-if="node.index.number">{{ locale.number(node.index.number) }}</span>
+                                <FontAwesomeIcon v-else icon="fal fa-empty-set" />
+                            </span>
+                        </div>
+                    </span>
+                </Link>
 
-                    <!-- Sublink on right each section -->
-                    <div class="relative">
-                        <a @click.prevent="() => { $inertia.visit(route(dummyLink)) }"
-                            class="w-8 h-8 grid justify-center items-center border-2 text-indigo-500 border-indigo-500 rounded-lg cursor-pointer hover:bg-indigo-500 hover:text-white transition-all duration-75 ease-in-out">
-                            <FontAwesomeIcon size="md" :icon="node.icon" class="flex-shrink-0 " aria-hidden="true" />
-                        </a>
-                        <!-- <div @click.prevent="" class="absolute py-1 px-2 text-[5px] inline bg-red-500">
+                <!-- Sublink on right each section -->
+                <div class="pr-4">
+                    <a @click.prevent="() => { $inertia.visit(route(dummyLink)) }"
+                        class="w-8 h-8 grid justify-center items-center border-2 text-indigo-500 border-indigo-500 rounded-lg cursor-pointer hover:bg-indigo-500 hover:text-white transition-all duration-75 ease-in-out">
+                        <FontAwesomeIcon size="md" :icon="node.icon" class="flex-shrink-0 " aria-hidden="true" />
+                    </a>
+                    <!-- <div @click.prevent="" class="absolute py-1 px-2 text-[5px] inline bg-red-500">
                             This is a button for {{ node.name }}
                         </div> -->
-                    </div>
-                </span>
-                </Link>
+                </div>
 
                 <template v-if="nodeIdx !== nodes.length - 1">
                     <!-- Arrow separator for lg screens and up -->
-                    <div class="absolute top-0 right-0 hidden h-full w-5 sm:block" aria-hidden="true">
+                    <div class="hidden h-full w-5 sm:block" aria-hidden="true">
                         <svg class="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
                             <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor"
                                 stroke-linejoin="round" />
