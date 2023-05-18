0e7bc1e39c39b8d180f7588be7cc1f5e25b84aef
Raul Perusquia <raul@inikoo.com>
2023-05-18 15:56:34 +0800

    GitHub actions env file, plus preparing db creation scripts

--- generate_testing_db_dumps.sh
+++ generate_testing_db_dumps.sh
@@ -4,10 +4,14 @@ ITALIC='\e[3m'
 NONE='\033[00m'
 
 DB=aiku_test
+DB_PORT=5432
+
+DB_PORT="${1:-$DEFAUL_DB_PORT}"
+
 
 echo -e "✨ Resetting database ${ITALIC}${DB}${NONE}"
-dropdb --if-exists ${DB}
-createdb ${DB}
+dropdb --if-exists -p "${DB_PORT}" ${DB}
+createdb -p "${DB_PORT}"  ${DB}
 echo "🌱 Migrating and seeding database"
 php artisan --env=testing migrate --path=database/migrations/central  --database=central
 php artisan --env=testing db:seed
