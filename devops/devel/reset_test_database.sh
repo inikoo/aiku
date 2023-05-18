65dc17afce4e984130c59b7e87107c09205285d0
Raul Perusquia <raul@inikoo.com>
2023-05-18 20:42:04 +0800

    GitHub actions env file, plus preparing environment and DB dumps

--- devops/devel/reset_test_database.sh
+++ devops/devel/reset_test_database.sh
@@ -1,3 +1,3 @@
-PGPASSWORD=$3 psql -U "$2" -h localhost  -c "drop database $1 WITH (FORCE)"
-PGPASSWORD=$3 psql -U "$2" -h localhost  -c "create database $1"
-PGPASSWORD=$3 pg_restore -U "$2" -c -d "$1" ./tests/datasets/db_dumps/"$4"
+PGPASSWORD=$4 psql -U "$3" -p "$2" -h localhost  -c "drop database $1 WITH (FORCE)"
+PGPASSWORD=$4 psql -U "$3" -p "$2" -h localhost  -c "create database $1"
+PGPASSWORD=$4 pg_restore -U "$3" -p "$2" -c -d "$1" ./tests/datasets/db_dumps/"$5"
