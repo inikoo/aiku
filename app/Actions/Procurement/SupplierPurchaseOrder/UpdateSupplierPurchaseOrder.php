eaf1657d33a2ad825de0019b2ed5cfa7c247b647
Wahyu <wuuuahyou@gmail.com>
2023-05-22 17:10:30 +0800

    PKA-620 Integrate purchase orders to UI in Agent

---
+++ app/Actions/Procurement/SupplierPurchaseOrder/UpdateSupplierPurchaseOrder.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Tue, 09 May 2023 14:50:49 Malaysia Time, Pantai Lembeng, Bali, Id
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Procurement\SupplierPurchaseOrder;
+
+use App\Actions\WithActionUpdate;
+use App\Models\Procurement\PurchaseOrder;
+use App\Models\Procurement\SupplierDelivery;
+use Lorisleiva\Actions\ActionRequest;
+
+class UpdateSupplierPurchaseOrder
+{
+    use WithActionUpdate;
+
+    public function handle(PurchaseOrder $supplierPurchaseOrder, array $modelData): PurchaseOrder
+    {
+        return $this->update($supplierPurchaseOrder, $modelData, ['data']);
+    }
+
+//    public function authorize(ActionRequest $request): bool
+//    {
+//        return $request->user()->hasPermissionTo("inventory.warehouses.edit");
+//    }
+
+    public function rules(): array
+    {
+        return [
+            'number'        => ['required', 'numeric', 'unique:group.supplier_deliveries'],
+            'date'          => ['required', 'date'],
+            'currency_id'   => ['required', 'exists:currencies,id'],
+            'exchange'      => ['required', 'numeric']
+        ];
+    }
+
+    public function action(PurchaseOrder $supplierPurchaseOrder, array $objectData): PurchaseOrder
+    {
+        $this->setRawAttributes($objectData);
+        $validatedData = $this->validateAttributes();
+
+        return $this->handle($supplierPurchaseOrder, $validatedData);
+    }
+
+    public function asController(PurchaseOrder $supplierPurchaseOrder, ActionRequest $request): PurchaseOrder
+    {
+        $request->validate();
+        return $this->handle($supplierPurchaseOrder, $request->all());
+    }
+
+
+}
