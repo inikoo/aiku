9b7235af173aed89c06d703f18cceefa5168e64e
artha <artha@aw-advantage.com>
2023-05-23 17:01:58 +0800

    PKA-631 Create purchase order detail in UI part

---
+++ app/Actions/Procurement/PurchaseOrderItem/UI/IndexPurchaseOrderItems.php
@@ -0,0 +1,113 @@
+<?php
+/*
+ * Author: Jonathan Lopez Sanchez <jonathan@ancientwisdom.biz>
+ * Created: Tue, 09 May 2023 09:25:51 Central European Summer Time, Malaga, Spain
+ * Copyright (c) 2023, Inikoo LTD
+ */
+
+namespace App\Actions\Procurement\PurchaseOrderItem\UI;
+
+use App\Actions\InertiaAction;
+use App\Actions\UI\Procurement\ProcurementDashboard;
+use App\Enums\UI\TabsAbbreviationEnum;
+use App\Http\Resources\Procurement\PurchaseOrderItemResource;
+use App\Http\Resources\Procurement\PurchaseOrderResource;
+use App\Models\Procurement\Agent;
+use App\Models\Procurement\PurchaseOrder;
+use App\Models\Procurement\PurchaseOrderItem;
+use App\Models\Procurement\Supplier;
+use App\Models\Tenancy\Tenant;
+use Closure;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Inertia\Inertia;
+use Lorisleiva\Actions\ActionRequest;
+use App\InertiaTable\InertiaTable;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class IndexPurchaseOrderItems extends InertiaAction
+{
+    public function handle(): LengthAwarePaginator
+    {
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->whereHas('supplierProduct', function ($query) use ($value) {
+                $query->where('code', 'LIKE', "$value%");
+            });
+        });
+
+        InertiaTable::updateQueryBuilderParameters(TabsAbbreviationEnum::ITEMS->value);
+
+        return QueryBuilder::for(PurchaseOrderItem::class)
+            ->defaultSort('purchase_order_items.id')
+            ->allowedFilters([$globalSearch])
+
+            ->paginate(
+                perPage: $this->perPage ?? config('ui.table.records_per_page'),
+                pageName: TabsAbbreviationEnum::ITEMS->value.'Page'
+            )
+            ->withQueryString();
+    }
+
+
+    public function tableStructure($parent): Closure
+    {
+        return function (InertiaTable $table) use ($parent) {
+            $table
+                ->name(TabsAbbreviationEnum::ITEMS->value)
+                ->pageName(TabsAbbreviationEnum::ITEMS->value.'Page');
+            $table
+                ->withGlobalSearch()
+                ->column(key: 'code', label: __('code'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'name', label: __('name'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'unit_price', label: __('unit price'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'unit_quantity', label: __('unit quantity'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'unit_cost', label: __('cost'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'state', label: __('state'), canBeHidden: false, sortable: true, searchable: true)
+                ->defaultSort('number');
+        };
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        $this->canEdit = $request->user()->can('procurement.edit');
+        return
+            (
+                $request->user()->tokenCan('root') or
+                $request->user()->hasPermissionTo('procurement.view')
+            );
+    }
+
+    public function asController(ActionRequest $request): LengthAwarePaginator
+    {
+        $this->routeName = $request->route()->getName();
+        $this->initialisation($request);
+        return $this->handle();
+    }
+
+    public function jsonResponse(LengthAwarePaginator $purchaseOrders): AnonymousResourceCollection
+    {
+        return PurchaseOrderItemResource::collection($purchaseOrders);
+    }
+
+
+    public function getBreadcrumbs(): array
+    {
+        return
+            array_merge(
+                ProcurementDashboard::make()->getBreadcrumbs(),
+                [
+                    [
+                        'type'   => 'simple',
+                        'simple' => [
+                            'route' => [
+                                'name' => 'procurement.purchase-orders.index'
+                            ],
+                            'label' => __('purchase orders'),
+                            'icon'  => 'fal fa-bars'
+                        ]
+                    ]
+                ]
+            );
+    }
+}
