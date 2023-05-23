ebb0823e8435dc8255b7d0554de2b81ba70bb54a
Raul Perusquia <raul@inikoo.com>
2023-05-23 16:08:49 +0800

    PKA-444 Aurora migration

--- app/Actions/SourceFetch/Aurora/FetchWarehouseAreas.php
+++ app/Actions/SourceFetch/Aurora/FetchWarehouseAreas.php
@@ -17,7 +17,7 @@ use JetBrains\PhpStorm\NoReturn;
 
 class FetchWarehouseAreas extends FetchAction
 {
-    public string $commandSignature = 'fetch:warehouse-areas {tenants?*} {--s|source_id=}';
+    public string $commandSignature = 'fetch:warehouse-areas {tenants?*} {--s|source_id=} {--d|db_suffix=}';
 
     #[NoReturn] public function handle(SourceTenantService $tenantSource, int $tenantSourceId): ?WarehouseArea
     {
--- app/Actions/SourceFetch/Aurora/FetchWarehouses.php
+++ app/Actions/SourceFetch/Aurora/FetchWarehouses.php
@@ -17,7 +17,7 @@ use JetBrains\PhpStorm\NoReturn;
 
 class FetchWarehouses extends FetchAction
 {
-    public string $commandSignature = 'fetch:warehouses {tenants?*} {--s|source_id=}';
+    public string $commandSignature = 'fetch:warehouses {tenants?*} {--s|source_id=} {--d|db_suffix=}';
 
     #[NoReturn] public function handle(SourceTenantService $tenantSource, int $tenantSourceId): ?Warehouse
     {
