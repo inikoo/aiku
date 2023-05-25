950fd825ec214bd2f97fa8f01d020c25e92fd910
aqordeon <aqordivika@yahoo.co.id>
2023-05-25 15:39:10 +0800

    PKA-660 Adjust UI (Breadcrumb, Table, Showcase

--- resources/js/Pages/Procurement/SupplierShowcase.vue
+++ resources/js/Pages/Procurement/SupplierShowcase.vue
@@ -5,7 +5,7 @@
   -->
 
 <script setup lang="ts">
-import ShowcaseStatistic from '@/Components/ShowcaseStatistic.vue'
+import ShowcaseStats from '@/Components/ShowcaseStats.vue'
 import ShowcaseContactCard from "@/Components/ShowcaseContactCard.vue";
 
 const props = defineProps<{
@@ -28,15 +28,15 @@ const props = defineProps<{
 </script>
   
 <template >
-  <div class="grid text-gray-600  grid-flow-col grid-cols-2">
+  <div class="grid text-gray-600  grid-flow-col grid-cols-2 px-4 pt-4">
     <!-- Section 1 -->
-    <div class="pl-2">
+    <div class="">
       <ShowcaseContactCard :data="data.contactCard" />
     </div>
 
     <!-- Section 2: Statistic -->
-    <div class="mt-4 px-4">
-      <ShowcaseStatistic :data="data.stats" />
+    <div class="pl-6">
+      <ShowcaseStats :data="data.stats" />
     </div>
 
 
