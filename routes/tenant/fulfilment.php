d1ad3d969cface2a118a847f2b9c9a1159f5bda3
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-06-01 11:24:52 +0200

    PKA-706 IndexFulfilmentCustomers

--- routes/tenant/fulfilment.php
+++ routes/tenant/fulfilment.php
@@ -7,6 +7,7 @@
 
 
 use App\Actions\Fulfilment\FulfilmentCustomer\IndexFulfilmentCustomers;
+use App\Actions\Fulfilment\FulfilmentCustomer\ShowFulfilmentCustomer;
 use App\Actions\Fulfilment\StoredItem\UI\IndexStoredItems;
 use App\Actions\Fulfilment\StoredItem\UI\ShowStoredItem;
 use App\Actions\UI\Fulfilment\FulfilmentDashboard;
@@ -17,3 +18,4 @@ Route::get('/stored-items', IndexStoredItems::class)->name('stored-items.index')
 Route::get('/stored-items/{storedItem}', ShowStoredItem::class)->name('stored-items.show');
 
 Route::get('/customers', IndexFulfilmentCustomers::class)->name('customers.index');
+Route::get('/customers/{customer}', ShowFulfilmentCustomer::class)->name('customers.show');
