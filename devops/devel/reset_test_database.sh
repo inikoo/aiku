1324e293dedb611faf53632b6dbfe7e8599dc07d
Raul Perusquia <raul@inikoo.com>
2023-06-03 04:47:03 +0800

    PostgreSQL 🐘 Localization 🌐 (Fix regression)

--- devops/devel/reset_test_database.sh
+++ devops/devel/reset_test_database.sh
@@ -1,3 +1,3 @@
 PGPASSWORD=$4 psql -U "$3" -p "$2" -h localhost  -c "drop database $1 WITH (FORCE)"
-PGPASSWORD=$4 psql -U "$3" -p "$2" -h localhost  -c "create database $1 WITH TEMPLATE=template0 LC_COLLATE=C.UTF-8 LC_CTYPE=C.UTF-8   "
+PGPASSWORD=$4 psql -U "$3" -p "$2" -h localhost  -c "create database $1"
 PGPASSWORD=$4 pg_restore -U "$3" -p "$2" -c -d "$1" ./tests/datasets/db_dumps/"$5"
