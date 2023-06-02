adf348247f16ef528d162567e5ebda64e1c374c9
Raul Perusquia <raul@inikoo.com>
2023-06-02 17:51:27 +0800

    PKA-444 small update in aurora crm migration script 🤏🏼

--- aurora_migration_crm.sh
+++ aurora_migration_crm.sh
@@ -7,13 +7,13 @@
 
 DB=aiku
 
-php artisan fetch:customers "$1" -w clients -d _crm
+php artisan fetch:customers "$1" -r -w clients -d _crm
 php artisan fetch:deleted-customers "$1" -d _crm
 php artisan fetch:web-users "$1" -d _crm
 php artisan fetch:prospects "$1" -d _crm
 pg_dump -Fc -f "devops/devel/snapshots/au_$1_crm.dump" ${DB}
-php artisan fetch:orders "$1"-w payments -d _crm
-php artisan fetch:invoices "$1" -d _crm
-php artisan fetch:delivery-notes "$1" -d _crm
+php artisan fetch:orders "$1" -r -w payments -d _crm
+php artisan fetch:invoices "$1" -r -d _crm
+php artisan fetch:delivery-notes "$1" -r -d _crm
 pg_dump -Fc -f "devops/devel/snapshots/au_$1_sales.dump" ${DB}
 
