c35be956eddce5912cfd8c566781c49255ee4f7d
artha <artha@aw-advantage.com>
2023-06-07 08:26:39 +0800

    PKA-729 Move URL position in table

--- app/Actions/Auth/User/UI/ShowUser.php
+++ app/Actions/Auth/User/UI/ShowUser.php
@@ -81,10 +81,11 @@ class ShowUser extends InertiaAction
             $table
                 ->withGlobalSearch()
                 ->column(key: 'ip_address', label: __('IP Address'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'url', label: __('URL'), canBeHidden: false, sortable: true)
+                ->column(key: 'url', label: __('Module'), canBeHidden: false, sortable: true)
                 ->column(key: 'device_type', label: __('Device Type'), canBeHidden: false, sortable: true)
                 ->column(key: 'platform', label: __('Platform'), canBeHidden: false, sortable: true)
                 ->column(key: 'browser', label: __('Browser'), canBeHidden: false, sortable: true)
-                ->column(key: 'url', label: __('URL'), canBeHidden: false, sortable: true)
                 ->column(key: 'location', label: __('location'), canBeHidden: false)
                 ->column(key: 'datetime', label: __('Date & Time'), canBeHidden: false, sortable: true)
                 ->defaultSort('datetime');
