3348a38726a534cddd767de6cff130851d7087f9
aqordeon <aqordivika@yahoo.co.id>
2023-06-08 16:44:30 +0800

    PKA-720 TopBar: if only 1 data then menu will directly link to that 1 instead of show single data

--- resources/js/Layouts/AppTopBar.vue
+++ resources/js/Layouts/AppTopBar.vue
@@ -8,10 +8,9 @@
 		</div>
 
 		<!-- Left Menu -->
-		<div
-			class="flex items-center divide-x divide-gray-100 justify-center  overflow-hidden">
+		<div class="flex items-center divide-x divide-gray-100 justify-center overflow-hidden">
 			<Link
-                v-if="currentUrl && layout.navigation[currentUrl]?.topMenu && layout.navigation[currentUrl]?.topMenu?.subSections"
+				v-if=" currentUrl && layout.navigation[currentUrl]?.topMenu && layout.navigation[currentUrl]?.topMenu?.subSections "
 				v-for="menu in layout.navigation[currentUrl].topMenu.subSections"
 				:href="route(menu.route.name)"
 				class="group flex justify-center items-center cursor-pointer space-x-1 px-4">
@@ -23,20 +22,29 @@
 			</Link>
 		</div>
 
-        <!-- Dropdown -->
-        <div v-if="currentUrl && layout.navigation[currentUrl].topMenu && layout.navigation[currentUrl].topMenu.dropdown && layout.navigation[currentUrl]?.topMenu?.dropdown.options.data.length > 1">
-            <TopBarDropdown :currentPage="currentUrl"/>
-        </div>
+		<!-- Dropdown -->
+		<div v-if=" currentUrl && layout.navigation[currentUrl].topMenu && layout.navigation[currentUrl].topMenu.dropdown && layout.navigation[currentUrl]?.topMenu?.dropdown.options.data.length > 1">
+			<TopBarDropdown :currentPage="currentUrl" />
+		</div>
 
-        <!-- Right Menu -->
-        <div class="flex items-center divide-x divide-gray-200 justify-center rounded overflow-hidden">
+		<!-- Right Menu -->
+		<div
+			class="flex items-center divide-x divide-gray-200 justify-center rounded overflow-hidden">
+			<!-- The href is if initial state then the menu will show all shop, but if data shop only 1 data then menu is directly to that 1 data -->
 			<Link
-                v-if="currentUrl && layout.navigation[currentUrl].topMenu && layout.navigation[currentUrl].topMenu.dropdown?.subsections"
+				v-if="
+					currentUrl &&
+					layout.navigation[currentUrl].topMenu &&
+					layout.navigation[currentUrl].topMenu.dropdown?.subsections
+				"
 				v-for="menu in layout.navigation[currentUrl].topMenu.dropdown.subsections"
-				:href="layout[currentUrl].currentData.slug ? route(menu.route?.selected, layout[currentUrl].currentData.slug) : route(menu.route?.all)"
-                :title="menu.tooltip"
-				class="group flex justify-center items-center cursor-pointer space-x-1 px-4"
-            >
+				:href=" layout[currentUrl].currentData.slug ?
+						route(menu.route?.selected, layout[currentUrl].currentData.slug) :
+						layout.navigation[currentUrl]?.topMenu?.dropdown.options.data.length == 1 ?
+							route(menu.route?.selected, layout.navigation[currentUrl]?.topMenu?.dropdown.options.data[0].slug) :
+							route(menu.route?.all)"
+				:title="menu.tooltip"
+				class="group flex justify-center items-center cursor-pointer space-x-1 px-4">
 				<FontAwesomeIcon
 					:icon="menu.icon"
 					class="h-3.5 w-3.5 pr-1 text-gray-600 group-hover:opacity-100 opacity-30 transition duration-100 ease-in-out"
@@ -54,10 +62,36 @@ import { router } from "@inertiajs/vue3"
 import TopBarDropdown from "@/Components/Navigation/TopBarDropdown.vue"
 import { useLayoutStore } from "@/Stores/layout"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
-import { faNetworkWired, faTerminal, faCalendar, faStopwatch, faChessClock, faUserAlien, faCog, faBox, faBoxesAlt, faWarehouse, faMapSigns, faBrowser } from "@/../private/pro-light-svg-icons"
+import {
+	faNetworkWired,
+	faTerminal,
+	faCalendar,
+	faStopwatch,
+	faChessClock,
+	faUserAlien,
+	faCog,
+	faBox,
+	faBoxesAlt,
+	faWarehouse,
+	faMapSigns,
+	faBrowser,
+} from "@/../private/pro-light-svg-icons"
 import { library } from "@fortawesome/fontawesome-svg-core"
 
-library.add(faNetworkWired, faTerminal, faCalendar, faStopwatch, faChessClock, faUserAlien, faCog, faBox, faBoxesAlt, faWarehouse, faMapSigns, faBrowser)
+library.add(
+	faNetworkWired,
+	faTerminal,
+	faCalendar,
+	faStopwatch,
+	faChessClock,
+	faUserAlien,
+	faCog,
+	faBox,
+	faBoxesAlt,
+	faWarehouse,
+	faMapSigns,
+	faBrowser
+)
 const layout = useLayoutStore()
 
 const props = defineProps<{
@@ -67,9 +101,8 @@ const props = defineProps<{
 const currentUrl = ref()
 router.on("navigate", (event) => {
 	currentUrl.value = event.detail.page.url.split("/")[1]
+	console.log(layout.navigation[currentUrl.value]?.topMenu?.dropdown.options.data[0].slug)
 })
-
-
 </script>
 
 <style scoped></style>
