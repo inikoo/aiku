c85883faaa87f337f4d5f3ae2bd5fed06a1fdaba
aqordeon <aqordivika@yahoo.co.id>
2023-06-08 17:10:36 +0800

    PKA-720 TopBar: add border to right menu

--- resources/js/Layouts/AppTopBar.vue
+++ resources/js/Layouts/AppTopBar.vue
@@ -29,7 +29,7 @@
 
 		<!-- Right Menu -->
 		<div
-			class="flex items-center divide-x divide-gray-200 justify-center rounded overflow-hidden">
+			class="text-gray-600 inline-flex place-self-center rounded-r justify-center text-sm ring-1 ring-inset ring-gray-300">
 			<!-- The href is if initial state then the menu will show all shop, but if data shop only 1 data then menu is directly to that 1 data -->
 			<Link
 				v-if="
@@ -44,12 +44,12 @@
 							route(menu.route?.selected, layout.navigation[currentUrl]?.topMenu?.dropdown.options.data[0].slug) :
 							route(menu.route?.all)"
 				:title="menu.tooltip"
-				class="group flex justify-center items-center cursor-pointer space-x-1 px-4">
+				class="group flex justify-center items-center cursor-pointer py-1 space-x-1 px-4 hover:font-semibold hover:text-indigo-600">
 				<FontAwesomeIcon
 					:icon="menu.icon"
-					class="h-3.5 w-3.5 pr-1 text-gray-600 group-hover:opacity-100 opacity-30 transition duration-100 ease-in-out"
+					class="h-3.5 w-3.5 pr-1 group-hover:opacity-100 opacity-30 transition duration-100 ease-in-out"
 					aria-hidden="true" />
-				<span class="text-gray-600 capitalize">{{ menu.label }}</span>
+				<span class="capitalize">{{ menu.label }}</span>
 			</Link>
 		</div>
 	</div>
