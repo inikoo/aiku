64226956fe0c0b026c1fb6d6f35bdd99533d2d3d
Wahyu <wuuuahyou@gmail.com>
2023-06-09 10:33:26 +0800

    PKA-436 HumanResourceTest

--- tests/Feature/DispatchTest.php
+++ tests/Feature/DispatchTest.php
@@ -20,14 +20,9 @@ use App\Actions\Tenancy\Tenant\StoreTenant;
 use App\Enums\Dispatch\DeliveryNote\DeliveryNoteStateEnum;
 use App\Enums\Dispatch\DeliveryNote\DeliveryNoteStatusEnum;
 use App\Enums\Mail\Outbox\OutboxTypeEnum;
-use App\Models\Dispatch\DeliveryNote;
-use App\Models\Dispatch\Shipment;
-use App\Models\Dispatch\Shipper;
 use App\Models\Helpers\Address;
 use App\Models\Inventory\Stock;
 use App\Models\Marketing\Shop;
-use App\Models\Sales\Customer;
-use App\Models\Sales\Order;
 use App\Models\Sales\Transaction;
 use App\Models\Tenancy\Group;
 use App\Models\Tenancy\Tenant;
@@ -48,115 +43,158 @@ beforeEach(function () {
 });
 
 test('create shipper', function () {
-    $shipper = StoreShipper::make()->action(Shipper::factory()->definition());
-    $this->assertModelExists($shipper);
-    return $shipper;
+    $arrayData =[
+        'code'  => 'ABC',
+        'name'  => 'ABC Shipping'
+    ];
+
+    $createdShipper = StoreShipper::make()->action($arrayData);
+    expect($createdShipper->code)->toBe($arrayData['code']);
+
+    return $createdShipper;
 });
 
-test('update shipper', function () {
-    $shipper = Shipper::latest()->first();
-    $shipper = UpdateShipper::make()->action($shipper, Shipper::factory()->definition());
-    $this->assertModelExists($shipper);
+test('update shipper', function ($createdShipper) {
+    $arrayData =[
+        'code'  => 'DEF',
+        'name'  => 'DEF Movers'
+    ];
+
+    $updatedShipper = UpdateShipper::make()->action($createdShipper, $arrayData);
+
+    expect($updatedShipper->code)->toBe($arrayData['code']);
+})->depends('create shipper');
+
+test('create shop', function () {
+    $shop = StoreShop::make()->action(Shop::factory()->definition());
+
+    expect($shop->paymentAccounts()->count())->toBe(1)
+        ->and($shop->outboxes()->count())->toBe(count(OutboxTypeEnum::values()));
+
+    return $shop;
 });
 
-test('create delivery note', function () {
+test('create customer', function ($shop) {
     try {
-        $shop     = StoreShop::make()->action(Shop::factory()->definition());
-        $customer = StoreCustomer::make()->action(
+        $arrayData = [
+            'contact_name'             => 'vica',
+            'company_name'             => 'aw advantage',
+            'email'                    => 'vica@awadvantage.com',
+            'phone'                    => '+62345555'
+        ];
+        $createdCustomer = StoreCustomer::make()->action(
             $shop,
-            Customer::factory()->definition(),
+            $arrayData,
             Address::factory()->definition()
         );
-        $order = StoreOrder::make()->action(
-            $customer,
-            Order::factory()->definition(),
-            Address::make(),
-            Address::make()
-        );
-        $order['customer_id'] = $customer->id;
-        $deliveryNote         = StoreDeliveryNote::make()->action($order, DeliveryNote::factory()->definition(), Address::make());
-        $this->assertModelExists($deliveryNote);
+        expect($createdCustomer->contact_name)->toBe($arrayData['contact_name']);
+    } catch (Throwable $e) {
+        echo $e->getMessage();
+        $createdCustomer = null;
+    }
+
+    return $createdCustomer;
+})->depends('create shop');
+
+test('create order', function ($createdCustomer) {
+    $arrayData =[
+        'number'      => '123456',
+        'date'        => date('Y-m-d'),
+        'customer_id' => $createdCustomer->id
+    ];
+
+    $createdOrder = StoreOrder::make()->action($createdCustomer, $arrayData, Address::make(), Address::make());
+
+    expect($createdOrder->number)->toBe($arrayData['number']);
+
+    return $createdOrder;
+})->depends('create customer');
+
+test('create delivery note', function ($createdOrder) {
+    try {
+        $arrayData = [
+            'number' => 123456,
+            'state'  => DeliveryNoteStateEnum::SUBMITTED,
+            'status' => DeliveryNoteStatusEnum::HANDLING,
+            'email'  => 'test@email.com',
+            'phone'  => '+0246578658',
+            'date'   => date('Y-m-d')
+        ];
+
+        $deliveryNote         = StoreDeliveryNote::make()->action($createdOrder, $arrayData, Address::make());
+
+        expect($deliveryNote->number)->toBe($arrayData['number']);
 
     } catch (Throwable $e) {
         echo $e->getMessage();
         $deliveryNote = null;
     }
     return $deliveryNote;
-});
+})->depends('create order');
 
-test('update delivery note', function ($deliveryNote) {
-    $updatedDeliveryNote = UpdateDeliveryNote::make()->action($deliveryNote, [
+test('update delivery note', function ($lastDeliveryNote) {
+    $arrayData = [
         'number' => 2321321,
         'state'  => DeliveryNoteStateEnum::PICKING,
         'status' => DeliveryNoteStatusEnum::DISPATCHED,
         'email'  => 'test@email.com',
         'phone'  => '+0246578658',
         'date'   => date('Y-m-d')
-    ]);
-    expect($updatedDeliveryNote->number)->toBe(2321321);
+    ];
+
+    $updatedDeliveryNote = UpdateDeliveryNote::make()->action($lastDeliveryNote, $arrayData);
+
+    expect($updatedDeliveryNote->number)->toBe($arrayData['number']);
 })->depends('create delivery note');
 
-test('create delivery note item', function ($deliveryNote) {
-    $shop     = StoreShop::make()->action(Shop::factory()->definition());
+test('create delivery note item', function ($customer, $order, $deliveryNote) {
     try {
-        $customer = StoreCustomer::make()->action(
-            $shop,
-            Customer::factory()->definition(),
-            Address::factory()->definition()
-        );
-        $order = StoreOrder::make()->action(
-            $customer,
-            Order::factory()->definition(),
-            Address::make(),
-            Address::make()
-        );
         $stock            = StoreStock::make()->action($customer, Stock::factory()->definition());
         $transaction      = StoreTransaction::make()->action($order, Transaction::factory()->definition());
-        $deliveryNoteItem = StoreDeliveryNoteItem::make()->action($deliveryNote, [
+        $deliveryNoteData = [
             'delivery_note_id'  => $deliveryNote->id,
             'stock_id'          => $stock->id,
             'transaction_id'    => $transaction->id,
-        ]);
-        $this->assertModelExists($deliveryNoteItem);
+        ];
+
+        $deliveryNoteItem = StoreDeliveryNoteItem::make()->action($deliveryNote, $deliveryNoteData);
+
+        expect($deliveryNoteItem->delivery_note_id)->toBe($deliveryNoteData['delivery_note_id']);
     } catch (Throwable $e) {
         echo $e->getMessage();
         $deliveryNoteItem = null;
     }
     return $deliveryNoteItem;
-})->depends('create delivery note');
+})->depends('create customer', 'create order', 'create delivery note');
 
 
 test('remove delivery note', function ($deliveryNote) {
     $success = DeleteDeliveryNote::make()->handle($deliveryNote);
+
     $this->assertModelExists($deliveryNote);
+
     return $success;
 })->depends('create delivery note', 'create delivery note item');
 
 
 test('create shipment', function ($deliveryNote, $shipper) {
+    $arrayData = [
+        'code' => 'ASTOID'
+    ];
     $shipper['api_shipper'] = '';
-    $shipment               = StoreShipment::make()->action($deliveryNote, $shipper, Shipment::factory()->definition());
-    $this->assertModelExists($shipment);
+
+    $shipment = StoreShipment::make()->action($deliveryNote, $shipper, $arrayData);
+    expect($shipment->code)->toBe($arrayData['code']);
+
     return $shipment;
 })->depends('create delivery note', 'create shipper');
 
-test('update shipment', function () {
-    $shipment = Shipment::latest()->first();
-    $shipment = UpdateShipment::make()->action($shipment, Shipment::factory()->definition());
-    $this->assertModelExists($shipment);
+test('update shipment', function ($lastShipment) {
+    $arrayData = [
+        'code' => 'ASTOAU'
+    ];
+
+    $shipment = UpdateShipment::make()->action($lastShipment, $arrayData);
+
+    expect($shipment->code)->toBe($arrayData['code']);
 })->depends('create shipment');
-
-//test('apc gb call shipper api', function ($deliveryNote, $shipper) {
-//    $responseJson = ApcGbCallShipperApi::make()->action($deliveryNote,$shipper);
-//    return $responseJson['data'];
-//})->depends('create delivery note','create shipper');
-
-test('create shop', function () {
-    $shop = StoreShop::make()->action(Shop::factory()->definition());
-    $this->assertModelExists($shop);
-    expect($shop->paymentAccounts()->count())->toBe(1)
-        ->and($shop->outboxes()->count())->toBe(count(OutboxTypeEnum::values()));
-
-
-    return $shop;
-});
