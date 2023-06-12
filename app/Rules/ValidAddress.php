b04cfd29389db1a3436b8b906cee37d683c8877a
Wahyu <wuuuahyou@gmail.com>
2023-06-12 17:09:33 +0800

    PKA-755 Fixing Working Places (Fixing HumanResourceTest.php)

--- app/Rules/ValidAddress.php
+++ app/Rules/ValidAddress.php
@@ -18,8 +18,8 @@ class ValidAddress implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $query         = DB::connection('tenant')->table('addresses');
-        if ($query->where("country_id", [$value])->count() >= 1) {
+        $query         = DB::table('countries');
+        if ($query->where("id", $value['country_id'])->count() <= 0) {
             $fail('The '.$attribute.' not a valid Address.');
         }
     }
