d2120843de72fbdfbf6a18dae0fb3b4a669d36fe
aqordeon <aqordivika@yahoo.co.id>
2023-06-13 14:11:34 +0800

    PKA-763 fix Form: Address

--- resources/js/Components/Forms/Fields/Address.vue
+++ resources/js/Components/Forms/Fields/Address.vue
@@ -26,58 +26,70 @@ for (const item in props.options.countriesAddressData) {
 }
 
 const administrativeAreas = (countryID: number) => props.options.countriesAddressData[countryID]['administrativeAreas']
-const inAdministrativeAreas = (administrativeArea: string, countryID: number) => !!props.options.countriesAddressData[countryID]['administrativeAreas'].find(c => c.name === administrativeArea);
+const inAdministrativeAreas = (administrativeArea: string, countryID: number) => {
+    !!props.options.countriesAddressData[countryID]['administrativeAreas'].find(c => c.name === administrativeArea);
+}
 const addressFields = (countryID: number) => {
     return props.options.countriesAddressData[countryID]['fields'];
 }
 const handleChange = () => props.form.clearErrors();
 
+console.log(props.form)
 </script>
 
 <template>
     <div class="grid grid-cols-2 gap-3">
 
+        <!-- Country Options -->
         <div class="col-span-2">
             <div class="relative">
-                <Multiselect
-                    :options="countries"
-                    v-model="addressValues['country_id']"
-                    searchable
+                <Multiselect :options="countries" v-model="addressValues['country_id']" searchable
                     :placeholder="props.fieldData.placeholder ?? 'Select a country'"
                     :canClear="props.fieldData.required ?? true"
-                    :class="{ 'pr-8': form.errors[fieldName] || form.recentlySuccessful }"
+                    :class="{ 'pr-8': form.errors[fieldName] || form.recentlySuccessful }" 
+                    :canDeselect="false"
+				    :hideSelected="false"
                 />
-                <div
-                    v-if="form.errors[fieldName] || form.recentlySuccessful"
-                    class="absolute inset-y-2/4 right-0 pr-3 flex items-center pointer-events-none bg-red-500"
-                >
-                    <FontAwesomeIcon icon="fas fa-exclamation-circle" v-if="form.errors[fieldName]" class="h-5 w-5 text-red-500" aria-hidden="true" />
-                    <FontAwesomeIcon icon="fas fa-check-circle" v-if="form.recentlySuccessful" class="h-5 w-5 text-green-500" aria-hidden="true" />
+                <div v-if="form.errors[fieldName] || form.recentlySuccessful"
+                    class="absolute inset-y-2/4 right-0 pr-3 flex items-center pointer-events-none bg-red-500">
+                    <FontAwesomeIcon icon="fas fa-exclamation-circle" v-if="form.errors[fieldName]"
+                        class="h-5 w-5 text-red-500" aria-hidden="true" />
+                    <FontAwesomeIcon icon="fas fa-check-circle" v-if="form.recentlySuccessful"
+                        class="h-5 w-5 text-green-500" aria-hidden="true" />
                 </div>
             </div>
-            <span v-if="form.errors[fieldName]" class="block mt-2 text-sm text-red-600">{{form.errors[fieldName] }}</span>
+            <span v-if="form.errors[fieldName]" class="block mt-2 text-sm text-red-600">{{ form.errors[fieldName] }}</span>
         </div>
-        
-        <template v-if="addressValues['country_id']" v-for="(addressFieldData, addressField) in addressFields(addressValues['country_id'])" :key="addressField">
+
+        <template v-if="addressValues['country_id']"
+            v-for="(addressFieldData, addressField) in addressFields(addressValues['country_id'])" :key="addressField">
             <div class="grid col-span-2">
                 <div class="w-full ">
-                    <div v-if="`${addressField}` === 'administrative_area'" >
-                        <label for="administrative_area" class="capitalize block text-sm font-medium text-gray-700">{{ addressFieldData.label }}</label>
-                        <Multiselect v-if="administrativeAreas(addressValues['country_id']).length && (!addressValues['administrative_area'] || inAdministrativeAreas(addressValues['administrative_area'],addressValues['country_id']))"
-                            :options="administrativeAreas(addressValues['country_id'])" :label="'name'" :value-prop="'name'" v-model="addressValues['administrative_area']"/>
-                        <input v-else v-model="addressValues['administrative_area']" type="text" name="administrative_area" id="administrative_area" autocomplete="password"
-                            class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
-                    </div>
-                    <div v-else >
-                        <label :for="`${addressField}`" class="capitalize block text-xs font-medium text-gray-700">
-                            {{ addressFieldData.label }}<span v-if="form.errors[addressField]" class="mt-2 text-sm text-red-600">{{form.errors[addressField] }}</span>
+                    <div v-if="`${addressField}` === 'administrative_area'">
+                        <label for="administrative_area" class="capitalize block text-sm font-medium text-gray-700">
+                            {{ addressFieldData.label }}
                         </label>
-                        <input @input="handleChange()" v-model="addressValues[addressField]" type="text" name="address_line_2" :id="`${addressField}`" autocomplete="password"
-                                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                        <Multiselect
+                            v-if="administrativeAreas(addressValues['country_id']).length && (!addressValues['administrative_area'] || inAdministrativeAreas(addressValues['administrative_area'], addressValues['country_id']))"
+                            :options="administrativeAreas(addressValues['country_id'])" :label="'name'" :value-prop="'name'"
+                            v-model="addressValues['administrative_area']" />
+                        <input v-else v-model="addressValues['administrative_area']" type="text" name="administrative_area"
+                            id="administrative_area" autocomplete="password"
+                            class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                    </div>
+                    <div v-else>
+                        <label :for="`${addressField}`" class="capitalize block text-xs font-medium text-gray-700">
+                            {{ addressFieldData.label }}
+                            <span v-if="form.errors[addressField]" class="mt-2 text-sm text-red-600">{{ form.errors[addressField] }}</span>
+                        </label>
+                        <input @input="handleChange()" v-model="addressValues[addressField]" type="text"
+                            name="address_line_2" :id="`${addressField}`" autocomplete="password"
+                            class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                     </div>
                 </div>
                 <div class="w-5 self-end">
-                    <FontAwesomeIcon icon="fas fa-exclamation-circle" v-if="form.errors[addressField]" class="h-5 w-5 text-red-500" aria-hidden="true" />
+                    <FontAwesomeIcon icon="fas fa-exclamation-circle" v-if="form.errors[addressField]"
+                        class="h-5 w-5 text-red-500" aria-hidden="true" />
                 </div>
             </div>
         </template>
@@ -87,16 +99,15 @@ const handleChange = () => props.form.clearErrors();
 
 <style>
 .slide-fade-enter-active {
-  transition: all 0.3s ease-out;
+    transition: all 0.3s ease-out;
 }
 
 .slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
 }
 
 .slide-fade-enter-from,
 .slide-fade-leave-to {
-  transform: translateX(20px);
-  opacity: 0;
-}
-</style>
+    transform: translateX(20px);
+    opacity: 0;
+}</style>
