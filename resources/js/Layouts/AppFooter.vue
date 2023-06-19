9d3622f28cf17c93ff03cf1248f62c11d1e0309b
aqordeon <aqordivika@yahoo.co.id>
2023-06-19 11:54:10 +0800

    PKA-797 Footer: adjust UI

--- resources/js/Layouts/AppFooter.vue
+++ resources/js/Layouts/AppFooter.vue
@@ -5,55 +5,40 @@
             :class="[isFooter ? '' : 'hidden']"></div>
         <div class="flex items-end flex-row-reverse text-sm">
 
-            <!-- Footer List -->
-            <div v-for="footer in footerList"
-                class="relative flex z-50 select-none justify-center w-40 min-w-min cursor-pointer hover:bg-gray-700 "
-                :class="[isFooter == footer.key ? 'bg-gray-700' : 'bg-gray-800']"
-                @click="isFooter == footer.key ? isFooter = !isFooter : isFooter = footer.key"
+            <!-- Tab: Language -->
+            <div
+                class="relative flex z-50 select-none justify-center w-40 min-w-min cursor-pointer"
+                :class="[isFooter == tabLanguage.key ? 'bg-gray-600' : 'bg-gray-800 hover:bg-gray-700']"
+                @click="isFooter == tabLanguage.key ? isFooter = !isFooter : isFooter = tabLanguage.key"
             >
-                <p>{{ footer.key }}</p>
-                
-                <!-- Footer Active: Box -->
+                <p>{{ tabLanguage.key }}</p>
                 <div class="absolute bottom-5 w-full overflow-hidden rounded-t"
-                    :class="[isFooter == footer.key ? 'h-max' : 'h-0']"
+                    :class="[isFooter == tabLanguage.key ? 'h-max' : 'h-0']"
                 >
-                    <FooterLanguageTab :active="isFooter == footer.key" :options="footer.options" />
+                    <FooterTabLanguage :active="isFooter == tabLanguage.key" :options="tabLanguage.options" />
                 </div>
             </div>
+            
         </div>
     </footer>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
-import FooterLanguageTab from '@/Components/Footer/FooterLanguageTab.vue'
+import FooterTabLanguage from '@/Components/Footer/FooterTabLanguage.vue'
 
-const footerList = ref([
-    {
+const tabLanguage = ref({
         'key': 'Language',
         'label': 'Change your language',
         'options': [
-            'English',
-            'Spanish',
-            'Russians',
-            'Bahasa Indonesia',
-            'Chinese',
-            'Japanese',
+            'English (EN-US)',
+            'Spanish (SP)',
+            'Russians (RU)',
+            'Bahasa Indonesia (ID)',
+            'Chinese (CN)',
+            'Japanese (JP)',
         ]
-    },
-    {
-        'key': 'Theme',
-        'label': 'Change your theme',
-    },
-    {
-        'key': 'Status',
-        'label': 'Change your status',
-    },
-    {
-        'key': 'Profile',
-        'label': 'Change your profile',
-    },
-])
+    },)
 
 const isFooter = ref(false)
 </script>
