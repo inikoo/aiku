ae770fb4c6d0ddae951a7411c5281af29b0fca88
artha <artha@aw-advantage.com>
2023-06-19 14:02:11 +0800

    PKA-805 Create validation for username/slug create and export will not able to execute in store user

--- routes/tenant/sysadmin.php
+++ routes/tenant/sysadmin.php
@@ -23,6 +23,8 @@ Route::get('/', SysAdminDashboard::class)->name('dashboard');
 Route::get('/settings', SysAdminSettings::class)->name('settings');
 
 Route::get('/users', IndexUsers::class)->name('users.index');
+Route::get('/users/export', IndexUsers::class)->name('users.index');
+
 Route::get('/users/create', CreateUser::class)->name('users.create');
 Route::get('/users/{user}', ShowUser::class)->name('users.show');
 Route::get('/users/{user}/edit', EditUser::class)->name('users.edit');
