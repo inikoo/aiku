9664c60a0a2cf0ba061f984165e511d1bcaf2605
Raul Perusquia <raul@inikoo.com>
2023-06-19 09:12:38 +0800

    preparing database for legacy source-password support 🦕

--- database/migrations/tenant/2022_10_27_162606_create_web_users_table.php
+++ database/migrations/tenant/2022_10_27_162606_create_web_users_table.php
@@ -41,6 +41,8 @@ return new class () extends Migration {
             $table->unique(['website_id', 'username']);
             $table->string('login_version')->index()->default(WebUserLoginVersionEnum::AIKU->value);
             $table->unsignedInteger('source_id')->nullable()->unique();
+            $table->string('source_password')->nullable()->index()->comment('source password');
+
         });
     }
 
