1a59bbcab241279dc19b4439a754f0e66fa32d94
artha <artha@aw-advantage.com>
2023-06-20 16:34:16 +0800

    PKA-837 Exporet stocks

---
+++ app/Actions/Inventory/Stock/ExportStocks.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 24 Apr 2023 20:23:18 Malaysia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Inventory\Stock;
+
+use App\Actions\Traits\WithExportData;
+use App\Exports\Inventory\StockFamiliesExport;
+use App\Exports\Inventory\StocksExport;
+use Lorisleiva\Actions\ActionRequest;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Lorisleiva\Actions\Concerns\WithAttributes;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class ExportStocks
+{
+    use AsAction;
+    use WithAttributes;
+    use WithExportData;
+
+    /**
+     * @throws \Throwable
+     */
+    public function handle(array $objectData): BinaryFileResponse
+    {
+        $type = $objectData['type'];
+
+        return $this->export(new StocksExport, 'stocks', $type);
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function asController(ActionRequest $request): BinaryFileResponse
+    {
+        $this->setRawAttributes($request->all());
+        $this->validateAttributes();
+
+        return $this->handle($request->all());
+    }
+}
