52c0feec6115e76140c249e1e82bc2d42c003981
artha <artha@aw-advantage.com>
2023-06-20 16:34:08 +0800

    PKA-832 Create warehoiuses area export

---
+++ app/Actions/Inventory/WarehouseArea/ExportWarehouseAreas.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 24 Apr 2023 20:23:18 Malaysia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Inventory\WarehouseArea;
+
+use App\Actions\Traits\WithExportData;
+use App\Exports\Inventory\WarehouseAreasExport;
+use App\Exports\Procurement\AgentsExport;
+use Lorisleiva\Actions\ActionRequest;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Lorisleiva\Actions\Concerns\WithAttributes;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class ExportWarehouseAreas
+{
+    use AsAction;
+    use WithAttributes;
+    use WithExportData;
+
+    /**
+     * @throws \Throwable
+     */
+    public function handle(array $objectData): BinaryFileResponse
+    {
+        $type = $objectData['type'];
+
+        return $this->export(new WarehouseAreasExport, 'warehouse-areas', $type);
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function asController(ActionRequest $request): BinaryFileResponse
+    {
+        $this->setRawAttributes($request->all());
+        $this->validateAttributes();
+
+        return $this->handle($request->all());
+    }
+}
