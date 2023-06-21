50f47ff3d1f86f9b29a25d2d519b609f26ab16f3
aqordeon <aqordivika@yahoo.co.id>
2023-06-21 11:09:25 +0800

    PKA-842 Adjust UI: responsive FlatTreeMap

--- resources/js/Components/Navigation/FlatTreeMap.vue
+++ resources/js/Components/Navigation/FlatTreeMap.vue
@@ -19,18 +19,18 @@ const locale = useLocaleStore();
 
 <template>
     <!-- {{ nodes[0] }} -->
-    <nav aria-label="Progress">
-        <ol v-if="nodes" role="list" class="divide-y divide-gray-300 rounded-md border border-gray-300 sm:flex sm:divide-y-0">
-            <li v-for="(node, nodeIdx) in nodes" :key="node.name" class="relative sm:flex sm:flex-1 items-center">
+    <nav aria-label="Progress" class="py-1 md:py-0">
+        <ol v-if="nodes" role="list" class="divide-y divide-gray-300 rounded-md border border-gray-300 md:flex md:divide-y-0">
+            <li v-for="(node, nodeIdx) in nodes" :key="node.name" class="relative flex flex-1 items-center">
                 <!-- Main Tree -->
                 <Link :href="route(node.href[0], node.href[1])" class="group flex-1 items-center">
-                    <span class="grid grid-flow-col justify-between items-center px-6 py-4 text-lg font-medium">
-                        <div>
-                            <FontAwesomeIcon size="lg" :icon="node.icon" class="flex-shrink-0 " aria-hidden="true" />
-                            <span class="ml-4 capitalize font-medium text-gray-500 group-hover:text-gray-900">
-                                {{ node.name }}
-                                <!-- {{ node.href[0] }} -->
-                            </span>
+                    <div class="flex items-center px-4 text-lg md:text-base lg:text-lg xl:px-6 py-4 font-medium">
+                            <FontAwesomeIcon size="lg" :icon="node.icon" class="flex-shrink-0 text-gray-400" aria-hidden="true" />
+                            <p class="ml-4 inline capitalize font-medium text-gray-500 group-hover:text-gray-700">
+                                <span class="hidden lg:inline">{{ node.name }}</span>
+                                <span class="inline lg:hidden">{{ node.shortName ?? node.name }}</span>
+                            </p>
+                            
                             <!-- Bars and count -->
                             <span v-if="node.index"
                                 class="ml-4 font-medium text-gray-500 group-hover:text-gray-900 whitespace-nowrap">
@@ -38,22 +38,21 @@ const locale = useLocaleStore();
                                 <span v-if="node.index.number">{{ locale.number(node.index.number) }}</span>
                                 <FontAwesomeIcon v-else icon="fal fa-empty-set" />
                             </span>
-                        </div>
-                    </span>
+                    </div>
                 </Link>
 
-                <!-- Sublink on right each section -->
-                <div v-if="node.rightSubLink" class="pr-4" :title="capitalize(node.rightSubLink.tooltip)">
+                <!-- Sublink on right each section (Marketplace) -->
+                <div v-if="node.rightSubLink" class="pr-4 " :title="capitalize(node.rightSubLink.tooltip)">
                     <!-- {{ importIcon(node.rightSubLink.icon) }} -->
                     <Link :href="route(node.rightSubLink.href[0])"
-                        class="w-8 h-8 grid justify-center items-center border-2 text-indigo-500 border-indigo-500 rounded-lg cursor-pointer hover:bg-indigo-500 hover:text-white transition-all duration-75 ease-in-out">
+                        class="w-9 h-9 flex flex-0 justify-center items-center border-2 text-indigo-500 border-indigo-500 rounded-lg cursor-pointer hover:bg-indigo-500 hover:text-white transition-all duration-75 ease-in-out">
                         <FontAwesomeIcon :icon="node.rightSubLink.icon" class="flex-shrink-0 " aria-hidden="true" />
                     </Link>
                 </div>
 
                 <template v-if="nodeIdx !== nodes.length - 1">
                     <!-- Arrow separator for lg screens and up -->
-                    <div class="hidden h-full w-5 sm:block" aria-hidden="true">
+                    <div class="hidden h-full w-5 md:block" aria-hidden="true">
                         <svg class="h-full w-full text-gray-300" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
                             <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor"
                                 stroke-linejoin="round" />
