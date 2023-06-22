a88a4b4a45c67f7b79c201b1196e8dd80784a38d
aqordeon <aqordivika@yahoo.co.id>
2023-06-22 14:33:40 +0800

    PKA-854 Footer: smaller size, restructure component

---
+++ resources/js/Components/Footer/FooterTab.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="absolute bottom-6 right-0 w-40 min-w-min overflow-hidden rounded-t">
+        <div class="w-full bg-gray-800 shadow-lg flex-row items-start text-gray-100 text-[11px] leading-none" >
+            <div class="flex flex-col justify-center text-center p-1 pb-3 gap-y-1">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped></style>
--- resources/js/Components/Footer/FooterTabActiveUsers.vue
+++ resources/js/Components/Footer/FooterTabActiveUsers.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="w-full min-w-max bg-gray-800 shadow-lg flex-row items-start text-gray-800 text-xs cursor-default" >
+    <div class="w-full min-w-max bg-gray-800 shadow-lg flex-row items-start text-gray-800 text-[11px] cursor-default" >
         <div class="flex flex-col justify-center text-center p-1 pb-3 gap-y-1">
             <div v-for="(option, index) in data" class="flex justify-start py-1 px-2 gap-x-1.5 hover:bg-gray-700">
                 <img :src="`/media/group/${option.user.avatar_id}`" :alt="option.user.contact_name" srcset="" class="h-4 rounded-full shadow">
                 <p class="text-left text-gray-100">
-                    <span class="font-semibold">{{ option.user.contact_name }}</span> 
-                    <span class="text-gray-300"> ({{ option.user.username }})</span> - 
-                    <span class="capitalize">{{ option.route.module }}</span>
+                    <!-- <span class="font-semibold">{{ option.user.contact_name }}</span>  -->
+                    <span class="font-semibold text-gray-100">{{ option.user.username }}</span> - 
+                    <span class="capitalize text-gray-300">{{ option.route.module }}</span>
                 </p>
             </div>
         </div>
