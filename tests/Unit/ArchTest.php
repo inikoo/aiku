bc89a08c7f250f5ebac96c954da8fcceddec6514
Raul Perusquia <raul@inikoo.com>
2023-06-25 14:09:59 +0800

    improve arch tests

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -6,6 +6,9 @@
  */
 
 
-test('globals')
-    ->expect(['dd', 'dump','print_r'])
+
+test('Not debugging statements are left in our code.')
+    ->expect(['dd', 'dump','print_r','ray'])
     ->not->toBeUsed();
+
+// Need to create a test that check if Store|Update actions has authorize method
