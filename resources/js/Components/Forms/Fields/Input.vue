3359f4a2a351808dc8e189d55af328e0769edf83
aqordeon <aqordivika@yahoo.co.id>
2023-06-28 16:26:50 +0800

    PKA-902 Remove Primitive Files: PrimitiveInput

--- resources/js/Components/Forms/Fields/Input.vue
+++ resources/js/Components/Forms/Fields/Input.vue
@@ -6,7 +6,10 @@
 
 
 <script setup lang="ts">
-import PrimitiveInput from '@/Components/Elements/Fields/PrimitiveInput.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faExclamationCircle, faCheckCircle } from "@/../private/pro-solid-svg-icons"
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(faExclamationCircle, faCheckCircle);
 
 const props = defineProps<{
     form: any,
@@ -17,23 +20,32 @@ const props = defineProps<{
     }
 }>()
 
-// let type = 'text'
-// if (props.options !== undefined && props.options.type) {
-//     type = props.options.type
-// }
-
-// // For Letters and Words Counter
-// let showStats = false
-// if(props.options){
-//     showStats = props.options.counter
-// }
-
 </script>
 <template>
     <div class="relative">
-        <PrimitiveInput v-model="form[fieldName]" :showStats="props.options?.counter ?? false" :type="props.options?.type ?? 'text'" :form="form" :fieldName="fieldName" :placeholder="fieldData.placeholder"/>
+        <div class="relative">
+            <input :type="props.options?.type ?? 'text'" v-model.trim="form[fieldName]" @input="form.errors[fieldName] = ''"
+                :placeholder="fieldData.placeholder" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+            <div v-if="form.errors[fieldName] || form.recentlySuccessful"
+                class="absolute inset-y-2/4 right-0 pr-3 flex items-center pointer-events-none">
+                <FontAwesomeIcon icon="fas fa-exclamation-circle" v-if="form.errors[fieldName]" class="h-5 w-5 text-red-500"
+                    aria-hidden="true" />
+                <FontAwesomeIcon icon="fas fa-check-circle" v-if="form.recentlySuccessful"
+                    class="mt-1.5  h-5 w-5 text-green-500" aria-hidden="true" />
+            </div>
+        </div>
+
+        <!-- Counter: Letters and Words -->
+        <div v-if="props.options?.counter" class="grid grid-flow-col text-xs italic text-gray-500 mt-2 space-x-12 justify-start">
+            <p class="">
+                Letters: {{ form[fieldName].length }}
+            </p>
+            <p class="">
+                Words: {{ form[fieldName].trim().split(/\s+/).filter(Boolean).length }}
+            </p>
+        </div>
     </div>
-    <p v-if="form.errors[fieldName]" class="mt-2 text-sm text-red-600" id="email-error">{{ form.errors[fieldName] }}</p>
+    <p v-if="form.errors[fieldName]" class="mt-2 text-sm text-red-600" :id="`${fieldName}-error`">{{ form.errors[fieldName] }}</p>
 </template>
 
 
