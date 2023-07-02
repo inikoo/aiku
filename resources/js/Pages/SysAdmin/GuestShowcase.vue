34590c5e35f965e889a1a915947eaeeb6f2ceb47
Raul Perusquia <raul@inikoo.com>
2023-07-02 14:27:16 +0800

    GuestShowcase shell 🐚

---
+++ resources/js/Pages/SysAdmin/GuestShowcase.vue
@@ -0,0 +1,27 @@
+<!--
+  - Author: Raul Perusquia <raul@inikoo.com>
+  - Created: Sat, 01 Jul 2023 09:05:55 Malaysia Time, Sanur, Indonesia
+  - Copyright (c) 2023, Raul A Perusquia Flores
+  -->
+
+<script setup lang="ts">
+
+const props = defineProps<{
+    data: {
+        contactCard: {
+        },
+        stats: [{
+            label: string,
+            value: number,
+        }],
+    },
+
+}>()
+</script>
+
+<template >
+    <div class="grid text-gray-600  grid-flow-col grid-cols-2 px-4 pt-4">
+
+    </div>
+</template>
+
