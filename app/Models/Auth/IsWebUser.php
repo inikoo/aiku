417601bf0fa69b31bc3618ab6e53500a9ccd16a3
Raul Perusquia <raul@inikoo.com>
2023-07-05 10:58:54 +0800

    Warning message for shared file ⚠️

--- app/Models/Auth/IsWebUser.php
+++ app/Models/Auth/IsWebUser.php
@@ -15,6 +15,8 @@ use Laravel\Sanctum\HasApiTokens;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
+// ⚠️ Warning shared Aiku/Iris file ⚠️
+// Edit only in aiku repo 💣
 trait IsWebUser
 {
     use HasApiTokens;
