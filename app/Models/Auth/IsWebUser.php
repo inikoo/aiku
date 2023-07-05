89927104569917d9010e3832edd14e1fb8602b17
Raul Perusquia <raul@inikoo.com>
2023-07-05 10:13:09 +0800

    Add factory trait to WebUser

--- app/Models/Auth/IsWebUser.php
+++ app/Models/Auth/IsWebUser.php
@@ -8,6 +8,7 @@
 namespace App\Models\Auth;
 
 use App\Models\CRM\Customer;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Sanctum\HasApiTokens;
@@ -19,6 +20,7 @@ trait IsWebUser
     use HasApiTokens;
     use SoftDeletes;
     use HasSlug;
+    use HasFactory;
 
 
     public function getSlugOptions(): SlugOptions
