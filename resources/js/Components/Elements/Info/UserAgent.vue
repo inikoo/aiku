ec4cb931395433f93fa5cb62db4d8af02bf80628
aqordeon <aqordivika@yahoo.co.id>
2023-07-14 09:50:03 +0800

    PKA-979 FontAwesome: adjust png to FontAwesome

--- resources/js/Components/Elements/Info/UserAgent.vue
+++ resources/js/Components/Elements/Info/UserAgent.vue
@@ -6,23 +6,30 @@
 
 <script setup lang="ts">
 import { capitalize } from "@/Composables/capitalize"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const props = defineProps<{
     data: string[]
 }>()
+console.log(typeof props.data)
 
-const deviceType = props.data[0]
-const platformName = props.data[1]
-const browserName = props.data[2].split(" ")[0]
+// const deviceType = props.data[0]
+// const platformName = props.data[1]
+// const browserName = props.data[2].split(" ")[0]
 
-const deviceIcon   = '/devices/'+deviceType.toLowerCase()+'.png';
-const platformIcon = '/os/'+platformName.toLowerCase()+'.png';
-const browserIcon  = '/browsers/'+browserName.toLowerCase()+'.png';
+// const deviceIcon = '/devices/' + deviceType.toLowerCase() + '.png';
+// const platformIcon = '/os/' + platformName.toLowerCase() + '.png';
+// const browserIcon = '/browsers/' + browserName.toLowerCase() + '.png';
 
 </script>
 
 <template>
-    <img class="inline pr-2 w-6" :src='deviceIcon' :alt="deviceType"   :title='capitalize(deviceType)'  />
-    <img class="inline pr-2 w-6" :src='platformIcon' :alt="platformName"   :title='capitalize(platformName)'  />
-    <img class="inline pr-1 w-6" :src='browserIcon' :alt="browserName"   :title='capitalize(browserName)'  />
+    <div class="flex">
+        <FontAwesomeIcon :icon='data[0].icon' class='w-6' aria-hidden='true' :title='capitalize(data[0].title)' />
+        <FontAwesomeIcon :icon='data[1].icon' class='w-6' aria-hidden='true' :title='capitalize(data[1].title)' />
+        <FontAwesomeIcon :icon='data[2].icon' class='w-6' aria-hidden='true' :title='capitalize(data[2].title)' />
+    </div>
+    <!-- <img class="inline pr-2 w-6" :src='deviceIcon' :alt="deviceType" :title='capitalize(deviceType)' />
+    <img class="inline pr-2 w-6" :src='platformIcon' :alt="platformName" :title='capitalize(platformName)' />
+    <img class="inline pr-1 w-6" :src='browserIcon' :alt="browserName" :title='capitalize(browserName)' /> -->
 </template>
