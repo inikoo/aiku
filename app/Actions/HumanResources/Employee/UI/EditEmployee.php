4c9f280bcd6efa2e57d8ce5de848510c3c6e4c24
Raul Perusquia <raul@inikoo.com>
2024-01-05 03:39:12 +0800

    feat: employeePosition scope resources 🐫

--- app/Actions/HumanResources/Employee/UI/EditEmployee.php
+++ app/Actions/HumanResources/Employee/UI/EditEmployee.php
@@ -7,17 +7,23 @@
 
 namespace App\Actions\HumanResources\Employee\UI;
 
-use App\Actions\InertiaAction;
+use App\Actions\InertiaOrganisationAction;
 use App\Enums\HumanResources\Employee\EmployeeStateEnum;
+use App\Http\Resources\HumanResources\JobPositionResource;
+use App\Http\Resources\Inventory\WarehouseResource;
+use App\Http\Resources\Market\ShopResource;
+use App\Http\Resources\SysAdmin\Organisation\OrganisationResource;
 use App\Models\HumanResources\Employee;
 use App\Models\HumanResources\JobPosition;
+use App\Models\Inventory\Warehouse;
+use App\Models\Market\Shop;
+use App\Models\SysAdmin\Organisation;
 use Exception;
 use Inertia\Inertia;
 use Inertia\Response;
 use Lorisleiva\Actions\ActionRequest;
-use Spatie\LaravelOptions\Options;
 
-class EditEmployee extends InertiaAction
+class EditEmployee extends InertiaOrganisationAction
 {
     public function handle(Employee $employee): Employee
     {
@@ -29,9 +35,9 @@ class EditEmployee extends InertiaAction
         return $request->user()->hasPermissionTo("human-resources.{$this->organisation->slug}.edit");
     }
 
-    public function asController(Employee $employee, ActionRequest $request): Employee
+    public function asController(Organisation $organisation, Employee $employee, ActionRequest $request): Employee
     {
-        $this->initialisation($request);
+        $this->initialisation($organisation, $request);
 
         return $this->handle($employee);
     }
@@ -46,7 +52,7 @@ class EditEmployee extends InertiaAction
             'EditModel',
             [
                 'title'       => __('employee'),
-                'breadcrumbs' => $this->getBreadcrumbs($employee),
+                'breadcrumbs' => $this->getBreadcrumbs($request->route()->originalParameters()),
                 'pageHead'    => [
                     'title'    => $employee->contact_name,
                     'actions'  => [
@@ -55,7 +61,7 @@ class EditEmployee extends InertiaAction
                             'style' => 'exitEdit',
                             'route' => [
                                 'name'       => preg_replace('/edit$/', 'show', $request->route()->getName()),
-                                'parameters' => array_values($this->originalParameters)
+                                'parameters' => array_values($request->route()->originalParameters())
                             ]
                         ]
                     ]
@@ -78,26 +84,99 @@ class EditEmployee extends InertiaAction
                                     'placeholder' => __('Date of birth'),
                                     'value'       => $employee->date_of_birth
                                 ],
-                                'job_title' => [
-                                    'type'        => 'select',
-                                    'label'       => __('position'),
-                                    'options'     => Options::forModels(JobPosition::class, label: 'name', value: 'name'),
-                                    'placeholder' => __('Select a job position'),
-                                    'mode'        => 'single',
-                                    'value'       => $employee->job_title,
-                                    'searchable'  => true
-                                ],
-                                'state' => [
-                                    'type'        => 'select',
-                                    'label'       => __('state'),
-                                    'options'     => Options::forEnum(EmployeeStateEnum::class),
-                                    'placeholder' => __('Select a state'),
-                                    'mode'        => 'single',
-                                    'value'       => $employee->state,
-                                    'searchable'  => true
-                                ]
+
+
                             ]
-                        ]
+                        ],
+                        [
+                            'title'  => __('Employment'),
+                            'fields' => [
+                                'worker_number'       => [
+                                    'type'     => 'input',
+                                    'label'    => __('worker number'),
+                                    'required' => true,
+                                    'value'    => $employee->worker_number
+                                ],
+                                'alias'               => [
+                                    'type'     => 'input',
+                                    'label'    => __('alias'),
+                                    'required' => true,
+                                    'value'    => $employee->alias
+                                ],
+                                'work_email'          => [
+                                    'type'  => 'input',
+                                    'label' => __('work email'),
+                                    'value' => $employee->work_email
+                                ],
+                                'state'               => [
+                                    'label'    => __('state'),
+                                    'type'     => 'radio',
+                                    'mode'     => 'card',
+                                    'required' => true,
+                                    'value'    => $employee->state,
+                                    'options'  => [
+                                        [
+                                            'title'       => __('Hired'),
+                                            'description' => __('Will start in future date'),
+                                            'value'       => EmployeeStateEnum::HIRED->value
+                                        ],
+                                        [
+                                            'title'       => __('Working'),
+                                            'description' => __('Employee already working'),
+                                            'value'       => EmployeeStateEnum::WORKING->value
+                                        ],
+                                    ]
+                                ],
+                                'employment_start_at' => [
+                                    'type'     => 'date',
+                                    'label'    => __('employment start at'),
+                                    'value'    => $employee->employment_start_at,
+                                    'required' => true
+                                ],
+                            ]
+                        ],
+                        [
+                            'title'  => __('job'),
+                            'fields' => [
+                                'job_title' => [
+                                    'type'        => 'input',
+                                    'label'       => __('job title'),
+                                    'placeholder' => __('Job title'),
+                                    'searchable'  => true,
+                                    'value'       => $employee->job_title
+                                ],
+                                'positions' => [
+                                    'type'     => 'employeePosition',
+                                    'required' => true,
+                                    'label'    => __('position'),
+                                    'options'  => [
+                                        'positions'     => JobPositionResource::collection(JobPosition::all()),
+                                        'organisations' => OrganisationResource::collection(Organisation::all()),
+                                        'shops'         => ShopResource::collection(Shop::all()),
+                                        'warehouses'    => WarehouseResource::collection(Warehouse::all()),
+                                    ],
+                                    'value'    => [],
+                                    'full'     => true
+                                ],
+                            ]
+                        ],
+                        [
+                            'title'  => __('User credentials'),
+                            'fields' => [
+
+                                'username' => [
+                                    'type'  => 'input',
+                                    'label' => __('username'),
+                                    'value' => ''
+
+                                ],
+                                'password' => [
+                                    'type'  => 'password',
+                                    'label' => __('password'),
+                                    'value' => ''
+                                ],
+                            ]
+                        ],
 
                     ],
                     'args'      => [
@@ -114,8 +193,8 @@ class EditEmployee extends InertiaAction
         );
     }
 
-    public function getBreadcrumbs(Employee $employee): array
+    public function getBreadcrumbs(array $routeParameters): array
     {
-        return ShowEmployee::make()->getBreadcrumbs(employee:$employee, suffix: '('.__('editing').')');
+        return ShowEmployee::make()->getBreadcrumbs(routeParameters:$routeParameters, suffix: '('.__('editing').')');
     }
 }
