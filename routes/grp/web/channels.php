f19eeb96162889f30d5dc3fcb1fe493316893386
Raul Perusquia <raul@inikoo.com>
2024-01-10 16:24:21 +0800

    feat: websockets server side init

--- routes/grp/web/channels.php
+++ routes/grp/web/channels.php
@@ -1,18 +1,25 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 10 Jan 2024 16:41:28 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2024, Raul A Perusquia Flores
+ */
 
+use App\Models\SysAdmin\User;
 use Illuminate\Support\Facades\Broadcast;
 
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
+Broadcast::channel('grp.personal.{userID}', function (User $user, int $userID) {
+    return $userID === $user->id;
+});
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('grp.general', function (User $user) {
+    return true;
+});
+
+Broadcast::channel('grp.live.users', function (User $user) {
+    return [
+        'id'          => $user->id,
+        'alias'       => $user->slug,
+        'name'        => $user->contact_name,
+    ];
 });
