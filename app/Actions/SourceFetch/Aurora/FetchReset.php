f3fabba123a767415adfb97fde86401a06de8a77
Raul Perusquia <raul@inikoo.com>
2024-01-12 22:32:40 +0800

    fetch: reset command 🌋

--- app/Actions/SourceFetch/Aurora/FetchReset.php
+++ app/Actions/SourceFetch/Aurora/FetchReset.php
@@ -14,17 +14,15 @@ use Illuminate\Support\Facades\DB;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Lorisleiva\Actions\Concerns\WithAttributes;
 
-/**
- * @property array|\ArrayAccess|mixed $timeStart
- * @property $timeLastStep
- */
 class FetchReset
 {
     use AsAction;
     use WithOrganisationsArgument;
     use WithAttributes;
 
-    public string $commandSignature = 'fetch:reset {organisations?*}';
+    public string $commandSignature = 'fetch:reset {organisations?*} {--a|all} {--b|base} {--c|crm} {--o|other}  ';
+    private int $timeStart;
+    private int $timeLastStep;
 
 
     private function setAuroraConnection($databaseName): void
@@ -38,82 +36,57 @@ class FetchReset
 
     public function asCommand(Command $command): int
     {
-        $organisations  = $this->getOrganisations($command);
-        $exitCode       = 0;
+        $organisations = $this->getOrganisations($command);
+        $exitCode      = 0;
 
         foreach ($organisations as $organisation) {
-            $result = (int)$organisation->execute(function () use ($command, $organisation) {
-                if ($databaseName = Arr::get($organisation->source, 'db_name')) {
-                    $command->line("🏃 $organisation->slug ");
-                    $this->setAuroraConnection($databaseName);
+            if ($databaseName = Arr::get($organisation->source, 'db_name')) {
+                $command->line("🏃 $organisation->slug ");
+                $this->setAuroraConnection($databaseName);
 
-                    DB::connection('aurora')->table('pika_fetch')->truncate();
-                    DB::connection('aurora')->table('pika_fetch_error')->truncate();
+                DB::connection('aurora')->table('pika_fetch')->truncate();
+                DB::connection('aurora')->table('pika_fetch_error')->truncate();
 
 
-                    $this->timeStart    = microtime(true);
-                    $this->timeLastStep =  microtime(true);
+                $this->timeStart    = microtime(true);
+                $this->timeLastStep = microtime(true);
 
-                    DB::connection('aurora')->table('Agent Dimension')
+
+                if ($command->option('all') || $command->option('base')) {
+                    DB::connection('aurora')->table('Staff Dimension')
                         ->update(
                             [
                                 'aiku_id'       => null,
-                                'agent_aiku_id' => null,
+                                'aiku_guest_id' => null
+                            ]
+                        );
+                    DB::connection('aurora')->table('Staff Deleted Dimension')
+                        ->update(
+                            [
+                                'aiku_id'       => null,
+                                'aiku_guest_id' => null
                             ]
                         );
 
-
-                    DB::connection('aurora')->table('Part Dimension')
-                        ->update(['aiku_agent_unit_id' => null]);
-
-                    DB::connection('aurora')->table('Supplier Part Dimension')
-                        ->update(['aiku_agent_unit_id' => null]);
-                    DB::connection('aurora')->table('Supplier Part Historic Dimension')
-                        ->update(['aiku_agent_unit_id' => null]);
-
+                    $command->line('✅ hr');
                     DB::connection('aurora')->table('User Dimension')
                         ->update(['aiku_id' => null]);
                     DB::connection('aurora')->table('User Deleted Dimension')
                         ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Attachment Bridge')
-                        ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Image Subject Bridge')
-                        ->update(['aiku_id' => null]);
 
-                    $command->line('✅ base');
+
+                    $command->line('✅ sysadmins');
 
                     DB::connection('aurora')->table('Store Dimension')
                         ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Shipping Zone Schema Dimension')
-                        ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Shipping Zone Dimension')
-                        ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Charge Dimension')
-                        ->update(['aiku_id' => null]);
+
                     DB::connection('aurora')->table('Shipper Dimension')
                         ->update(['aiku_id' => null]);
                     DB::connection('aurora')->table('Website Dimension')
                         ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Category Dimension')
-                        ->update([
-                                     'aiku_department_id' => null,
-                                     'aiku_family_id'     => null
-                                 ]);
+
 
                     $command->line('✅ shops');
-
-
-                    DB::connection('aurora')->table('Email Campaign Type Dimension')
-                        ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Email Campaign Dimension')
-                        ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Email Tracking Dimension')
-                        ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Email Tracking Event Dimension')
-                        ->update(['aiku_id' => null]);
-
-                    $command->line("✅ mailroom \t\t".$this->stepTime());
-
 
                     DB::connection('aurora')->table('Warehouse Dimension')
                         ->update(['aiku_id' => null]);
@@ -124,14 +97,16 @@ class FetchReset
                     DB::connection('aurora')->table('Location Deleted Dimension')
                         ->update(['aiku_id' => null]);
 
-                    DB::connection('aurora')->table('Fulfilment Rent Transaction Fact')
-                        ->update(['aiku_id' => null]);
 
-                    DB::connection('aurora')->table('Fulfilment Asset Dimension')
-                        ->update(['aiku_id' => null]);
+                    $command->line('✅ warehouses');
 
-                    $command->line("✅ warehouses \t\t".$this->stepTime());
-
+                    DB::connection('aurora')->table('Agent Dimension')
+                        ->update(
+                            [
+                                'aiku_id'       => null,
+                                'agent_aiku_id' => null,
+                            ]
+                        );
 
                     DB::connection('aurora')->table('Supplier Dimension')
                         ->update(
@@ -148,23 +123,86 @@ class FetchReset
                             ]
                         );
 
+                    $command->line('✅ agents/suppliers');
+
+
+                    DB::connection('aurora')->table('Attachment Bridge')
+                        ->update(['aiku_id' => null]);
+                    DB::connection('aurora')->table('Image Subject Bridge')
+                        ->update(['aiku_id' => null]);
+
+                    $command->line('🆗 base');
+                }
+
+                if ($command->option('all') || $command->option('crm')) {
+
+                    DB::connection('aurora')->table('Customer Dimension')
+                        ->update(['aiku_id' => null]);
+                    DB::connection('aurora')->table('Customer Deleted Dimension')
+                        ->update(['aiku_id' => null]);
+                    DB::connection('aurora')->table('Customer Client Dimension')
+                        ->update(['aiku_id' => null]);
+                    DB::connection('aurora')->table('Website User Dimension')
+                        ->update(['aiku_id' => null]);
+
+
+                    $command->line('🆗 crm');
+                }
+
+
+                if ($command->option('all') || $command->option('other')) {
+                    DB::connection('aurora')->table('Part Dimension')
+                        ->update(['aiku_agent_unit_id' => null]);
+
+                    DB::connection('aurora')->table('Supplier Part Dimension')
+                        ->update(['aiku_agent_unit_id' => null]);
+                    DB::connection('aurora')->table('Supplier Part Historic Dimension')
+                        ->update(['aiku_agent_unit_id' => null]);
+
+
+
+                    $command->line('✅ base');
+
+
+                    DB::connection('aurora')->table('Shipping Zone Schema Dimension')
+                        ->update(['aiku_id' => null]);
+                    DB::connection('aurora')->table('Shipping Zone Dimension')
+                        ->update(['aiku_id' => null]);
+                    DB::connection('aurora')->table('Charge Dimension')
+                        ->update(['aiku_id' => null]);
+
+                    DB::connection('aurora')->table('Category Dimension')
+                        ->update([
+                            'aiku_department_id' => null,
+                            'aiku_family_id'     => null
+                        ]);
+
+                    $command->line('✅ shops');
+
+
+                    DB::connection('aurora')->table('Email Campaign Type Dimension')
+                        ->update(['aiku_id' => null]);
+                    DB::connection('aurora')->table('Email Campaign Dimension')
+                        ->update(['aiku_id' => null]);
+                    DB::connection('aurora')->table('Email Tracking Dimension')
+                        ->update(['aiku_id' => null]);
+                    DB::connection('aurora')->table('Email Tracking Event Dimension')
+                        ->update(['aiku_id' => null]);
+
+                    $command->line("✅ mailroom \t\t".$this->stepTime());
+
+
+                    DB::connection('aurora')->table('Fulfilment Rent Transaction Fact')
+                        ->update(['aiku_id' => null]);
+
+                    DB::connection('aurora')->table('Fulfilment Asset Dimension')
+                        ->update(['aiku_id' => null]);
+
+                    $command->line("✅ warehouses \t\t".$this->stepTime());
+
 
                     $command->line("✅ suppliers \t\t".$this->stepTime());
 
-                    DB::connection('aurora')->table('Staff Dimension')
-                        ->update(
-                            [
-                                'aiku_id'       => null,
-                                'aiku_guest_id' => null
-                            ]
-                        );
-                    DB::connection('aurora')->table('Staff Deleted Dimension')
-                        ->update(
-                            [
-                                'aiku_id'       => null,
-                                'aiku_guest_id' => null
-                            ]
-                        );
 
                     DB::connection('aurora')->table('Timesheet Dimension')
                         ->update(['aiku_id' => null]);
@@ -176,9 +214,9 @@ class FetchReset
 
                     DB::connection('aurora')->table('Part Dimension')
                         ->update([
-                                     'aiku_unit_id' => null,
-                                     'aiku_id'      => null
-                                 ]);
+                            'aiku_unit_id' => null,
+                            'aiku_id'      => null
+                        ]);
 
                     DB::connection('aurora')->table('Part Deleted Dimension')
                         ->update(['aiku_id' => null]);
@@ -211,11 +249,11 @@ class FetchReset
                     $command->line("✅ supplier products and PO \t".$this->stepTime());
                     DB::connection('aurora')->table('Inventory Transaction Fact')
                         ->update([
-                                     'aiku_id'         => null,
-                                     'aiku_dn_item_id' => null,
-                                     'aiku_picking_id' => null,
+                            'aiku_id'         => null,
+                            'aiku_dn_item_id' => null,
+                            'aiku_picking_id' => null,
 
-                                 ]);
+                        ]);
 
                     $command->line("✅ stock movements \t".$this->stepTime());
 
@@ -226,12 +264,7 @@ class FetchReset
 
                     $command->line("✅ products \t\t".$this->stepTime());
 
-                    DB::connection('aurora')->table('Customer Dimension')
-                        ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Customer Deleted Dimension')
-                        ->update(['aiku_id' => null]);
-                    DB::connection('aurora')->table('Customer Client Dimension')
-                        ->update(['aiku_id' => null]);
+
                     DB::connection('aurora')->table('Customer Favourite Product Fact')
                         ->update(['aiku_id' => null]);
                     DB::connection('aurora')->table('Back in Stock Reminder Fact')
@@ -269,15 +302,8 @@ class FetchReset
                     DB::connection('aurora')->table('Payment Dimension')->update(['aiku_id' => null]);
                     DB::connection('aurora')->table('Payment Account Dimension')->update(['aiku_id' => null]);
                     DB::connection('aurora')->table('Payment Service Provider Dimension')->update(['aiku_id' => null]);
-
-
-
                     $command->line("✅ orders \t\t".$this->stepTime());
                 }
-            });
-
-            if ($result !== 0) {
-                $exitCode = $result;
             }
         }
 
@@ -286,10 +312,10 @@ class FetchReset
 
     public function stepTime(): string
     {
-        $rollTime          = microtime(true) - $this->timeStart;
-        $diff              = microtime(true) - $this->timeLastStep;
-        $this->timeLastStep=microtime(true);
+        $rollTime           = microtime(true) - $this->timeStart;
+        $diff               = microtime(true) - $this->timeLastStep;
+        $this->timeLastStep = microtime(true);
 
-        return "\t".round($rollTime, 2).'s' . "\t\t".round($diff, 2).'s';
+        return "\t".round($rollTime, 2).'s'."\t\t".round($diff, 2).'s';
     }
 }
