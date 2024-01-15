af13789aea5398e24e7a03bb2901d82b1b2c78f7
Raul Perusquia <raul@inikoo.com>
2024-01-15 12:06:39 +0800

    feat: working places more data

--- app/Http/Resources/Helpers/AddressResource.php
+++ app/Http/Resources/Helpers/AddressResource.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Author: Raul Perusquia <raul@inikoo.com>
- * Created: Fri, 12 May 2023 13:27:22 Malaysia Time, Pantai Lembeng, Bali, Id
+ * Created: Fri, 12 May 2023 13:27:22 Malaysia Time, Pantai Lembeng, Bali, Indonesia
  * Copyright (c) 2023, Raul A Perusquia Flores
  */
 
@@ -9,48 +9,60 @@ namespace App\Http\Resources\Helpers;
 
 use App\Http\Resources\Assets\CountryResource;
 use App\Http\Resources\HasSelfCall;
+use App\Models\Helpers\Address;
+use CommerceGuys\Addressing\Address as Adr;
+use CommerceGuys\Addressing\AddressFormat\AddressFormatRepository;
+use CommerceGuys\Addressing\Country\CountryRepository;
+use CommerceGuys\Addressing\Formatter\DefaultFormatter;
+use CommerceGuys\Addressing\Subdivision\SubdivisionRepository;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/**
- *
- * @property string $address_line_1
- * @property string $address_line_2
- * @property string $sorting_code
- * @property string $postal_code
- * @property string $locality
- * @property string $dependant_locality
- * @property string $administrative_area
- * @property string $country_code
- * @property integer $country_id
- * @property string $checksum
- * @property string $created_at
- * @property string $updated_at
- * @property mixed $country
- *
- */
 class AddressResource extends JsonResource
 {
     use HasSelfCall;
 
     public function toArray($request): array
     {
+
+        /** @var Address $address */
+        $address = $this;
+
+        $addressFormatRepository = new AddressFormatRepository();
+        $countryRepository       = new CountryRepository();
+        $subdivisionRepository   = new SubdivisionRepository();
+        $formatter               = new DefaultFormatter($addressFormatRepository, $countryRepository, $subdivisionRepository);
+
+
+        $adr = new Adr();
+        $adr = $adr
+            ->withCountryCode($address->country_code)
+            ->withAdministrativeArea($address->administrative_area)
+            ->withDependentLocality($address->dependant_locality)
+            ->withLocality($address->locality)
+            ->withPostalCode($address->postal_code)
+            ->withSortingCode($address->sorting_code)
+            ->withAddressLine2($address->address_line_2)
+            ->withAddressLine1($address->address_line_1);
+
         return [
-            'address_line_1'      => $this->address_line_1,
-            'address_line_2'      => $this->address_line_2,
-            'sorting_code'        => $this->sorting_code,
-            'postal_code'         => $this->postal_code,
-            'locality'            => $this->locality,
-            'dependant_locality'  => $this->dependant_locality,
-            'administrative_area' => $this->administrative_area,
-            'country_code'        => $this->country_code,
-            'country_id'          => $this->country_id,
-            'checksum'            => $this->checksum,
-            'created_at'          => $this->created_at,
-            'updated_at'          => $this->updated_at,
+            'address_line_1'      => $address->address_line_1,
+            'address_line_2'      => $address->address_line_2,
+            'sorting_code'        => $address->sorting_code,
+            'postal_code'         => $address->postal_code,
+            'locality'            => $address->locality,
+            'dependant_locality'  => $address->dependant_locality,
+            'administrative_area' => $address->administrative_area,
+            'country_code'        => $address->country_code,
+            'country_id'          => $address->country_id,
+            'checksum'            => $address->checksum,
+            'created_at'          => $address->created_at,
+            'updated_at'          => $address->updated_at,
             'country'             => $this->whenLoaded(
                 'country',
-                CountryResource::make($this->country)
+                CountryResource::make($address->country)
             ),
+            'formatted_address'   => $formatter->format($adr),
+
 
         ];
     }
