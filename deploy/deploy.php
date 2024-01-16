b2aceffdb103c30810c741ced61a7c5d3e9f5c53
Raul Perusquia <raul@inikoo.com>
2024-01-16 13:11:30 +0800

    refactor: add migration script tp shared files 🦈

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -23,7 +23,7 @@ task('deploy:set-release', function () {
 });
 
 set('shared_dirs', ['storage','private']);
-
+set('shared_files', ['.env','aurora_migration_base.sh']);
 desc('Deploys your project');
 task('deploy', [
     'deploy:prepare',
