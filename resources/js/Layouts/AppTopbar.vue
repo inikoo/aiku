1bf0e5f77209b23b95bf517c1ea0b61c00fb1278
aqordeon <aqordivika@yahoo.co.id>
2024-01-18 13:46:51 +0800

    fix: TopBar: stacked

--- resources/js/Layouts/AppTopbar.vue
+++ resources/js/Layouts/AppTopbar.vue
@@ -89,7 +89,7 @@ const logoutAuth = () => {
                             <!-- Dropdown: Organisations -->
                             <Menu as="div" class="relative inline-block text-left">
                                 <MenuButton
-                                    class="inline-flex min-w-fit w-32 max-w-full whitespace-nowrap justify-between items-center gap-x-2 rounded px-2.5 py-2 text-xs font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+                                    class="inline-flex min-w-32 max-w-full whitespace-nowrap justify-between items-center gap-x-2 rounded px-2.5 py-2 text-xs font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
                                     :class="[ layout.organisations.data.find((item) => item.slug == layout.currentParams.organisation) ? 'bg-indigo-500 text-white hover:bg-indigo-600' : 'bg-slate-100 hover:bg-slate-200 text-slate-600 ring-1 ring-slate-300']"
                                 >
                                     <div class="flex items-center gap-x-1">
@@ -148,11 +148,11 @@ const logoutAuth = () => {
                                 </transition>
                             </Menu>
 
-                            <!-- Dropdown: Shops -->
+                            <!-- Dropdown: Shops (If shops already selected) -->
                             <Menu v-if="useLayoutStore().organisations.data.find(organisation => organisation.slug == layout.currentParams.organisation) && (route(useLayoutStore().currentRoute, useLayoutStore().currentParams)).includes('shops')"
                                 as="div" class="relative inline-block text-left">
                                 <MenuButton
-                                    class="inline-flex min-w-fit w-32 max-w-full whitespace-nowrap justify-between items-center gap-x-2 rounded px-2.5 py-2 text-xs font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+                                    class="inline-flex min-w-32 max-w-full whitespace-nowrap justify-between items-center gap-x-2 rounded px-2.5 py-2 text-xs font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
                                     :class="[ layout.currentParams.shop ? 'bg-indigo-500 text-white hover:bg-indigo-600' : 'bg-slate-100 hover:bg-slate-200 text-slate-600 ring-1 ring-slate-300']"
                                 >
                                     <div class="flex items-center gap-x-1">
@@ -196,7 +196,7 @@ const logoutAuth = () => {
                             <Menu v-if="useLayoutStore().navigation.org[layout.currentParams.organisation]?.warehouses_navigation && (route(useLayoutStore().currentRoute, useLayoutStore().currentParams)).includes('warehouse')"
                                 as="div" class="relative inline-block text-left">
                                 <MenuButton
-                                    class="inline-flex min-w-fit w-32 max-w-full whitespace-nowrap justify-between items-center gap-x-2 rounded px-2.5 py-2 text-xs font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+                                    class="inline-flex min-w-32 max-w-full whitespace-nowrap justify-between items-center gap-x-2 rounded px-2.5 py-2 text-xs font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
                                     :class="[ layout.currentParams.warehouse ? 'bg-indigo-500 text-white hover:bg-indigo-600' : 'bg-slate-100 hover:bg-slate-200 text-slate-600 ring-1 ring-slate-300']"
                                 >
                                     <div class="flex items-center gap-x-1">
