289dd08ddfac25fdb8a8fa9a5416cee38f8e4af1
aqordeon <aqordivika@yahoo.co.id>
2024-01-23 11:17:43 +0800

    feat: Component: PageHeading & Action (from Wowsbar)

--- resources/js/Components/Headings/PageHeading.vue
+++ resources/js/Components/Headings/PageHeading.vue
@@ -4,267 +4,128 @@
   -  Copyright (c) 2022, Raul A Perusquia Flores
   -->
 
-<script setup>
-import { Link } from "@inertiajs/vue3";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEmptySet, faMoneyCheckAlt, faPeopleArrows,faSlidersH } from '@fal';
-import { faPencil, faArrowLeft, faBorderAll, faTrashAlt } from '@far';
+<script setup lang="ts">
+import { Link } from "@inertiajs/vue3"
 
-import { faPlus } from '@fas';
-import Button from "@/Components/Elements/Buttons/Button.vue";
-import { capitalize } from "@/Composables/capitalize";
-import { useLocaleStore } from "@/Stores/locale";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { trans } from "laravel-vue-i18n";
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { } from '@fal'
+import { } from '@far'
+import { } from '@fas'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { capitalize } from "@/Composables/capitalize"
+import MetaLabel from "@/Components/Headings/MetaLabel.vue"
+import Container from "@/Components/Headings/Container.vue"
+import Action from "@/Components/Forms/Fields/Action.vue"
+import SubNavigation from "@//Components/Navigation/SubNavigation.vue"
+import { Action as ActionTS } from "@/types/Action"
+import { routeType } from '@/types/route'
 
-library.add(faEmptySet, faMoneyCheckAlt, faPeopleArrows, faSlidersH,faPlus, faPencil, faArrowLeft, faBorderAll, faTrashAlt);
-const props = defineProps(["data"])
-const locale = useLocaleStore()
-
-const getActionLabel = function(action) {
-    if (action.hasOwnProperty("label")) {
-        return action.label
-    } else {
-        switch (action.style) {
-            case "edit":
-                return trans("edit")
-
-            case "exitEdit":
-                return trans("exit edit")
-
-            case "cancel":
-                return trans("cancel")
-
-            case "delete":
-                return trans("delete")
-            case "clearMulti" :
-                return trans("clear")
-            default:
-                return ""
-        }
-    }
-};
-
-
-// Set icon depends on the Style props
-const getActionIcon = (action) => {
-    if (action.hasOwnProperty("icon")) {
-        return action.icon
-    } else {
-        switch (action.style) {
-            case "edit":
-                return ["far", "fa-pencil"]
-
-            case "exit":
-            case "exitEdit":
-                return ["far", "fa-arrow-left"]
-
-            case "create":
-                return ["fas", "fa-plus"]
-
-            case "cancel":
-                return ["far", "fa-arrow-left"]
-
-            case "delete":
-                return ["far", "fa-trash-alt"]
-
-            case "withMulti":
-                return ["far", "fa-border-all"]
-
-            default:
-                return null
-        }
-    }
+interface Icon {
+    icon: string[] | string
 }
 
+library.add()
+
+const props = defineProps<{
+    data: {
+        title: string
+        noCapitalise?:boolean
+        icon: {
+            icon: Icon
+            tooltip: string
+        }
+        actionActualMethod?: string
+        subNavigation?: any
+        meta?: any
+        actions?: ActionTS[]
+        iconRight?: {
+            title: string
+            icon: string
+            tooltip?: string
+            class: string
+        }
+        container: {
+            tooltip: any
+            icon: Icon
+            label: string
+            href: routeType
+        }
+    },
+    dataToSubmit?: any
+    dataToSubmitIsDirty?: any
+}>()
+
+
+if (props.dataToSubmit && props.data.actionActualMethod) {
+    props.dataToSubmit['_method'] = props.data.actionActualMethod
+}
+
+const originUrl = location.origin
 </script>
 
 
 <template>
-    <div class="mx-4 my-4 md:my-2 grid grid-flow-col justify-between items-center">
+    <div class="mx-4 py-4 md:pb-2 md:pt-3 lg:py-2 grid grid-flow-col justify-between items-center">
         <div>
-            <h2 class="font-bold text-gray-900 text-2xl tracking-tight capitalize">
+            <!-- Sub Navigation -->
+            <SubNavigation v-if="data.subNavigation" :dataNavigation="data.subNavigation" />
 
-                <span v-if="data.container" class="text-indigo-500 font-medium mr-2">
-                    <FontAwesomeIcon v-if="data.container.icon" :title="capitalize(data.container.tooltip)" aria-hidden="true"
-                                     :icon="data.container.icon" size="xs" />
-                    {{ data.container.label }}
-                </span>
+            <!-- Section: Main Title -->
+            <div class="flex leading-none py-1 items-center gap-x-2 font-bold text-gray-700 text-2xl tracking-tight ">
+                <div v-if="data.container" class="text-slate-500 text-lg">
+                    <Link v-if="data.container.href"
+                        :href="route(
+                            data.container.href['name'],
+                            data.container.href['parameters']
+                    )">
+                        <Container :data="data.container"/>
+                    </Link>
+                    <div v-else class="flex items-center gap-x-1">
+                        <Container :data="data.container" />
+                    </div>
+                </div>
+                <div v-if="data.icon" class="inline text-gray-400">
+                    <FontAwesomeIcon :title="capitalize(data.icon.tooltip ?? '')" aria-hidden="true"
+                        :icon="data.icon.icon" size="sm" class=""/>
+                    <!-- <FontAwesomeIcon v-if="data.iconBis" :title="capitalize(data.iconBis.tooltip ?? '')" aria-hidden="true"
+                        :icon="data.iconBis.icon" size="sm" class="" :class="data.iconBis.class"/> -->
+                </div>
+                <h2 :class="!data.noCapitalise? 'capitalize' : ''">{{ data.title }}</h2>
+                <FontAwesomeIcon v-if="data.iconRight" :title="capitalize(data.iconRight.tooltip ?? '')" aria-hidden="true"
+                    :icon="data.iconRight.icon" class="h-4" :class="data.iconRight.class"/>
+            </div>
 
-                <FontAwesomeIcon v-if="data.icon" :title="capitalize(data.icon.title)" aria-hidden="true"
-                                 :icon="data.icon.icon" size="xs" class="pr-2" />
-                {{ data.title }}
-                <FontAwesomeIcon v-if="data.iconRight" :title="capitalize(data.iconRight.title)" aria-hidden="true"
-                                 :icon="data.iconRight.icon"  class="pl-1 h-4 mb-0.5" />
-            </h2>
-            <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-                <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-                    <div v-for="item in data.meta" :key="item.name"
-                         class="mt-2 flex items-center text-sm text-gray-500">
-                        <FontAwesomeIcon
-                            v-if="item['leftIcon']"
-                            :title="capitalize(item['leftIcon']['tooltip'])"
-                            aria-hidden="true"
-                            :icon="item['leftIcon']['icon']"
-                            size="lg"
-                            class="text-gray-400 pr-2" />
-                        <Link v-if="item.href" :href="route(item.href['name'],item.href['parameters'])">
-                            <span v-if="item.number">{{ locale.number(item.number) }}</span>
-                            <FontAwesomeIcon v-else icon="fal fa-empty-set" />
-                            {{ item.name }}
+            <!-- Section: mini Tabs -->
+            <div v-if="data.meta" class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+                <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6 text-gray-500 text-xs pt-2">
+                    <div v-for="item in data.meta" class="flex items-center">
+                        <FontAwesomeIcon v-if="item.leftIcon"
+                            :title="capitalize(item.leftIcon.tooltip)"
+                            aria-hidden="true" :icon="item.leftIcon.icon"  class="text-gray-400 pr-2"/>
+                        <Link v-if="item.href" :href="`${route(item.href.name, item.href.parameters)}`"
+                            :class="[
+                                $page.url.startsWith((route(item.href.name, item.href.parameters)).replace(new RegExp(originUrl, 'g'), '')) ? 'text-org-600 font-medium' : 'text-org-300 hover:text-org-500'
+                            ]"
+                        >
+                            <MetaLabel :item=item />
                         </Link>
-                        <template v-else-if="item['emptyWithCreateAction']">
-                            <FontAwesomeIcon icon="fal fa-empty-set" class="mr-2" />
-                            <Button type="submit" size="xs" action="create">
-                                {{ item["emptyWithCreateAction"]["label"] }}
-                            </Button>
-                        </template>
-                        <span v-else><span v-if="item.number">{{ locale.number(item.number) }}</span> {{ item.name }} </span>
+                        <span v-else>
+                            <MetaLabel :item=item />
+                        </span>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="flex items-center gap-2">
-            <span v-for="action in data.actions">
-
-                <!-- Button -->
-                 <Link v-if="action.type === 'button'" :href="route(action['route']['name'], action['route']['parameters'])">
-                    <Button
-                        :disabled="action.disabled"
-                        size="xs"
-                        :style="action.style"
-                        class="capitalize inline-flex items-center rounded-md border text-sm font-medium shadow-sm gap-x-2"
-                    >
-                        <FontAwesomeIcon v-if="getActionIcon(action)" :icon="getActionIcon(action)" class="" aria-hidden="true" />
-                        {{ getActionLabel(action) }}
-                     </Button>
-                </Link>
-
-                <!-- Button Group () -->
-                <!--suppress HtmlUnknownTag -->
-                <div v-if="action.type === 'buttonGroup'" class="first:rounded-l-md overflow-hidden last:rounded-r-md">
-                    <Link v-for="button in action.buttons" :href="route(button['route']['name'], button['route']['parameters'])" class="">
-                        <Button
-                            size="xs"
-                            :style="button.style"
-                            class="capitalize inline-flex items-center rounded-none border text-sm font-medium shadow-sm "
-                        >
-                            <div class="">
-                                <FontAwesomeIcon v-if="getActionIcon(button)" :icon="getActionIcon(button)" class="" aria-hidden="true" />
-                                <span v-if="button.label" class="ml-2">{{ getActionLabel(button) }}</span>
-                            </div>
-                         </Button>
-                    </Link>
+        <!-- Section: Button and/or ButtonGroup -->
+        <slot name="button" :dataPageHead="{...props }">
+            <div class="flex items-center gap-2">
+                <div v-for="action in data.actions">
+                    <Action v-if="action" :action="action" :dataToSubmit="dataToSubmit"/>
                 </div>
-
-            </span>
-
-            <!-- <span v-if="data['delete']">
-                <Link as="button" :href="route(data['delete']['route']['name'],data['delete']['route']['parameters'])">
-                    <FontAwesomeIcon class="text-red-500 hover:text-red-700 mr-3" icon="far fa-trash-alt" />
-                </Link>
-            </span>
-
-            <span v-if="data['edit']">
-                <Link :href="route(data['edit']['route']['name'],data['edit']['route']['parameters'])">
-                    <Button
-                        size="xs"
-                        type="button"
-                        class="inline-flex items-center rounded-md border border-gray-300 bg-white text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        <FontAwesomeIcon icon="far fa-pencil" class="-ml-1 mr-2 text-gray-500" aria-hidden="true" />
-                        {{ trans("Edit") }}
-                    </Button>
-                </Link>
-            </span> -->
-
-            <!-- If the button is more than 1 in one component -->
-            <!-- <div v-if="data['create'] && data['create']['withMulti']" class="flex">
-                <Link :href="route(data['create']['withMulti']['route']['name'],data['create']['withMulti']['route']['parameters'])">
-                    <Button type="secondary" class="capitalize rounded-r-none">
-                        <FontAwesomeIcon icon="far fa-border-all" class="text-xl" />
-                    </Button>
-                </Link>
-                <Link :href="route(data['create']['route']['name'], data['create']['route']['parameters'])">
-                    <Button button="xs" type="secondary" action="create" class="capitalize rounded-l-none text-sm">
-                        {{ data["create"]["label"] }}
-                    </Button>
-                </Link>
+                <slot name="other" :dataPageHead="{...props }"/>
             </div>
-
-
-            <span v-if="data['create'] && !data['create']['withMulti']">
-                <Link :href="route(data['create']['route']['name'], data['create']['route']['parameters'])">
-                   <Button size="xs" type="secondary" action="create" class="capitalize">
-                      {{ data["create"]["label"] }}
-                   </Button>
-                </Link>
-            </span>
-
-            <span v-if="data['create_direct']">
-                <Link as="button" method="post" :href="route(data['create_direct']['route']['name'],data['create_direct']['route']['parameters'])">
-                    <Button type="secondary" action="create" class="capitalize">
-                        {{ data["create_direct"]["label"] }}
-                    </Button>
-                </Link>
-            </span>
-
-            <span v-if="data['cancelCreate']">
-                <Link :href="route(data['cancelCreate']['route']['name'],data['cancelCreate']['route']['parameters'])">
-                    <Button type="danger" action="cancel" class="capitalize">
-                        {{ trans("Cancel") }}
-                    </Button>
-                </Link>
-            </span>
-
-            <span v-if="data['exitEdit']">
-                <Link :href="route(data['exitEdit']['route']['name'],data['exitEdit']['route']['parameters'])">
-                    <Button type="button"
-                            class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        <FontAwesomeIcon icon="far fa-arrow-left" class="-ml-1 mr-2 h-5 w-5 text-gray-500" aria-hidden="true" />
-                        <span v-if="data['exitEdit']['label']">{{ trans(data["exitEdit"]["label"]) }}</span>
-                        <span v-if="!data['exitEdit']['label']"> {{ trans("Exit edit") }}</span>
-                    </Button>
-                </Link>
-            </span>
-
-            <span v-if="data['clearMulti']">
-                <Link :href="route(data['clearMulti']['route']['name'], data['clearMulti']['route']['parameters'])">
-                    <Button type="button"
-                            class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        <FontAwesomeIcon icon="far fa-trash-alt" class="h-5 w-5 text-gray-500" aria-hidden="true" />
-                    </Button>
-                </Link>
-            </span> -->
-
-            <!-- Dropdown -->
-            <!-- <Menu as="div" class="relative ml-3 sm:hidden">
-                <MenuButton
-                    class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    More
-                    <FontAwesomeIcon aria-hidden="true" class="-mr-1 ml-2 h-5 w-5 text-gray-500" icon="fa-regular fa-chevron-down"/>
-
-                </MenuButton>
-
-                <transition enter-active-class="transition ease-out duration-200" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
-                            leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                    <MenuItems class="absolute left-0 z-10 mt-2 -mr-1 w-48 origin-top-left rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <MenuItem v-slot="{ active }">
-                            <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Edit</a>
-                        </MenuItem>
-                        <MenuItem v-slot="{ active }">
-                            <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">View</a>
-                        </MenuItem>
-                    </MenuItems>
-                </transition>
-            </Menu> -->
-
-        </div>
+        </slot>
     </div>
-    <hr />
+    <hr class="border-gray-300"/>
 </template>
-
-<style>
-</style>
-
-
