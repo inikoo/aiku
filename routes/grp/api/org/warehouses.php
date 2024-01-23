6ecfb09a833b34729e18e1af465a316b2fe59c53
artha <artha@aw-advantage.com>
2024-01-23 14:12:29 +0800

    fix: show location api

--- routes/grp/api/org/warehouses.php
+++ routes/grp/api/org/warehouses.php
@@ -6,6 +6,7 @@
  */
 
 use App\Actions\Inventory\Location\UI\IndexLocations;
+use App\Actions\Inventory\Location\UI\ShowLocation;
 use App\Actions\Inventory\Warehouse\UI\IndexWarehouses;
 use App\Actions\Inventory\WarehouseArea\UI\IndexWarehouseAreas;
 
@@ -13,3 +14,5 @@ Route::get('/', [IndexWarehouses::class, 'inOrganisation'])->name('index');
 Route::get('areas', [IndexWarehouseAreas::class, 'inOrganisation'])->name('areas.index');
 Route::get('/{warehouse:id}/locations', [IndexLocations::class, 'inWarehouse'])->name('locations.index');
 Route::get('areas/{warehouseArea:id}/locations', [IndexLocations::class, 'inWarehouseArea'])->name('areas.locations.index');
+
+Route::get('locations/{location:id}', ShowLocation::class)->name('locations.show');
