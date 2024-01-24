27d865175d3119ff935b1e01203de0ff2a337d25
artha <artha@aw-advantage.com>
2024-01-24 17:01:49 +0800

    PKA-1332 Pallet migrations and model

---
+++ app/Actions/Fulfilment/Pallet/UI/IndexPallets.php
@@ -0,0 +1,149 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 25 May 2023 21:14:38 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Fulfilment\Pallet\UI;
+
+use App\Actions\InertiaAction;
+use App\Actions\UI\Fulfilment\FulfilmentDashboard;
+use App\Enums\UI\TabsAbbreviationEnum;
+use App\Http\Resources\Fulfilment\PalletResource;
+use App\Http\Resources\Fulfilment\StoredItemResource;
+use App\Models\CRM\Customer;
+use App\Models\Fulfilment\StoredItem;
+use App\Models\SysAdmin\Organisation;
+use Closure;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Inertia\Inertia;
+use Inertia\Response;
+use Lorisleiva\Actions\ActionRequest;
+use App\InertiaTable\InertiaTable;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class IndexPallets extends InertiaAction
+{
+    /** @noinspection PhpUndefinedMethodInspection */
+    public function handle(Organisation|Customer $parent, $prefix = null): LengthAwarePaginator
+    {
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                $query->where('slug', 'ILIKE', "%$value%");
+            });
+        });
+
+        if ($prefix) {
+            InertiaTable::updateQueryBuilderParameters($prefix);
+        }
+
+        return QueryBuilder::for(StoredItem::class)
+            ->defaultSort('slug')
+            ->with('customer')
+            ->when($parent, function ($query) use ($parent) {
+                if(class_basename($parent) == "Customer") {
+                    $query->where('customer_id', $parent->id);
+                }
+            })
+            ->allowedSorts(['slug', 'state'])
+            ->allowedFilters([$globalSearch, 'slug', 'state'])
+            ->withPaginator($prefix)
+            ->withQueryString();
+    }
+
+    public function tableStructure($parent): Closure
+    {
+        return function (InertiaTable $table) use ($parent) {
+            $table
+                ->name(TabsAbbreviationEnum::STORED_ITEMS->value)
+                ->pageName(TabsAbbreviationEnum::STORED_ITEMS->value.'Page')
+
+                ->withGlobalSearch()
+                ->withEmptyState(
+                    [
+                        'title' => __("No stored items found"),
+                        'count' => $parent->count()
+                    ]
+                )
+                ->column(key: 'reference', label: __('reference'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'customer_name', label: __('Customer Name'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'location', label: __('Location'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'state', label: __('State'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'status', label: __('Status'), canBeHidden: false, sortable: true, searchable: true)
+                ->column(key: 'notes', label: __('Notes'), canBeHidden: false, sortable: true, searchable: true)
+                ->defaultSort('slug');
+        };
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        $this->canEdit = $request->user()->hasPermissionTo('fulfilment.edit');
+
+        return
+            (
+                $request->user()->tokenCan('root') or
+                $request->user()->hasPermissionTo('hr.view')
+            );
+    }
+
+
+    public function jsonResponse(LengthAwarePaginator $pallets): AnonymousResourceCollection
+    {
+        return PalletResource::collection($pallets);
+    }
+
+
+    public function htmlResponse(LengthAwarePaginator $pallets): Response
+    {
+        return Inertia::render(
+            'Fulfilment/StoredItems',
+            [
+                'breadcrumbs' => $this->getBreadcrumbs(),
+                'title'       => __('stored items'),
+                'pageHead'    => [
+                    'title'   => __('stored items'),
+                    'actions' => [
+                        'buttons' => [
+                            'route' => [
+                                'name'       => 'grp.org.hr.employees.create',
+                                'parameters' => array_values($request->route()->originalParameters())
+                            ],
+                            'label' => __('stored items')
+                        ]
+                    ],
+                ],
+                'data' => StoredItemResource::collection($pallets),
+            ]
+        )->table($this->tableStructure($pallets));
+    }
+
+    public function asController(ActionRequest $request): LengthAwarePaginator
+    {
+        $this->initialisation($request);
+
+        return $this->handle(app('currentTenant'));
+    }
+
+    public function getBreadcrumbs(): array
+    {
+        return array_merge(
+            (new FulfilmentDashboard())->getBreadcrumbs(),
+            [
+                [
+                    'type'   => 'simple',
+                    'simple' => [
+                        'route' => [
+                            'name' => 'grp.fulfilment.stored-items.index'
+                        ],
+                        'label' => __('stored items'),
+                        'icon'  => 'fal fa-bars',
+                    ],
+
+                ]
+            ]
+        );
+    }
+}
--- routes/grp/web/org/fulfilments.php
+++ routes/grp/web/org/fulfilments.php
@@ -29,7 +29,7 @@ Route::prefix('{fulfilment}')
     ->group(function () {
         Route::get('', ShowFulfilment::class)->name('show');
 
-
+        Route::get('/pallets', IndexPallets::class)->name('stored-items.index');
     });
 
 /*
