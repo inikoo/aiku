f1400665f8aaba1620ff37a126f8a29dd315b10b
artha <artha@aw-advantage.com>
2024-01-24 15:21:00 +0800

    PKA-1332 Pallet migrations and model

--- app/Models/Fulfilment/Pallet.php
+++ app/Models/Fulfilment/Pallet.php
@@ -1,6 +1,11 @@
 <?php
+/*
+ * Author: Artha <artha@aw-advantage.com>
+ * Created: Wed, 24 Jan 2024 15:20:49 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2024, Raul A Perusquia Flores
+ */
 
-namespace App\Models;
+namespace App\Models\Fulfilment;
 
 use App\Stubs\Migrations\HasSoftDeletes;
 use Illuminate\Database\Eloquent\Model;
