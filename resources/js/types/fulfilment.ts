d4a3b9949666cefc41871c51e645343af230cf19
Raul Perusquia <raul@inikoo.com>
2024-01-24 14:57:06 +0800

    feat: fulfilment basic routes ⛽ 2

---
+++ resources/js/types/fulfilment.ts
@@ -0,0 +1,18 @@
+
+
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 24 Jan 2024 15:08:10 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2024, Raul A Perusquia Flores
+ */
+
+export interface Fulfilment {
+    id:string,
+    slug: string,
+    state: string,
+    code: string,
+    name: string,
+    created_at: string,
+    updated_at: string,
+
+}
