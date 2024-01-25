1e412525ad62606d3527fbdf1df4db1e2757eddb
artha <artha@aw-advantage.com>
2024-01-25 16:44:18 +0800

    PKA-1335 Customer fulfilment

--- app/Actions/Fulfilment/FulfilmentCustomer/Hydrators/HydrateFulfilmentCustomer.php
+++ app/Actions/Fulfilment/FulfilmentCustomer/Hydrators/HydrateFulfilmentCustomer.php
@@ -1,11 +1,11 @@
 <?php
 /*
- * Author: Raul Perusquia <raul@inikoo.com>
- * Created: Sat, 25 Mar 2023 01:37:38 Malaysia Time, Kuala Lumpur, Malaysia
- * Copyright (c) 2023, Raul A Perusquia Flores
+ * Author: Artha <artha@aw-advantage.com>
+ * Created: Thu, 25 Jan 2024 16:42:23 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2024, Raul A Perusquia Flores
  */
 
-namespace App\Actions\Accounting\Invoice\Hydrators;
+namespace App\Actions\Fulfilment\FulfilmentCustomer\Hydrators;
 
 use App\Actions\Traits\WithActionUpdate;
 use App\Models\Fulfilment\Pallet;
