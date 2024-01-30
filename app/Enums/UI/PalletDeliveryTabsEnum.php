586f5f2d5663101ee9d8eb8b6a897c83b2c00026
artha <artha@aw-advantage.com>
2024-01-30 14:20:44 +0800

    PKA-1345 Fulfilment Customer

---
+++ app/Enums/UI/PalletDeliveryTabsEnum.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Author: Jonathan Lopez Sanchez <jonathan@ancientwisdom.biz>
+ * Created: Mon, 20 Mar 2023 14:46:07 Central European Standard Time, Malaga, Spain
+ * Copyright (c) 2023, Inikoo LTD
+ */
+
+namespace App\Enums\UI;
+
+use App\Enums\EnumHelperTrait;
+use App\Enums\HasTabs;
+
+enum PalletDeliveryTabsEnum: string
+{
+    use EnumHelperTrait;
+    use HasTabs;
+
+    case SHOWCASE = 'showcase';
+    case HISTORY = 'history';
+
+    public function blueprint(): array
+    {
+        return match ($this) {
+            PalletDeliveryTabsEnum::HISTORY => [
+                'title' => __('history'),
+                'icon'  => 'fal fa-clock',
+                'type'  => 'icon',
+                'align' => 'right',
+            ],
+            PalletDeliveryTabsEnum::SHOWCASE => [
+                'title' => __('pallet delivery'),
+                'icon'  => 'fas fa-info-circle',
+            ],
+        };
+    }
+}
