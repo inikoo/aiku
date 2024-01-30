102fe6da790273ce85e4ee395fbf35b035793446
YudhistaA <aryarajasa0@gmail.com>
2024-01-30 11:17:16 +0800

    popover components

---
+++ resources/js/Components/Popover.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
+
+const props = defineProps({
+  width: {
+    type: String,
+    default: 'w-4/5',
+  },
+  position: {
+    type: String,
+    default: 'right-0',
+  },
+});
+</script>
+
+<template>
+  <Popover :popover-placement="'bottom-start'" class="focus-visible:ring-0">
+    <PopoverButton tabindex="-1" v-slot="{ open, close }"  class="focus-visible:ring-0">
+      <slot name="button" :open="open" :close="close"></slot>
+    </PopoverButton>
+
+    <transition
+      enter-active-class="transition duration-200 ease-out"
+      enter-from-class="opacity-0 scale-95"
+      enter-to-class="opacity-100 scale-100"
+      leave-active-class="transition duration-150 ease-in"
+      leave-from-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-95"
+    >
+      <PopoverPanel v-slot="{ open, close }" ref="panelPopover"
+        :class="`absolute z-50 mt-3 transform py-3 px-4 bg-white rounded-md shadow-md w-fit ${position}`" >
+        <!-- Pass closePopover method to content slot -->
+        <slot name="content" :open="open" :close="close"></slot>
+      </PopoverPanel>
+    </transition>
+  </Popover>
+</template>
+
+<style lang="scss">
+[data-headlessui-state] {
+  @apply focus-visible:ring-transparent;
+}
+</style>
