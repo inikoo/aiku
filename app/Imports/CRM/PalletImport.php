83d88eb031b1cc7737dbdb88f001189a0778c11c
Artha <manager.arthachan@gmail.com>
2024-02-06 09:51:31 +0800

    fix: import pallets

---
+++ app/Imports/CRM/PalletImport.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 28 Sep 2023 01:20:30 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Imports\CRM;
+
+use App\Actions\Fulfilment\Pallet\StorePalletFromDelivery;
+use App\Imports\WithImport;
+use App\Models\Fulfilment\PalletDelivery;
+use App\Models\Helpers\Upload;
+use Exception;
+use Illuminate\Support\Arr;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
+
+class PalletImport implements ToCollection, WithHeadingRow, SkipsOnFailure, WithValidation
+{
+    use WithImport;
+
+    protected PalletDelivery $scope;
+    public function __construct(PalletDelivery $palletDelivery, Upload $upload)
+    {
+        $this->upload = $upload;
+        $this->scope  = $palletDelivery;
+    }
+
+    public function storeModel($row, $uploadRecord): void
+    {
+        $fields =
+            array_merge(
+                array_keys(
+                    Arr::except(
+                        $this->rules(),
+                        ['reference', 'customer_reference']
+                    )
+                )
+            );
+
+        $modelData = $row->only($fields)->all();
+
+        data_set($modelData, 'reference', $this->scope->reference);
+        data_set($modelData, 'data.bulk_import', [
+            'id'   => $this->upload->id,
+            'type' => 'Upload',
+        ]);
+
+        try {
+            StorePalletFromDelivery::run(
+                $this->scope,
+                $modelData
+            );
+
+            $this->setRecordAsCompleted($uploadRecord);
+        } catch (Exception $e) {
+            $this->setRecordAsFailed($uploadRecord, [$e->getMessage()]);
+        } catch (\Throwable $e) {
+            $this->setRecordAsFailed($uploadRecord, [$e->getMessage()]);
+        }
+    }
+
+    public function rules(): array
+    {
+        return [
+            'reference'          => ['required'],
+            'customer_reference' => ['nullable'],
+            'notes'              => ['nullable']
+        ];
+    }
+}
