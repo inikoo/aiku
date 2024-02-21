a237461ee809dffc3d8db9421c32a108413b914a
Raul Perusquia <raul@inikoo.com>
2024-02-21 11:06:42 -0600

    refactor: layout files structure 🖍️ 3

---
+++ app/Actions/UI/Retina/Layout/GetRetinaFulfilmentNavigation.php
@@ -0,0 +1,162 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 21 Feb 2024 22:34:23 Central Standard Time, Mexico City, Mexico
+ * Copyright (c) 2024, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\UI\Retina\Layout;
+
+use App\Models\CRM\WebUser;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class GetRetinaFulfilmentNavigation
+{
+    use AsAction;
+
+    public function handle(WebUser $webUser): array
+    {
+        $groupNavigation = [];
+
+        $groupNavigation['dashboard'] = [
+            'label'   => __('Dashboard'),
+            'icon'    => ['fal', 'fa-tachometer-alt'],
+            'route'   => [
+                'name' => 'retina.dashboard.show'
+            ],
+            'topMenu' => [
+
+            ]
+        ];
+
+        $groupNavigation['storage'] = [
+            'label'   => __('Storage'),
+            'icon'    => ['fal', 'fa-pallet'],
+            'route'   => [
+                'name' => 'retina.sysadmin.dashboard'
+            ],
+            'topMenu' => [
+                'subSections' => [
+                    [
+                        'label' => __('users'),
+                        'icon'  => ['fal', 'fa-terminal'],
+                        'root'  => 'retina.sysadmin.users.',
+                        'route' => [
+                            'name' => 'retina.sysadmin.web-users.index',
+
+                        ]
+                    ],
+
+                    [
+                        'label' => __('system settings'),
+                        'icon'  => ['fal', 'fa-cog'],
+                        'root'  => 'retina.sysadmin.settings.',
+                        'route' => [
+                            'name' => 'retina.sysadmin.settings.edit',
+
+                        ]
+                    ],
+                ]
+            ]
+        ];
+        $groupNavigation['dropshipping'] = [
+            'label'   => __('Dropshipping'),
+            'icon'    => ['fal', 'fa-hand-holding-box'],
+            'route'   => [
+                'name' => 'retina.sysadmin.dashboard'
+            ],
+            'topMenu' => [
+                'subSections' => [
+                    [
+                        'label' => __('users'),
+                        'icon'  => ['fal', 'fa-terminal'],
+                        'root'  => 'retina.sysadmin.users.',
+                        'route' => [
+                            'name' => 'retina.sysadmin.web-users.index',
+
+                        ]
+                    ],
+
+                    [
+                        'label' => __('system settings'),
+                        'icon'  => ['fal', 'fa-cog'],
+                        'root'  => 'retina.sysadmin.settings.',
+                        'route' => [
+                            'name' => 'retina.sysadmin.settings.edit',
+
+                        ]
+                    ],
+                ]
+            ]
+        ];
+
+        if ($webUser->is_root) {
+
+            $groupNavigation['billing'] = [
+                'label'   => __('billing'),
+                'icon'    => ['fal', 'fa-file-invoice-dollar'],
+                'route'   => [
+                    'name' => 'retina.sysadmin.dashboard'
+                ],
+                'topMenu' => [
+                    'subSections' => [
+                        [
+                            'label' => __('users'),
+                            'icon'  => ['fal', 'fa-terminal'],
+                            'root'  => 'retina.sysadmin.users.',
+                            'route' => [
+                                'name' => 'retina.sysadmin.web-users.index',
+
+                            ]
+                        ],
+
+                        [
+                            'label' => __('system settings'),
+                            'icon'  => ['fal', 'fa-cog'],
+                            'root'  => 'retina.sysadmin.settings.',
+                            'route' => [
+                                'name' => 'retina.sysadmin.settings.edit',
+
+                            ]
+                        ],
+                    ]
+                ]
+            ];
+
+            $groupNavigation['sysadmin'] = [
+                'label'   => __('manage account'),
+                'icon'    => ['fal', 'fa-users-cog'],
+                'route'   => [
+                    'name' => 'retina.sysadmin.dashboard'
+                ],
+                'topMenu' => [
+                    'subSections' => [
+                        [
+                            'label' => __('users'),
+                            'icon'  => ['fal', 'fa-terminal'],
+                            'root'  => 'retina.sysadmin.users.',
+                            'route' => [
+                                'name' => 'retina.sysadmin.web-users.index',
+
+                            ]
+                        ],
+
+                        [
+                            'label' => __('system settings'),
+                            'icon'  => ['fal', 'fa-cog'],
+                            'root'  => 'retina.sysadmin.settings.',
+                            'route' => [
+                                'name' => 'retina.sysadmin.settings.edit',
+
+                            ]
+                        ],
+                    ]
+                ]
+            ];
+
+        }
+
+
+        return $groupNavigation;
+    }
+}
--- resources/js/Layouts/Retina/RetinaNavigationSimple.vue
+++ resources/js/Layouts/Retina/RetinaNavigationSimple.vue
@@ -1,40 +1,35 @@
-<script setup lang='ts'>
-import { useLayoutStore } from '@/Stores/retinaLayout'
-import { Navigation } from '@/types/Navigation'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faRoute } from '@fal'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { Link } from '@inertiajs/vue3'
-import { capitalize } from "@/Composables/capitalize"
-import { isNavigationActive } from '@/Composables/useUrl'
-import { onMounted, ref, onUnmounted } from 'vue'
-import RetinaTopBarSubsections from '@/Layouts/Retina/RetinaTopBarSubsections.vue'
-import {faHandHoldingBox} from '@fal'
-library.add(faRoute, faHandHoldingBox)
+<script setup lang="ts">
+import { useLayoutStore } from "@/Stores/retinaLayout";
+import { Navigation } from "@/types/Navigation";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faRoute } from "@fal";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { Link } from "@inertiajs/vue3";
+import { capitalize } from "@/Composables/capitalize";
+import { isNavigationActive } from "@/Composables/useUrl";
+import { onMounted, ref, onUnmounted } from "vue";
+import RetinaTopBarSubsections from "@/Layouts/Retina/RetinaTopBarSubsections.vue";
+import { faTachometerAlt, faFileInvoiceDollar, faHandHoldingBox, faPallet } from "@fal";
+
+library.add(faTachometerAlt, faFileInvoiceDollar, faRoute, faPallet, faHandHoldingBox);
 
 const props = defineProps<{
     navKey: string | number  // shops_navigation | warehouses_navigation
     nav: Navigation
-}>()
+}>();
 
-// From 'shops_index' to 'shops'
-// const separateUnderscore = (str: string | number) => {
-//     const realString = str.toString()
 
-//     return realString.split('_')
-// }
-
-const layout = useLayoutStore()
-const isTopMenuActive = ref(false)
+const layout = useLayoutStore();
+const isTopMenuActive = ref(false);
 
 onMounted(() => {
-    isTopMenuActive.value = true
+    isTopMenuActive.value = true;
     // console.log('NavigationSimple.vue', props.navKey, props.nav)
-})
+});
 
 onUnmounted(() => {
-    isTopMenuActive.value = false
-})
+    isTopMenuActive.value = false;
+});
 
 // Check if this route has nav.root
 // const isRouteActive = () => {
@@ -48,24 +43,24 @@ onUnmounted(() => {
     <!-- {{ layout.currentRoute }} -->
     <!-- <div class="text-xxs">{{ layout.currentRoute }} <br> {{ nav.route.name }}</div> -->
     <Link :href="nav.route?.name ? route(nav.route.name, nav.route.parameters) : '#'"
-        class="group flex items-center px-2 text-sm gap-x-2" :class="[
+          class="group flex items-center px-2 text-sm gap-x-2" :class="[
             isNavigationActive(props.nav.root)
                 ? 'navigationActive'
                 : 'navigation',
             layout.leftSidebar.show ? '' : '',
         ]"
-        :style="[isNavigationActive(props.nav.root) ? {
+          :style="[isNavigationActive(props.nav.root) ? {
             'background-color': layout.app?.theme[1],
             'color': layout.app?.theme[2]
         } : {} ]"
 
-        :aria-current="navKey === layout.currentModule ? 'page' : undefined"
-        v-tooltip="layout.leftSidebar.show ? false : capitalize(nav.label)"
+          :aria-current="navKey === layout.currentModule ? 'page' : undefined"
+          v-tooltip="layout.leftSidebar.show ? false : capitalize(nav.label)"
     >
         <FontAwesomeIcon v-if="nav.icon" aria-hidden="true" class="flex-shrink-0 h-4 w-4" fixed-width :icon="nav.icon" />
         <Transition name="slide-to-left">
             <span v-if="layout.leftSidebar.show" class="capitalize leading-none whitespace-nowrap block md:block"
-                :class="[layout.leftSidebar.show ? '' : 'block md:hidden']">
+                  :class="[layout.leftSidebar.show ? '' : 'block md:hidden']">
                 {{ nav.label }}
             </span>
             <span v-else class="capitalize leading-none whitespace-nowrap block md:hidden">
