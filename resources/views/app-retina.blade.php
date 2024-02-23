8f2d201151ed90adbaf5fe060896e40bc9253b0d
aqordeon <aqordivika@yahoo.co.id>
2024-02-23 16:46:34 +0800

    fix: Retina: favicon

--- resources/views/app-retina.blade.php
+++ resources/views/app-retina.blade.php
@@ -7,10 +7,10 @@
         @include('new-relic', ['appID' => Config::get('new-relic.application_id.retina')])
         @endenv
         <title inertia>{{ config('app.name', 'Retina') }}</title>
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ url('favicons/aiku-favicon-16x16.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ url('favicons/aiku-favicon-32x32.png') }}">
-        <link rel="icon" type="image/png" sizes="48x48" href="{{ url('favicons/aiku-favicon-48x48.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ url('favicons/aiku-favicon-180x180.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ url('favicons/favicon-16x16.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ url('favicons/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="48x48" href="{{ url('favicons/favicon-48x48.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ url('favicons/favicon-180x180.png') }}">
 
 
         <!-- Scripts -->
