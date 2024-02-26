843b812b4e4a24caa865a6cb69d3aada4ed3a524
Raul Perusquia <raul@inikoo.com>
2024-02-25 23:21:13 -0600

    refactor: palled delivery state done -> booken in

--- app/Actions/Fulfilment/PalletDelivery/BookInPalletDelivery.php
+++ app/Actions/Fulfilment/PalletDelivery/BookInPalletDelivery.php
@@ -11,9 +11,7 @@ use App\Actions\OrgAction;
 use App\Actions\Traits\WithActionUpdate;
 use App\Enums\Fulfilment\Pallet\PalletStateEnum;
 use App\Enums\Fulfilment\PalletDelivery\PalletDeliveryStateEnum;
-use App\Http\Resources\Fulfilment\PalletDeliveryResource;
 use App\Models\Fulfilment\PalletDelivery;
-use Illuminate\Http\Resources\Json\JsonResource;
 use Lorisleiva\Actions\ActionRequest;
 
 class BookInPalletDelivery extends OrgAction
@@ -21,7 +19,7 @@ class BookInPalletDelivery extends OrgAction
     use WithActionUpdate;
 
 
-    public function handle(PalletDelivery $palletDelivery, array $modelData): PalletDelivery
+    public function handle(PalletDelivery $palletDelivery): PalletDelivery
     {
         $modelData['booked_in_at']                                 = now();
         $modelData['state']                                        = PalletDeliveryStateEnum::BOOKED_IN;
@@ -40,15 +38,5 @@ class BookInPalletDelivery extends OrgAction
         return $request->user()->hasPermissionTo("fulfilments.{$this->fulfilment->id}.edit");
     }
 
-    public function jsonResponse(PalletDelivery $palletDelivery): JsonResource
-    {
-        return new PalletDeliveryResource($palletDelivery);
-    }
 
-    public function asController(PalletDelivery $palletDelivery, ActionRequest $request): PalletDelivery
-    {
-        $this->initialisationFromFulfilment($palletDelivery->fulfilment, $request);
-
-        return $this->handle($palletDelivery, $this->validatedData);
-    }
 }
