db12a74d8df94a7492de75720d000e3fd874dee0
aqordeon <aqordivika@yahoo.co.id>
2024-02-26 14:22:17 +0800

    feat: Page: Pallet Delivery

--- app/Actions/Retina/Storage/PalletDelivery/UI/ShowPalletDelivery.php
+++ app/Actions/Retina/Storage/PalletDelivery/UI/ShowPalletDelivery.php
@@ -44,7 +44,7 @@ class ShowPalletDelivery extends RetinaAction
 
 
         return Inertia::render(
-            'Org/Fulfilment/PalletDelivery',
+            'Storage/PalletDelivery',
             [
                 'title'       => __('pallet delivery'),
                 'breadcrumbs' => $this->getBreadcrumbs(
