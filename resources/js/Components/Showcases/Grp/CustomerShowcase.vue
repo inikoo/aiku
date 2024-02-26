2b2f47a110a17118a4c2168a1a140dba2116b33e
aqordeon <aqordivika@yahoo.co.id>
2024-02-26 16:01:28 +0800

    feat: Customer: Showcase page

--- resources/js/Components/Showcases/Grp/CustomerShowcase.vue
+++ resources/js/Components/Showcases/Grp/CustomerShowcase.vue
@@ -5,71 +5,104 @@
   -->
 
 <script setup lang="ts">
-import ShowcaseStats from '@/Components/ShowcaseStats.vue'
-import ShowcaseContactCard from "@/Components/ShowcaseContactCard.vue";
-import FieldForm from "@/Components/Forms/FieldForm.vue";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { useLayoutStore } from "@/Stores/layout"
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
+import { Line } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.js'
+
+ChartJS.register( CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend )
 
 const props = defineProps<{
-  data: {
-    blueprint:{
-        title:string
-        icon:Array<string>
-    },
-    current:string,
+    data: {
+        blueprint: {
+            title: string
+            icon: Array<string>
+        },
+        current: string,
 
-    contactCard: {
+        contactCard: {
+        },
+        stats: [{
+            label: string,
+            value: number,
+        }],
     },
-    stats: [{
-      label: string,
-      value: number,
-    }],
-  },
-
 }>()
 
+const customerStats = [
+    {
+        title: 'Pallets',
+        value: 152,
+        icon: 'fal fa-pallet'
+    },
+    {
+        title: 'Stored items',
+        value: 1956,
+        icon: 'fal fa-narwhal'
+    },
+    {
+        title: 'Deliveries',
+        value: 152,
+        icon: 'fal fa-truck-couch'
+    },
+    {
+        title: 'Return',
+        value: 10,
+        icon: 'fal fa-sign-out-alt '
+    },
+]
 
+const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'Bills',
+        data: [65, 78, 50, 71, 60, 85, 40],
+        fill: false,
+        borderColor: useLayoutStore().app.theme[2],
+        tension: 0.3
+    }]
+}
+
+const config = {
+    type: 'line',
+    data: data,
+};
 </script>
 
 <template >
+    <div class="px-4 py-5 md:px-6 lg:px-8 grid grid-cols-2 gap-x-3">
+        <div class="h-full w-full bg-slate-800 rounded text-white p-4">
+            <div class="text-gray-400">Bills (2024)</div>
+            <div class="text-4xl">$<span class="text-white font-bold">449</span></div>
+            <div class="">
+                <Line :data="data" :options="{ responsive: true, maintainAspectRatio: false }" />
+            </div>
+        </div>
 
-
-      <div class="rounded-lg bg-white shadow">
-          <div class="divide-y divide-gray-200 lg:grid grid-flow-col lg:grid-cols-12 lg:divide-y-0 lg:divide-x">
-
-              <!-- Left Tab: Navigation -->
-              <aside class="py-0 lg:col-span-3 lg:h-full">
-                  <nav role="navigation" class="space-y-1">
-                      <ul>
-                          <li v-for="(item, key) in data.blueprint" @click="data.current = key" :class="[
-                            key == data.current
-                                ? 'bg-indigo-50 border-indigo-500 text-indigo-700 hover:bg-indigo-50 hover:text-indigo-700'
-                                : 'border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900',
-                            'cursor-pointer group border-l-4 px-3 py-2 flex items-center text-sm font-medium',
-                        ]" :aria-current="key === data.current ? 'page' : undefined">
-                              <FontAwesomeIcon v-if="item.icon" aria-hidden="true" :class="[
-                                key === data.current
-                                    ? 'text-indigo-500 group-hover:text-indigo-500'
-                                    : 'text-gray-400 group-hover:text-gray-500',
-                                'flex-shrink-0 -ml-1 mr-3 h-6 w-6',
-                            ]" :icon="item.icon" />
-
-                              <span class="capitalize truncate">{{ item.title }}</span>
-                          </li>
-                      </ul>
-                  </nav>
-              </aside>
-
-              <!-- Content of forms -->
-              <div class="px-4 sm:px-6 md:px-4 col-span-9">
-                  <div class="divide-y divide-grey-200">
-
-
-                  </div>
-              </div>
-          </div>
-      </div>
-
-
+        <div class="grid grid-cols-2 gap-y-2 gap-x-2 text-gray-600">
+            <div v-for="stat in customerStats" class="border border-gray-50 rounded p-3"
+                :style="{
+                    border: `1px solid ${useLayoutStore().app.theme[4] + '22'}`
+                }"
+            >
+                <div class="flex justify-between mb-1">
+                    <div>
+                        <span class="block text-gray-400 font-medium mb-2">{{ stat.title }}</span>
+                        <div class="font-bold text-2xl">{{ stat.value }}</div>
+                    </div>
+                    <div class="h-10 aspect-square flex items-center justify-center rounded"
+                        :style="{
+                            backgroundColor: useLayoutStore().app?.theme[2] + '22',
+                            color: useLayoutStore().app.theme[2]
+                        }"
+                    >
+                        <FontAwesomeIcon :icon='stat.icon' class='' fixed-width aria-hidden='true' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
