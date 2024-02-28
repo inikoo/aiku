b387323fd9921b8a05d6ffadb5851b4fa5243723
YudhistaA <aryarajasa0@gmail.com>
2024-02-28 10:28:53 +0800

    Booked In

---
+++ resources/js/Components/ButtonEditTable.vue
@@ -0,0 +1,55 @@
+<!--
+  Author: Raul Perusquia <raul@inikoo.com>
+  Created: Mon, 20 Mar 2023 23:18:59 Malaysia Time, Kuala Lumpur, Malaysia
+  Copyright (c) 2023, Raul A Perusquia Flores
+-->
+
+<script setup lang="ts">
+import { defineProps, withDefaults, ref } from "vue"
+import { useLayoutStore } from "@/Stores/retinaLayout"
+import Button from "@/Components/Elements/Buttons/Button.vue"
+import { router } from "@inertiajs/vue3"
+
+const props = withDefaults(
+    defineProps<{
+        data: object
+        routeName: string
+        style?: string
+        size?: string
+        icon?: string | string[]
+        iconRight?: string | string[]
+        action?: string
+        label?: string
+        full?: boolean
+        capitalize?: boolean
+        tooltip?: string
+    }>(),
+    {
+        style: "primary",
+        size: "m",
+        capitalize: true,
+    }
+)
+
+const loadingState = ref(false)
+
+const handleClick = (action) => {
+    router[action.method](
+        action.route,
+        {},
+        {
+            onStart: () => { loadingState.value = true },
+            onFinish: () => { loadingState.value = false },
+        })
+}
+
+</script>
+
+<template>
+    <Button v-bind="props" :loading="loadingState" @click="
+        handleClick({
+            route: route(data[routeName].name, data[routeName].parameters),
+            method: 'patch',
+        })
+        " />
+</template>
