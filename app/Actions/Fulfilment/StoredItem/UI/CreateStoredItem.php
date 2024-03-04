ae33fab77fa339b15bcabd5ce128f73e7a540015
Artha <manager.arthachan@gmail.com>
2024-03-04 08:48:54 +0800

    feat: stored items

--- app/Actions/Fulfilment/StoredItem/UI/CreateStoredItem.php
+++ app/Actions/Fulfilment/StoredItem/UI/CreateStoredItem.php
@@ -7,16 +7,19 @@
 
 namespace App\Actions\Fulfilment\StoredItem\UI;
 
-use App\Actions\InertiaAction;
+use App\Actions\Fulfilment\Fulfilment\UI\ShowFulfilment;
+use App\Actions\OrgAction;
 use App\Enums\Fulfilment\StoredItem\StoredItemTypeEnum;
 use App\Http\Resources\Fulfilment\StoredItemResource;
+use App\Models\Fulfilment\Fulfilment;
+use App\Models\SysAdmin\Organisation;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Inertia\Inertia;
 use Inertia\Response;
 use Lorisleiva\Actions\ActionRequest;
 
-class CreateStoredItem extends InertiaAction
+class CreateStoredItem extends OrgAction
 {
     public function handle($prefix=null): Response
     {
@@ -25,12 +28,12 @@ class CreateStoredItem extends InertiaAction
 
     public function authorize(ActionRequest $request): bool
     {
-        $this->canEdit = $request->user()->hasPermissionTo('fulfilment.edit');
+        $this->canEdit = $request->user()->hasPermissionTo("fulfilments.{$this->fulfilment->id}.edit");
 
         return
             (
                 $request->user()->tokenCan('root') or
-                $request->user()->hasPermissionTo('hr.view')
+                $request->user()->hasPermissionTo("human-resources.{$this->organisation->id}.view")
             );
     }
 
@@ -82,16 +85,23 @@ class CreateStoredItem extends InertiaAction
                     ],
                     'route' => [
                         'name'      => 'grp.models.stored-items.store',
-                        'arguments' => array_values($request->route()->originalParameters())
+                        'arguments' => array_values(request()->route()->originalParameters())
                     ]
                 ],
             ]
         );
     }
 
-    public function asController(ActionRequest $request): Response
+    public function asController(Organisation $organisation, ActionRequest $request): Response
     {
-        $this->initialisation($request);
+        $this->initialisation($organisation, $request);
+
+        return $this->handle();
+    }
+
+    public function inFulfilment(Organisation $organisation, Fulfilment $fulfilment, ActionRequest $request): Response
+    {
+        $this->initialisationFromFulfilment($fulfilment, $request);
 
         return $this->handle();
     }
@@ -99,6 +109,7 @@ class CreateStoredItem extends InertiaAction
 
     public function getBreadcrumbs(): array
     {
+        return [];
         return array_merge(
             ShowFulfilment::make()->getBreadcrumbs(),
             [
