58ab4411e4472889b25b8064a122d51450b03b03
Artha <manager.arthachan@gmail.com>
2024-03-05 13:56:20 +0800

    fix: route isnt exists

--- app/Actions/Fulfilment/StoredItem/UI/IndexStoredItems.php
+++ app/Actions/Fulfilment/StoredItem/UI/IndexStoredItems.php
@@ -68,7 +68,6 @@ class IndexStoredItems extends OrgAction
                 )
                 ->column(key: 'reference', label: __('reference'), canBeHidden: false, sortable: true, searchable: true)
                 ->column(key: 'customer_name', label: __('Customer Name'), canBeHidden: false, sortable: true, searchable: true)
-                ->column(key: 'location', label: __('Location'), canBeHidden: false, sortable: true, searchable: true)
                 ->column(key: 'state', label: __('State'), canBeHidden: false, sortable: true, searchable: true)
                 ->column(key: 'status', label: __('Status'), canBeHidden: false, sortable: true, searchable: true)
                 ->column(key: 'notes', label: __('Notes'), canBeHidden: false, sortable: true, searchable: true)
