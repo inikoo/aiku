1cf8a20eaabdca7808dca6aa81cca41e736e825b
YudhistaA <aryarajasa0@gmail.com>
2024-03-12 09:23:09 +0800

    RTE

--- resources/js/Components/CMS/Footer/FooterTheme2.vue
+++ resources/js/Components/CMS/Footer/FooterTheme2.vue
@@ -16,12 +16,6 @@ import {
 } from "@fortawesome/free-solid-svg-icons"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import draggable from "vuedraggable"
-import Input from "../Fields/Input.vue"
-import TextArea from "../Fields/TextArea.vue"
-import HyperLink from "../Fields/Hyperlink.vue"
-import IconPicker from "../Fields/IconPicker/IconPicker.vue"
-import SocialMediaPicker from "../Fields/SocialMediaTools.vue"
-import TextEditor from "@/Components/Forms/Fields/TextEditor.vue"
 import BubleTextEditor from "@/Components/Forms/Fields/BubleTextEditor/BubleTextEditor.vue"
 
 library.add(
@@ -52,37 +46,23 @@ console.log("theme1", props)
 			<div class="xl:grid xl:gap-x-24 xl:px-6">
 				<draggable
 					:list="footerDataLayout.initialColumns"
-					group="navigation"
-					itemKey="id"
+					group="column"
+					itemKey="column_id"
 					:disabled="tool.hand !== 'grab'"
-					:class="[
-						'flex',
-						'gap-8',
-						'xl:col-span-2',
-						tool.hand !== 'grab' ? 'cursor-pointer' : 'cursor-grab',
-					]">
+					:class="['flex gap-8 xl:col-span-2', tool.hand !== 'grab' ? 'cursor-pointer' : 'cursor-grab']">
 					<template #item="{ element, index }">
 						<div :class="['space-y-3 w-1/4']">
-							<Input
-								:data="element"
-								:save="props.saveItemTitle"
-								keyValue="title"
-								cssClass="text-sm font-bold leading-6 text-gray-700 capitalize" />
+							<BubleTextEditor :form="element" field-name="title" />
 							<div v-if="element.type == 'list'">
 								<draggable
 									:list="element.data"
-									group="list"
-									itemKey="name"
+									group="menu"
+									itemKey="menu_id"
 									:disabled="tool.hand !== 'grab'">
 									<template #item="{ element: child, index: childIndex }">
 										<ul role="list">
-											<li :key="child.name">
-												<HyperLink
-													valueKeyLabel="name"
-													valueKeyLink="href"
-													:useDelete="true"
-													:data="child"
-													cssClass="space-y-3 text-sm leading-6 text-gray-600 hover:text-indigo-500" />
+											<li :key="child.column_id">
+												<BubleTextEditor :form="child" field-name="content" />
 											</li>
 										</ul>
 									</template>
@@ -90,13 +70,10 @@ console.log("theme1", props)
 							</div>
 
 							<div v-if="element.type == 'description'">
-								<TextArea
-									:data="element"
-									:save="props.saveTextArea"
-									:cssClass="'space-y-3 text-sm leading-6 text-gray-600 hover:text-indigo-500'" />
+								<BubleTextEditor :form="element" field-name="content" />
 							</div>
 
-							<div v-if="element.type == 'info'">
+						<!-- 	<div v-if="element.type == 'info'">
 								<div class="flex flex-col gap-y-5">
 									<draggable
 										:list="element.data"
@@ -105,12 +82,8 @@ console.log("theme1", props)
 										itemKey="name"
 										:disabled="tool.hand.name !== 'grab'">
 										<template #item="{ element: child, index: childIndex }">
-											<div
-												class="grid grid-cols-[auto,1fr] gap-4 items-center justify-start gap-y-3 mb-2.5">
-												<div
-													class="w-5 flex items-center justify-center text-gray-400">
-													<!-- <FontAwesomeIcon :icon="child.icon" :title="child.title"
-                                                aria-hidden="true" /> -->
+											<div class="grid grid-cols-[auto,1fr] gap-4 items-center justify-start gap-y-3 mb-2.5">
+												<div class="w-5 flex items-center justify-center text-gray-400">
 													<IconPicker
 														:modelValue="child.icon"
 														:data="child"
@@ -123,23 +96,12 @@ console.log("theme1", props)
 																})
 														" />
 												</div>
-												<Input
-													:data="child"
-													:save="
-														(value) =>
-															props.saveInfo({
-																parentId: element.id,
-																type: 'value',
-																...value,
-															})
-													"
-													keyValue="value"
-													cssClass="leading-5 text-gray-600" />
+												<BubleTextEditor :form="child" field-name="value" />
 											</div>
 										</template>
 									</draggable>
 								</div>
-							</div>
+							</div> -->
 						</div>
 					</template>
 				</draggable>
@@ -148,7 +110,7 @@ console.log("theme1", props)
 			<!-- Social Media -->
 			<div
 				class="border-t border-gray-900/10 pt-8 sm:mt-10 flex flex-col md:flex-row items-center justify-between mt-16 lg:mt-18 xl:px-3">    
-				<div class="md:order-2">
+			<!-- 	<div class="md:order-2">
 					<draggable
 						:list="footerDataLayout.socials"
 						group="socialMedia"
@@ -170,19 +132,11 @@ console.log("theme1", props)
 							</div>
 						</template>
 					</draggable>
-				</div>
+				</div> -->
 
 				<div class="flex">
 					<div class="mt-4 text-xs flex gap-1 leading-6 text-gray-500 md:order-1 md:mt-0">
-						&copy; 2023
-						<span class="font-bold">
-							<HyperLink
-								:useDelete="false"
-								:data="footerDataLayout.copyRight"
-								:save="copyRightSave"
-								valueKeyLabel="label"
-								valueKeyLink="href" /> </span
-						>, Inc. All rights reserved.
+						<BubleTextEditor :form="footerDataLayout" field-name="copyRight" />
 					</div>
 				</div>
 			</div>
--- resources/js/Components/CMS/Workshops/Footer/FooterWorkshop.vue
+++ resources/js/Components/CMS/Workshops/Footer/FooterWorkshop.vue
@@ -6,44 +6,20 @@
   -->
 
 <script setup>
-import { ref, watch } from 'vue'
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue'
 import Footer from '@/Components/CMS/Footer/index.vue'
-import { faHandPointer, faHandRock, faPlus } from '@fas';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { v4 as uuidv4 } from 'uuid';
-import HyperlinkTools from '@/Components/CMS/Fields/Hyperlinktools.vue'
-import { get } from 'lodash'
-import HyperInfoTools from '@/Components/CMS/Fields/InfoFieldTools.vue'
-import SocialMediaPicker from "@/Components/CMS/Fields/SocialMediaTools.vue"
-import Descriptor from './Descriptor'
+import { menuTypeList,  menuTypeDescription, menuTypeInfo, footerDataLayout, footerDataTools } from './Descriptor'
 import Tools from './Tools.vue'
 import { useForm } from '@inertiajs/vue3';
 
-library.add(faHandPointer, faHandRock, faPlus)
 
 //InitialData
-const ToolsBluprint = Descriptor.footerDataTools
-const ColumnsBluprint = Descriptor.footerDataLayout.initialColumns
-const copyRight = Descriptor.footerDataLayout.copyRight
-const socials = Descriptor.footerDataLayout.socials
+const ToolsBluprint = footerDataTools
 const toolsValue = useForm({
     theme : 'light-theme',
     hand  : 'click',
     columnType : null
 })
-const footerDataLayout = useForm({...Descriptor.footerDataLayout})
-
-// selectedData
-const selectedTheme = ref(ToolsBluprint.theme[0])
-const columnsTypeTheme = ref(ToolsBluprint.columnsType[0])
-const tool = ref(ToolsBluprint.hand[0])
-const columnSelected = ref(ColumnsBluprint[0]);
-
-
-
-
+const footerDataLayoutForm = useForm({...footerDataLayout})
 
 </script>
 
@@ -58,7 +34,7 @@ const columnSelected = ref(ColumnsBluprint[0]);
         <div class="flex justify-center items-center bg-gray-200 border border-gray-300 transform scale-80 w-full">
             <Footer 
               :tools="toolsValue"
-              :footerDataLayout="footerDataLayout"
+              :footerDataLayout="footerDataLayoutForm"
             />
         </div>
         </div>
