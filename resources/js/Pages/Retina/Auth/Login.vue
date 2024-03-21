adc0c880442e47f94211aa93a98d89fe55a6ae2e
aqordeon <aqordivika@yahoo.co.id>
2024-03-21 15:42:59 +0800

    feat: PKA-1475 Retina: Forgot Password

--- resources/js/Pages/Retina/Auth/Login.vue
+++ resources/js/Pages/Retina/Auth/Login.vue
@@ -44,6 +44,7 @@ onMounted(async () => {
             <div class="mt-1">
                 <input v-model="form.username" ref="inputUsername" id="username" name="username" :autofocus="true"
                     autocomplete="username" required
+                    placeholder="johndoe"
                     class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     @keydown.enter="submit"
                 />
@@ -53,14 +54,15 @@ onMounted(async () => {
         <div>
             <label for="password" class="block text-sm font-medium text-gray-700"> {{ trans('Password') }} </label>
             <div class="mt-1 flex flex-col rounded-md shadow-sm">
-                <LoginPassword :showProcessing="false" id="password" name="password" :form="form" fieldName="password" @keydown.enter="submit" />
+                <LoginPassword :showProcessing="false" id="password" name="password" :form="form" fieldName="password" @keydown.enter="submit" placeholder="********"/>
+                <Link href="/app/resetpassword" class="text-xs mt-2 italic text-gray-600 hover:underline cursor-pointer">Forgot password?</Link>
             </div>
         </div>
 
         <div class="flex items-center justify-between">
             <div class="flex items-center">
                 <Checkbox name="remember-me" id="remember-me" v-model:checked="form.remember" />
-                <label for="remember-me" class="ml-2 block text-sm text-gray-900"> {{ trans('Remember me') }} </label>
+                <label for="remember-me" class="ml-2 block text-sm select-none"> {{ trans('Remember me') }} </label>
             </div>
         </div>
 
