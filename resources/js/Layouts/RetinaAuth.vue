36de51ace574e5d3f7818704beb8d36d96d9b099
aqordeon <aqordivika@yahoo.co.id>
2024-03-22 09:11:35 +0800

    fix: Retina Auth: Revamp

--- resources/js/Layouts/RetinaAuth.vue
+++ resources/js/Layouts/RetinaAuth.vue
@@ -19,15 +19,17 @@ if (usePage().props.language) {
 </script>
 
 <template>
-    <div :style="{ 'background-image': `${breakpointType() !== 'xs' ? 'url(/art/backgroundWarehouse.jpg)' : false}`, 'background-repeat': 'no-repeat', 'background-size': 'cover', 'background-position': 'center' }"
-        class="relative h-screen w-screen bg-gradient-to-bl from-indigo-400 to-indigo-600 pt-64 sm:px-6 lg:px-8">
-        <div class="absolute bottom-5 left-10 flex items-center justify-center gap-x-2">
+    <div class="relative h-screen w-screen bg-gradient-to-bl from-sky-100 to-indigo-200 pt-64 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-center gap-x-2">
             <img class="h-12 w-auto" src="/art/logo-yellow.svg" alt="Aiku" />
             <span style="font-family: Fira" class="text-4xl text-white leading-none">{{ usePage().props.iris?.name }}</span>
         </div>
 
-        <div class="mt-8 mx-auto md:w-full max-w-md">
-            <div class="backdrop-blur-sm relative bg-white/65 py-8 px-4 shadow rounded-lg md:px-10">
+        <div class="grid grid-cols-7 mt-8 mx-auto md:w-full max-w-4xl shadow-lg rounded-lg overflow-hidden">
+            <div class="col-span-3 bg-[url('/art/backgroundWarehouse.jpg')] bg-cover bg-center">
+                
+            </div>
+            <div class="col-span-4 backdrop-blur-sm relative bg-white/65 py-8 px-4 md:px-10">
                 <slot />
             </div>
         </div>
