38d44a210ad8dabbf0bc935de9e43d8b43f99af8
Raul Perusquia <raul@inikoo.com>
2024-03-25 22:17:43 +0800

    chores: scrip to upload FX database to staging 🆙

---
+++ push_currency_exchange_db_to_staging.sh
@@ -0,0 +1,7 @@
+#
+# Author: Raul Perusquia <raul@inikoo.com>
+# Created: Mon, 25 Mar 2024 22:12:01 Malaysia Time, Mexico City, Mexico
+# Copyright (c) 2024, Raul A Perusquia Flores
+#
+
+scp database/seeders/datasets/currency-exchange/currency_exchanges.dump staging:aiku/shared/database/seeders/datasets/currency-exchange/
