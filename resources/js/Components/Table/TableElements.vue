b8018aa91ca6e01ddaf56b88110c089fe41080fa
aqordeon <aqordivika@yahoo.co.id>
2024-03-27 09:11:38 +0800

    fix: TableElement: UI

--- resources/js/Components/Table/TableElements.vue
+++ resources/js/Components/Table/TableElements.vue
@@ -3,14 +3,17 @@ import { trans } from 'laravel-vue-i18n'
 import { ref, reactive } from 'vue'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faChevronDown, faCheckSquare, faSquare } from '@far'
+import { faChevronDown, faCheckSquare, faSquare } from '@fal'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { onMounted } from 'vue'
+import { useLocaleStore } from '@/Stores/locale'
 library.add(faChevronDown, faCheckSquare, faSquare)
 
 
 const props = defineProps<{
-    elements: any
+    elements: {
+        [key: string]: number[] | string[]
+    }
     title: {
         title: string
         leftIcon: any
@@ -20,22 +23,14 @@ const props = defineProps<{
 // console.log(props.elements)
 
 const emits = defineEmits(['checkboxChanged'])
-const isChecked = ref({})
 const selectedGroup = ref(Object.keys(props.elements)[0]) ?? ref('')
 const selectedElement: any = reactive({
     [selectedGroup.value]: props.elements[selectedGroup.value]?.elements ? Object.keys(props.elements[selectedGroup.value].elements) : []
 })
 
-// to store the props to valid data for query
-// props.elements.forEach(item => {
-//     const key = item.key;
-//     const values = Object.keys(item.elements);
-
-//     isChecked.value[key] = values;
-// });
-
-
 let timeout: any = null
+
+// Method: Click the box
 const onClickCheckbox = (element: any, group: string) => {
     // Set timeout to prevent single on running twice on doubleclick
     clearTimeout(timeout)
@@ -55,6 +50,7 @@ const onClickCheckbox = (element: any, group: string) => {
     }, 200)
 }
 
+// Method: Double click the box
 const onDoubleClickCheckbox = (element: any, group: string) => {
     clearTimeout(timeout)
     if(!selectedElement[group]) selectedElement[group] = []
@@ -80,26 +76,24 @@ onMounted(() => {
 <template>
     <!-- If props.element not empty -->
     <div v-if="!!selectedGroup" class="flex items-center text-xs justify-between w-fit">
-        <!-- <div v-if="title.leftIcon || title.title" class="text-2xl flex items-center gap-x-2">
-            <FontAwesomeIcon v-if="title.leftIcon" :icon="title.leftIcon" aria-hidden="true" />
-            <p class="inline font-semibold leading-none capitalize">{{ title.title ? (title.title) : '' }}</p>
-        </div> -->
 
         <div class="w-fit flex gap-x-1 lg:gap-x-0 items-center justify-end border border-gray-200 divide-x divide-gray-200 rounded">
             <!-- List of element (checkbox) -->
             <div class="rounded overflow-hidden grid grid-rows-2 xl:grid-rows-1 grid-flow-col w-fit flex-wrap justify-end gap-0.5 ">
-                <div v-for="(value, element, index) of props.elements[selectedGroup]?.elements" :key="element"
-                    class="bg-white hover:bg-gray-200/60 flex items-center gap-x-1 px-3 py-2.5 cursor-pointer select-none"
+                <div v-for="(value, element, index) of elements[selectedGroup]?.elements" :key="element"
+                    class="hover:bg-gray-100/60 flex items-center gap-x-1 px-3 py-2.5 cursor-pointer select-none"
+                    :class="[selectedElement[selectedGroup]?.includes(element) ? 'bg-gray-50' : 'bg-white']"
                     @click="onClickCheckbox(element, selectedGroup)"
                     @dblclick="onDoubleClickCheckbox(element, selectedGroup)"
                     role="filter"
                     :id="value[0].replace(' ','-')"
                 >
-                    <FontAwesomeIcon v-if="selectedElement[selectedGroup]?.includes(element)" icon="far fa-check-square" aria-hidden="true" />
-                    <FontAwesomeIcon v-else icon="far fa-square" aria-hidden="true" />
-                    <div :class="[ isChecked ? 'text-gray-600' : 'text-gray-400',
-                        'grid justify-center grid-flow-col items-center capitalize hover:text-gray-600']">
-                        {{ value[0] }} ({{ value[1] }})
+                    <FontAwesomeIcon v-if="selectedElement[selectedGroup]?.includes(element)" icon="fal fa-check-square" aria-hidden="true" />
+                    <FontAwesomeIcon v-else icon="fal fa-square" aria-hidden="true" />
+                    <div :class="[ selectedElement[selectedGroup]?.includes(element) ? 'text-gray-600' : 'text-gray-600',
+                        'capitalize space-x-1']">
+                        <span class="font-normal">{{ value[0] }}</span>
+                        <span :class="[value[1] ? 'font-semibold' : 'text-gray-400']" class="">({{ useLocaleStore().number(value[1]) }})</span>
                     </div>
                 </div>
             </div>
@@ -112,7 +106,7 @@ onMounted(() => {
                         :class="[Object.keys(props.elements).length > 1 ? '' : 'cursor-default']"
                     >
                         <span v-if="Object.keys(props.elements).length > 1" class="pl-2 flex items-center justify-center">
-                            <FontAwesomeIcon icon="far fa-chevron-down" class="transition-all duration-200 ease-in-out" :class="[open ? 'rotate-180' : '']" aria-hidden="true" />
+                            <FontAwesomeIcon icon="fal fa-chevron-down" class="transition-all duration-200 ease-in-out" :class="[open ? 'rotate-180' : '']" aria-hidden="true" />
                         </span>
                         <span class="px-4">{{ selectedGroup }}</span>
                     </MenuButton>
@@ -124,14 +118,14 @@ onMounted(() => {
                     leave-active-class="transition duration-75 ease-in" leave-from-class="transform scale-100 opacity-100"
                     leave-to-class="transform scale-95 opacity-0">
                     <MenuItems
-                        class="absolute right-0 mt-2 w-40 min-w-min origin-top-right divide-y overflow-hidden divide-gray-100 rounded bg-gray-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
+                        class="absolute right-0 mt-2 w-40 min-w-min origin-top-right divide-y overflow-hidden divide-gray-100 rounded bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
                         <div class="">
                             <MenuItem v-for="element in props.elements" v-slot="{ active }" @click="selectedGroup = element.key"
-                                :class="[selectedGroup == element.key ? 'bg-gray-300' : '']"
+                                :class="[selectedGroup == element.key ? 'bg-gray-200' : '']"
                                 :disabled="selectedGroup == element.key"
                             >
                                 <button :class="[
-                                    active ? 'bg-gray-300' : 'text-gray-600',
+                                    active ? 'bg-gray-100' : 'text-gray-600',
                                     'group flex w-full items-center pl-4 py-2 capitalize',
                                 ]">
                                     <!-- <EditIcon :active="active" class="mr-2 h-5 w-5 text-orange-400" aria-hidden="true" /> -->
@@ -143,11 +137,5 @@ onMounted(() => {
                 </transition>
             </Menu>
         </div>
-
-
-        <!-- <div v-s -->
     </div>
-
-    <!-- <pre>{{ Object.keys(props.elements[selectedGroup].elements) }}
-{{ selectedElement }}</pre> -->
 </template>
