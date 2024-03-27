5a80283062a45cddeb3bcb8c8b1599a462c5680b
Raul Perusquia <raul@inikoo.com>
2024-03-27 13:04:20 +0800

    partial: top bar fulfillment

--- resources/js/Layouts/Grp/TopBar.vue
+++ resources/js/Layouts/Grp/TopBar.vue
@@ -39,14 +39,15 @@ import {
     faMoneyCheckAlt,
     faCashRegister,
     faCoins,
-    faFileInvoiceDollar
+    faFileInvoiceDollar,
+  faReceipt
 } from "@fal";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { useTruncate } from "@/Composables/useTruncate";
 import MenuTopRight from "@/Layouts/Grp/MenuTopRight.vue";
 
 library.add(faChevronDown, faTerminal, faUserAlien, faCog, faCity, faBuilding, faNetworkWired, faUserHardHat, faCalendar, faStopwatch, faStoreAlt, faWarehouseAlt, faChartNetwork, faFolderTree, faFolder, faCube, faUserPlus,
-    faBox, faBoxesAlt, faMoneyCheckAlt, faCashRegister, faCoins, faFileInvoiceDollar
+    faBox, faBoxesAlt, faMoneyCheckAlt, faCashRegister, faCoins, faFileInvoiceDollar, faReceipt
 );
 
 const props = defineProps<{
