8c38b2dde182fbb976562aab68fa6b82b7ceba9b
Raul Perusquia <raul@inikoo.com>
2024-04-01 22:19:11 +0800

    redis prefix same as horizon on♦️e

--- config/database.php
+++ config/database.php
@@ -124,7 +124,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix'  => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'aiku'), '_').'_database_'),
+            'prefix'  => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'aiku'), '_').'_'.env('APP_ENV').'_db_'),
         ],
 
         'default' => [
