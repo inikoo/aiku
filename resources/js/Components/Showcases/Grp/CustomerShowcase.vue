08cdd944408c10ac0fdcf4a3b05fe4c333e28199
aqordeon <aqordivika@yahoo.co.id>
2024-04-01 10:12:49 +0800

    fix: Fulfilment: Customer Showcase (profile box)

--- resources/js/Components/Showcases/Grp/CustomerShowcase.vue
+++ resources/js/Components/Showcases/Grp/CustomerShowcase.vue
@@ -16,12 +16,12 @@ import { trans } from 'laravel-vue-i18n'
 import TabSelector from '@/Components/Elements/TabSelector.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faLink} from '@far'
-import { faSync } from '@fal'
+import { faSync, faCalendarAlt, faEnvelope, faPhone } from '@fal'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import Button from '@/Components/Elements/Buttons/Button.vue'
 import axios from 'axios'
 import { notify } from '@kyvg/vue3-notification'
-library.add(faLink, faSync)
+library.add(faLink, faSync, faCalendarAlt, faEnvelope, faPhone)
 
 const props = defineProps<{
     data: {
@@ -108,36 +108,74 @@ const onRetrieveWebhook = () => {
         </div>
         
         <!-- Section: Profile box -->
-        <div class="border border-slate-200 text-retina-600 p-6 flex flex-col justify-between rounded-lg shadow overflow-hidden">
-            <div class="w-full">
-                <h2 v-if="data.customer?.name" class="text-3xl font-bold text-slate-600">{{ data.customer?.name }}</h2>
-                <h2 v-else class="text-3xl font-light italic brightness-75">{{ trans('No name') }}</h2>
-                <div class="text-lg">
-                    {{ data.customer?.shop }}
-                    <span class="text-gray-400">
-                        ({{ data.customer?.number_active_clients || 0 }} clients)
-                    </span>
-                </div>
-            </div>
-            <div class="space-y-3 text-sm text-slate-500">
-                <div class="border-l-2 border-slate-500 pl-4">
-                    <h3 class="font-light">Phone</h3>
-                    <address class="text-base font-bold not-italic text-slate-600">
-                        <p>{{ data.customer?.phone || '-' }}</p>
-                    </address>
-                </div>
-                <div class="border-l-2 border-slate-500 pl-4">
-                    <h3 class="font-light">Email</h3>
-                    <address class="text-base font-bold not-italic text-slate-600">
-                        <p>{{ data.customer?.email || '-' }}</p>
-                    </address>
-                </div>
-                <div class="border-l-2 border-slate-500 pl-4">
-                    <h3 class="font-light">Member since</h3>
-                    <address class="text-base font-bold not-italic text-slate-600">
-                        <p>{{ useFormatTime(data.customer?.created_at) || '-' }}</p>
-                    </address>
-                </div>
+        <div v-if="props.data.fulfilment_customer.radioTabs.dropshipping" class="">
+            <h2 class="sr-only">Customer profile</h2>
+            <div class="rounded-lg shadow-sm ring-1 ring-gray-900/5">
+                <dl class="flex flex-wrap">
+                    <!-- Profile: Header -->
+                    <div class="flex w-full py-6">
+                        <div class="flex-auto pl-6">
+                            <dt class="text-sm font-semibold leading-6 text-gray-900">Total Clients</dt>
+                            <dd class="mt-1 text-base font-semibold leading-6 text-gray-900">{{ data.customer.number_active_clients || 0 }}</dd>
+                        </div>
+                        <div class="flex-none self-end px-6 pt-4">
+                            <dt class="sr-only">Reference</dt>
+                            <dd class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                                {{ data.customer.reference }}
+                            </dd>
+                        </div>
+                    </div>
+                    
+                    <!-- Section: Field -->
+                    <div class="flex flex-col gap-y-3 border-t border-gray-900/5 w-full py-6">
+                        <!-- Field: Contact name -->
+                        <div v-if="data.customer.contact_name" class="flex items-center w-full flex-none gap-x-4 px-6">
+                            <dt v-tooltip="'Contact name'" class="flex-none">
+                                <span class="sr-only">Contact name</span>
+                                <FontAwesomeIcon icon='fal fa-user' class='text-gray-400' fixed-width aria-hidden='true' />
+                            </dt>
+                            <dd class="text-gray-500">{{ data.customer.contact_name }}</dd>
+                        </div>
+
+                        <!-- Field: Contact name -->
+                        <div v-if="data.customer.company_name" class="flex items-center w-full flex-none gap-x-4 px-6">
+                            <dt v-tooltip="'Company name'" class="flex-none">
+                                <span class="sr-only">Company name</span>
+                                <FontAwesomeIcon icon='fal fa-building' class='text-gray-400' fixed-width aria-hidden='true' />
+                            </dt>
+                            <dd class="text-gray-500">{{ data.customer.company_name }}</dd>
+                        </div>
+
+                        <!-- Field: Created at -->
+                        <div v-if="data.customer?.created_at" class="flex items-center w-full flex-none gap-x-4 px-6">
+                            <dt v-tooltip="'Created at'" class="flex-none">
+                                <span class="sr-only">Created at</span>
+                                <FontAwesomeIcon icon='fal fa-calendar-alt' class='text-gray-400' fixed-width aria-hidden='true' />
+                            </dt>
+                            <dd class="text-gray-500">
+                                <time datetime="2023-01-31">{{ useFormatTime(data.customer?.created_at) }}</time>
+                            </dd>
+                        </div>
+                        
+                        <!-- Field: Email -->
+                        <div v-if="data.customer?.email" class="flex items-center w-full flex-none gap-x-4 px-6">
+                            <dt v-tooltip="'Email'" class="flex-none">
+                                <span class="sr-only">Email</span>
+                                <FontAwesomeIcon icon='fal fa-envelope' class='text-gray-400' fixed-width aria-hidden='true' />
+                            </dt>
+                            <dd class="text-gray-500">{{ data.customer?.email }}</dd>
+                        </div>
+                        
+                        <!-- Field: Phone -->
+                        <div v-if="data.customer?.phone" class="flex items-center w-full flex-none gap-x-4 px-6">
+                            <dt v-tooltip="'Phone'" class="flex-none">
+                                <span class="sr-only">Phone</span>
+                                <FontAwesomeIcon icon='fal fa-phone' class='text-gray-400' fixed-width aria-hidden='true' />
+                            </dt>
+                            <dd class="text-gray-500">{{ data.customer?.phone }}</dd>
+                        </div>
+                    </div>
+                </dl>
             </div>
         </div>
 
