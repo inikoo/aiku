0424e4015e076ded71ef157cf46014c205d283ef
Artha <manager.arthachan@gmail.com>
2024-04-08 13:40:46 +0800

    fix: notifications

--- app/Notifications/MeasurementShareNotification.php
+++ app/Notifications/MeasurementShareNotification.php
@@ -5,6 +5,7 @@ namespace App\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Arr;
 use NotificationChannels\Fcm\FcmChannel;
 use NotificationChannels\Fcm\FcmMessage;
 
@@ -76,10 +77,10 @@ class MeasurementShareNotification extends Notification
         $measurement = $this->measurement;
 
         return [
-            'title' => $measurement['title'],
-            'body'  => $measurement['body'],
-            'type'  => $measurement['type'],
-            'id'    => $measurement['id']
+            'title' => Arr::get($measurement, 'title'),
+            'body'  => Arr::get($measurement, 'body'),
+            'type'  => Arr::get($measurement, 'type'),
+            'id'    => Arr::get($measurement, 'id')
         ];
     }
 }
