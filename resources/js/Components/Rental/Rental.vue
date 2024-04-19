4ad92cd7cb29928014e7c6e3de5ebd0dc7a82ed6
YudhistaA <aryarajasa0@gmail.com>
2024-04-19 15:39:49 +0800

    rental

--- resources/js/Components/Rental/Rental.vue
+++ resources/js/Components/Rental/Rental.vue
@@ -12,7 +12,7 @@ import { faCopy } from '@fal'
 import { faSpinnerThird } from '@fad'
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { set, get } from "lodash"
-import { ref, watch, defineEmits, onMounted} from "vue"
+import { ref, watch, defineEmits, onMounted } from "vue"
 import SelectQuery from "@/Components/SelectQuery.vue"
 import Button from "../Elements/Buttons/Button.vue"
 
@@ -34,11 +34,12 @@ const props = defineProps<{
 
 const emits = defineEmits()
 
+
 const defaultValue = [
     { rental: null, price: 0, disc: 0, amount: 0 },
 ]
 
-  
+
 onMounted(() => {
     props.form[props.fieldName] = props.form[props.fieldName] ? props.form[props.fieldName] : defaultValue
 })
@@ -66,7 +67,7 @@ const deleteRowRow = (index: number) => {
                             <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                                 Price</th>
                             <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                Disc</th>
+                                Discount (%)</th>
                             <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                                 Amount</th>
                             <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
@@ -75,27 +76,34 @@ const deleteRowRow = (index: number) => {
                         </tr>
                     </thead>
                     <tbody class="divide-y divide-gray-200 bg-white">
-                        <tr v-for="(person, index) in form[props.fieldName]" :key="person.email">
+                        <tr v-for="(itemData, index) in form[props.fieldName]" :key="itemData.email">
                             <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 w-80">
                                 <div class="relative w-full">
                                     <SelectQuery
                                         :urlRoute="route(fieldData.indexRentalRoute.name, fieldData.indexRentalRoute.parameters)"
-                                        :value="person" :placeholder="'Select or add rental'" :required="true"
+                                        :value="itemData" :placeholder="'Select or add rental'" :required="true"
                                         :label="'name'" :valueProp="'id'" :closeOnSelect="true" :clearOnSearch="false"
                                         :fieldName="'rental'" />
                                 </div>
 
                             </td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                <PureInput v-model="person.price" :placeholder="'Input Price'" type="number"
-                                    :minValue="0" />
+                                <PureInput v-model="itemData.price" :placeholder="'Input Price'" type="number"
+                                    :minValue="0" step="0.01" />
                             </td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                <PureInput v-model="person.disc" :placeholder="'Input Price'" type="number"
-                                    :minValue="0" />
+                                <PureInput v-model="itemData.disc" :placeholder="'Input Price'" type="number"
+                                    :suffix="true" :minValue="0" step="0.01">
+                                    <template #suffix>
+                                        <div
+                                            class="flex justify-center items-center px-2 absolute inset-y-0 right-0 gap-x-1 cursor-pointer opacity-20 hover:opacity-75 active:opacity-100 text-black">
+                                            %
+                                        </div>
+                                    </template>
+                                </PureInput>
                             </td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                {{ person.amount }}
+                                {{ itemData.price  - (itemData.price * (itemData.disc / 100 )) }}
                             </td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm">
                                 <font-awesome-icon :icon="['fas', 'trash']" class="text-red-500"
@@ -111,33 +119,6 @@ const deleteRowRow = (index: number) => {
                         </tr>
                     </tfoot>
                 </table>
-            </div>
-            <div class="flex justify-end w-full">
-                <section aria-labelledby="summary-heading" class="mt-auto w-[40%]">
-                    <div class="py-6 px-0 sm:rounded-lg sm:py-8">
-                        <h2 id="summary-heading" class="sr-only">Order summary</h2>
-                        <div class="flow-root">
-                            <dl class="-my-4 divide-y divide-gray-200 text-sm">
-                                <div class="flex items-center justify-between py-2">
-                                    <dt class="text-gray-600">Subtotal</dt>
-                                    <dd class="font-medium text-gray-900">$262.00</dd>
-                                </div>
-                                <div class="flex items-center justify-between py-2">
-                                    <dt class="text-gray-600">Discount</dt>
-                                    <dd class="font-medium text-gray-900">$5.00</dd>
-                                </div>
-                                <div class="flex items-center justify-between py-2">
-                                    <dt class="text-gray-600">Tax</dt>
-                                    <dd class="font-medium text-gray-900">$53.40</dd>
-                                </div>
-                                <div class="flex items-center justify-between py-2">
-                                    <dt class="text-base font-medium text-gray-900">Total</dt>
-                                    <dd class="text-base font-medium text-gray-900">$320.40</dd>
-                                </div>
-                            </dl>
-                        </div>
-                    </div>
-                </section>
             </div>
         </div>
     </div>
