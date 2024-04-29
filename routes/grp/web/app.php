d8fb46a6bb1e20d229adf81d0dcc42738894eff9
Artha <manager.arthachan@gmail.com>
2024-04-29 10:18:50 +0800

    feat: vika request to show routes in development

--- routes/grp/web/app.php
+++ routes/grp/web/app.php
@@ -109,3 +109,27 @@ Route::middleware(["auth"])->group(function () {
 */
 });
 require __DIR__ . "/auth.php";
+
+
+if(!app()->isProduction()) {
+    Route::get('routes', function () {
+        $routeCollection = Route::getRoutes();
+
+        echo "<table style='width:100%'>";
+        echo "<tr>";
+        echo "<td width='10%'><h4>HTTP Method</h4></td>";
+        echo "<td width='10%'><h4>Route</h4></td>";
+        echo "<td width='10%'><h4>Name</h4></td>";
+        echo "<td width='70%'><h4>Corresponding Action</h4></td>";
+        echo "</tr>";
+        foreach ($routeCollection as $value) {
+            echo "<tr>";
+            echo "<td>" . $value->methods()[0] . "</td>";
+            echo "<td>" . $value->uri() . "</td>";
+            echo "<td>" . $value->getName() . "</td>";
+            echo "<td>" . $value->getActionName() . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    });
+}
