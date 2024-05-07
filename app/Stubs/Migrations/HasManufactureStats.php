185d8777c461f776628168924bb4fed2eeb804ab
KirinZero0 <aryapermana02@gmail.com>
2024-05-07 13:10:23 +0800

    Add Raw Material Stats to HasManufactureStats Trait

--- app/Stubs/Migrations/HasManufactureStats.php
+++ app/Stubs/Migrations/HasManufactureStats.php
@@ -8,6 +8,10 @@
 namespace App\Stubs\Migrations;
 
 use App\Enums\Manufacturing\Production\ProductionStateEnum;
+use App\Enums\Manufacturing\RawMaterial\RawMaterialStateEnum;
+use App\Enums\Manufacturing\RawMaterial\RawMaterialStockStatusEnum;
+use App\Enums\Manufacturing\RawMaterial\RawMaterialTypeEnum;
+use App\Enums\Manufacturing\RawMaterial\RawMaterialUnitEnum;
 use Illuminate\Database\Schema\Blueprint;
 
 trait HasManufactureStats
@@ -20,6 +24,26 @@ trait HasManufactureStats
         }
         return $table;
     }
+
+    public function rawMaterialStats(Blueprint $table): Blueprint
+    {
+        $table->unsignedSmallInteger('number_raw_materials')->default(0);
+        foreach (RawMaterialTypeEnum::cases() as $case) {
+            $table->unsignedInteger('number_raw_materials_type_'.$case->snake())->default(0);
+        }
+        foreach (RawMaterialStateEnum::cases() as $case) {
+            $table->unsignedInteger('number_raw_materials_state_'.$case->snake())->default(0);
+        }
+        foreach (RawMaterialUnitEnum::cases() as $case) {
+            $table->unsignedInteger('number_raw_materials_unit_'.$case->snake())->default(0);
+        }
+        foreach (RawMaterialStockStatusEnum::cases() as $case) {
+            $table->unsignedInteger('number_raw_materials_stocks_status_'.$case->snake())->default(0);
+        }
+
+        return $table;
+    }
+
 
 
 
