3324273d03cce06b9f12e7dee33a8b07da37ea19
KirinZero0 <aryapermana02@gmail.com>
2024-05-08 11:23:34 +0800

    Manufacture Task Hydrator for Universal Search

--- app/Actions/Manufacturing/ManufactureTask/Hydrators/ManufactureTaskHydrateUniversalSearch.php
+++ app/Actions/Manufacturing/ManufactureTask/Hydrators/ManufactureTaskHydrateUniversalSearch.php
@@ -1,9 +1,4 @@
 <?php
-/*
- * Author: Jonathan Lopez Sanchez <jonathan@ancientwisdom.biz>
- * Created: Mon, 13 Mar 2023 10:02:57 Central European Standard Time, Malaga, Spain
- * Copyright (c) 2023, Inikoo LTD
- */
 
 namespace App\Actions\Manufacturing\ManufactureTask\Hydrators;
 
