68b845432bead09a1a488c7c05590d94aaca4766
KirinZero0 <aryapermana02@gmail.com>
2024-05-10 16:44:47 +0800

    #205: feat: JobOrderItem Action Untested

---
+++ app/Actions/Manufacturing/JobOrderItem/StoreJobOrderItem.php
@@ -0,0 +1,129 @@
+<?php
+
+ namespace App\Actions\Manufacturing\JobOrderItem;
+
+use App\Actions\OrgAction;
+use App\Enums\Manufacturing\JobOrderItem\JobOrderItemStateEnum;
+use App\Enums\Manufacturing\JobOrderItem\JobOrderItemStatusEnum;
+use App\Models\Manufacturing\JobOrder;
+use App\Models\Manufacturing\JobOrderItem;
+use App\Models\SysAdmin\Organisation;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Lorisleiva\Actions\ActionRequest;
+
+ class StoreJobOrderItem extends OrgAction
+ {
+    public function handle(JobOrder $jobOrder, array $modelData): JobOrderItem
+    {
+        if (Arr::get($modelData, 'notes') === null) {
+            data_set($modelData, 'notes', '');
+        }
+
+        if (Arr::exists($modelData, 'state') and Arr::get($modelData, 'state') != JobOrderItemStateEnum::IN_PROCESS) {
+            if (!Arr::get($modelData, 'reference')) {
+                data_set(
+                    $modelData,
+                    'reference',
+                    Str::random(10) //TODO: make a reference generator for Job Order Item
+                );
+            }
+        }
+
+        data_set($modelData, 'group_id', $jobOrder->group_id);
+        data_set($modelData, 'organisation_id', $jobOrder->organisation_id);
+
+        /** @var Pallet $pallet */
+        $jobOrderItem = $jobOrder->jobOrderItems()->create($modelData);
+
+        if ($jobOrderItem->reference) {
+            $jobOrderItem->generateSlug();
+            $jobOrderItem->save();
+        }
+        $jobOrderItem->refresh();
+
+
+        // if ($this->parent instanceof PalletDelivery) {
+        // }
+        // FulfilmentCustomerHydratePallets::dispatch($fulfilmentCustomer);
+        // FulfilmentHydratePallets::dispatch($fulfilmentCustomer->fulfilment);
+        // OrganisationHydratePallets::dispatch($fulfilmentCustomer->organisation);
+        // WarehouseHydratePallets::dispatch($pallet->warehouse);
+        // if ($pallet->location && $pallet->location->warehouseArea) {
+        //     WarehouseAreaHydratePallets::dispatch($pallet->location->warehouseArea);
+        // }
+        // PalletHydrateUniversalSearch::dispatch($pallet);
+
+        return $jobOrderItem;
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        if ($this->asAction) {
+            return true;
+        }
+
+        return $request->user()->hasPermissionTo("productions-view.{$this->organisation->id}");
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            'artifact_id'       => ['required', 'integer', 'exists:artifacts,id'],
+            'status'             => [
+                'sometimes',
+                Rule::enum(JobOrderItemStatusEnum::class)
+            ],
+            'state'              => [
+                'sometimes',
+                Rule::enum(JobOrderItemStateEnum::class)
+            ],
+            'notes'              => ['sometimes', 'nullable', 'string', 'max:1024'],
+            'quantity'          => ['required', 'integer', 'min:1'],
+            'created_at'         => ['sometimes', 'date'],
+            'received_at'        => ['sometimes', 'nullable', 'date'],
+        ];
+    }
+
+
+    public function asController(Organisation $organisation, JobOrder $jobOrder, ActionRequest $request): JobOrderItem
+    {
+        $this->initialisation($organisation, $request);
+
+        return $this->handle($jobOrder, $this->validatedData);
+    }
+
+
+    public function action(JobOrder $jobOrder, array $modelData): JobOrderItem
+    {
+        $this->asAction = true;
+        $this->initialisation($jobOrder->organisation, $modelData);
+
+        return $this->handle($jobOrder, $this->validatedData);
+    }
+
+
+
+    // public function htmlResponse(Pallet $pallet, ActionRequest $request): RedirectResponse
+    // {
+    //     if ($this->parent instanceof PalletDelivery) {
+    //         return Redirect::route('grp.org.fulfilments.show.crm.customers.show.pallet-deliveries.show', [
+    //             'organisation'       => $pallet->organisation->slug,
+    //             'fulfilment'         => $pallet->fulfilment->slug,
+    //             'fulfilmentCustomer' => $pallet->fulfilmentCustomer->slug,
+    //             'palletDelivery'     => $this->parent->slug
+    //         ]);
+    //     }
+
+    //     return Redirect::route(
+    //         'grp.org.fulfilments.show.crm.customers.show',
+    //         [
+    //             'organisation'       => $pallet->organisation->slug,
+    //             'fulfilment'         => $pallet->fulfilment->slug,
+    //             'fulfilmentCustomer' => $pallet->fulfilmentCustomer->slug,
+    //         ]
+    //     );
+    // }
+ }
---
+++ app/Actions/Manufacturing/JobOrderItem/UpdateJobOrderItem.php
@@ -0,0 +1,115 @@
+<?php
+
+ namespace App\Actions\Manufacturing\JobOrderItem;
+
+use App\Actions\OrgAction;
+use App\Actions\Traits\WithActionUpdate;
+use App\Enums\Manufacturing\JobOrderItem\JobOrderItemStateEnum;
+use App\Enums\Manufacturing\JobOrderItem\JobOrderItemStatusEnum;
+use App\Models\CRM\WebUser;
+use App\Models\Manufacturing\JobOrderItem;
+use Illuminate\Validation\Rule;
+use Lorisleiva\Actions\ActionRequest;
+
+ class UpdateJobOrderItem extends OrgAction
+{
+    use WithActionUpdate;
+
+
+    private JobOrderItem $jobOrderItem;
+
+    public function handle(JobOrderItem $jobOrderItem, array $modelData): JobOrderItem
+    {
+        $pallet = $this->update($jobOrderItem, $modelData);
+
+        // if($pallet->wasChanged('state')) {
+
+        //     if($pallet->pallet_delivery_id) {
+        //         UpdatePalletDeliveryStateFromItems::run($pallet->palletDelivery);
+        //     }
+        //     if($pallet->pallet_return_id) {
+        //         UpdatePalletReturnStateFromItems::run($pallet->palletReturn);
+        //     }
+
+        //     HydrateFulfilmentCustomer::dispatch($pallet->fulfilmentCustomer);
+        //     FulfilmentCustomerHydratePallets::dispatch($pallet->fulfilmentCustomer);
+        //     FulfilmentHydratePallets::dispatch($pallet->fulfilment);
+        //     OrganisationHydratePallets::dispatch($pallet->organisation);
+        //     WarehouseHydratePallets::dispatch($pallet->warehouse);
+        // }
+
+        return $jobOrderItem;
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        if ($this->asAction) {
+            return true;
+        }
+
+        if ($request->user() instanceof WebUser) {
+            return true;
+        }
+
+        return $request->user()->hasPermissionTo("productions-view.{$this->organisation->id}");
+    }
+
+    public function rules(): array
+    {
+        return [
+            'status'             => [
+                'sometimes',
+                Rule::enum(JobOrderItemStatusEnum::class)
+            ],
+            'state'              => [
+                'sometimes',
+                Rule::enum(JobOrderItemStateEnum::class)
+            ],
+            'notes'              => ['sometimes', 'nullable', 'string', 'max:1024'],
+            'quantity'          =>  ['sometimes', 'integer', 'min:1'],
+            'received_at'        => ['sometimes', 'nullable', 'date'],
+        ];
+    }
+
+    // public function fromRetina(Pallet $pallet, ActionRequest $request): Pallet
+    // {
+    //     /** @var FulfilmentCustomer $fulfilmentCustomer */
+    //     $fulfilmentCustomer = $request->user()->customer->fulfilmentCustomer;
+    //     $this->fulfilment   = $fulfilmentCustomer->fulfilment;
+    //     $this->pallet       = $pallet;
+
+    //     $this->initialisation($request->get('website')->organisation, $request);
+    //     return $this->handle($pallet, $this->validatedData);
+    // }
+
+    public function asController(JobOrderItem $jobOrderItem, ActionRequest $request): JobOrderItem
+    {
+        $this->jobOrderItem = $jobOrderItem;
+        $this->initialisation($jobOrderItem->organisation, $request);
+
+        return $this->handle($jobOrderItem, $this->validatedData);
+    }
+
+    // public function fromApi(Organisation $organisation, Warehouse $warehouse, Fulfilment $fulfilment, Pallet $pallet, ActionRequest $request): Pallet
+    // {
+    //     $this->pallet = $pallet;
+    //     $this->initialisationFromFulfilment($pallet->fulfilment, $request);
+
+    //     return $this->handle($pallet, $this->validatedData);
+    // }
+
+    public function action(JobOrderItem $jobOrderItem, array $modelData, int $hydratorsDelay = 0): JobOrderItem
+    {
+        $this->jobOrderItem         = $jobOrderItem;
+        $this->asAction       = true;
+        $this->hydratorsDelay = $hydratorsDelay;
+        $this->initialisation($jobOrderItem->organisation, $modelData);
+
+        return $this->handle($jobOrderItem, $this->validatedData);
+    }
+
+    // public function jsonResponse(Pallet $pallet): PalletResource
+    // {
+    //     return new PalletResource($pallet);
+    // }
+}
