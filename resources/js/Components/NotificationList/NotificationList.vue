4c85951767444e01fecbc4026eb4cfb3f1f16c32
aqordeon <aqordivika@yahoo.co.id>
2024-05-10 13:23:13 +0800

    #122 Notifications: state no notif

--- resources/js/Components/NotificationList/NotificationList.vue
+++ resources/js/Components/NotificationList/NotificationList.vue
@@ -1,39 +1,57 @@
 <script setup lang='ts'>
-import { faEnvelope, faEnvelopeOpenText } from "@fal";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faEnvelope, faEnvelopeOpenText } from "@fal"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { useFormatTime } from '@/Composables/useFormatTime'
+import { routeType } from "@/types/route"
+import { Link } from "@inertiajs/vue3"
+
+library.add(faEnvelope, faEnvelopeOpenText)
 
 const props = defineProps<{
-    messages: []
+    messages: {
+        data: {
+            id: number
+            read: boolean
+            route: routeType
+            title: string
+            body: string
+            created_at: Date | string
+        }[]
+    }
 }>()
 
-library.add(faEnvelope, faEnvelopeOpenText);
-
-const messages = props.messages.data
-// console.log('rrr', props.messages)
 </script>
 
 <template>
-    <div class="overflow-auto min-h-11 max-h-96">
-      <ul role="list" class="divide-y divide-gray-100 overflow-hidden bg-white">
-        <li v-for="message in messages" :key="message.id" class="relative flex justify-between gap-x-6 px-1 py-2 hover:bg-gray-50 sm:px-2">
-          <div class="flex min-w-full gap-x-4">
-            <font-awesome-icon :icon="message.read ? ['fal', 'envelope-open-text'] : ['fal', 'envelope']" :class="['h-8 w-8 flex-none m-auto', message.read && 'text-gray-400']" />
-            <div class="min-w-0 flex-auto relative">
-              <p :class="['text-sm font-semibold leading-6', message.read ? 'text-gray-400' : 'text-gray-900']">
-                <a :href="message.href">
-                  <span :class="['absolute inset-x-0 -top-px bottom-0']"></span>
-                  {{ message.title }}
-                </a>
-              </p>
-              <span class="text-[10px] text-gray-500 absolute top-0 right-0 mt-1 mr-1">{{useFormatTime(message.created_at)}}</span>
-              <p :class="['mt-1 flex text-xs leading-5', message.read ? 'text-gray-400' : 'text-gray-500']">
-                <span :name="message.body" class="relative truncate hover:underline">{{ message.body }}</span>
-              </p>
-            </div>
-          </div>
-        </li>
-      </ul>
+    <div class="flex items-center w-full overflow-auto min-h-11 max-h-96 ">
+        <ul v-if="messages.data.length" role="list" class="divide-y divide-gray-100 overflow-hidden">
+            <li v-for="message in messages.data" :key="message.id"
+                class="relative flex justify-between gap-x-6 px-1 py-2 hover:bg-gray-50 sm:px-2">
+                <div class="flex min-w-full gap-x-4">
+                    <font-awesome-icon :icon="message.read ? ['fal', 'envelope-open-text'] : ['fal', 'envelope']"
+                        :class="['h-8 w-8 flex-none m-auto', message.read && 'text-gray-400']" />
+                    <div class="min-w-0 flex-auto relative">
+                        <div
+                            :class="['text-sm font-semibold leading-6', message.read ? 'text-gray-400' : '']">
+                            <component :is="message.route?.name ? Link : 'div'" :href="route(message.route.name, message.route?.parameters)">
+                                <span :class="['absolute inset-x-0 -top-px bottom-0']"></span>
+                                {{ message.title }}
+                            </component>
+                        </div>
+                        <span
+                            class="text-[10px] text-gray-500 absolute top-0 right-0 mt-1 mr-1">{{ useFormatTime(message.created_at) }}</span>
+                        <p :class="['mt-1 flex text-xs leading-5', message.read ? 'text-gray-400' : 'text-gray-500']">
+                            <span :name="message.body" class="relative truncate hover:underline">{{ message.body
+                                }}</span>
+                        </p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+        <div class="mx-auto italic text-gray-500">
+            You have no notifications.
+        </div>
     </div>
-  </template>
+</template>
