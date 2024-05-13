ebbc1e47a8f1812afcd49711bcd87b3baa2f61cc
Artha <manager.arthachan@gmail.com>
2024-05-13 16:46:41 +0800

    comment notes stored items

--- app/Actions/Fulfilment/StoredItem/UI/IndexStoredItems.php
+++ app/Actions/Fulfilment/StoredItem/UI/IndexStoredItems.php
@@ -84,7 +84,7 @@ class IndexStoredItems extends OrgAction
                 ->column(key: 'customer_name', label: __('Customer Name'), canBeHidden: false, sortable: true, searchable: true)
                 ->column(key: 'state', label: __('Delivery State'), canBeHidden: false, sortable: true, searchable: true)
                 ->column(key: 'status', label: __('Item Status'), canBeHidden: false, sortable: true, searchable: true)
-                ->column(key: 'notes', label: __('Notes'), canBeHidden: false, sortable: true, searchable: true)
+                // ->column(key: 'notes', label: __('Notes'), canBeHidden: false, sortable: true, searchable: true)
                 ->defaultSort('slug');
         };
     }
