3cc3aeec1085f86d8b13595d628d4aefbc35c8c4
YudhistaA <aryarajasa0@gmail.com>
2024-05-13 10:17:17 +0800

    ShowUser Showcase tab #180

--- resources/js/Components/Showcases/Grp/SysadminUserShowcase.vue
+++ resources/js/Components/Showcases/Grp/SysadminUserShowcase.vue
@@ -4,6 +4,8 @@ import { layoutStructure } from '@/Composables/useLayoutStructure'
 import { useLiveUsers } from '@/Stores/active-users'
 import { Image as ImageTS } from '@/types/Image'
 import { inject } from 'vue'
+import Tag from '@/Components/Tag.vue'
+import { useFormatTime } from '@/Composables/useFormatTime'
 
 const props = defineProps<{
     tab: string
@@ -30,35 +32,57 @@ const props = defineProps<{
 const layout = inject('layout', layoutStructure)
 const activeUsers = useLiveUsers().liveUsers
 console.log('qq', activeUsers)
+console.log('qq', props)
 
 </script>
 
 <template>
-    <div>
-        <!-- <pre>{{ data }}</pre> -->
-        <!-- <pre>{{ activeUsers }}</pre> -->
-    </div>
-
-    <div class="py-4 px-8">
-        <div class="h-40 aspect-square rounded-full overflow-hidden shadow">
+    <div class="flex py-4 px-8">
+        <div class="h-40 aspect-square rounded-full overflow-hidden shadow m-5">
             <Image :src="data.data.avatar" />
         </div>
 
-        <div class="flex gap-x-1">
-            <div>Email:</div>
-            <div class="font-semibold">{{ data.data.email || '-' }}</div>
-        </div>
-        <div class="flex gap-x-1">
-            <div>Name:</div>
-            <div class="font-semibold">{{ data.data.contact_name }}</div>
-        </div>
-        <div class="flex gap-x-1">
-            <div>Status:</div>
-            <div class="font-semibold">{{ activeUsers[data.id] ? 'Online' : 'Offline' }}</div>
-        </div>
-        <div class="flex gap-x-1">
-            <div>Last Online:</div>
-            <div class="font-semibold">{{ data.data.lastLogged || 'Never' }}</div>
+        <div class="w-full">
+            <dl class="grid grid-cols-1 sm:grid-cols-2">
+
+                <div class="px-4 py-6 sm:col-span-1 sm:px-0">
+                    <dt class="text-sm font-medium leading-6 text-gray-900">Email :</dt>
+                    <dd class="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">{{ data.data.email || '-' }}</dd>
+                </div>
+
+
+                <div class="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
+                    <dt class="text-sm font-medium leading-6 text-gray-900">Name :</dt>
+                    <dd class="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">{{ data.data.contact_name }}</dd>
+                </div>
+            </dl>
+
+            <div class="mt-6">
+                <dl class="grid grid-cols-1 sm:grid-cols-2">
+
+                    <div class="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Status :</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">  <Tag :label="activeUsers[data.data.id] ? 'Online' : 'Offline'" :theme="activeUsers[data.data.id] ? 3 : ''"></Tag></dd>
+                    </div>
+
+                    <div class="border-t border-gray-100 px-4 py-6 sm:col-span-1 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Last Active :</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">{{ activeUsers[data.data.id]?.last_active ? useFormatTime(activeUsers[data.data.id].last_active) : 'Never' }}
+                        </dd>
+                    </div>
+
+                    <div class="border-t border-gray-100 px-4 py-6 sm:col-span-2 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Authorized Organisations : </dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:mt-2"
+                            style="display: flex; flex-wrap: wrap;">
+                            <div v-for="item of data.data.authorizedOrganisations" class="m-1">
+                                <Tag :label="item.name"></Tag>
+                            </div>
+                        </dd>
+                    </div>
+
+                </dl>
+            </div>
         </div>
     </div>
 </template>
