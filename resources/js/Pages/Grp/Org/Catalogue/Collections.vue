4535616ce27031cb67547398159f3ebcaba4a21e
KirinZero0 <aryapermana02@gmail.com>
2024-05-14 08:26:28 +0800

    Collections.vue for Navigation

---
+++ resources/js/Pages/Grp/Org/Catalogue/Collections.vue
@@ -0,0 +1,27 @@
+<!--
+  -  Author: Jonathan Lopez <raul@inikoo.com>
+  -  Created: Wed, 12 Oct 2022 16:50:56 Central European Summer Time, Benalmádena, Malaga,Spain
+  -  Copyright (c) 2022, Jonathan Lopez
+  -->
+
+  <script setup lang="ts">
+  import {Head} from '@inertiajs/vue3';
+  import PageHeading from '@/Components/Headings/PageHeading.vue';
+  import TableCollections from "@/Components/Tables/TableCollections.vue";
+  import { capitalize } from "@/Composables/capitalize"
+  
+  const props = defineProps <{
+      pageHead: object
+      data: object
+      title: string
+  }>()
+  
+  </script>
+  
+  <template>
+      <Head :title="capitalize(title)"/>
+      <PageHeading :data="pageHead"></PageHeading>
+      <TableCollections :data="data" />
+  </template>
+  
+  
