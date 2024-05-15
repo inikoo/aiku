cb70417b966a933e6ae0bf9a653f9dc719072cd0
Artha <manager.arthachan@gmail.com>
2024-05-15 15:40:25 +0800

    fix: structure data edit user permissiosn

--- app/Actions/SysAdmin/User/UI/EditUser.php
+++ app/Actions/SysAdmin/User/UI/EditUser.php
@@ -10,6 +10,8 @@ namespace App\Actions\SysAdmin\User\UI;
 use App\Actions\InertiaAction;
 use App\Enums\SysAdmin\Authorisation\RolesEnum;
 use App\Models\Catalogue\Shop;
+use App\Models\Fulfilment\Fulfilment;
+use App\Models\SysAdmin\Group;
 use App\Models\SysAdmin\Organisation;
 use App\Models\SysAdmin\User;
 use Inertia\Inertia;
@@ -41,11 +43,23 @@ class EditUser extends InertiaAction
         $roles       = collect(RolesEnum::cases());
         $permissions = $roles->map(function ($role) {
             return [$role->label() => match ($role->scope()) {
+                class_basename(Group::class) => Group::all()->map(function (Group $group) {
+                    return [$group->name => [
+                        'organisations' => $group->organisations->pluck('slug')
+                    ]];
+                }),
                 class_basename(Organisation::class) => [
                     'organisations' => Organisation::all()->pluck('slug')
                 ],
                 class_basename(Shop::class) => Organisation::all()->map(function (Organisation $organisation) {
-                    return ['shops' => $organisation->shops->pluck('slug')];
+                    return [$organisation->name => [
+                        'shops' => $organisation->shops->pluck('slug')
+                    ]];
+                }),
+                class_basename(Fulfilment::class) => Organisation::all()->map(function (Organisation $organisation) {
+                    return [$organisation->name => [
+                        'fulfilments' => $organisation->fulfilments->pluck('slug')
+                    ]];
                 }),
                 default => []
             }];
