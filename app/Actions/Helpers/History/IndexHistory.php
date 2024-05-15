f774b627816c3b3e06a664e428c176b8a0b21ec5
Artha <manager.arthachan@gmail.com>
2024-05-15 10:15:33 +0800

    fix: table histiry

--- app/Actions/Helpers/History/IndexHistory.php
+++ app/Actions/Helpers/History/IndexHistory.php
@@ -72,9 +72,9 @@ class IndexHistory
                 ->withGlobalSearch()
                 ->withExportLinks($exportLinks)
                 ->withModelOperations($modelOperations)
-                ->column(key: 'created_at', label: __('Date'), canBeHidden: false, sortable: true)
-                ->column(key: 'user_id', label: __('User'), canBeHidden: false, sortable: true)
-                ->column(key: 'action', label: __('Action'), canBeHidden: false, sortable: true)
+                ->column(key: 'datetime', label: __('Date'), canBeHidden: false, sortable: true)
+                ->column(key: 'user_name', label: __('User'), canBeHidden: false, sortable: true)
+                ->column(key: 'event', label: __('Action'), canBeHidden: false, sortable: true)
                 ->defaultSort('ip_address');
         };
     }
