b21c7b259abd195f98b5dc60ce6eb98c69dac11c
KirinZero0 <aryapermana02@gmail.com>
2024-05-16 09:47:40 +0800

    fix pin in migration

--- database/migrations/2024_05_16_014055_add_pin_to_employees_table.php
+++ database/migrations/2024_05_16_014055_add_pin_to_employees_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('employees', function (Blueprint $table) {
-            $table->string('pin')->unique();
+            $table->string('pin')->unique()->nullable();
         });
     }
 
