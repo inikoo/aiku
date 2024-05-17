d01177d74d5c949d3b1c740bab6f229dcd6c6b7a
Artha <manager.arthachan@gmail.com>
2024-05-17 10:02:54 +0800

    fix: clocking resource

---
+++ app/Http/Resources/HumanResources/ClockingResource.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ *  Author: Raul Perusquia <raul@inikoo.com>
+ *  Created: Thu, 21 Oct 2021 12:37:51 Malaysia Time, Kuala Lumpur, Malaysia
+ *  Copyright (c) 2021, Inikoo
+ *  Version 4.0
+ */
+
+namespace App\Http\Resources\HumanResources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property int $id
+ * @property string $slug
+ * @property string $type
+ * @property string $notes
+ * @property string $workplace_slug
+ * @property string $clocking_machine_slug
+ */
+class ClockingResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id'                    => $this->id,
+            'type'                  => $this->type,
+            'notes'                 => $this->notes,
+            'workplace_slug'        => $this->workplace->slug,
+            'clocking_machine_slug' => $this->clockingMachine->slug,
+            'employee'              => EmployeeResource::make($this->subject),
+            'photo'                 => $this->photoImageSources()
+        ];
+    }
+}
