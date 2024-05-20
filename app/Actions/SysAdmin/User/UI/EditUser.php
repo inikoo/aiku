d4b7908b0998858cc27353ff0a73d723d4be9fb6
Artha <manager.arthachan@gmail.com>
2024-05-20 14:00:56 +0800

    fix: edit user

--- app/Actions/SysAdmin/User/UI/EditUser.php
+++ app/Actions/SysAdmin/User/UI/EditUser.php
@@ -8,11 +8,6 @@
 namespace App\Actions\SysAdmin\User\UI;
 
 use App\Actions\InertiaAction;
-use App\Enums\SysAdmin\Authorisation\RolesEnum;
-use App\Models\Catalogue\Shop;
-use App\Models\Fulfilment\Fulfilment;
-use App\Models\SysAdmin\Group;
-use App\Models\SysAdmin\Organisation;
 use App\Models\SysAdmin\User;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -40,39 +35,6 @@ class EditUser extends InertiaAction
 
     public function htmlResponse(User $user, ActionRequest $request): Response
     {
-
-
-        $orgTypeShop=[];
-
-        $roles       = collect(RolesEnum::cases());
-        $permissions = $roles->map(function ($role) {
-
-
-
-
-            return [$role->label() => match ($role->scope()) {
-                class_basename(Group::class) => Group::all()->map(function (Group $group) {
-                    return [$group->name => [
-                        'organisations' => $group->organisations->pluck('slug')
-                    ]];
-                }),
-                class_basename(Organisation::class) => [
-                    'organisations' => Organisation::all()->pluck('slug')
-                ],
-                class_basename(Shop::class) => Organisation::all()->map(function (Organisation $organisation) {
-                    return [$organisation->name => [
-                        'shops' => $organisation->shops->pluck('slug')
-                    ]];
-                }),
-                class_basename(Fulfilment::class) => Organisation::all()->map(function (Organisation $organisation) {
-                    return [$organisation->name => [
-                        'fulfilments' => $organisation->fulfilments->pluck('slug')
-                    ]];
-                }),
-                default => []
-            }];
-        });
-
         return Inertia::render("EditModel", [
             "title"       => __("user"),
             "breadcrumbs" => $this->getBreadcrumbs(
