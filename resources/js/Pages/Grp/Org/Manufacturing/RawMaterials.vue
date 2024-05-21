ceb73b9b7d90726ff4f81559eccd900dee720d9e
KirinZero0 <aryapermana02@gmail.com>
2024-05-21 08:44:50 +0800

    #275: Fix Index Raw Materials History

--- resources/js/Pages/Grp/Org/Manufacturing/RawMaterials.vue
+++ resources/js/Pages/Grp/Org/Manufacturing/RawMaterials.vue
@@ -16,6 +16,7 @@ import { useTabChange } from "@/Composables/tab-change";
 import Tabs from "@/Components/Navigation/Tabs.vue";
 import { PageHeading as PageHeadingTypes } from "@/types/PageHeading";
 import type { Navigation } from "@/types/Tabs";
+import TableHistories from "@/Components/Tables/Grp/Helpers/TableHistories.vue"
 
 
 library.add(faBars, faIndustry);
@@ -29,6 +30,7 @@ const props = defineProps<{
   },
   title: string
   raw_materials?: object
+  raw_materials_histories?: {}
 }>();
 
 let currentTab = ref(props.tabs.current);
@@ -37,7 +39,8 @@ const handleTabUpdate = (tabSlug) => useTabChange(tabSlug, currentTab);
 const component = computed(() => {
 
   const components = {
-    raw_materials: TableRawMaterials
+    raw_materials: TableRawMaterials,
+    raw_materials_histories: TableHistories,
   };
   return components[currentTab.value];
 
