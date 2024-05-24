1abd391ea1c110821188920a51e901351497b628
YudhistaA <aryarajasa0@gmail.com>
2024-05-24 09:42:21 +0800

    Password: the checkbox and eye is stacking

--- resources/js/Components/Forms/Fields/Password.vue
+++ resources/js/Components/Forms/Fields/Password.vue
@@ -35,20 +35,19 @@ const showPassword = ref(true);
             <input @input="handleChange(form)" v-model="form[fieldName]" :type="showPassword ? 'password' : 'text'"
                 autocomplete="off" :placeholder="fieldData.placeholder"
                 class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 w-full border-gray-300 rounded-l-md" />
-            <button type="button" @click="showPassword = !showPassword"
-                class="w-min px-3 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
-                <font-awesome-icon aria-hidden="true" class="h-5 w-5 text-gray-400"
-                    :icon="showPassword ? 'fas fa-eye' : 'fas fa-eye-slash'" />
-            </button>
-        </div>
-
-        <div v-if="form.errors[fieldName] || form.recentlySuccessful"
-            class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                <div v-if="form.errors[fieldName] || form.recentlySuccessful"
+            class="absolute inset-y-0 right-11 pr-3 flex items-center pointer-events-none">
             <FontAwesomeIcon icon="fas fa-exclamation-circle" v-if="form.errors[fieldName]" class="h-5 w-5 text-red-500"
                 aria-hidden="true" />
             <FontAwesomeIcon icon="fas fa-check-circle" v-if="form.recentlySuccessful"
                 class="mt-1.5  h-5 w-5 text-green-500" aria-hidden="true" />
 
+        </div>
+            <button type="button" @click="showPassword = !showPassword"
+                class="w-min px-3 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+                <font-awesome-icon aria-hidden="true" class="h-5 w-5 text-gray-400"
+                    :icon="showPassword ? 'fas fa-eye' : 'fas fa-eye-slash'" />
+            </button>
         </div>
     </div>
     <p v-if="form.errors[fieldName]" class="mt-2 text-sm text-red-600" id="email-error">{{ form.errors[fieldName] }}</p>
