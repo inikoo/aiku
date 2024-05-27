ebe26ad648cdff950a24af0581df1bffee41d5b2
aqordeon <aqordivika@yahoo.co.id>
2024-05-27 10:47:55 +0800

    fix: UI: move Logout to LeftSidebar

--- resources/js/Layouts/Grp/MenuTopRight.vue
+++ resources/js/Layouts/Grp/MenuTopRight.vue
@@ -1,9 +1,7 @@
 <script setup lang='ts'>
 import { trans } from 'laravel-vue-i18n'
 import { useLayoutStore } from '@/Stores/layout'
-import { router } from '@inertiajs/vue3'
 import { ref, onMounted, onUnmounted, inject, defineAsyncComponent } from 'vue'
-import { useLiveUsers } from '@/Stores/active-users'
 import SearchBar from "@/Components/SearchBar.vue"
 import Image from '@/Components/Image.vue'
 import Popover from '@/Components/Popover.vue'
@@ -27,27 +25,9 @@ const props = defineProps<{
 
 const layout = inject('layout', layoutStructure)
 
-const layoutStore = useLayoutStore()
+// const layoutStore = useLayoutStore()
 const showSearchDialog = ref(false)
 
-const logoutAuth = () => {
-    router.post(route(props.urlPrefix + 'logout'))
-
-    const dataActiveUser = {
-        ...layoutStore.user,
-        name: null,
-        last_active: new Date(),
-        action: 'logout',
-        current_page: {
-            label: trans('Logout'),
-            url: null,
-            icon_left: null,
-            icon_right: null,
-        },
-    }
-    window.Echo.join(`grp.live.users`).whisper('otherIsNavigating', dataActiveUser)
-    useLiveUsers().unsubscribe()  // Unsubscribe from Laravel Echo
-}
 
 onMounted(() => {
     if (typeof window !== 'undefined') {
@@ -66,7 +46,7 @@ onUnmounted(() => {
 })
 
 const isUserMac = navigator.platform.includes('Mac')  // To check the user's Operating System
-const notifications = layoutStore.user.notifications
+const notifications = layout.user.notifications
 
 </script>
 
@@ -104,31 +84,11 @@ const notifications = layoutStore.user.notifications
                 </Popover>
             </div>
 
-            <!-- Button: Logout -->
-            <div class="relative">
-                <Popover width="w-full">
-                    <template #button>
-                      <div v-tooltip="trans('log out')" class="text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 mr-2">
-                        <FontAwesomeIcon icon='far fa-door-open'  fixed-width aria-hidden='true' size="lg" />
-                      </div>
-                    </template>
-
-                    <template #content="{ close: closed }">
-                        <div class="min-w-32 flex flex-col justify-center gap-y-2">
-                            <div class="whitespace-nowrap text-gray-500 text-xs">Are you sure want to logout?</div>
-                            <div class="mx-auto">
-                                <Button @click="logoutAuth()" label="Yes, Logout" type="negative" />
-                            </div>
-                        </div>
-                    </template>
-                </Popover>
-            </div>
-
-            <!-- Button: Logout -->
+            <!-- Button: Profile -->
             <div @click="layout.stackedComponents.push({ component: Profile})"
                 class="flex max-w-xs overflow-hidden items-center rounded-full bg-gray-100 text-sm focus:outline-none focus:ring-2 focus:ring-gray-500 cursor-pointer">
                 <span class="sr-only">{{ trans("Open user menu") }}</span>
-                <Image class="h-8 w-8 rounded-full" :src="layoutStore.user.avatar_thumbnail" alt="" />
+                <Image class="h-8 w-8 rounded-full" :src="layout.user.avatar_thumbnail" alt="" />
             </div>
         </div>
 
