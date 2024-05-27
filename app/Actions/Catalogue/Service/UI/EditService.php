48ab689c56abb36cc82ef32c880d6fa554fa2471
KirinZero0 <aryapermana02@gmail.com>
2024-05-27 14:00:02 +0800

    #329: Feat: Edit Service (unfinished)

---
+++ app/Actions/Catalogue/Service/UI/EditService.php
@@ -0,0 +1,205 @@
+<?php
+/*
+ * Author: Jonathan Lopez Sanchez <jonathan@ancientwisdom.biz>
+ * Created: Mon, 13 Mar 2023 15:09:31 Central European Standard Time, Malaga, Spain
+ * Copyright (c) 2023, Inikoo LTD
+ */
+
+ namespace App\Actions\Catalogue\Service\UI;
+
+use App\Actions\InertiaAction;
+use App\Actions\OrgAction;
+use App\Enums\Catalogue\Product\ProductTypeEnum;
+use App\Enums\UI\Catalogue\ProductTabsEnum;
+use App\Models\Catalogue\Product;
+use App\Models\Catalogue\Service;
+use App\Models\Catalogue\Shop;
+use App\Models\Fulfilment\Fulfilment;
+use App\Models\SysAdmin\Organisation;
+use Inertia\Inertia;
+use Inertia\Response;
+use Lorisleiva\Actions\ActionRequest;
+use Spatie\LaravelOptions\Options;
+
+class EditService extends OrgAction
+{
+    public function handle(Product $product): Product
+    {
+        return $product;
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        // dd($this->shop);
+        $this->canEdit   = $request->user()->hasPermissionTo("fulfilment-shop.{$this->fulfilment->id}.edit");
+            $this->canDelete = $request->user()->hasPermissionTo("fulfilment-shop.{$this->fulfilment->id}.edit");
+            return $request->user()->hasPermissionTo("fulfilment-shop.{$this->fulfilment->id}.view");
+    }
+
+    // public function inOrganisation(Product $product, ActionRequest $request): Product
+    // {
+    //     $this->initialisation($request);
+
+    //     return $this->handle($product);
+    // }
+
+    // /** @noinspection PhpUnusedParameterInspection */
+    // public function inShop(Shop $shop, Product $product, ActionRequest $request): Product
+    // {
+    //     $this->initialisation($request);
+
+    //     return $this->handle($product);
+    // }
+
+    public function inFulfilment(Organisation $organisation, Fulfilment $fulfilment, Service $service, ActionRequest $request): Product
+    {
+        $this->parent= $fulfilment;
+        $this->initialisationFromFulfilment($fulfilment, $request);
+        return $this->handle($service->product);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function htmlResponse(Product $product, ActionRequest $request): Response
+    {
+        return Inertia::render(
+            'EditModel',
+            [
+                'title'       => __('product'),
+                'breadcrumbs' => $this->getBreadcrumbs(
+                    $request->route()->getName(),
+                    $request->route()->originalParameters()
+                ),
+                'navigation'                            => [
+                    'previous' => $this->getPrevious($product, $request),
+                    'next'     => $this->getNext($product, $request),
+                ],
+                'pageHead'    => [
+                    'title'    => $product->code,
+                    'icon'     =>
+                        [
+                            'icon'  => ['fal', 'fa-cube'],
+                            'title' => __('product')
+                        ],
+                    'actions'  => [
+                        [
+                            'type'  => 'button',
+                            'style' => 'exitEdit',
+                            'route' => [
+                                'name'       => preg_replace('/edit$/', 'show', $request->route()->getName()),
+                                'parameters' => array_values($request->route()->originalParameters())
+                            ]
+                        ]
+                    ]
+                ],
+                'formData'    => [
+                    'blueprint' => [
+                        [
+                            'title'  => __('id'),
+                            'fields' => [
+                                'code' => [
+                                    'type'  => 'input',
+                                    'label' => __('code'),
+                                    'value' => $product->code
+                                ],
+                                'name' => [
+                                    'type'  => 'input',
+                                    'label' => __('label'),
+                                    'value' => $product->name
+                                ],
+                                'description' => [
+                                    'type'  => 'input',
+                                    'label' => __('description'),
+                                    'value' => $product->description
+                                ],
+                                'units' => [
+                                    'type'  => 'input',
+                                    'label' => __('units'),
+                                    'value' => $product->service->unit
+                                ],
+                                'price' => [
+                                    'type'    => 'input',
+                                    'label'   => __('price'),
+                                    'required'=> true,
+                                    'value'   => $product->service->price
+                                ],
+                                // 'type' => [
+                                //     'type'          => 'select',
+                                //     'label'         => __('type'),
+                                //     'placeholder'   => 'Select a Product Type',
+                                //     'options'       => Options::forEnum(ProductTypeEnum::class)->toArray(),
+                                //     'required'      => true,
+                                //     'mode'          => 'single',
+                                //     'value'         => $product->type
+                                // ]
+                            ]
+                        ]
+
+                    ],
+                    'args'      => [
+                        'updateRoute' => [
+                            'name'       => 'grp.models.product.update',
+                            'parameters' => $product->id
+
+                        ],
+                    ]
+                ]
+
+            ]
+        );
+    }
+
+    public function getBreadcrumbs(string $routeName, array $routeParameters): array
+    {
+        return ShowService::make()->getBreadcrumbs(
+            routeName: preg_replace('/edit$/', 'show', $routeName),
+            routeParameters: $routeParameters,
+            suffix: '('.__('editing').')'
+        );
+    }
+
+    public function getPrevious(Product $product, ActionRequest $request): ?array
+    {
+        $previous = Product::where('slug', '<', $product->slug)->orderBy('slug', 'desc')->first();
+        return $this->getNavigation($previous, $request->route()->getName());
+
+    }
+
+    public function getNext(Product $product, ActionRequest $request): ?array
+    {
+        $next = Product::where('slug', '>', $product->slug)->orderBy('slug')->first();
+        return $this->getNavigation($next, $request->route()->getName());
+    }
+
+    private function getNavigation(?Product $product, string $routeName): ?array
+    {
+        if(!$product) {
+            return null;
+        }
+        return match ($routeName) {
+            'shops.products.edit'=> [
+                'label'=> $product->name,
+                'route'=> [
+                    'name'      => $routeName,
+                    'parameters'=> [
+                        'product'=> $product->slug
+                    ]
+
+                ]
+            ],
+            'shops.show.products.edit'=> [
+                'label'=> $product->name,
+                'route'=> [
+                    'name'      => $routeName,
+                    'parameters'=> [
+                        'shop'   => $product->shop->slug,
+                        'product'=> $product->slug
+                    ]
+
+                ]
+            ],
+            default => null,
+        };
+    }
+}
