818d1fcf986589c4d8dadf4e64b06942e2c148d8
aqordeon <aqordivika@yahoo.co.id>
2024-05-29 11:48:40 +0800

    #249 Sysadmin: User roles and User permissions tab

--- resources/js/Components/Sysadmin/UserRoles.vue
+++ resources/js/Components/Sysadmin/UserRoles.vue
@@ -1,14 +1,27 @@
 <script setup lang='ts'>
+import Tag from '@/Components/Tag.vue'
 
 const props = defineProps<{
-    data: {
-
-    }
+    data: string[]
 }>()
 </script>
 
 <template>
     <div class="px-6 py-4">
-        {{ data }}
+        <div class="grid grid-cols-2">
+            <div>
+                <div class="font-semibold text-lg">
+                    Roles ({{ data.length }}):
+                </div>
+                <div class="flex gap-x-2.5 gap-y-2 flex-wrap">
+                    <Tag v-for="role in data" :label="role" size="base">
+                        <template #label>
+                            <div class="whitespace-nowrap">{{ role }}</div>
+                        </template>
+                    </Tag>
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
