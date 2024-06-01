69136425c8b36275ff2d660cf07f9684d4d78348
Raul Perusquia <raul@inikoo.com>
2024-06-01 11:00:44 +0200

    try to fix tests (3rd try)

--- devops/devel/reset_test_database.sh
+++ devops/devel/reset_test_database.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 PGPASSWORD=$4 psql -U "$3" -p "$2" -h localhost  -c "drop database $1 WITH (FORCE)"
 PGPASSWORD=$4 psql -U "$3" -p "$2" -h localhost  -c "create database $1"
 PGPASSWORD=$4 pg_restore --no-owner --no-acl --clean --if-exists -j 15 -U "$3" -p "$2" -c -d "$1" "$5"
