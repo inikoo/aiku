60132e4e8d3cd5867f11658db17056a10e1c6bc0
Raul Perusquia <raul@inikoo.com>
2024-06-05 15:42:35 +0200

    set php version in script 🍄

--- generate_testing_db_dumps.sh
+++ generate_testing_db_dumps.sh
@@ -12,13 +12,13 @@ DB_PORT="${1:-$DB_PORT}"
 DB_COLLATE="${2:-$DB_COLLATE}"
 
 echo -e "✨ Resetting elasticsearch"
-php artisan es:refresh --env=testing
+php8.3 artisan es:refresh --env=testing
 echo -e "✨ Resetting database ${ITALIC}${DB}${NONE}"
 dropdb --if-exists -p "${DB_PORT}" ${DB}
 createdb -p "${DB_PORT}" --template=template0 --lc-collate="${DB_COLLATE}" --lc-ctype="${DB_COLLATE}" ${DB}
 echo "🌱 Migrating and seeding database"
-php artisan --env=testing migrate
-php artisan --env=testing db:seed
+php8.3 artisan --env=testing migrate
+php8.3 artisan --env=testing db:seed
 echo -e "💾 Saving ${PURPLE}fresh_with_assets.dump${NONE}"
 pg_dump -Fc -p "${DB_PORT}" -f "tests/datasets/db_dumps/test_base_database.dump" ${DB}
 echo "Test DB dumped 👍"
