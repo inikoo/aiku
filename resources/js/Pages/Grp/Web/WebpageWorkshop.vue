bcf85c75d219ed4f08b63a584036965e0761d2ba
YudhistaA <aryarajasa0@gmail.com>
2024-06-05 15:47:12 +0800

    Update saved Workshop

--- resources/js/Pages/Grp/Web/WebpageWorkshop.vue
+++ resources/js/Pages/Grp/Web/WebpageWorkshop.vue
@@ -17,13 +17,43 @@ import Button from '@/Components/Elements/Buttons/Button.vue';
 import Modal from "@/Components/Utils/Modal.vue"
 import BlockList from './Block/BlockList.vue'
 import WowsbarBanner from './Block/WowsbarBanner.vue'
+import axios from 'axios'
 
 library.add(faCoins, faMoneyCheckAlt, faCashRegister, faFileInvoiceDollar, faTimes, faBrowser);
 
-defineProps(['title', 'pageHead', 'flatTreeMaps']);
+const props = defineProps<{
+    title: string,
+    pageHead: PageHeadingTypes,
+    webpageID : String,
+    websiteState : String,
+    webpageState : String,
+    isDirty : String,
+    pageCode : String,
+    imagesUploadRoute : Object,
+    publishRoute : Object,
+    setAsReadyRoute : Object,
+    updateRoute : Object,
+    loadRoute : Object
+    
+}>()
+
+console.log(props)
 
 const openModal = ref(false)
 const data = ref([])
+
+
+const onUpdated = async() =>{
+  console.log('masuk')
+  try {
+			await axios.patch(route(props.updateRoute.name, props.deleteRoute.parameters),data)
+			console.log('saved')
+
+		} catch (error: any) {
+			console.log('error',error)
+		}
+
+} 
 
 
 const getComponent = (componentName: string) => {
@@ -36,10 +66,13 @@ const getComponent = (componentName: string) => {
 const onPickBlock = (e) => {
   data.value.push(e)
   openModal.value = false
+  onUpdated()
 }
 
 const deleteBlock = (index) =>{
+  console.log('mmmm')
   data.value.splice(index,1)
+  onUpdated()
 }
 
 
@@ -57,11 +90,11 @@ const setData = ()=>{
   <PageHeading :data="pageHead"></PageHeading>
 
 
-  <div class="mx-auto px-4 py-16 sm:px-6 lg:px-8 w-full">
-    <div class="mx-auto grid grid-cols-4 gap-4 lg:mx-0 lg:max-w-none">
-      <div class="col-span-3">
+  <div class="mx-auto px-4 py-4 sm:px-6 lg:px-8 w-full h-screen" >
+    <div class="mx-auto grid grid-cols-4 gap-1 lg:mx-0 lg:max-w-none">
+      <div class="col-span-3 h-screen">
         <div v-if="data.length == 0"
-          class="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+          class="relative block w-full h-full  border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
           <font-awesome-icon :icon="['fal', 'browser']" class="mx-auto h-12 w-12 text-gray-400" />
           <span class="mt-2 block text-sm font-semibold text-gray-900">You dont have block</span>
         </div>
@@ -72,8 +105,8 @@ const setData = ()=>{
           </div>
       </div>
 
-      <div class="col-span-1">
-        <div class="rounded-lg border-2  bg-gray-200 p-3">
+      <div class="col-span-1  h-screen">
+        <div class=" border-2  bg-gray-200 p-3 h-full">
           <div class="flex justify-between">
             <h2 class="text-sm font-semibold leading-6 text-gray-900">Block List</h2>
             <Button label="Block" type="create" size="xs" @click="() => openModal = true" />
@@ -93,7 +126,7 @@ const setData = ()=>{
           </ul>
 
           <div v-else
-            class="relative mt-4 block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            class="relative mt-4 block  h-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
             <font-awesome-icon :icon="['fal', 'browser']" class="mx-auto h-12 w-12 text-gray-400" />
             <span class="mt-2 block text-sm font-semibold text-gray-900">You dont have block</span>
           </div>
