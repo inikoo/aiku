e6f8de8622dae39a6345750978a015b774c65c9e
Raul Perusquia <raul@inikoo.com>
2024-06-05 16:21:55 +0200

    devops testing config 🧪

--- generate_testing_db_dumps.sh
+++ generate_testing_db_dumps.sh
@@ -6,20 +6,20 @@ NONE='\033[00m'
 DB=aiku_test
 DB_PORT=5432
 DB_COLLATE=C.UTF-8
+PHP=php8.3
 
-
-DB_PORT="${1:-$DB_PORT}"
-DB_COLLATE="${2:-$DB_COLLATE}"
+PHP="${1:-PHP}"
+DB_PORT="${2:-$DB_PORT}"
+DB_COLLATE="${3:-$DB_COLLATE}"
 
 echo -e "✨ Resetting elasticsearch"
-php8.3 artisan es:refresh --env=testing
+${PHP} artisan es:refresh --env=testing
 echo -e "✨ Resetting database ${ITALIC}${DB}${NONE}"
 dropdb --if-exists -p "${DB_PORT}" ${DB}
 createdb -p "${DB_PORT}" --template=template0 --lc-collate="${DB_COLLATE}" --lc-ctype="${DB_COLLATE}" ${DB}
 echo "🌱 Migrating and seeding database"
-php8.3 artisan --env=testing migrate
-php8.3 artisan --env=testing db:seed
+${PHP} artisan --env=testing migrate
+${PHP} artisan --env=testing db:seed
 echo -e "💾 Saving ${PURPLE}fresh_with_assets.dump${NONE}"
 pg_dump -Fc -p "${DB_PORT}" -f "tests/datasets/db_dumps/test_base_database.dump" ${DB}
 echo "Test DB dumped 👍"
-
