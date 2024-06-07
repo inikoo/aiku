93394dc9a4096cfb45a2363be1f4bc632c4d0688
YudhistaA <aryarajasa0@gmail.com>
2024-06-07 10:52:39 +0800

    TestEditor tiptap

--- resources/js/Pages/Grp/Web/Block/TextContent.vue
+++ resources/js/Pages/Grp/Web/Block/TextContent.vue
@@ -30,6 +30,8 @@ const parsedHtml = computed(() => {
         <div :class="editMode ? '' : 'hidden'">
             <Editor v-model="modelValue.value">
             </Editor>
+            <!--             <TextEditor field-name="value" :form="modelValue"/>
+ -->
         </div>
 
         <div class="p-4  overflow-y-auto outline-none prose max-w-none">
@@ -44,25 +46,58 @@ const parsedHtml = computed(() => {
 /* Basic editor styles */
 
 blockquote {
-    padding-left: 1rem !important;
-    border-left: 3px solid rgba(#0D0D0D, 0.1) !important;
+    padding-left: 1rem;
+    border-left: 3px solid rgba(#0D0D0D, 0.1);
 }
+
 
 ul,
 ol {
-    padding: 0 1rem !important;
+    padding: 0 1rem;
 }
 
 ul {
-    list-style: disc !important
+    list-style: disc
 }
 
 ol {
-    list-style: decimal !important
+    list-style: decimal
+}
+
+h1 {
+    display: block;
+    font-size: 2em;
+    margin-block-start: 0.67em;
+    margin-block-end: 0.67em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+    unicode-bidi: isolate;
+}
+
+h2 {
+    display: block;
+    font-size: 1.5em;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+    unicode-bidi: isolate;
+}
+
+h3 {
+    display: block;
+    font-size: 1.17em;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+    unicode-bidi: isolate;
 }
 
 p:empty::after {
     content: "\00A0";
 }
-
 </style>
