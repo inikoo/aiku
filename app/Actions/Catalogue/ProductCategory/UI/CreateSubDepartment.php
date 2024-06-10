1019da3c48dd5080615f8d153b09c11f5de16904
KirinZero0 <aryapermana02@gmail.com>
2024-06-10 13:56:11 +0800

    #388: Feat: CReate and Edit

---
+++ app/Actions/Catalogue/ProductCategory/UI/CreateSubDepartment.php
@@ -0,0 +1,173 @@
+<?php
+/*
+ * Author: Jonathan Lopez Sanchez <jonathan@ancientwisdom.biz>
+ * Created: Tue, 14 Mar 2023 09:31:03 Central European Standard Time, Malaga, Spain
+ * Copyright (c) 2023, Inikoo LTD
+ */
+
+namespace App\Actions\Catalogue\ProductCategory\UI;
+
+use App\Actions\Catalogue\HasMarketAuthorisation;
+use App\Actions\OrgAction;
+use App\Enums\Catalogue\ProductCategory\ProductCategoryTypeEnum;
+use App\Models\Catalogue\ProductCategory;
+use App\Models\Catalogue\Shop;
+use App\Models\SysAdmin\Organisation;
+use Inertia\Inertia;
+use Inertia\Response;
+use Lorisleiva\Actions\ActionRequest;
+use Spatie\LaravelOptions\Options;
+
+class CreateSubDepartment extends OrgAction
+{
+    use HasMarketAuthorisation;
+    private Shop|ProductCategory $parent;
+
+    public function authorize(ActionRequest $request): bool
+    {
+        if ($this->parent instanceof Organisation) {
+            $this->canEdit = $request->user()->hasAnyPermission(
+                [
+                    'org-supervisor.'.$this->organisation->id,
+                ]
+            );
+
+            return $request->user()->hasAnyPermission(
+                [
+                    'org-supervisor.'.$this->organisation->id,
+                    'shops-view'.$this->organisation->id,
+                ]
+            );
+        } else {
+            $this->canEdit = $request->user()->hasPermissionTo("products.{$this->shop->id}.edit");
+            return $request->user()->hasPermissionTo("products.{$this->shop->id}.view");
+        }
+    }
+
+
+    /** @noinspection PhpUnusedParameterInspection */
+    public function inOrganisation(Organisation $organisation, ActionRequest $request): ActionRequest
+    {
+        $this->initialisation($organisation, $request);
+
+        return $request;
+    }
+
+    /** @noinspection PhpUnusedParameterInspection */
+    public function inShop(Organisation $organisation, Shop $shop, ActionRequest $request): ActionRequest
+    {
+        $this->initialisationFromShop($shop, $request);
+
+        return $request;
+    }
+
+    public function inDepartment(Organisation $organisation, Shop $shop, ProductCategory $department, ActionRequest $request): Response
+    {
+        $this->parent = $department;
+        $this->initialisationFromShop($shop, $request);
+        return $this->handle(parent: $department, request: $request);
+    }
+
+
+    public function handle(Shop|ProductCategory $parent, ActionRequest $request): Response
+    {
+        return Inertia::render(
+            'CreateModel',
+            [
+                'breadcrumbs' => $this->getBreadcrumbs(
+                    $request->route()->getName(),
+                    $request->route()->originalParameters()
+                ),
+                'title'       => __('New Sub Department'),
+                'pageHead'    => [
+                    'title'        => __('new sub department'),
+                    'actions'      => [
+                        [
+                            'type'  => 'button',
+                            'style' => 'cancel',
+                            'label' => __('cancel'),
+                            'route' => [
+                                'name'       => class_basename($this->parent) == 'ProductCategory'
+                                                 ? 'grp.org.shops.show.catalogue.departments.show.sub-departments.index'
+                                            : (class_basename($this->parent) == 'Shop'
+                                                ? 'grp.org.shops.show.catalogue.families.index'
+                                                : ''),
+                                'parameters' => array_values($request->route()->originalParameters())
+                            ],
+                        ]
+                    ]
+                ],
+                'formData'    => [
+                    'blueprint' =>
+                        [
+                            [
+                                'title'  => __('sub department'),
+                                'fields' => [
+                                    'type' => [
+                                        'type'     => 'select',
+                                        'label'    => __('type'),
+                                        'required' => true,
+                                        'options'  => Options::forEnum(ProductCategoryTypeEnum::class),
+                                        'value'    => ProductCategoryTypeEnum::SUB_DEPARTMENT->value,
+                                        'readonly' => true
+                                    ],
+                                    'code' => [
+                                        'type'     => 'input',
+                                        'label'    => __('code'),
+                                        'required' => true
+                                    ],
+                                    'name' => [
+                                        'type'     => 'input',
+                                        'label'    => __('name'),
+                                        'required' => true
+                                    ],
+                                    'description' => [
+                                        'type'     => 'textEditor',
+                                        'label'    => __('name'),
+                                        'required' => true
+                                    ],
+                                ]
+                            ]
+                        ],
+                    'route' => match ($request->route()->getName()) {
+                        'shops.show.families.create' => [
+                            'name'      => 'grp.models.shop.family.store',
+                            'arguments' => $this->shop->id
+                        ],
+                        'grp.org.shops.show.catalogue.departments.show.sub-departments.create' => [
+                            'name'       => 'grp.models.org.catalogue.departments.sub-department.store',
+                            'parameters' => [
+                                'organisation'    => $this->organisation->id,
+                                'shop'            => $this->shop->id,
+                                'productCategory' => $this->parent->id
+                            ]
+                        ],
+                        default => [
+                            'name' => 'grp.models.family.store'
+                        ]
+                    }
+                ]
+            ]
+        );
+    }
+
+
+
+    public function getBreadcrumbs(string $routeName, array $routeParameters): array
+    {
+        return array_merge(
+            IndexSubDepartment::make()->getBreadcrumbs(
+                routeName: preg_replace('/create$/', 'index', $routeName),
+                routeParameters: $routeParameters,
+            ),
+            [
+                [
+                    'type'         => 'creatingModel',
+                    'creatingModel'=> [
+                        'label'=> __('Creating sub department'),
+                    ]
+                ]
+            ]
+        );
+    }
+}
---
+++ app/Actions/Catalogue/ProductCategory/UI/EditSubDepartment.php
@@ -0,0 +1,170 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 27 Apr 2023 16:36:34 Malaysia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Catalogue\ProductCategory\UI;
+
+use App\Actions\OrgAction;
+use App\Models\Catalogue\ProductCategory;
+use App\Models\Catalogue\Shop;
+use App\Models\SysAdmin\Organisation;
+use Inertia\Inertia;
+use Inertia\Response;
+use Lorisleiva\Actions\ActionRequest;
+
+class EditSubDepartment extends OrgAction
+{
+    public function handle(ProductCategory $subDepartment): ProductCategory
+    {
+        return $subDepartment;
+    }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        if ($this->parent instanceof Organisation) {
+            $this->canEdit = $request->user()->hasAnyPermission(
+                [
+                    'org-supervisor.'.$this->organisation->id,
+                ]
+            );
+
+            return $request->user()->hasAnyPermission(
+                [
+                    'org-supervisor.'.$this->organisation->id,
+                    'shops-view'.$this->organisation->id,
+                ]
+            );
+        } else {
+            $this->canEdit = $request->user()->hasPermissionTo("products.{$this->shop->id}.edit");
+            return $request->user()->hasPermissionTo("products.{$this->shop->id}.view");
+        }
+    }
+
+    public function inOrganisation(Organisation $organisation, ProductCategory $subDepartment, ActionRequest $request): ProductCategory
+    {
+        $this->initialisation($organisation, $request);
+
+        return $this->handle($subDepartment);
+    }
+
+    /** @noinspection PhpUnusedParameterInspection */
+    public function inShop(Organisation $organisation, Shop $shop, ProductCategory $subDepartment, ActionRequest $request): ProductCategory
+    {
+        $this->initialisationFromShop($shop, $request);
+
+        return $this->handle($subDepartment);
+    }
+
+    public function htmlResponse(ProductCategory $subDepartment, ActionRequest $request): Response
+    {
+        return Inertia::render(
+            'EditModel',
+            [
+                'title'       => __('sub department'),
+                'breadcrumbs' => $this->getBreadcrumbs(
+                    $request->route()->getName(),
+                    $request->route()->originalParameters()
+                ),
+                'navigation'                            => [
+                    'previous' => $this->getPrevious($subDepartment, $request),
+                    'next'     => $this->getNext($subDepartment, $request),
+                ],
+                'pageHead'    => [
+                    'title'    => $subDepartment->code,
+                    'actions'  => [
+                        [
+                            'type'  => 'button',
+                            'style' => 'exitEdit',
+                            'route' => [
+                                'name'       => preg_replace('/edit$/', 'show', $request->route()->getName()),
+                                'parameters' => array_values($request->route()->originalParameters())
+                            ]
+                        ]
+                    ]
+                ],
+
+                'formData' => [
+                    'blueprint' => [
+                        [
+                            'title'  => __('id'),
+                            'fields' => [
+                                'code' => [
+                                    'type'  => 'input',
+                                    'label' => __('code'),
+                                    'value' => $subDepartment->code
+                                ],
+                                'name' => [
+                                    'type'  => 'input',
+                                    'label' => __('name'),
+                                    'value' => $subDepartment->name
+                                ],
+                            ]
+                        ]
+
+                    ],
+                    'args'      => [
+                        'updateRoute' => [
+                            'name'       => 'grp.models.department.update',
+                            'parameters' => $subDepartment->slug
+                        ],
+                    ]
+                ]
+            ]
+        );
+    }
+
+
+    public function getBreadcrumbs(string $routeName, array $routeParameters): array
+    {
+        return ShowFamily::make()->getBreadcrumbs(
+            routeName: preg_replace('/edit$/', 'show', $routeName),
+            routeParameters: $routeParameters,
+            suffix: '('.__('Editing').')'
+        );
+    }
+
+    public function getPrevious(ProductCategory $subDepartment, ActionRequest $request): ?array
+    {
+        $previous = ProductCategory::where('code', '<', $subDepartment->code)->orderBy('code', 'desc')->first();
+        return $this->getNavigation($previous, $request->route()->getName());
+
+    }
+
+    public function getNext(ProductCategory $subDepartment, ActionRequest $request): ?array
+    {
+        $next = ProductCategory::where('code', '>', $subDepartment->code)->orderBy('code')->first();
+        return $this->getNavigation($next, $request->route()->getName());
+    }
+
+    private function getNavigation(?ProductCategory $subDepartment, string $routeName): ?array
+    {
+        if (!$subDepartment) {
+            return null;
+        }
+        return match ($routeName) {
+            'shops.families.edit' => [
+                'label' => $subDepartment->name,
+                'route' => [
+                    'name'       => $routeName,
+                    'parameters' => [
+                        'department' => $subDepartment->slug
+                    ]
+                ]
+            ],
+            'shops.show.families.edit' => [
+                'label' => $subDepartment->name,
+                'route' => [
+                    'name'       => $routeName,
+                    'parameters' => [
+                        'shop'       => $subDepartment->shop->slug,
+                        'department' => $subDepartment->slug
+                    ]
+                ]
+            ],
+            default => []
+        };
+    }
+}
