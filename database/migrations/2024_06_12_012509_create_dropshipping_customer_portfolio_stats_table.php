d6f9b50de4771cfaaa00eb57463a7dd6d470f360
Raul Perusquia <raul@inikoo.com>
2024-06-12 20:32:30 +0800

    fix migration wrong integer size 🦎

--- database/migrations/2024_06_12_012509_create_dropshipping_customer_portfolio_stats_table.php
+++ database/migrations/2024_06_12_012509_create_dropshipping_customer_portfolio_stats_table.php
@@ -10,17 +10,14 @@ return new class () extends Migration {
         Schema::create('dropshipping_customer_portfolio_stats', function (Blueprint $table) {
             $table->id();
 
-            $table->unsignedSmallInteger('dropshipping_customer_portfolio_id')->index();
+            $table->unsignedInteger('dropshipping_customer_portfolio_id')->index();
             $table->foreign('dropshipping_customer_portfolio_id')->references('id')->on('dropshipping_customer_portfolios');
 
             $table->unsignedSmallInteger('amount')->default(0);
             $table->unsignedSmallInteger('number_orders')->default(0);
             $table->unsignedSmallInteger('number_ordered_quantity')->default(0);
             $table->unsignedSmallInteger('number_clients')->default(0);
-
-            $table->unsignedSmallInteger('removed_at')->nullable();
             $table->unsignedSmallInteger('last_ordered_at')->nullable();
-
             $table->timestampsTz();
         });
     }
