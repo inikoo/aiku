edd0a676f31c03fb738b71320cb23c07d1dc0837
aqordeon <aqordivika@yahoo.co.id>
2024-06-12 10:48:35 +0800

    #400 fix: Iris (Search): search result to page instead popup

--- routes/iris/root.php
+++ routes/iris/root.php
@@ -8,7 +8,6 @@
 
 use App\Actions\Fulfilment\FulfilmentCustomer\IndexFulfilmentCustomerFromWebhook;
 use App\Actions\UI\Iris\Appointment\ShowPublicAppointment;
-use Inertia\Inertia;
 use App\Actions\UI\Iris\ShowHome;
 use App\Actions\UI\Iris\ShowAbout;
 use App\Actions\UI\Iris\ShowContact;
@@ -19,6 +18,7 @@ use App\Actions\UI\Iris\ShowShipping;
 use App\Actions\UI\Iris\ShowStorage;
 use App\Actions\UI\Iris\ShowTnc;
 use App\Actions\UI\Iris\ShowWelcome;
+use App\Actions\UI\Iris\ShowSearchResult;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', ShowHome::class)->name('home');
@@ -32,6 +32,8 @@ Route::get('/storage', ShowStorage::class)->name('storage');
 Route::get('/tnc', ShowTnc::class)->name('tnc');
 Route::get('/welcome', ShowWelcome::class)->name('welcome');
 
+Route::get('/search', ShowSearchResult::class)->name('search');
+
 Route::get('webhooks/{fulfilmentCustomer:webhook_access_key}', IndexFulfilmentCustomerFromWebhook::class)->name('fulfilment-customer.webhook.show');
 
 Route::prefix("disclosure")
@@ -43,39 +45,3 @@ Route::get('/appointment', ShowPublicAppointment::class)->name('.appointment');
 Route::prefix("crm")
     ->name("crm.")
     ->group(__DIR__."/crm.php");
-
-
-/*
-
-Route::get('/contact', function () {
-    return Inertia::render('Contact');
-})->name('.contact');
-
-Route::get('/about', function () {
-    return Inertia::render('About');
-})->name('.about');
-
-Route::get('/tnc', function () {
-    return Inertia::render('Tnc');
-})->name('.tnc');
-
-Route::get('/storage', function () {
-    return Inertia::render('Storage');
-})->name('.storage');
-
-Route::get('/pick_pack', function () {
-    return Inertia::render('Pick_pack');
-})->name('.pick_pack');
-
-Route::get('/rework', function () {
-    return Inertia::render('Rework');
-})->name('.rework');
-
-Route::get('/pricing', function () {
-    return Inertia::render('Pricing');
-})->name('.pricing');
-
-Route::get('/shipping', function () {
-    return Inertia::render('Shipping');
-})->name('.shipping');
-*/
