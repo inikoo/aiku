5074b802edfa3cfe6d5e11b9d6e2725e962c6e8f
KirinZero0 <aryapermana02@gmail.com>
2024-06-13 17:17:35 +0800

    6th test FulfilmentUITest EditFulfilmentCustomer

--- tests/Feature/FulfilmentUITest.php
+++ tests/Feature/FulfilmentUITest.php
@@ -5,12 +5,19 @@
  * Copyright (c) 2024, Raul A Perusquia Flores
  */
 
+use App\Actions\Catalogue\Shop\StoreShop;
+use App\Actions\Catalogue\Shop\UpdateShop;
 use App\Actions\Fulfilment\Fulfilment\UI\IndexFulfilmentAssets;
+use App\Actions\Fulfilment\FulfilmentCustomer\StoreFulfilmentCustomer;
 use App\Actions\Inventory\Location\StoreLocation;
+use App\Enums\Catalogue\Shop\ShopStateEnum;
+use App\Enums\Catalogue\Shop\ShopTypeEnum;
 use App\Enums\UI\Fulfilment\FulfilmentAssetsTabsEnum;
 use App\Enums\UI\Fulfilment\PhysicalGoodsTabsEnum;
 use App\Enums\UI\Fulfilment\RentalsTabsEnum;
 use App\Enums\UI\Fulfilment\ServicesTabsEnum;
+use App\Models\Catalogue\Shop;
+use App\Models\Fulfilment\FulfilmentCustomer;
 use App\Models\Inventory\Location;
 use Illuminate\Http\Request;
 use Inertia\Testing\AssertableInertia;
@@ -40,6 +47,22 @@ beforeEach(function () {
         );
     }
 
+    $shop = Shop::first();
+    if (!$shop) {
+        $storeData = Shop::factory()->definition();
+        data_set($storeData, 'type', ShopTypeEnum::FULFILMENT);
+        data_set($storeData,'warehouses',[$this->warehouse->id]);
+
+        $shop = StoreShop::make()->action(
+            $this->organisation,
+            $storeData
+        );
+    }
+    $this->shop = $shop;
+
+    $this->shop = UpdateShop::make()->action($this->shop, ['state' => ShopStateEnum::OPEN]);
+
+    $this->customer = createCustomer($this->shop);
 
     Config::set(
         'inertia.testing.page_paths',
@@ -112,3 +135,21 @@ test('UI create fulfilment customer', function () {
             ->has('title')->has('formData')->has('pageHead')->has('breadcrumbs', 4);
     });
 });
+
+test('UI edit fulfilment customer', function () {
+    $response = get(route('grp.org.fulfilments.show.crm.customers.show.edit', [$this->organisation->slug, $this->fulfilment->slug, $this->customer->slug]));
+    $response->assertInertia(function (AssertableInertia $page) {
+        $page
+            ->component('EditModel')
+            ->has('title')
+            ->has('formData.blueprint.0.fields', 4)
+            ->has('pageHead')
+            ->has('formData.args.updateRoute', fn (AssertableInertia $page) => $page
+                        ->where('name', 'grp.models.fulfilment-customer.update')
+                        ->where('parameters', [$this->customer->fulfilmentCustomer->id])
+                    )
+            ->has('breadcrumbs', 3);
+    });
+});
+
+
