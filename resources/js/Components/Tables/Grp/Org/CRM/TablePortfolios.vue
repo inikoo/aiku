c3615cf8dff4de94db3cd2167241a99e058405a1
aqordeon <aqordivika@yahoo.co.id>
2024-06-14 10:33:13 +0800

    #395 feat: Portfolio: delete product action

--- resources/js/Components/Tables/Grp/Org/CRM/TablePortfolios.vue
+++ resources/js/Components/Tables/Grp/Org/CRM/TablePortfolios.vue
@@ -5,52 +5,36 @@
   -->
 
 <script setup lang="ts">
-import { Link } from "@inertiajs/vue3";
-import Table from "@/Components/Table/Table.vue";
-import { FulfilmentCustomer } from "@/types/Customer";
-import AddressLocation from "@/Components/Elements/Info/AddressLocation.vue";
-import { useFormatTime } from "@/Composables/useFormatTime";
-import { useLocaleStore } from "@/Stores/locale";
+import { Link, router } from "@inertiajs/vue3"
+import Table from "@/Components/Table/Table.vue"
+import { FulfilmentCustomer } from "@/types/Customer"
+import AddressLocation from "@/Components/Elements/Info/AddressLocation.vue"
+import { useFormatTime } from "@/Composables/useFormatTime"
+import { useLocaleStore } from "@/Stores/locale"
+import Button from "@/Components/Elements/Buttons/Button.vue"
+import { ref } from "vue"
+import { routeType } from "@/types/route"
+import { notify } from "@kyvg/vue3-notification"
+
 
 const props = defineProps<{
-    data: object,
+    data: {}
     tab?: string
-}>();
+}>()
 
-const locale = useLocaleStore();
+const locale = useLocaleStore()
 
-
-function customerRoute(customer: FulfilmentCustomer) {
-    switch (route().current()) {
-        case "grp.org.shops.show.crm.customers.show.customer-clients.index":
-            return route(
-                "grp.org.shops.show.crm.customers.show.customer-clients.show",
-                [route().params["organisation"],route().params["shop"],route().params["customer"],customer.slug]);
-        case "grp.fulfilment.customers.index":
-            return route(
-                "grp.fulfilment.customers.show",
-                [customer.slug]);
-        default:
-            return route(
-                "grp.org.shops.show.crm.customers.show",
-                [
-                    route().params["organisation"],
-                    route().params["shop"],
-                    customer.slug
-                ]);
-    }
-}
 
 function shopRoute(customer: FulfilmentCustomer) {
     switch (route().current()) {
         case "shops.show.customers.index":
             return route(
                 "shops.show",
-                [customer.shop_slug]);
+                [customer.shop_slug])
         default:
             return route(
                 "shops.show",
-                [customer.shop_slug]);
+                [customer.shop_slug])
     }
 }
 
@@ -59,35 +43,71 @@ function dropshipRoute(customer: FulfilmentCustomer) {
         case "grp.org.shops.show.crm.customers.show.portfolios.index":
             return route(
                 "grp.org.shops.show.catalogue.products.show",
-                [route().params['organisation'], route().params['shop'], customer.slug]);
+                [route().params['organisation'], route().params['shop'], customer.slug])
         default:
             return route(
                 "shops.show",
-                [customer.shop_slug]);
+                [customer.shop_slug])
     }
 }
+
+const isDeleteLoading = ref<boolean | string>(false)
+const onDeletePortfolio = async (routeDelete: routeType, custSlug: string) => {
+    isDeleteLoading.value = custSlug
+    try {
+        router[routeDelete.method || 'get'](route(routeDelete.name, routeDelete.parameters))
+        notify({
+            title: 'Success',
+            text: `Portfolio ${custSlug} has been deleted`,
+            type: 'success',
+        })
+    } catch {
+        notify({
+            title: 'Something went wrong.',
+            // text: 'Portfolio has been deleted',
+            type: 'error',
+        })
+    } finally {
+        isDeleteLoading.value = false
+    }
+
+}
+
 </script>
 
 <template>
+    <!-- <pre>{{ data.data[0] }}</pre> -->
     <Table :resource="data" :name="tab" class="mt-5">
         <template #cell(product_code)="{ item: customer }">
             <Link :href="dropshipRoute(customer)" class="primaryLink">
-                {{ customer["product_code"] }}
+            {{ customer["product_code"] }}
             </Link>
         </template>
+
+        <!-- Column: Product -->
         <template #cell(shop)="{ item: customer }" class="primaryLink">
             <Link :href="shopRoute(customer)">
-                {{ customer["shop"] }}
+            {{ customer["shop"] }}
             </Link>
         </template>
+
+        <!-- Column: Customer Reference -->
         <template #cell(location)="{ item: customer }">
             <AddressLocation :data="customer['location']" />
         </template>
+
+        <!-- Column: Created At -->
         <template #cell(created_at)="{ item: customer }">
-            <div class="text-gray-500">{{ useFormatTime(customer["created_at"], { localeCode: locale.language.code, formatTime: "Ymd" }) }}</div>
+            <div class="text-gray-500">{{ useFormatTime(customer["created_at"], {
+                localeCode: locale.language.code,
+                formatTime: "Ymd" }) }}</div>
         </template>
-        <!-- TODO: #395 pls add trashcan and modal to add product -->
+
+        <!-- Column: Action (delete) -->
+        <template #cell(action)="{ item: customer }">
+            <Button @click="() => onDeletePortfolio(customer.routes.delete_route, customer.slug)" :key="customer.slug"
+                icon="fal fa-trash-alt" type="negative" :disabled="isDeleteLoading === customer.slug"
+                :loading="isDeleteLoading === customer.slug" />
+        </template>
     </Table>
 </template>
-
-
