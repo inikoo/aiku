f09c34af19856dd75ed86c98f2c8242a0126a1d5
YudhistaA <aryarajasa0@gmail.com>
2024-06-26 17:01:09 +0800

    Menu Workshop

---
+++ resources/js/Components/CMS/Workshops/Menu/EditMode.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import { ref, onMounted, inject } from 'vue'
+import draggable from "vuedraggable";
+import Button from '@/Components/Elements/Buttons/Button.vue';
+import Popover from '@/Components/Popover.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faChevronRight, faSignOutAlt, faShoppingCart, faSearch, faChevronDown, faTimes, faPlusCircle, faBars } from '@fas';
+import { faHeart } from '@far';
+import PureInput from '@/Components/Pure/PureInput.vue';
+import PureMultiselect from '@/Components/Pure/PureMultiselect.vue';
+import { v4 as uuidv4 } from "uuid"
+
+library.add(faChevronRight, faSignOutAlt, faShoppingCart, faHeart, faSearch, faChevronDown, faTimes, faPlusCircle, faBars);
+
+const props = defineProps<{
+    Navigation: Array,
+    selectedNav : Number | null
+}>()
+
+
+const addLink = (data) => {
+  data.links.push(
+    { label: "New Link", link: "", id: uuidv4() }
+  )
+}
+
+const deleteLink = (data, index) => {
+  data.splice(index, 1)
+}
+
+
+
+</script>
+
+<template>
+      <div class="bg-slate-100 h-screen overflow-auto">
+        <div class="grid grid-flow-row-dense grid-cols-4 gap-4 px-4 pt-4 bg-slate-10">
+          <div v-if="selectedNav" class="bg-white py-2 rounded-lg m-1 col-span-1 px-2 cursor-grab">
+            <div class="font-bold text-xs mb-3">Navigation Title :</div>
+            <PureInput v-model="Navigation[selectedNav].label"></PureInput>
+          </div>
+
+          <div v-if="selectedNav" class="bg-white py-2 rounded-lg m-1 col-span-1 px-2 cursor-grab">
+            <div class="font-bold text-xs mb-3">Type :</div>
+            <PureMultiselect :required="true" v-model="Navigation[selectedNav].type" label="label" value-prop="value"
+              :options="[{ label: 'Single', value: 'single' }, { label: 'Multiple', value: 'multiple' }]">
+            </PureMultiselect>
+          </div>
+
+
+          <div v-if="selectedNav && Navigation[selectedNav].type == 'single'"
+            class="bg-white py-2 rounded-lg m-1 col-span-1 px-2 cursor-grab">
+            <div class="font-bold text-xs mb-3">link :</div>
+            <PureInput v-model="Navigation[selectedNav].label"></PureInput>
+          </div>
+        </div>
+
+
+        <draggable v-if="selectedNav" :list="Navigation[selectedNav].subnavs" ghost-class="ghost" group="subnav"
+          itemKey="id" class="grid grid-flow-row-dense grid-cols-4 gap-4 p-4 bg-slate-10">
+          <template #item="{ element, index }">
+            <div class="bg-white h-[26rem] rounded-lg p-4 col-span-1 cursor-grab">
+              <div class="flex justify-between">
+                <div class="font-bold text-xs mb-3">{{ element.title }}</div>
+                <div v-if="element.links.length < 8"><font-awesome-icon icon="fas fa-plus-circle"
+                    @click="() => addLink(element)" class="cursor-pointer text-gray-400 mb-3"></font-awesome-icon></div>
+              </div>
+
+              <draggable :list="element.links" ghost-class="ghost" group="link" itemKey="id"
+                class="flex flex-col gap-y-2 p-3 relative">
+                <template #item="{ element: link, index: linkIndex }">
+
+                  <div class="flex items-center gap-x-2 p-1">
+                    <font-awesome-icon icon="fas fa-bars" class="text-[13px] text-gray-400 pr-2"></font-awesome-icon>
+                    <Popover position="">
+                      <template #button>
+                        <span class="text-gray-500 hover:text-gray-600 hover:underline cursor-pointer text-xs">{{
+                          link.label }}</span>
+                      </template>
+
+                      <template #content="{ close: closed }">
+                        <div class="p-1 my-1">
+                          <div class="font-bold text-xs mb-1">Label :</div>
+                          <PureInput v-model="link.label"></PureInput>
+                        </div>
+
+                        <div class="p-1 mb-1">
+                          <div class="font-bold text-xs mb-1">Link :</div>
+                          <PureInput v-model="link.link"></PureInput>
+                        </div>
+
+                        <div class="p-1 mb-1">
+                          <Button type="delete" label="" size="xs"
+                            @click="() => deleteLink(element.links, linkIndex)"></Button>
+                        </div>
+
+                      </template>
+                    </Popover>
+                  </div>
+
+
+                </template>
+              </draggable>
+            </div>
+          </template>
+        </draggable>
+
+      </div>
+</template>
+
+
+<style scss></style>
--- resources/js/Components/CMS/Workshops/Menu/MenuWorkshop.vue
+++ resources/js/Components/CMS/Workshops/Menu/MenuWorkshop.vue
@@ -6,59 +6,76 @@ import Button from '@/Components/Elements/Buttons/Button.vue';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faChevronRight, faSignOutAlt, faShoppingCart, faSearch, faChevronDown, faTimes } from '@fas';
+import { faChevronRight, faSignOutAlt, faShoppingCart, faSearch, faChevronDown, faTimes, faPlusCircle, faBars } from '@fas';
 import { faHeart } from '@far';
+import { v4 as uuidv4 } from "uuid"
+import { Switch } from '@headlessui/vue'
+import EditMode from './EditMode.vue';
+import PreviewMode from './PreviewMode.vue';
 
-library.add(faChevronRight, faSignOutAlt, faShoppingCart, faHeart, faSearch, faChevronDown, faTimes);
+library.add(faChevronRight, faSignOutAlt, faShoppingCart, faHeart, faSearch, faChevronDown, faTimes, faPlusCircle, faBars);
 
 const Navigation = ref(navigation)
 const selectedNav = ref(null)
+const previewMode = ref(false)
 
+const addNavigation = () => {
+  Navigation.value.push({
+    label: "New Navigation",
+    id: uuidv4(),
+    type: 'single'
+  })
+}
 
+const deleteNavigation = (index) => {
+  selectedNav.value = null
+  Navigation.value.splice(index, 1)
+}
 
 </script>
 
 <template>
   <div class="grid grid-flow-row-dense grid-cols-4">
-    <div class="col-span-1 h-screen bg-slate-200 px-3 py-2">
+    <div class="col-span-1 h-screen bg-slate-200 px-3 py-2 relative">
       <div class="flex justify-between">
-        <div class="font-bold text-sm">Navigations : </div>
-        <Button type="create" label="Add Navigation" size="xs"></Button>
+        <div class="font-bold text-sm">Navigations:</div>
+        <Button type="create" label="Add Navigation" size="xs" @click="addNavigation"></Button>
       </div>
       <draggable :list="Navigation" ghost-class="ghost" group="column" itemKey="id" class="mt-2 space-y-1">
         <template #item="{ element, index }">
-          <div @click="selectedNav = element"
+          <div @click="selectedNav = index"
             class="flex-auto rounded-md p-3 ring-1 ring-inset ring-gray-200 bg-white cursor-grab">
             <div class="flex justify-between gap-x-4">
               <div class="py-0.5 text-xs leading-5 text-gray-500">
                 <span class="font-medium text-gray-900">{{ element.label }}</span>
               </div>
               <div class="flex-none py-0 text-xs leading-5 text-gray-500 cursor-pointer">
-                <font-awesome-icon :icon="['fal', 'times']" />
+                <font-awesome-icon :icon="['fal', 'times']" @click="() => deleteNavigation(index)" />
               </div>
             </div>
           </div>
         </template>
       </draggable>
-    </div>
-    <div class="col-span-3">
-      <div>
-        <draggable v-if="selectedNav" :list="selectedNav.subnavs" ghost-class="ghost" group="subnav" itemKey="id"
-          class="grid grid-flow-row-dense grid-cols-4 bg-slate-100 h-screen gap-4 p-4">
-          <template #item="{ element, index }">
-            <div class="bg-white h-[26rem] rounded-lg p-4 col-span-1 cursor-grab">
-              <div class="font-bold text-xs mb-3">{{ element.title }}</div>
-              <div v-for="link in element.links" class="flex flex-col gap-y-2 p-3">
-                <div class="flex items-center gap-x-2">
-                  <font-awesome-icon icon="fas fa-chevron-right" class="text-[10px] text-gray-400"></font-awesome-icon>
-                  <span class="text-gray-500 hover:text-gray-600 hover:underline cursor-pointer text-xs">{{ link}}</span>
-                </div>
-              </div>
-            </div>
-          </template>
-        </draggable>
 
+      <!-- New bottom div with red background and absolute positioning -->
+      <div class="absolute inset-x-0 bottom-0 bg-gray-300 p-4 text-white text-center">
+        <div class="flex items-center gap-x-2">
+          <Switch @click="previewMode = !previewMode"
+            class="pr-1 relative inline-flex h-6 w-12 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors bg-white ring-1 ring-slate-300 duration-200 shadow ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+            <span aria-hidden="true" :class="previewMode ? 'translate-x-6 bg-indigo-500' : 'translate-x-0 bg-slate-300'"
+              class="pointer-events-none inline-block h-full w-1/2 transform rounded-full  shadow-lg ring-0 transition duration-200 ease-in-out" />
+          </Switch>
+          <div class="text-xs leading-none font-medium cursor-pointer select-none"
+            :class="previewMode ? 'text-indigo-500' : ' text-gray-400'">
+            Preview Mode
+          </div>
+        </div>
       </div>
+    </div>
+
+    <div class="col-span-3">
+      <EditMode v-if="!previewMode" :Navigation="Navigation" :selectedNav="selectedNav" ></EditMode>
+      <PreviewMode v-if="previewMode"></PreviewMode>
     </div>
   </div>
 
