35b36d3f6bee9d60c7b7a15b4b40c44642674ae7
KirinZero0 <aryapermana02@gmail.com>
2024-06-27 15:21:17 +0800

    1st CRMUITest Index Customers

---
+++ tests/Feature/CRMUITest.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ * Author: Arya Permana <aryapermana02@gmail.com>
+ * Created: Thu, 13 Jun 2024 13:27:40 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2024, Raul A Perusquia Flores
+ */
+
+use App\Actions\Catalogue\Shop\StoreShop;
+use App\Actions\Catalogue\Shop\UpdateShop;
+use App\Actions\CRM\Customer\StoreCustomer;
+use App\Actions\Inventory\Location\StoreLocation;
+use App\Enums\Catalogue\Shop\ShopStateEnum;
+use App\Enums\Catalogue\Shop\ShopTypeEnum;
+use App\Models\Catalogue\Shop;
+use App\Models\CRM\Customer;
+use App\Models\Inventory\Location;
+use Inertia\Testing\AssertableInertia;
+
+ use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+beforeAll(function () {
+    loadDB();
+});
+
+beforeEach(function () {
+
+    $this->organisation = createOrganisation();
+    $this->adminGuest   = createAdminGuest($this->organisation->group);
+    $this->warehouse    = createWarehouse();
+    $location           = $this->warehouse->locations()->first();
+    if (!$location) {
+        StoreLocation::run(
+            $this->warehouse,
+            Location::factory()->definition()
+        );
+        StoreLocation::run(
+            $this->warehouse,
+            Location::factory()->definition()
+        );
+    }
+
+    $shop = Shop::first();
+    if (!$shop) {
+        $storeData = Shop::factory()->definition();
+        data_set($storeData, 'type', ShopTypeEnum::B2B);
+        data_set($storeData, 'warehouses', [$this->warehouse->id]);
+
+        $shop = StoreShop::make()->action(
+            $this->organisation,
+            $storeData
+        );
+    }
+    $this->shop = $shop;
+
+    $this->shop = UpdateShop::make()->action($this->shop, ['state' => ShopStateEnum::OPEN]);
+
+    $customer = Customer::first();
+    if (!$customer) {
+        $storeData = Customer::factory()->definition();
+
+        $customer = StoreCustomer::make()->action(
+            $this->shop,
+            $storeData
+        );
+    }
+    $this->customer = $customer;
+
+    Config::set(
+        'inertia.testing.page_paths',
+        [resource_path('js/Pages/Grp')]
+    );
+    actingAs($this->adminGuest->user);
+});
+
+test('UI Index customers', function () {
+    $response = $this->get(route('grp.org.shops.show.crm.customers.index', [$this->organisation->slug, $this->shop->slug]));
+
+    $response->assertInertia(function (AssertableInertia $page) {
+        $page
+            ->component('Org/Shop/CRM/Customers')
+            ->has('title')
+            ->has('breadcrumbs', 3)
+            ->has('pageHead')
+            ->has(
+                'pageHead',
+                fn (AssertableInertia $page) => $page
+                        ->where('title', 'customers')
+                        ->etc()
+            )
+            ->has('data');
+    });
+});
