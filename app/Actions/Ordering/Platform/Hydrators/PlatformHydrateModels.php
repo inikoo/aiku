9435c34b7c46b814f0e26188e12ae83968643e04
Artha <manager.arthachan@gmail.com>
2024-06-28 10:50:25 +0800

    fix: platform hydrate models

--- app/Actions/Ordering/Platform/Hydrators/PlatformHydrateModels.php
+++ app/Actions/Ordering/Platform/Hydrators/PlatformHydrateModels.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2024, Raul A Perusquia Flores
  */
 
-namespace App\Actions\Platforming\Platform\Hydrators;
+namespace App\Actions\Ordering\Platform\Hydrators;
 
 use App\Actions\Traits\WithEnumStats;
 
