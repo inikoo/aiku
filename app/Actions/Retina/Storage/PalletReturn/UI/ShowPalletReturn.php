1288a198f9d531834c6614c3ffc5315468f1e136
Artha <manager.arthachan@gmail.com>
2024-07-01 14:31:31 +0800

    pallet stored item retina

--- app/Actions/Retina/Storage/PalletReturn/UI/ShowPalletReturn.php
+++ app/Actions/Retina/Storage/PalletReturn/UI/ShowPalletReturn.php
@@ -10,8 +10,6 @@ namespace App\Actions\Retina\Storage\PalletReturn\UI;
 use App\Actions\Fulfilment\PalletReturn\UI\IndexPhysicalGoodInPalletReturn;
 use App\Actions\Fulfilment\PalletReturn\UI\IndexServiceInPalletReturn;
 use App\Actions\Retina\Storage\Pallet\UI\IndexPallets;
-use App\Actions\Retina\Storage\StoredItems\UI\IndexStoredItems;
-use App\Actions\Retina\Storage\StoredItems\UI\IndexStoredItemsInPalletDelivery;
 use App\Actions\RetinaAction;
 use App\Actions\UI\Retina\Storage\UI\ShowStorageDashboard;
 use App\Enums\Fulfilment\PalletReturn\PalletReturnStateEnum;
@@ -21,7 +19,6 @@ use App\Http\Resources\Fulfilment\PalletReturnResource;
 use App\Http\Resources\Fulfilment\PalletReturnsResource;
 use App\Http\Resources\Fulfilment\PalletsResource;
 use App\Http\Resources\Fulfilment\PhysicalGoodsResource;
-use App\Http\Resources\Fulfilment\StoredItemResource;
 use App\Models\Fulfilment\FulfilmentCustomer;
 use App\Models\Fulfilment\PalletReturn;
 use Inertia\Inertia;
@@ -170,10 +167,6 @@ class ShowPalletReturn extends RetinaAction
                     fn () => PalletsResource::collection(IndexPallets::run($palletReturn))
                     : Inertia::lazy(fn () => PalletsResource::collection(IndexPallets::run($palletReturn))),
 
-                PalletReturnTabsEnum::STORED_ITEMS->value => $this->tab == PalletReturnTabsEnum::STORED_ITEMS->value ?
-                    fn () => StoredItemResource::collection(IndexStoredItemsInPalletDelivery::run($palletReturn))
-                    : Inertia::lazy(fn () => StoredItemResource::collection(IndexStoredItemsInPalletDelivery::run($palletReturn))),
-
                 PalletReturnTabsEnum::SERVICES->value => $this->tab == PalletReturnTabsEnum::SERVICES->value ?
                     fn () => ServicesResource::collection(IndexServiceInPalletReturn::run($palletReturn))
                     : Inertia::lazy(fn () => ServicesResource::collection(IndexServiceInPalletReturn::run($palletReturn))),
@@ -186,11 +179,6 @@ class ShowPalletReturn extends RetinaAction
             IndexPallets::make()->tableStructure(
                 $palletReturn,
                 prefix: PalletReturnTabsEnum::PALLETS->value
-            )
-        )->table(
-            IndexStoredItems::make()->tableStructure(
-                $palletReturn,
-                prefix: PalletReturnTabsEnum::STORED_ITEMS->value
             )
         )->table(
             IndexServiceInPalletReturn::make()->tableStructure(
