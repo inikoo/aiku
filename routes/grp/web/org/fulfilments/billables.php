8677bf97b32527f4718c451185841613ef0e1e8e
KirinZero0 <aryapermana02@gmail.com>
2024-07-08 16:15:59 +0800

    #494: Feat: Specific Actions for Delivery and Return

--- routes/grp/web/org/fulfilments/billables.php
+++ routes/grp/web/org/fulfilments/billables.php
@@ -16,6 +16,10 @@ use App\Actions\Fulfilment\Fulfilment\UI\IndexFulfilmentAssets;
 use App\Actions\Fulfilment\Fulfilment\UI\IndexFulfilmentPhysicalGoods;
 use App\Actions\Fulfilment\Fulfilment\UI\IndexFulfilmentRentals;
 use App\Actions\Fulfilment\Fulfilment\UI\IndexFulfilmentServices;
+use App\Actions\Fulfilment\PalletDelivery\GetDeliveryPhysicalGoods;
+use App\Actions\Fulfilment\PalletDelivery\GetDeliveryServices;
+use App\Actions\Fulfilment\PalletReturn\GetReturnPhysicalGoods;
+use App\Actions\Fulfilment\PalletReturn\GetReturnServices;
 use App\Actions\Fulfilment\Rental\UI\CreateRental;
 use App\Actions\Fulfilment\Rental\UI\EditRental;
 use App\Actions\Fulfilment\Rental\UI\ShowRental;
@@ -29,7 +33,8 @@ Route::get('rentals/create', CreateRental::class)->name('rentals.create');
 Route::get('rentals/{rental}', [ShowRental::class, 'inFulfilment'])->name('rentals.show');
 Route::get('rentals/{rental}/edit', [EditRental::class, 'inFulfilment'])->name('rentals.edit');
 
-
+Route::get('pallet-delivery/{palletDelivery}/services', GetDeliveryServices::class)->name('pallet-delivery.services.index');
+Route::get('pallet-return/{palletDelivery}/services', GetReturnServices::class)->name('pallet-return.services.index');
 Route::get('services', IndexFulfilmentServices::class)->name('services.index');
 Route::get('services/create', CreateService::class)->name('services.create');
 Route::get('services/{service}', [ShowService::class, 'inFulfilment'])->name('services.show');
@@ -37,7 +42,8 @@ Route::get('services/{service}/edit', [EditService::class, 'inFulfilment'])->nam
 
 
 
-
+Route::get('pallet-delivery/{palletDelivery}/physical-goods', GetDeliveryPhysicalGoods::class)->name('pallet-delivery.physical-goods.index');
+Route::get('pallet-return/{palletDelivery}/physical-goods', GetReturnPhysicalGoods::class)->name('pallet-return.physical-goods.index');
 Route::get('physical-goods', IndexFulfilmentPhysicalGoods::class)->name('outers.index');
 Route::get('physical-goods/create', CreatePhysicalGoods::class)->name('outers.create');
 Route::get('physical-goods/{product}', [ShowPhysicalGoods::class, 'inFulfilment'])->name('outers.show');
