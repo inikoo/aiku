688d5172c179df6b8aed2d5b1b4765d659aad614
KirinZero0 <aryapermana02@gmail.com>
2024-07-10 14:30:12 +0800

    Pass to vika

---
+++ resources/js/Pages/Retina/Billing/RetinaRecurringBills.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3'
+import PageHeading from '@/Components/Headings/PageHeading.vue'
+
+import { capitalize } from "@/Composables/capitalize"
+import { ref } from 'vue'
+
+import { PageHeading as TSPageHeading } from '@/types/PageHeading'
+
+// import FileShowcase from '@/xxxxxxxxxxxx'
+
+const props = defineProps<{
+    title: string,
+    pageHead: TSPageHeading
+    data: {}
+    
+}>()
+
+
+
+</script>
+
+
+<template>
+    <Head :title="capitalize(title)" />
+    <PageHeading :data="pageHead" />
+    {{ data }}
+xx
+</template>
