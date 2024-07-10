0ea19ea373790a2a381369e6f4f2ba0e17fe57be
KirinZero0 <aryapermana02@gmail.com>
2024-07-10 15:23:12 +0800

    #511: Feat: Index and Show Recurring Bill in Retina

--- resources/js/Pages/Retina/Billing/RetinaRecurringBills.vue
+++ resources/js/Pages/Retina/Billing/RetinaRecurringBills.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { Head } from '@inertiajs/vue3'
+import { Head, Link } from '@inertiajs/vue3'
 import PageHeading from '@/Components/Headings/PageHeading.vue'
 
 import { capitalize } from "@/Composables/capitalize"
 import { ref } from 'vue'
 
 import { PageHeading as TSPageHeading } from '@/types/PageHeading'
+import Table from "@/Components/Table/Table.vue"
+import { RecurringBill } from '@/types/recurring_bill'
 
 // import FileShowcase from '@/xxxxxxxxxxxx'
 
@@ -16,7 +18,17 @@ const props = defineProps<{
     
 }>()
 
+function recurringBillRoute(recurringBill: RecurringBill) {
+    switch (route().current()) {
+        default:
+            return route(
+                'retina.billing.recurring.show',
+                [
+                    recurringBill.slug
+                ])
 
+    }
+}
 
 </script>
 
@@ -24,6 +36,11 @@ const props = defineProps<{
 <template>
     <Head :title="capitalize(title)" />
     <PageHeading :data="pageHead" />
-    {{ data }}
-xx
+    <Table :resource="data" class="mt-5">
+        <template #cell(reference)="{ item: recurringBill }">
+            <Link :href="recurringBillRoute(recurringBill)" class="primaryLink">
+                {{ recurringBill['reference'] }}
+            </Link>
+        </template>
+    </Table>
 </template>
