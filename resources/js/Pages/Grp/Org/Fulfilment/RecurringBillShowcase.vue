8a101cae4ecffdb9fc495cb0001c6eb5b4140949
aqordeon <aqordivika@yahoo.co.id>
2024-07-11 11:22:49 +0800

    #514 feat: Fulfilment: Recurring Bill showcase

--- resources/js/Pages/Grp/Org/Fulfilment/RecurringBillShowcase.vue
+++ resources/js/Pages/Grp/Org/Fulfilment/RecurringBillShowcase.vue
@@ -1,9 +1,19 @@
 <script setup lang='ts'>
+import OrderSummary from '@/Components/Summary/OrderSummary.vue'
+import { FieldOrderSummary } from '@/types/Pallet'
+
+const props = defineProps<{
+    data: {
+        order_summary: FieldOrderSummary[][]
+    }
+}>()
     
 </script>
 
 <template>
-    <div>
+    
+    <div class="px-6 py-4 grid grid-cols-2 gap-x-5">
+        <OrderSummary :order_summary="data.order_summary" />
         Recurring Bills Showcase
     </div>
 </template>
