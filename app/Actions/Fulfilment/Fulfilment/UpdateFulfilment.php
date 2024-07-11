63d640b487fac9b101d26267e3d33dd7a2aa7e8d
Raul Perusquia <raul@inikoo.com>
2024-07-11 18:33:51 +0800

    #231 Fetch Aurora Tax Categories

--- app/Actions/Fulfilment/Fulfilment/UpdateFulfilment.php
+++ app/Actions/Fulfilment/Fulfilment/UpdateFulfilment.php
@@ -7,27 +7,9 @@
 
 namespace App\Actions\Fulfilment\Fulfilment;
 
-use App\Actions\Fulfilment\Fulfilment\Hydrators\FulfilmentHydratePallets;
-use App\Actions\Fulfilment\FulfilmentCustomer\HydrateFulfilmentCustomer;
-use App\Actions\Fulfilment\FulfilmentCustomer\Hydrators\FulfilmentCustomerHydratePallets;
-use App\Actions\Fulfilment\PalletDelivery\UpdatePalletDeliveryStateFromItems;
-use App\Actions\Fulfilment\PalletReturn\UpdatePalletReturnStateFromItems;
-use App\Actions\Inventory\Warehouse\Hydrators\WarehouseHydratePallets;
 use App\Actions\OrgAction;
-use App\Actions\SysAdmin\Organisation\Hydrators\OrganisationHydratePallets;
 use App\Actions\Traits\WithActionUpdate;
-use App\Enums\Fulfilment\Pallet\PalletStateEnum;
-use App\Enums\Fulfilment\Pallet\PalletStatusEnum;
-use App\Enums\Fulfilment\Pallet\PalletTypeEnum;
-use App\Enums\Fulfilment\RentalAgreement\RentalAgreementBillingCycleEnum;
-use App\Http\Resources\Fulfilment\PalletResource;
-use App\Models\CRM\WebUser;
 use App\Models\Fulfilment\Fulfilment;
-use App\Models\Fulfilment\FulfilmentCustomer;
-use App\Models\Fulfilment\Pallet;
-use App\Models\Inventory\Warehouse;
-use App\Models\SysAdmin\Organisation;
-use App\Rules\IUnique;
 use Illuminate\Support\Arr;
 use Illuminate\Validation\Rule;
 use Lorisleiva\Actions\ActionRequest;
@@ -43,13 +25,13 @@ class UpdateFulfilment extends OrgAction
     {
 
 
-        $settings = $fulfilment->settings;
+        $settings       = $fulfilment->settings;
         $updateSettings = false;
 
 
         if(Arr::exists($modelData, 'weekly_cut_off_day')) {
             $settings['rental_agreement_cut_off']['weekly']['day'] = $modelData['weekly_cut_off_day'];
-            $updateSettings = true;
+            $updateSettings                                        = true;
             data_forget($modelData, 'weekly_cut_off_day');
         }
 
@@ -63,7 +45,7 @@ class UpdateFulfilment extends OrgAction
             data_forget($modelData, 'monthly_only_weekdays');
         }
 
-        if($updateSettings){
+        if($updateSettings) {
             $modelData['settings'] = $settings;
         }
 
@@ -86,8 +68,8 @@ class UpdateFulfilment extends OrgAction
     public function rules(): array
     {
         return [
-            'weekly_cut_off_day'  => ['sometimes','string', Rule::in(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']) ],
-            'monthly_cut_off_day'  => ['sometimes','integer', 'min:1', 'max:31'],
+            'weekly_cut_off_day'     => ['sometimes','string', Rule::in(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']) ],
+            'monthly_cut_off_day'    => ['sometimes','integer', 'min:1', 'max:31'],
             'monthly_only_weekdays'  => ['sometimes','boolean'],
         ];
     }
---
+++ app/Enums/Helpers/TaxCategories/TaxCategoryTypeEnum.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Fri, 31 May 2024 20:15:32 Central European Summer Time, Mijas Costa, Spain
+ * Copyright (c) 2024, Raul A Perusquia Flores
+ */
+
+namespace App\Enums\Helpers\TaxCategories;
+
+use App\Enums\EnumHelperTrait;
+
+enum TaxCategoryTypeEnum: string
+{
+    use EnumHelperTrait;
+
+    case STANDARD    = 'standard';
+    case REDUCED     = 'reduced';
+    case OUTSIDE     = 'outside';
+    case EXEMPT      = 'exempt';
+    case EU_VTC      = 'eu_vtc';
+    case SPECIAL     = 'special';
+    case STANDARD_RE = 'standard_re';
+    case REDUCED_RE  = 'reduced_re';
+    case LEGACY      = 'legacy';
+
+
+}
