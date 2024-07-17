868fb8c5bffc2ca9d0bcb9ba1a70d9331f2d0f94
Raul Perusquia <raul@inikoo.com>
2024-07-17 18:49:44 +0800

    staging psql use normal port

--- pull_stagging_db.sh
+++ pull_stagging_db.sh
@@ -1,4 +1,4 @@
-ssh staging pg_dump -p 5433 -Fc -x -f "paso/aiku-staging.dump" aiku_staging
+ssh staging pg_dump -Fc -x -f "paso/aiku-staging.dump" aiku_staging
 scp staging:paso/aiku-staging.dump devops/devel/snapshots/aiku-staging.dump
 echo "loading ..";
 pg_restore -x --no-owner -j 15  -U "${USER}" -c -d aiku  "devops/devel/snapshots/aiku-staging.dump"
