b1c80f9f376956eb44d96e20fc863371f8a3c168
Artha <manager.arthachan@gmail.com>
2024-07-17 10:16:12 +0800

    AuthorizeClientGoogleDrive.php and CallbackClientGoogleDrive.php and out it in a Trait

--- app/Actions/Helpers/GoogleDrive/CallbackClientGoogleDrive.php
+++ app/Actions/Helpers/GoogleDrive/CallbackClientGoogleDrive.php
@@ -7,71 +7,21 @@
 
 namespace App\Actions\Helpers\GoogleDrive;
 
-use App\Actions\Helpers\GoogleDrive\Traits\WithTokenPath;
+use App\Actions\Helpers\GoogleDrive\Traits\WithGoogleDrive;
 use App\Models\SysAdmin\Organisation;
-use Exception;
-use Google_Client;
-use Google_Service_Drive;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Arr;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class CallbackClientGoogleDrive
 {
     use AsAction;
-    use WithTokenPath;
-
-    /**
-     * @throws \Exception
-     */
-    public function handle(Organisation $organisation): RedirectResponse
-    {
-        $client = new Google_Client();
-
-        $tokenPath       = $this->getTokenPath();
-        $authCode        = request()->query('code');
-
-        $client->setRedirectUri('http://localhost:5173');
-        $client->setApplicationName('Aiku google drive manager');
-        $client->setAuthConfig([
-            'client_id'     => Arr::get($organisation->settings, 'google.id'),
-            'client_secret' => Arr::get($organisation->settings, 'google.secret')
-        ]);
-
-        $client->setAccessType('offline');
-        $client->setScopes(
-            [
-                Google_Service_Drive::DRIVE_METADATA,
-                Google_Service_Drive::DRIVE_FILE,
-                Google_Service_Drive::DRIVE
-            ]
-        );
-
-        // Exchange authorization code for an access token.
-        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-
-        $client->setAccessToken($accessToken);
-
-        // Check to see if there was an error.
-        if (array_key_exists('error', $accessToken)) {
-            throw new Exception(join(', ', $accessToken));
-        }
-
-        // Save the token to a file.
-        if (!file_exists(dirname($tokenPath))) {
-            mkdir(dirname($tokenPath), 0700, true);
-        }
-        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-
-        return redirect()->route('grp.sysadmin.settings.edit');
-    }
+    use WithGoogleDrive;
 
     /**
      * @throws \Exception
      */
     public function asController(Organisation $organisation): RedirectResponse
     {
-        return $this->handle($organisation);
+        return $this->callback($organisation);
     }
-
 }
