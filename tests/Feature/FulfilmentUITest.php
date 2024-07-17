6e8d63f4efb2e5aafd0e01a7f512886ae665f74c
Raul Perusquia <raul@inikoo.com>
2024-07-17 19:05:31 +0800

    update tests 🧪

--- tests/Feature/FulfilmentUITest.php
+++ tests/Feature/FulfilmentUITest.php
@@ -382,7 +382,7 @@ test('UI edit fulfilment customer', function () {
         $page
             ->component('EditModel')
             ->has('title')
-            ->has('formData.blueprint.0.fields', 4)
+            ->has('formData.blueprint.0.fields', 5)
             ->has('pageHead')
             ->has(
                 'formData.args.updateRoute',
