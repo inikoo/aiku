f119b790e8d0fde4b2ca3ecf98ff9ac99c3a1332
Raul Perusquia <raul@inikoo.com>
2024-07-18 15:51:52 +0800

    fix error in shops index

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -3,27 +3,22 @@
 namespace App\Notifications;
 
 use App\Enums\Mail\Outbox\OutboxTypeEnum;
-use App\Models\CRM\Customer;
 use App\Models\CRM\WebUser;
 use App\Models\Mail\Outbox;
 use App\Models\SysAdmin\User;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Lang;
 
-class ResetPasswordNotification extends Notification implements ShouldQueue
+class ResetPasswordNotification extends Notification
 {
-    use Queueable;
 
     public string $url;
 
     public function __construct(string $url)
     {
         $this->url = $url;
-        $this->queue= 'urgent';
     }
 
 
@@ -39,20 +34,15 @@ class ResetPasswordNotification extends Notification implements ShouldQueue
         $outbox = $notifiable->shop->outboxes()->where('type', OutboxTypeEnum::PASSWORD_REMINDER->value)
             ->first();
 
-        $data = $outbox->emailTemplate->published_layout ?? [
-                'subject'=> 'Reset Password Notification',
-                'header' => 'You are receiving this email because we received a password reset request for your account.',
-                'action' => 'Reset Password',
-                'footer' => 'This password reset link will expire in :count minutes.',
-                'notes'  => 'If you did not request a password reset, no further action is required.',
-        ];
+        $data = $outbox->emailTemplate->published_layout;
+
 
         $message= (new MailMessage())
-            ->subject(Lang::get(Arr::get($data, 'subject')))
-            ->line(Lang::get(Arr::get($data, 'header')))
-            ->action(Lang::get(Arr::get($data, 'action')), $this->url)
-            ->line(Lang::get(Arr::get($data, 'footer'), ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get(Arr::get($data, 'notes')));
+            ->subject(Lang::get(Arr::get($data, 'subject','Reset Password Notification')))
+            ->line(Lang::get(Arr::get($data, 'header', 'You are receiving this email because we received a password reset request for your account.')))
+            ->action(Lang::get(Arr::get($data, 'action','Reset Password')), $this->url)
+            ->line(Lang::get(Arr::get($data, 'footer','This password reset link will expire in :count minutes.'), ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(Lang::get(Arr::get($data, 'notes','If you did not request a password reset, no further action is required.')));
 
         if(app()->isProduction()) {
             if ($notifiable instanceof WebUser) {
