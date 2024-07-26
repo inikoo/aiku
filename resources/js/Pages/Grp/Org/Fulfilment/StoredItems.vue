dd6e0131027fe5a978cc5abaaaff3be9090f7227
KirinZero0 <aryapermana02@gmail.com>
2024-07-26 16:18:57 +0800

    #702: FEat

--- resources/js/Pages/Grp/Org/Fulfilment/StoredItems.vue
+++ resources/js/Pages/Grp/Org/Fulfilment/StoredItems.vue
@@ -13,18 +13,44 @@ import TableStoredItems from "@/Components/Tables/Grp/Org/Fulfilment/TableStored
 import { faNarwhal } from '@fal'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { PageHeading as TSPageHeading } from '@/types/PageHeading'
+import { computed, ref } from 'vue'
+import { useTabChange } from '@/Composables/tab-change'
+import Tabs from '@/Components/Navigation/Tabs.vue'
+import Table from '@/Components/Table/Table.vue'
 library.add(faNarwhal)
 
 const props = defineProps<{
     data: {}
     title: string
     pageHead: TSPageHeading
+    tabs: {
+        current: string;
+        navigation: object;
+    }
+    stored_items : {}
+    pallet_stored_items : {}
 }>()
+
+let currentTab = ref(props.tabs.current);
+const handleTabUpdate = (tabSlug) => useTabChange(tabSlug, currentTab);
+
+
+
+const component = computed(() => {
+
+    const components = {
+        stored_items:TableStoredItems,
+        pallet_stored_items: Table
+    };
+    return components[currentTab.value];
+
+});
 
 </script>
 
 <template>
     <Head :title="capitalize(title)" />
     <PageHeading :data="pageHead"></PageHeading>
-    <TableStoredItems :data="data" />
+    <Tabs :current="currentTab" :navigation="tabs['navigation']" @update:tab="handleTabUpdate"/>
+    <component :is="component" :data="props[currentTab]" :resource="props[currentTab]" :tab="currentTab" :name="currentTab"></component>
 </template>
