2ab7122318d781efe3a11cfe1eb2e7ac1562edc2
KirinZero0 <aryapermana02@gmail.com>
2024-07-29 15:35:27 +0800

    #707: Separate Action to insert clause id in Fulfilment Transaction

---
+++ app/Actions/Fulfilment/FulfilmentTransaction/SetClausesInFulfilmentTransaction.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 04 Jul 2024 19:55:52 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2024, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Fulfilment\FulfilmentTransaction;
+
+use App\Actions\OrgAction;
+use App\Actions\Traits\WithActionUpdate;
+use App\Models\Fulfilment\FulfilmentTransaction;
+
+class SetClausesInFulfilmentTransaction extends OrgAction
+{
+    public function handle(FulfilmentTransaction $fulfilmentTransaction)
+    {
+        $rentalAgreementClauses = $fulfilmentTransaction->parent->fulfilmentCustomer->rentalAgreementClauses;
+        $percentageOff = 0;
+        $found = false;
+        foreach ($rentalAgreementClauses as $clause) {
+            if ($clause->asset_id === $fulfilmentTransaction->asset_id) {
+                data_set($modelData, 'rental_agreement_clause_id', $clause->id);
+                $percentageOff = $clause->percentage_off / 100;
+                $found = true;
+                break;
+            }
+        }
+
+        if (!$found) {
+            data_set($modelData, 'rental_agreement_clause_id', null);
+        }
+
+        $net = $fulfilmentTransaction->net_amount;
+        $net -= $net * $percentageOff;
+
+        data_set($modelData, 'net_amount', $net);
+        data_set($modelData, 'grp_net_amount', $net * $fulfilmentTransaction->grp_exchange);
+        data_set($modelData, 'org_net_amount', $net * $fulfilmentTransaction->org_exchange);
+        
+        $fulfilmentTransaction->update($modelData);
+    }
+}
