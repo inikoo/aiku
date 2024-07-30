391254cd64e2eae9d2e3cfccc86d7d12ff016062
aqordeon <aqordivika@yahoo.co.id>
2024-07-30 14:45:17 +0800

    #715 feat: Webpage: preview in Storefront

--- resources/js/Components/Pure/BrowserView.vue
+++ resources/js/Components/Pure/BrowserView.vue
@@ -1,15 +1,29 @@
 <script setup lang='ts'>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faCircle, faPlus, faRedoAlt, faPuzzlePiece, faStar as fasStar } from '@fas'
-import { faEllipsisV, faStar } from '@fal'
+import { faEllipsisV, faStar, faGlobe } from '@fal'
 import { faTimes, faArrowLeft, faArrowRight } from '@far'
-import { faChrome, faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { faChrome } from '@fortawesome/free-brands-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { ref } from 'vue'
-library.add(faEllipsisV, faStar, faCircle, faChrome, faGoogle, faTimes, faRedoAlt, faArrowLeft, faArrowRight, faPlus, faPuzzlePiece, fasStar)    
+import { trans } from 'laravel-vue-i18n'
+library.add(faEllipsisV, faStar, faGlobe, faCircle, faChrome, faTimes, faRedoAlt, faArrowLeft, faArrowRight, faPlus, faPuzzlePiece, fasStar)    
+
+const props = defineProps<{
+    tab?: {
+        icon?: string | string[]
+        label?: string
+    }
+    url?: {
+        domain?: string
+        page?: string
+    }
+}>()
 
 const isStar = ref(false)
 const isLoadingRefreshPage = ref(false)
+const keyIconPlus = ref(0)
+const keyIconTimes = ref(0)
 
 const onRefreshPage = () => {
     isLoadingRefreshPage.value = true
@@ -20,7 +34,7 @@ const onRefreshPage = () => {
 </script>
 
 <template>
-    <div class="w-full max-w-4xl mx-auto mt-4 rounded-2xl overflow-hidden shadow-xl">
+    <div class="w-full max-w-4xl mx-auto mt-4 rounded-2xl overflow-hidden shadow-xl h-fit">
         <div class="bg-gray-200 pt-1 flex">
             <div class="bg-white">
                 <div class="bg-gray-200 space-x-2 rounded-br-lg w-fit px-4 py-2">
@@ -32,18 +46,22 @@ const onRefreshPage = () => {
 
             <div class="w-full max-w-64 bg-white rounded-t-lg px-3 flex justify-between items-center">
                 <div class="flex gap-x-2 items-center ">
-                    <FontAwesomeIcon icon='fab fa-chrome' class='' fixed-width aria-hidden='true' />
-                    <div>New Tab</div>
+                    <FontAwesomeIcon :icon='tab?.icon || "fab fa-chrome"' class='' fixed-width aria-hidden='true' />
+                    <div>{{ tab?.label || trans('New Tab')}}</div>
                 </div>
-                <div class="h-5 w-5 flex items-center rounded-full hover:bg-gray-200 cursor-pointer">
-                    <FontAwesomeIcon icon='far fa-times' class='text-gray-500' fixed-width aria-hidden='true' />
+                <div @click="() => keyIconTimes++" class="h-5 w-5 flex items-center rounded-full hover:bg-gray-200 cursor-pointer">
+                    <Transition name="spin-to-right">
+                        <FontAwesomeIcon :key="keyIconTimes" icon='far fa-times' class='text-gray-500' fixed-width aria-hidden='true' />
+                    </Transition>
                 </div>
             </div>
-
+            
             <div class="bg-white">
                 <div class="bg-gray-200 h-full rounded-bl-lg flex items-center px-2 py-1">
-                    <div class="h-full px-2 flex items-center rounded-md hover:bg-black/10 cursor-pointer">
-                        <FontAwesomeIcon icon='fas fa-plus' class='' fixed-width aria-hidden='true' />
+                    <div @click="() => keyIconPlus++" class="relative h-full px-2 flex items-center rounded-md hover:bg-black/10 cursor-pointer">
+                        <Transition name="spin-to-right">
+                            <FontAwesomeIcon :key="keyIconPlus" icon='fas fa-plus' class='' fixed-width aria-hidden='true' />
+                        </Transition>
                     </div>
                 </div>
             </div>
@@ -64,9 +82,9 @@ const onRefreshPage = () => {
             <div class="px-3 py-1 text-sm bg-gray-100 rounded-full w-full flex items-center justify-between">
                 <div class="flex items-center gap-x-2">
                     <div class="rounded-full bg-gray-300 h-5 w-5 flex justify-center items-center">
-                        <FontAwesomeIcon icon='fab fa-google' class='text-xs' fixed-width aria-hidden='true' />
+                        <FontAwesomeIcon icon='fal fa-globe' class='text-xs' fixed-width aria-hidden='true' />
                     </div>
-                    <div class="font-medium">www.website.com<span class="font-normal text-gray-400">/navigation/hello/world</span></div>
+                    <div class="font-medium truncate">{{ url?.domain || 'www.website.com' }}<span class="font-normal text-gray-400">/{{ url?.page }}</span></div>
                 </div>
                 <FontAwesomeIcon v-if="isStar" @click="() => isStar = !isStar" icon='fas fa-star' class='text-xs self cursor-pointer' fixed-width aria-hidden='true' />
                 <FontAwesomeIcon v-else @click="() => isStar = !isStar" icon='fal fa-star' class='text-gray-500 hover:text-gray-700 text-xs self cursor-pointer' fixed-width aria-hidden='true' />
@@ -79,8 +97,10 @@ const onRefreshPage = () => {
         </div>
 
 
-        <div class="bg-indigo-500 w-full h-auto aspect-[16/9] overflow-hidden">
-            <slot name="page" />
+        <div class="w-full h-auto aspect-[16/9] overflow-hidden overflow-y-auto">
+            <slot name="page">
+                <div class="bg-indigo-500 w-full h-full"></div>
+            </slot>
         </div>
     </div>
 </template>
