810783c334db69a0adb1e7eed99cff7807c4de0c
aqordeon <aqordivika@yahoo.co.id>
2024-08-06 10:52:27 +0800

    #725 fix: Customer: Showcase UI

--- resources/js/Components/Showcases/Grp/CustomerShowcase.vue
+++ resources/js/Components/Showcases/Grp/CustomerShowcase.vue
@@ -10,14 +10,29 @@ import { routeType } from '@/types/route'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faLink} from '@far'
-import { faSync, faCalendarAlt, faEnvelope, faPhone } from '@fal'
+import { faSync, faCalendarAlt, faEnvelope, faPhone, faMapMarkerAlt } from '@fal'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import AddressLocation from '@/Components/Elements/Info/AddressLocation.vue'
+import { trans } from 'laravel-vue-i18n'
 
-library.add(faLink, faSync, faCalendarAlt, faEnvelope, faPhone)
+library.add(faLink, faSync, faCalendarAlt, faEnvelope, faPhone, faMapMarkerAlt)
+
+interface CustomerDropshipping {
+    slug: string
+    reference: string
+    name: string
+    contact_name: string
+    company_name: string
+    location: string[]
+    email: string
+    phone: string
+    created_at: string
+    number_current_clients: number | null
+}
 
 const props = defineProps<{
     data: {
-        customer: {}
+        customer: CustomerDropshipping
         updateRoute: routeType
 
     },
@@ -30,8 +45,7 @@ const props = defineProps<{
 <template>
     <!-- Section: Stats box -->
     <div class="px-4 py-5 md:px-6 lg:px-8 grid grid-cols-2 gap-x-8 gap-y-3">
-        {{data}}
-     
+        <!-- <pre>{{data}}</pre> -->
         
         <!-- Section: Profile box -->
         <div >
@@ -40,14 +54,14 @@ const props = defineProps<{
                 <dl class="flex flex-wrap">
                     <!-- Profile: Header -->
                     <div class="flex w-full py-6">
-                        <div v-if="data.number_current_clients" class="flex-auto pl-6">
-                            <dt class="text-sm font-semibold leading-6 text-gray-900">Total Clients</dt>
-                            <dd class="mt-1 text-base font-semibold leading-6 text-gray-900">{{ data.number_current_clients || 0 }}</dd>
+                        <div class="flex-auto pl-6">
+                            <dt class="text-sm text-gray-500">{{ trans('Total Clients') }}</dt>
+                            <dd class="mt-1 text-base font-semibold leading-6">{{ data.customer.number_current_clients || 0 }}</dd>
                         </div>
                         <div class="flex-none self-end px-6 pt-4">
                             <dt class="sr-only">Reference</dt>
                             <dd class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                                {{ data.reference }}
+                                {{ data.customer.reference }}
                             </dd>
                         </div>
                     </div>
@@ -55,26 +69,26 @@ const props = defineProps<{
                     <!-- Section: Field -->
                     <div class="flex flex-col gap-y-3 border-t border-gray-900/5 w-full py-6">
                         <!-- Field: Contact name -->
-                        <div v-if="data.contact_name" class="flex items-center w-full flex-none gap-x-4 px-6">
-                            <dt v-tooltip="'Contact name'" class="flex-none">
+                        <div v-if="data.customer.contact_name" class="flex items-center w-full flex-none gap-x-4 px-6">
+                            <dt v-tooltip="trans('Contact name')" class="flex-none">
                                 <span class="sr-only">Contact name</span>
                                 <FontAwesomeIcon icon='fal fa-user' class='text-gray-400' fixed-width aria-hidden='true' />
                             </dt>
-                            <dd class="text-gray-500">{{ data.contact_name }}</dd>
+                            <dd class="text-gray-500">{{ data.customer.contact_name }}</dd>
                         </div>
 
                         <!-- Field: Contact name -->
-                        <div v-if="data.company_name" class="flex items-center w-full flex-none gap-x-4 px-6">
-                            <dt v-tooltip="'Company name'" class="flex-none">
+                        <div v-if="data.customer.company_name" class="flex items-center w-full flex-none gap-x-4 px-6">
+                            <dt v-tooltip="trans('Company name')" class="flex-none">
                                 <span class="sr-only">Company name</span>
                                 <FontAwesomeIcon icon='fal fa-building' class='text-gray-400' fixed-width aria-hidden='true' />
                             </dt>
-                            <dd class="text-gray-500">{{ data.company_name }}</dd>
+                            <dd class="text-gray-500">{{ data.customer.company_name }}</dd>
                         </div>
 
                         <!-- Field: Created at -->
                         <div v-if="data.customer?.created_at" class="flex items-center w-full flex-none gap-x-4 px-6">
-                            <dt v-tooltip="'Created at'" class="flex-none">
+                            <dt v-tooltip="trans('Created at')" class="flex-none">
                                 <span class="sr-only">Created at</span>
                                 <FontAwesomeIcon icon='fal fa-calendar-alt' class='text-gray-400' fixed-width aria-hidden='true' />
                             </dt>
@@ -85,7 +99,7 @@ const props = defineProps<{
                         
                         <!-- Field: Email -->
                         <div v-if="data.customer?.email" class="flex items-center w-full flex-none gap-x-4 px-6">
-                            <dt v-tooltip="'Email'" class="flex-none">
+                            <dt v-tooltip="trans('Email')" class="flex-none">
                                 <span class="sr-only">Email</span>
                                 <FontAwesomeIcon icon='fal fa-envelope' class='text-gray-400' fixed-width aria-hidden='true' />
                             </dt>
@@ -94,11 +108,22 @@ const props = defineProps<{
                         
                         <!-- Field: Phone -->
                         <div v-if="data.customer?.phone" class="flex items-center w-full flex-none gap-x-4 px-6">
-                            <dt v-tooltip="'Phone'" class="flex-none">
+                            <dt v-tooltip="trans('Phone')" class="flex-none">
                                 <span class="sr-only">Phone</span>
                                 <FontAwesomeIcon icon='fal fa-phone' class='text-gray-400' fixed-width aria-hidden='true' />
                             </dt>
                             <dd class="text-gray-500">{{ data.customer?.phone }}</dd>
+                        </div>
+                        
+                        <!-- Field: Phone -->
+                        <div v-if="data.customer?.location.length" class="flex items-center w-full flex-none gap-x-4 px-6">
+                            <dt v-tooltip="trans('Location')" class="flex-none">
+                                <span class="sr-only">Location</span>
+                                <FontAwesomeIcon icon='fal fa-map-marker-alt' class='text-gray-400' fixed-width aria-hidden='true' />
+                            </dt>
+                            <dd class="text-gray-500">
+                                <AddressLocation :data="data.customer.location" />
+                            </dd>
                         </div>
                     </div>
                 </dl>
