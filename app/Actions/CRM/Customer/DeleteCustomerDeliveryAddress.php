86f9aa78c49ea5e36df1bbe23bdb1b88e4608285
KirinZero0 <aryapermana02@gmail.com>
2024-08-06 11:15:25 +0800

    Address fix in Pallet Return

--- app/Actions/CRM/Customer/DeleteCustomerDeliveryAddress.php
+++ app/Actions/CRM/Customer/DeleteCustomerDeliveryAddress.php
@@ -9,30 +9,43 @@ namespace App\Actions\CRM\Customer;
 
 use App\Actions\Helpers\Address\Hydrators\AddressHydrateUsage;
 use App\Actions\OrgAction;
+use App\Enums\Fulfilment\PalletReturn\PalletReturnStateEnum;
 use App\Models\CRM\Customer;
 use App\Models\Fulfilment\FulfilmentCustomer;
+use App\Models\Fulfilment\PalletReturn;
 use App\Models\Helpers\Address;
 use Illuminate\Support\Arr;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\Validator;
 use Lorisleiva\Actions\ActionRequest;
 
 class DeleteCustomerDeliveryAddress extends OrgAction
 {
+
+    protected Address $address;
     public function handle(Customer $customer, Address $address)
     {
         $customer->addresses()->detach($address->id);
 
         $address->delete();
-
+        
         AddressHydrateUsage::dispatch($address);
         return $customer;
     }
 
-
-    public function asController(FulfilmentCustomer $fulfilmentCustomer, Address $address, ActionRequest $actionRequest): void
+    public function afterValidator(Validator $validator)
     {
-        $this->initialisationFromFulfilment($fulfilmentCustomer->fulfilment, $actionRequest);
+        if(PalletReturn::where('delivery_address_id', $this->address->id)->where('state', PalletReturnStateEnum::IN_PROCESS)->exists())
+        {
+            abort(419);
+            // dd('hello');
+        };
+    }
 
+    public function asController(FulfilmentCustomer $fulfilmentCustomer, Address $address, ActionRequest $request): void
+    {
+        $this->address = $address;
+        $this->initialisationFromFulfilment($fulfilmentCustomer->fulfilment, $request);
         $this->handle($fulfilmentCustomer->customer, $address);
     }
 }
