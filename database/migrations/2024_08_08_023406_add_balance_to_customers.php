f06f249391744e04ec5f31bbc7474e8ed766bd7c
Raul Perusquia <raul@inikoo.com>
2024-08-08 10:57:09 +0800

    #740 TopUps (DS) 💵

---
+++ database/migrations/2024_08_08_023406_add_balance_to_customers.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 08 Aug 2024 10:34:13 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2024, Raul A Perusquia Flores
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->decimal('balance', 16);
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn('balance');
+        });
+    }
+};
