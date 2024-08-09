0763815dfa4d66c0bb538b6de93ff63856f4bb73
aqordeon <aqordivika@yahoo.co.id>
2024-08-09 14:52:27 +0800

    fix: Group Dashboard: translation

--- app/Actions/UI/Grp/Dashboard/ShowDashboard.php
+++ app/Actions/UI/Grp/Dashboard/ShowDashboard.php
@@ -22,9 +22,9 @@ class ShowDashboard
     {
 
         /** @var Group $group */
-        $group = Group::first();
+        $group   = Group::first();
         $testOrg = $group->organisations->skip(1)->first();
-        $sales = [
+        $sales   = [
             'sales'         => JsonResource::make($group->salesStats),
             'currency'      => $group->currency,
             'organisations' => $group->organisations->map(function (Organisation $organisation) {
@@ -98,7 +98,7 @@ class ShowDashboard
                         ],
                     ];
                 }
-        
+
                 return $responseData;
             })
         ];
@@ -108,73 +108,7 @@ class ShowDashboard
             [
                 'breadcrumbs'       => $this->getBreadcrumbs(__('Dashboard')),
                 'groupStats'        => $sales,
-                'interval_options'  => [
-                    [
-                        'label'      => trans('Year to date'),
-                        'labelShort' => trans('Ytd'),
-                        'value'      => 'ytd'
-                    ],
-                    [
-                        'label'      => trans('Quarter to date'),
-                        'labelShort' => trans('Qtd'),
-                        'value'      => 'qtd'
-                    ],
-                    [
-                        'label'      => trans('Month to date'),
-                        'labelShort' => trans('Mtd'),
-                        'value'      => 'mtd'
-                    ],
-                    [
-                        'label'      => trans('Week to date'),
-                        'labelShort' => trans('Wtd'),
-                        'value'      => 'wtd'
-                    ],
-                    [
-                        'label'      => trans('Last month'),
-                        'labelShort' => trans('lm'),
-                        'value'      => 'lm'
-                    ],
-                    [
-                        'label'      => trans('Last week'),
-                        'labelShort' => trans('lw'),
-                        'value'      => 'lw'
-                    ],
-                    [
-                        'label'      => trans('Yesterday'),
-                        'labelShort' => trans('y'),
-                        'value'      => 'yda'
-                    ],
-                    [
-                        'label'      => trans('Today'),
-                        'labelShort' => trans('t'),
-                        'value'      => 'tdy'
-                    ],
-                    [
-                        'label'      => trans('1 Year'),
-                        'labelShort' => trans('1y'),
-                        'value'      => '1y'
-                    ],
-                    [
-                        'label'      => trans('1 Quarter'),
-                        'labelShort' => trans('1q'),
-                        'value'      => '1q'
-                    ],
-                    [
-                        'label'      => trans('1 Month'),
-                        'labelShort' => trans('1m'),
-                        'value'      => '1m'
-                    ],
-                    [
-                        'label'      => trans('1 Week'),
-                        'labelShort' => trans('1w'),
-                        'value'      => '1w'
-                    ],
-                    [
-                        'label'      => trans('All'),
-                        'labelShort' => trans('All'),
-                        'value'      => 'all'
-                    ],
-                ]
+
             ]
         );
     }
