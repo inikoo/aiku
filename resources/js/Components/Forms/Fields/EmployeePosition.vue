867872acf18eb5e78e0ca9a9f068cae63ed52dc1
KirinZero0 <aryapermana02@gmail.com>
2024-08-09 09:56:20 +0800

    PErmission stuff

--- resources/js/Components/Forms/Fields/EmployeePosition.vue
+++ resources/js/Components/Forms/Fields/EmployeePosition.vue
@@ -495,7 +495,8 @@ const productionLength = layout.organisations.data.find(organisation => organisa
 
 <template>
     <div class="relative">
-    <pre>{{ fieldData.length?.[fieldName] }}</pre>
+        ful:{{ fulfilmentLength }} -- shop:{{ shopLength }} -- warehouse:{{ warehouseLength }} -- production:{{ productionLength }}
+    <pre>{{ fieldData.length }}</pre>
         <div class="flex flex-col text-xs divide-y-[1px]">
             <template v-for="(jobGroup, departmentName, idxJobGroup) in optionsJob" :key="departmentName + idxJobGroup">
                 <div v-if="(departmentName === 'prod'  && productionLength > 0) || departmentName !== 'prod'" class="grid grid-cols-3 gap-x-1.5 px-2 items-center even:bg-gray-50 transition-all duration-200 ease-in-out">
