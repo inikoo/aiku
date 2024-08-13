f129869e43cebce55584b8737c96728fd22b776b
Artha <manager.arthachan@gmail.com>
2024-08-13 09:43:52 +0800

    fix: pallet select & unselect

--- app/Actions/Fulfilment/PalletReturn/Json/GetReturnPallets.php
+++ app/Actions/Fulfilment/PalletReturn/Json/GetReturnPallets.php
@@ -10,9 +10,14 @@ namespace App\Actions\Fulfilment\PalletReturn\Json;
 use App\Actions\OrgAction;
 use App\Enums\Fulfilment\Pallet\PalletStateEnum;
 use App\Enums\Fulfilment\Pallet\PalletStatusEnum;
+use App\Enums\Fulfilment\PalletReturn\PalletReturnStateEnum;
+use App\Enums\Fulfilment\StoredItem\StoredItemInReturnOptionEnum;
 use App\Http\Resources\Fulfilment\PalletsResource;
+use App\InertiaTable\InertiaTable;
 use App\Models\Fulfilment\FulfilmentCustomer;
 use App\Models\Fulfilment\Pallet;
+use App\Models\Fulfilment\PalletReturn;
+use Closure;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Lorisleiva\Actions\ActionRequest;
@@ -22,7 +27,27 @@ use App\Services\QueryBuilder;
 
 class GetReturnPallets extends OrgAction
 {
-    public function handle(FulfilmentCustomer $fulfilmentCustomer): LengthAwarePaginator
+    protected function getElementGroups(PalletReturn $palletReturn): array
+    {
+        return [
+            'option' => [
+                'label'    => __('Option'),
+                'elements' => array_merge_recursive(
+                    StoredItemInReturnOptionEnum::labels(),
+                    StoredItemInReturnOptionEnum::count()
+                ),
+                'engine' => function ($query, $elements) use ($palletReturn) {
+                    if (in_array(StoredItemInReturnOptionEnum::SELECTED->value, $elements)) {
+                        $query->where('pallets.pallet_return_id', $palletReturn->id);
+                    } elseif (in_array(StoredItemInReturnOptionEnum::UNSELECTED->value, $elements)) {
+                        $query->whereNull('pallets.pallet_return_id');
+                    }
+                }
+            ],
+        ];
+    }
+
+    public function handle(PalletReturn $palletReturn, $prefix = null): LengthAwarePaginator
     {
         $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
             $query->where(function ($query) use ($value) {
@@ -33,17 +58,27 @@ class GetReturnPallets extends OrgAction
 
         $query = QueryBuilder::for(Pallet::class);
 
-        $query->where('fulfilment_customer_id', $fulfilmentCustomer->id);
+        $query->where('fulfilment_customer_id', $palletReturn->fulfilment_customer_id);
         $query->where('pallets.state', PalletStateEnum::STORING);
         $query->where('pallets.status', '!=', PalletStatusEnum::RETURNED);
 
         $query->leftJoin('locations', 'locations.id', 'pallets.location_id');
         $query->leftJoin('pallet_return_items', 'pallet_return_items.pallet_id', 'pallets.id');
 
+        if ($palletReturn->state === PalletReturnStateEnum::IN_PROCESS) {
+            foreach ($this->getElementGroups($palletReturn) as $key => $elementGroup) {
+                $query->whereElementGroup(
+                    key: $key,
+                    allowedElements: array_keys($elementGroup['elements']),
+                    engine: $elementGroup['engine'],
+                    prefix: $prefix
+                );
+            }
+        }
+
         $query->defaultSort('pallets.id')
             ->select(
-                'pallet_return_items.id',
-                'pallets.id as pallet_id',
+                'pallets.id',
                 'pallets.slug',
                 'pallets.reference',
                 'pallets.customer_reference',
@@ -65,7 +100,7 @@ class GetReturnPallets extends OrgAction
 
         return $query->allowedSorts(['customer_reference', 'reference', 'fulfilment_customer_name'])
             ->allowedFilters([$globalSearch, 'customer_reference', 'reference'])
-            ->withPaginator(null)
+            ->withPaginator($prefix)
             ->withQueryString();
     }
 
@@ -100,6 +135,64 @@ class GetReturnPallets extends OrgAction
 
         $this->initialisation($request->get('website')->organisation, $request);
         return $this->handle($fulfilmentCustomer);
+    }
+
+    public function tableStructure(PalletReturn $palletReturn, $request, $prefix = null, $modelOperations = []): Closure
+    {
+        return function (InertiaTable $table) use ($prefix, $modelOperations, $request, $palletReturn) {
+            if ($prefix) {
+                $table
+                    ->name($prefix)
+                    ->pageName($prefix.'Page');
+            }
+
+            $emptyStateData = [
+                'icons' => ['fal fa-pallet'],
+                'title' => '',
+                'count' => $palletReturn->fulfilmentCustomer->number_pallets_state_storing
+            ];
+
+            if ($palletReturn->state === PalletReturnStateEnum::IN_PROCESS) {
+                foreach ($this->getElementGroups($palletReturn) as $key => $elementGroup) {
+                    $table->elementGroup(
+                        key: $key,
+                        label: $elementGroup['label'],
+                        elements: $elementGroup['elements']
+                    );
+                }
+            }
+
+            $table->withGlobalSearch();
+
+
+            $table->withEmptyState($emptyStateData)
+                ->withModelOperations($modelOperations);
+
+
+            $table->column(key: 'type_icon', label: ['fal', 'fa-yin-yang'], type: 'icon');
+
+
+            /* $table->column(key: 'state', label: ['fal', 'fa-yin-yang'], type: 'icon'); */
+
+
+            $table->column(key: 'reference', label: __('reference'), canBeHidden: false, sortable: true, searchable: true);
+
+
+            $customersReferenceLabel = __("Pallet reference (customer's), notes");
+
+
+            $table->column(key: 'customer_reference', label: $customersReferenceLabel, canBeHidden: false, sortable: true, searchable: true);
+
+            if (!$request->user() instanceof WebUser) {
+                $table->column(key: 'location', label: __('Location'), canBeHidden: false, searchable: true);
+            }
+
+
+            $table->column(key: 'actions', label: ' ', canBeHidden: false, searchable: true);
+
+
+            $table->defaultSort('reference');
+        };
     }
 
 }
--- app/Actions/Fulfilment/PalletReturn/UI/ShowPalletReturn.php
+++ app/Actions/Fulfilment/PalletReturn/UI/ShowPalletReturn.php
@@ -9,7 +9,6 @@ namespace App\Actions\Fulfilment\PalletReturn\UI;
 
 use App\Actions\Fulfilment\Fulfilment\UI\ShowFulfilment;
 use App\Actions\Fulfilment\FulfilmentCustomer\ShowFulfilmentCustomer;
-use App\Actions\Fulfilment\Pallet\UI\IndexPalletsInReturn;
 use App\Actions\Fulfilment\PalletReturn\Json\GetReturnPallets;
 use App\Actions\Fulfilment\StoredItem\UI\IndexStoredItemsInReturn;
 use App\Actions\Inventory\Warehouse\UI\ShowWarehouse;
@@ -639,8 +638,8 @@ class ShowPalletReturn extends OrgAction
                 ],
 
                 PalletReturnTabsEnum::PALLETS->value => $this->tab == PalletReturnTabsEnum::PALLETS->value ?
-                    fn () => PalletReturnItemsResource::collection(GetReturnPallets::run($palletReturn->fulfilmentCustomer))
-                    : Inertia::lazy(fn () => PalletReturnItemsResource::collection(GetReturnPallets::run($palletReturn->fulfilmentCustomer))),
+                    fn () => PalletReturnItemsResource::collection(GetReturnPallets::run($palletReturn, PalletReturnTabsEnum::PALLETS->value))
+                    : Inertia::lazy(fn () => PalletReturnItemsResource::collection(GetReturnPallets::run($palletReturn, PalletReturnTabsEnum::PALLETS->value))),
 
                 PalletReturnTabsEnum::STORED_ITEMS->value => $this->tab == PalletReturnTabsEnum::STORED_ITEMS->value ?
                     fn () => PalletReturnStoredItemsResource::collection(IndexStoredItemsInReturn::run($palletReturn, PalletReturnTabsEnum::STORED_ITEMS->value)) //todo idk if this is right
@@ -655,7 +654,7 @@ class ShowPalletReturn extends OrgAction
                     : Inertia::lazy(fn () => FulfilmentTransactionsResource::collection(IndexPhysicalGoodInPalletReturn::run($palletReturn))),
             ]
         )->table(
-            IndexPalletsInReturn::make()->tableStructure(
+            GetReturnPallets::make()->tableStructure(
                 $palletReturn,
                 request: $request,
                 prefix: PalletReturnTabsEnum::PALLETS->value
