2110b378afb9c973344c7b81cd09903802055a45
aqordeon <aqordivika@yahoo.co.id>
2024-08-15 14:40:44 +0800

    feat: Shopify: setup Vue

--- config/shopify-app.php
+++ config/shopify-app.php
@@ -580,7 +580,7 @@ return [
     | No changes are made for Vue.js and Blade.
     |
     */
-    'frontend_engine' => env('SHOPIFY_FRONTEND_ENGINE', 'BLADE'),
+    'frontend_engine' => env('SHOPIFY_FRONTEND_ENGINE', 'VUE'),
 
     'iframe_ancestors' => '',
 ];
---
+++ resources/js/Pages/Shopify/Dashboard/ShopifyDashboard.vue
@@ -0,0 +1,9 @@
+<script setup lang='ts'>
+    
+</script>
+
+<template>
+    <div>
+        <h1>Hello from Vue Dashboard!</h1>
+    </div>
+</template>
