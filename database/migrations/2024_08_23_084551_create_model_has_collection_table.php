99de1ef065932ebfa06101f209172221eb724472
KirinZero0 <aryapermana02@gmail.com>
2024-08-23 16:47:40 +0800

    model_has_collection migration

---
+++ database/migrations/2024_08_23_084551_create_model_has_collection_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('model_has_collection', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('collection_id');
+            $table->foreign('collection_id')->references('id')->on('collections');
+            $table->string('model_type');
+            $table->unsignedInteger('model_id');
+            $table->index(['model_type','model_id']);
+            $table->unique(['collection_id','model_type','model_id']);
+            $table->timestampsTz();
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('model_has_collection');
+    }
+};
