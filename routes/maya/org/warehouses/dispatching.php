2b59f1136529e50ca1c80cf5f7c69c1e597a6899
KirinZero0 <aryapermana02@gmail.com>
2024-08-27 11:05:40 +0800

    Routes for maya

--- routes/maya/org/warehouses/dispatching.php
+++ routes/maya/org/warehouses/dispatching.php
@@ -7,8 +7,10 @@
 
 use App\Actions\Dispatching\DeliveryNote\UI\IndexDeliveryNotes;
 use App\Actions\Dispatching\DeliveryNote\UI\ShowDeliveryNote;
+use App\Actions\Fulfilment\Pallet\UI\IndexPalletsInReturn;
 use App\Actions\Fulfilment\PalletReturn\UI\IndexPalletReturns;
 use App\Actions\Fulfilment\PalletReturn\UI\ShowPalletReturn;
+use App\Actions\Fulfilment\StoredItem\UI\IndexStoredItemsInReturn;
 use App\Actions\UI\Dispatch\ShowDispatchHub;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +18,7 @@ Route::get('/', ShowDispatchHub::class)->name('backlog');
 Route::get('/delivery-notes', IndexDeliveryNotes::class)->name('delivery-notes');
 Route::get('/delivery-notes/{deliveryNote}', [ShowDeliveryNote::class, 'inWarehouse'])->name('delivery-notes.show');
 
-Route::get('returns', [IndexPalletReturns::class, 'inWarehouse'])->name('pallet-returns.index');
-Route::get('returns/{palletReturn:id}', [ShowPalletReturn::class, 'inWarehouse'])->name('pallet-returns.show')->withoutScopedBindings();
+Route::get('fulfilment-returns', [IndexPalletReturns::class, 'inWarehouse'])->name('pallet-returns.index');
+Route::get('fulfilment-returns/{palletReturn:id}', [ShowPalletReturn::class, 'inWarehouse'])->name('pallet-returns.show')->withoutScopedBindings();
+Route::get('fulfilment-returns/{palletReturn:id}/pallets', IndexPalletsInReturn::class)->name('pallet-returns.pallets.index')->withoutScopedBindings();
+Route::get('fulfilment-returns/{palletReturn:id}/stored-items', IndexStoredItemsInReturn::class)->name('pallet-returns.stored-items.index')->withoutScopedBindings();
