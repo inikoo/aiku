170be5f298db63a8015d3ce7525c460bcfdc2906
Artha <manager.arthachan@gmail.com>
2024-08-29 10:58:55 +0800

    fiox: routes

--- routes/grp/web/models/inventory/location_org_stock.php
+++ routes/grp/web/models/inventory/location_org_stock.php
@@ -12,6 +12,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::name('location_org_stock.')->prefix('location-org-stock/{locationOrgStock:id}')->group(function () {
     Route::delete('', DeleteLocationOrgStock::class)->name('delete')->withoutScopedBindings();
-    Route::patch('audit', AuditLocationOrgStock::class)->name('audit')->withoutScopedBindings();
+    Route::patch('audit', AuditLocationOrgStock::class)->name('audit');
     Route::patch('move/{targetLocation:id}', MoveOrgStockToOtherLocation::class)->name('move')->withoutScopedBindings();
 });
