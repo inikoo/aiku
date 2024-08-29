cc9bbcabe3e3929fcbc1dcf2614e40e98fd4faaf
KirinZero0 <aryapermana02@gmail.com>
2024-08-29 09:36:01 +0800

    fix without scoped binding

--- routes/maya/actions/inventory/location_org_stock.php
+++ routes/maya/actions/inventory/location_org_stock.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 Route::name('location_org_stock.')->prefix('location-org-stock/{locationOrgStock:id}')->group(function () {
     Route::delete('', DeleteLocationOrgStock::class)->name('delete');
     Route::patch('audit', AuditLocationOrgStock::class)->name('audit');
-    Route::patch('move/{locationOrgStock:id}', MoveOrgStockToOtherLocation::class)->name('move');
+    Route::patch('move/{locationOrgStock:id}', MoveOrgStockToOtherLocation::class)->name('move')->withoutScopedBindings();
 });
