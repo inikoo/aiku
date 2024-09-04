60985fdcf2f8d049cce20f0a114aca153fe6d041
KirinZero0 <aryapermana02@gmail.com>
2024-09-04 16:22:07 +0800

    Order Stat

--- app/Actions/Ordering/Order/UI/IndexOrders.php
+++ app/Actions/Ordering/Order/UI/IndexOrders.php
@@ -218,6 +218,13 @@ class IndexOrders extends OrgAction
                 ],
             ];
         }
+
+        if ($this->parent instanceof Shop)
+        {
+            $shop = $this->parent;
+        } else {
+            $shop = $this->parent->shop;
+        }
         return Inertia::render(
             'Ordering/Orders',
             [
@@ -241,6 +248,10 @@ class IndexOrders extends OrgAction
                     'current'    => $this->tab,
                     'navigation' => OrdersTabsEnum::navigation(),
                 ],
+
+                OrdersTabsEnum::STATS->value => $this->tab == OrdersTabsEnum::STATS->value ?
+                    fn () => GetOrderStats::run($shop)
+                    : Inertia::lazy(fn () => GetOrderStats::run($shop)),
 
                 OrdersTabsEnum::ORDERS->value => $this->tab == OrdersTabsEnum::ORDERS->value ?
                     fn () => OrdersResource::collection($orders)
