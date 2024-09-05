1a651d7c98ad7aaf373c64589b1c30c8b56bc777
YudhistaA <aryarajasa0@gmail.com>
2024-09-05 08:23:47 +0800

    set type location

--- resources/js/Components/StockCard/InfoCard.vue
+++ resources/js/Components/StockCard/InfoCard.vue
@@ -5,8 +5,10 @@ import { stockLocation, Datum } from "@/types/StockLocation"
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
 import PureTextarea from '@/Components/Pure/PureTextarea.vue'
 import { useFormatTime } from "@/Composables/useFormatTime"
-import { useForm, router } from '@inertiajs/vue3'
+import { router } from '@inertiajs/vue3'
 import { notify } from "@kyvg/vue3-notification"
+import Popover from '@/Components/Popover.vue'
+import Button from "@/Components/Elements/Buttons/Button.vue"
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faShoppingBasket, faClock, faPencil, faSave, faTimes } from '@far'
@@ -39,11 +41,13 @@ return(differenceDay)
 }
 
 
-const sendNotes = (item : Datum) =>{
+const SendEditLocation = (item : Datum) =>{
     router.patch(route(
         props.associateLocationRoute.name,
         { ...props.associateLocationRoute.parameters, location: item.id }),
-        { notes : item.notes},
+        { notes : item.notes,
+          type : item.type
+        },
         {
             onBefore: () => { loading.value = true },
             onSuccess: () => {
@@ -61,6 +65,11 @@ const sendNotes = (item : Datum) =>{
         })
 }
 
+const onSetPickingLocation = (item : Datum) =>{
+    item.type = 'picking'
+    SendEditLocation(item)
+}
+
 const hideOther = (id : Number) => {
   disclosure.value.filter((d, i) => i !== id).forEach(c => c())
 }
@@ -69,80 +78,96 @@ const hideOther = (id : Number) => {
 
 <template>
     <ul class="divide-y divide-gray-100 bg-white shadow-sm ring-1 ring-gray-900/5">
-            <Disclosure v-for="(location, index) in data?.locations?.data" :key="index" as="li" v-slot="{ open, close }">
-                <div class="relative flex justify-between gap-x-6 px-4 py-4 hover:bg-gray-50 sm:px-6 w-full" :class="open && 'bg-gray-100'"
-                     >
-                    <div class="flex items-center w-1/2 gap-x-2">
-                        <!-- Location Icon -->
-                        <DisclosureButton class="flex-none rounded-full focus:outline-none"
-                            :class="location.notes && 'text-yellow-400'"
-                            @click="hideOther(index)"
-                            :ref="el => (disclosure[index] = close)"
-                        >
-                            <FontAwesomeIcon v-tooltip="'Notes'"
-                                :icon="location.notes ? fasStickyNote : faStickyNote" />
-                        </DisclosureButton>
+        <Disclosure v-for="(location, index) in data?.locations?.data" :key="index" as="li" v-slot="{ open, close }">
+            <div class="relative flex justify-between gap-x-6 px-4 py-4 hover:bg-gray-50 sm:px-6 w-full"
+                :class="open && 'bg-gray-100'">
+                <div class="flex items-center w-1/2 gap-x-2">
+                    <!-- Location Icon -->
+                    <DisclosureButton class="flex-none rounded-full focus:outline-none"
+                        :class="location.notes && 'text-yellow-400'" @click="hideOther(index)"
+                        :ref="el => (disclosure[index] = close)">
+                        <FontAwesomeIcon v-tooltip="'Notes'" :icon="location.notes ? fasStickyNote : faStickyNote" />
+                    </DisclosureButton>
 
-                        <FontAwesomeIcon  :class="location.type == 'picking' && 'text-indigo-500'" class="h-5 w-5 flex-none rounded-full bg-gray-50" :icon="faShoppingBasket" />
-
-                        <div class="px-2">
-                            <div class="text-sm font-semibold leading-6 text-gray-900">
-                                <span v-tooltip="'location'">{{ location.location.code }} {{ " " }}</span>
-                                <span v-if="location.settings.min_stock || location.settings.max_stock"
-                                    class="text-gray-400">
-                                    (<span v-tooltip="'minimum stock'">{{ location?.settings?.min_stock }}</span>,
-                                    <span v-tooltip="'maximum stock'">{{ location?.settings?.max_stock }}</span>)
-                                </span>
-                                <span v-else class="text-gray-400">( ? )</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Right Side: Stock Information -->
-                    <div class="flex items-center w-1/4 gap-x-4">
-                        <div class="flex sm:flex-col sm:items-end">
-                            <div class="flex gap-x-1" v-tooltip="`Audited At : ${useFormatTime(location.audited_at)} `">
-                                <div class="flex-auto">
-                                    <div class="text-sm font-semibold leading-6 text-gray-900" >{{daysAudit(location.audited_at)}}</div>
+                    <div class="relative">
+                        <Popover position="left-0 top-[-120px]">
+                            <template #button>
+                                <FontAwesomeIcon :class="location.type == 'picking' && 'text-indigo-500'"
+                                    class="h-5 w-5 flex-none rounded-full bg-gray-50" :icon="faShoppingBasket" />
+                            </template>
+                            <template #content="{ close: closed }">
+                                <div class="w-[250px]">
+                                    <p class="text-xs pb-4">
+                                        Do you want set <strong>{{ location.code }}</strong> to be the Picking location
+                                        ?
+                                    </p>
+                                    <div class="flex justify-end gap-2">
+                                        <Button type="gray" size="xs" label="NO" @click="closed()"></Button>
+                                        <Button type="save" size="xs" label="Yes" @click="()=>onSetPickingLocation(location)"></Button>
+                                    </div>
                                 </div>
-                                <FontAwesomeIcon class="h-4 w-4 mt-1 flex-none rounded-full bg-gray-50"
-                                    :icon="faClock" />
-                            </div>
-                        </div>
+                            </template>
+                        </Popover>
                     </div>
 
-                    <!-- Right Side: Stock Information (Duplicated) -->
-                    <slot name="Quantity" :itemData="location" :index="index">
-                        <div class="flex justify-end w-1/4">
-                            <div class="flex justify-end">
-                                <div v-tooltip="'Quantity'" class="text-sm font-semibold leading-6 text-gray-900">{{
-                                    location.quantity }}</div>
-                            </div>
+                    <div class="px-2">
+                        <div class="text-sm font-semibold leading-6 text-gray-900">
+                            <span v-tooltip="'location'">{{ location.location.code }} {{ " " }}</span>
+                            <span v-if="location.settings.min_stock || location.settings.max_stock"
+                                class="text-gray-400">
+                                (<span v-tooltip="'minimum stock'">{{ location?.settings?.min_stock }}</span>,
+                                <span v-tooltip="'maximum stock'">{{ location?.settings?.max_stock }}</span>)
+                            </span>
+                            <span v-else class="text-gray-400">( ? )</span>
                         </div>
-                    </slot>
+                    </div>
                 </div>
 
-                <DisclosurePanel class="px-4 pb-2 pt-4 text-sm text-gray-500">
-                    <PureTextarea :modelValue="location.notes" @update:modelValue="(value)=>location.notes  = value" :disabled="!editNotes" :rows="4" placeholder="Write a notes ....">
-                        <template v-if="!loading" #stateIcon>
-                            <div v-if="!editNotes" @click="()=>editNotes=true"
+                <!-- Right Side: Stock Information -->
+                <div class="flex items-center w-1/4 gap-x-4">
+                    <div class="flex sm:flex-col sm:items-end">
+                        <div class="flex gap-x-1" v-tooltip="`Audited At : ${useFormatTime(location.audited_at)} `">
+                            <div class="flex-auto">
+                                <div class="text-sm font-semibold leading-6 text-gray-900">
+                                    {{daysAudit(location.audited_at)}}</div>
+                            </div>
+                            <FontAwesomeIcon class="h-4 w-4 mt-1 flex-none rounded-full bg-gray-50" :icon="faClock" />
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Right Side: Stock Information (Duplicated) -->
+                <slot name="Quantity" :itemData="location" :index="index">
+                    <div class="flex justify-end w-1/4">
+                        <div class="flex justify-end">
+                            <div v-tooltip="'Quantity'" class="text-sm font-semibold leading-6 text-gray-900">{{
+                                location.quantity }}</div>
+                        </div>
+                    </div>
+                </slot>
+            </div>
+
+            <DisclosurePanel class="px-4 pb-2 pt-4 text-sm text-gray-500">
+                <PureTextarea :modelValue="location.notes" @update:modelValue="(value)=>location.notes  = value"
+                    :disabled="!editNotes" :rows="4" placeholder="Write a notes ....">
+                    <template v-if="!loading" #stateIcon>
+                        <div v-if="!editNotes" @click="()=>editNotes=true"
+                            class="w-8 h-8 flex items-center justify-center text-sm text-white rounded-full bg-indigo-500 cursor-pointer">
+                            <FontAwesomeIcon :icon="faPencil" />
+                        </div>
+                        <div v-else class="flex gap-3">
+                            <div @click="()=>editNotes=false"
+                                class="w-8 h-8 flex items-center justify-center text-sm text-red-500 rounded-full bg-white border border-red-500 cursor-pointer">
+                                <FontAwesomeIcon :icon="faTimes" />
+                            </div>
+                            <div @click="()=>SendEditLocation(location)"
                                 class="w-8 h-8 flex items-center justify-center text-sm text-white rounded-full bg-indigo-500 cursor-pointer">
-                                <FontAwesomeIcon :icon="faPencil" />
+                                <FontAwesomeIcon :icon="faSave" />
                             </div>
-                            <div v-else class="flex gap-3">
-                                <div 
-                                    @click="()=>editNotes=false"
-                                    class="w-8 h-8 flex items-center justify-center text-sm text-red-500 rounded-full bg-white border border-red-500 cursor-pointer">
-                                    <FontAwesomeIcon :icon="faTimes" />
-                                </div>
-                                <div @click="()=>sendNotes(location)"
-                                    class="w-8 h-8 flex items-center justify-center text-sm text-white rounded-full bg-indigo-500 cursor-pointer">
-                                    <FontAwesomeIcon :icon="faSave" />
-                                </div>
-                            </div>
-                        </template>
-                    </PureTextarea>
-                </DisclosurePanel>
-            </Disclosure>
+                        </div>
+                    </template>
+                </PureTextarea>
+            </DisclosurePanel>
+        </Disclosure>
     </ul>
 </template>
