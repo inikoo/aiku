44370c0cb8c07514ae28ade4921bbab67bb7d6c6
Raul Perusquia <raul@inikoo.com>
2024-09-10 13:09:14 +0800

    offers permissions

--- app/Actions/UI/Dropshipping/Offers/ShowOfferDashboard.php
+++ app/Actions/UI/Dropshipping/Offers/ShowOfferDashboard.php
@@ -11,7 +11,6 @@ use App\Actions\Catalogue\Shop\UI\ShowShop;
 use App\Actions\OrgAction;
 use App\Enums\UI\Dropshipping\OffersTabsEnum;
 use App\Models\Catalogue\Shop;
-use App\Models\Inventory\Warehouse;
 use App\Models\SysAdmin\Organisation;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -19,10 +18,10 @@ use Lorisleiva\Actions\ActionRequest;
 
 class ShowOfferDashboard extends OrgAction
 {
-    // public function authorize(ActionRequest $request): bool
-    // {
-    //     return $request->user()->hasPermissionTo("fulfilment.{$this->warehouse->id}.view");
-    // }
+    public function authorize(ActionRequest $request): bool
+    {
+        return $request->user()->hasPermissionTo("offers.{$this->shop->id}.view");
+    }
 
 
     public function asController(Organisation $organisation, Shop $shop, ActionRequest $request): ActionRequest
