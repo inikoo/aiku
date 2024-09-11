f22fba65c8aee948f36848cb4f5d8f463ec5869e
KirinZero0 <aryapermana02@gmail.com>
2024-09-11 16:39:21 +0800

    #858

---
+++ app/Http/Resources/Catalogue/OffersResource.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Author: Artha <artha@aw-advantage.com>
+ * Created: Tue, 18 Apr 2023 15:23:04 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2023, Raul A Perusquia Flores
+ */
+
+namespace App\Http\Resources\Catalogue;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property int $shop_id
+ * @property int $offer_campaign_id
+ * @property string $slug
+ * @property string $code
+ * @property string $data
+ * @property mixed $created_at
+ * @property mixed $updated_at
+ * @property string $name
+ *
+ */
+class OffersResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'shop_slug'           => $this->shop_slug,
+            'offer_campaign_slug' => $this->offer_campaign_slug,
+            'slug'                => $this->slug,
+            'code'                => $this->code,
+            'name'                => $this->name,
+        ];
+    }
+}
--- routes/grp/web/org/shops/discounts.php
+++ routes/grp/web/org/shops/discounts.php
@@ -5,11 +5,11 @@
  * Copyright (c) 2024, Raul A Perusquia Flores
  */
 
+use App\Actions\Discounts\Offer\UI\IndexOffers;
 use App\Actions\Discounts\OfferCampaign\UI\IndexOfferCampaigns;
 use App\Actions\UI\Dropshipping\Offers\ShowOfferDashboard;
 use App\Stubs\UIDummies\CreateDummy;
 use App\Stubs\UIDummies\EditDummy;
-use App\Stubs\UIDummies\IndexDummies;
 use App\Stubs\UIDummies\ShowDummy;
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +23,7 @@ Route::name("campaigns.")->prefix('campaigns')
 
 Route::name("offers.")->prefix('offers')
     ->group(function () {
-        Route::get('', IndexDummies::class)->name('index');
+        Route::get('', IndexOffers::class)->name('index');
         Route::get('create', CreateDummy::class)->name('create');
         Route::get('{offer}', ShowDummy::class)->name('show');
         Route::get('{offer}/edit', EditDummy::class)->name('edit');
