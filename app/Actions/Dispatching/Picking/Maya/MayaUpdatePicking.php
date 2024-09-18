5c6285527d0d025ff45d0dd66785069420037b6d
KirinZero0 <aryapermana02@gmail.com>
2024-09-18 09:44:19 +0800

    Fix Namespace

--- app/Actions/Dispatching/Picking/Maya/MayaUpdatePicking.php
+++ app/Actions/Dispatching/Picking/Maya/MayaUpdatePicking.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2023, Raul A Perusquia Flores
  */
 
-namespace App\Actions\Dispatching\Picking;
+namespace App\Actions\Dispatching\Picking\Maya;
 
 use App\Actions\OrgAction;
 use App\Actions\Traits\WithActionUpdate;
