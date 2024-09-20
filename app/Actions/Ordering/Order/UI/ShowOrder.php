d6462cd9463a6058992de3ae19aaff13b109105b
Artha <manager.arthachan@gmail.com>
2024-09-20 15:43:00 +0800

    fix: delivery note route

--- app/Actions/Ordering/Order/UI/ShowOrder.php
+++ app/Actions/Ordering/Order/UI/ShowOrder.php
@@ -318,6 +318,10 @@ class ShowOrder extends OrgAction
                             'shop'  => $order->shop->slug,
                             'scope' => $order->slug
                         ]
+                    ],
+                    'delivery_note' => [
+                        'name'       => 'grp.org.shops.show.ordering.orders.show.delivery-note',
+                        'parameters' => $request->route()->originalParameters()
                     ]
                 ],
                 // 'alert'   => [  // TODO
@@ -432,7 +436,6 @@ class ShowOrder extends OrgAction
                 ],
                 'currency'       => CurrencyResource::make($order->currency)->toArray(request()),
                 'data'           => OrderResource::make($order),
-                'delivery_note'  => DeliveryNotesResource::make($order->deliveryNotes()),
                 // 'nonProductItems' => $nonProductItems,
                 // 'showcase'=> GetOrderShowcase::run($order),
 
