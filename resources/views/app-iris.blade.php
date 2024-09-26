a204871e91e72e7e7072a2b9815c27e902ab9721
Raul Perusquia <raul@inikoo.com>
2024-09-26 18:48:22 +0800

    remove new relic browser
    it is maybe outdated

--- resources/views/app-iris.blade.php
+++ resources/views/app-iris.blade.php
@@ -3,9 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @env(['staging', 'production'])
-        @include('new-relic', ['appID' => Config::get('new-relic.application_id.iris')])
-        @endenv
         <title inertia>{{ config('app.name', 'Iris') }}</title>
         
         <!-- Fonts -->
--- resources/views/app-pupil.blade.php
+++ resources/views/app-pupil.blade.php
@@ -4,9 +4,6 @@
         <meta charset="utf-8">
         <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @env(['staging', 'production'])
-        @include('new-relic', ['appID' => Config::get('new-relic.application_id.pupil')])
-        @endenv
         <title inertia>{{ config('app.name', 'Pupil') }}</title>
 
         <!-- Fonts -->
