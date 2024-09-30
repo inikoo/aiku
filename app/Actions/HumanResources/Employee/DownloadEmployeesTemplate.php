c1d5aad1c8efb5f6b3de61b982fe606f09888915
Artha <manager.arthachan@gmail.com>
2024-09-30 15:22:50 +0800

    fix: header & footer preview

--- app/Actions/HumanResources/Employee/DownloadEmployeesTemplate.php
+++ app/Actions/HumanResources/Employee/DownloadEmployeesTemplate.php
@@ -8,11 +8,6 @@
 namespace App\Actions\HumanResources\Employee;
 
 use App\Exports\HumanResources\EmployeesTemplateExport;
-use App\Exports\Pallets\PalletTemplateExport;
-use App\Models\Fulfilment\Fulfilment;
-use App\Models\Fulfilment\FulfilmentCustomer;
-use App\Models\Fulfilment\PalletDelivery;
-use App\Models\Fulfilment\PalletReturn;
 use App\Models\SysAdmin\Organisation;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Lorisleiva\Actions\Concerns\WithAttributes;
---
+++ app/Actions/Web/Webpage/UI/ShowHeaderPreview.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Author: Artha <artha@aw-advantage.com>
+ * Created: Thu, 25 Apr 2024 16:56:22 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2024, Raul A Perusquia Flores
+ */
+
+namespace App\Actions\Web\Webpage\UI;
+
+use App\Actions\OrgAction;
+use App\Actions\Web\Website\GetWebsiteWorkshopHeader;
+use App\Models\Catalogue\Shop;
+use App\Models\Fulfilment\Fulfilment;
+use App\Models\SysAdmin\Organisation;
+use App\Models\Web\Webpage;
+use App\Models\Web\Website;
+use Inertia\Inertia;
+use Inertia\Response;
+use Lorisleiva\Actions\ActionRequest;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ShowHeaderPreview extends OrgAction
+{
+    use AsAction;
+
+    private Website $website;
+
+    private Webpage|Website $parent;
+    private Fulfilment|Shop $scope;
+
+    public function handle(Website $website): Website
+    {
+        return $website;
+    }
+
+    public function htmlResponse(Website $website, ActionRequest $request): Response
+    {
+        return Inertia::render(
+            'Web/PreviewWorkshop',
+            [
+                'data' => GetWebsiteWorkshopHeader::run($website)
+            ]
+        );
+    }
+
+    public function asController(Organisation $organisation, Fulfilment $fulfilment, Website $website, ActionRequest $request): Website
+    {
+        $this->parent = $website;
+        $this->scope  = $fulfilment;
+        $this->initialisationFromFulfilment($fulfilment, $request);
+
+        return $website;
+    }
+}
