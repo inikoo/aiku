8fbcde3edbff3ceba5c243e04c8594602e7c3ce2
KirinZero0 <aryapermana02@gmail.com>
2024-10-02 15:22:09 +0800

    Index and Show Org Partner UI Test

--- tests/Feature/ProcurementUITest.php
+++ tests/Feature/ProcurementUITest.php
@@ -6,13 +6,18 @@
  */
 
 use App\Actions\Procurement\OrgAgent\StoreOrgAgent;
+use App\Actions\Procurement\OrgPartner\StoreOrgPartner;
 use App\Actions\Procurement\OrgSupplier\StoreOrgSupplier;
 use App\Actions\SupplyChain\Agent\StoreAgent;
 use App\Actions\SupplyChain\Supplier\StoreSupplier;
+use App\Actions\SysAdmin\Organisation\StoreOrganisation;
+use App\Enums\SysAdmin\Organisation\OrganisationTypeEnum;
 use App\Models\Procurement\OrgAgent;
+use App\Models\Procurement\OrgPartner;
 use App\Models\Procurement\OrgSupplier;
 use App\Models\SupplyChain\Agent;
 use App\Models\SupplyChain\Supplier;
+use App\Models\SysAdmin\Organisation;
 use Inertia\Testing\AssertableInertia;
 
 use function Pest\Laravel\actingAs;
@@ -28,6 +33,16 @@ beforeAll(function () {
 beforeEach(function () {
     $this->organisation = createOrganisation();
     $this->adminGuest   = createAdminGuest($this->organisation->group);
+
+    $otherOrg = Organisation::skip(1)->take(1)->first();
+    if(!$otherOrg){
+        $orgData = Organisation::factory()->definition();
+        data_set($orgData, 'code', 'acmo');
+        data_set($orgData, 'type', OrganisationTypeEnum::SHOP);
+        $otherOrg = StoreOrganisation::make()->action($this->organisation->group, $orgData);
+    }
+
+    $this->otherOrg = $otherOrg;
 
     $agent = Agent::first();
     if (!$agent) {
@@ -72,6 +87,16 @@ beforeEach(function () {
     }
 
     $this->orgSupplier = $orgSupplier;
+
+    $orgPartner = OrgPartner::first();
+    if (!$orgPartner) {
+        $orgPartner = StoreOrgPartner::make()->action(
+            $this->organisation,
+            $this->otherOrganisation,
+        );
+    }
+
+    $this->orgPartner = $orgPartner;
 
     Config::set(
         'inertia.testing.page_paths',
@@ -150,5 +175,34 @@ test('UI Index purchase orders', function () {
             ->component('Procurement/PurchaseOrders')
             ->has('title')
             ->has('breadcrumbs', 3);
+    });
+});
+
+test('UI Index org partners', function () {
+    $response = $this->get(route('grp.org.procurement.org_partners.index', [$this->organisation->slug]));
+
+    $response->assertInertia(function (AssertableInertia $page) {
+        $page
+            ->component('Org/Procurement/Partners')
+            ->has('title')
+            ->has('breadcrumbs', 3);
+    });
+});
+
+test('UI show org partners', function () {
+    $response = $this->get(route('grp.org.procurement.org_partners.show', [$this->organisation->slug, $this->orgPartner->id]));
+    $response->assertInertia(function (AssertableInertia $page) {
+        $page
+            ->component('Org/Procurement/Partner')
+            ->has('title')
+            ->has('breadcrumbs', 3)
+            ->has(
+                'pageHead',
+                fn (AssertableInertia $page) => $page
+                        ->where('title', $this->orgPartner->partner->name)
+                        ->etc()
+            )
+            ->has('tabs');
+
     });
 });
