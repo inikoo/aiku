220e2d6ab531c17f2623ba029bf82a3d843328f2
ghanes556 <gustiganes@gmail.com>
2024-10-07 14:57:31 +0800

    #946: inventory ui test show location (showcase tab)

--- tests/Feature/InventoryUITest.php
+++ tests/Feature/InventoryUITest.php
@@ -6,10 +6,11 @@
  * Github: https://github.com/ganes556
  * Copyright: 2024
  *
-*/
+ */
 
 use App\Actions\Inventory\Location\StoreLocation;
 use App\Actions\Inventory\Warehouse\StoreWarehouse;
+use App\Enums\UI\Inventory\LocationTabsEnum;
 use App\Models\Inventory\Location;
 use App\Models\Inventory\Warehouse;
 use Inertia\Testing\AssertableInertia;
@@ -22,90 +23,112 @@ beforeAll(function () {
 });
 
 beforeEach(function () {
-
     $this->organisation = createOrganisation();
-    $this->adminGuest   = createAdminGuest($this->organisation->group);
-
+    $this->adminGuest = createAdminGuest($this->organisation->group);
 
     $warehouse = Warehouse::first();
     if (!$warehouse) {
-        data_set($storeData, 'code', 'CODE');
-        data_set($storeData, 'name', 'NAME');
+        data_set($storeData, "code", "CODE");
+        data_set($storeData, "name", "NAME");
 
-        $warehouse = StoreWarehouse::make()->action(
-            $this->organisation,
-            $storeData
-        );
+        $warehouse = StoreWarehouse::make()->action($this->organisation, $storeData);
     }
     $this->warehouse = $warehouse;
 
     $location = Location::first();
     if (!$location) {
-        $location = StoreLocation::make()->action(
-            $warehouse,
-            $storeData
-        );
+        $location = StoreLocation::make()->action($warehouse, $storeData);
     }
     $this->location = $location;
 
-    Config::set(
-        'inertia.testing.page_paths',
-        [resource_path('js/Pages/Grp')]
-    );
+    Config::set("inertia.testing.page_paths", [resource_path("js/Pages/Grp")]);
     actingAs($this->adminGuest->getUser());
 });
 
-
-test('UI Index locations', function () {
-    $response = get(route('grp.org.warehouses.show.infrastructure.locations.index', [$this->organisation->slug, $this->warehouse->slug]));
+test("UI Index locations", function () {
+    $response = get(
+        route("grp.org.warehouses.show.infrastructure.locations.index", [
+            $this->organisation->slug,
+            $this->warehouse->slug,
+        ])
+    );
     $response->assertInertia(function (AssertableInertia $page) {
         $page
-            ->component('Org/Warehouse/Locations')
-            ->has('title')
-            ->has('breadcrumbs', 3)
+            ->component("Org/Warehouse/Locations")
+            ->has("title")
+            ->has("breadcrumbs", 3)
             ->has(
-                'pageHead',
-                fn (AssertableInertia $page) => $page
-                        ->where('title', 'locations')
-                        ->etc()
+                "pageHead",
+                fn (AssertableInertia $page) => $page->where("title", "locations")->etc()
             )
-            ->has('tagRoute')
-            ->has('tagsList')
-            ->has('data');
+            ->has("tagRoute")
+            ->has("tagsList")
+            ->has("data");
     });
 });
 
-test('UI Create location', function () {
-    $response = get(route('grp.org.warehouses.show.infrastructure.locations.create', [$this->organisation->slug, $this->warehouse->slug,]));
+test("UI Create location", function () {
+    $response = get(
+        route("grp.org.warehouses.show.infrastructure.locations.create", [
+            $this->organisation->slug,
+            $this->warehouse->slug,
+        ])
+    );
     $response->assertInertia(function (AssertableInertia $page) {
         $page
-            ->component('CreateModel')
-            ->has('title')
-            ->has('breadcrumbs', 4)
+            ->component("CreateModel")
+            ->has("title")
+            ->has("breadcrumbs", 4)
             ->has(
-                'pageHead',
-                fn (AssertableInertia $page) => $page
-                        ->where('title', 'new location')
-                        ->etc()
+                "pageHead",
+                fn (AssertableInertia $page) => $page->where("title", "new location")->etc()
             )
-            ->has('formData');
+            ->has("formData");
     });
 });
 
-test('UI Show location', function () {
-    $response = get(route('grp.org.warehouses.show.infrastructure.locations.show', [$this->organisation->slug, $this->warehouse->slug, $this->location->slug]));
+test("UI Show location", function () {
+    $response = get(
+        route("grp.org.warehouses.show.infrastructure.locations.show", [
+            $this->organisation->slug,
+            $this->warehouse->slug,
+            $this->location->slug,
+        ])
+    );
     $response->assertInertia(function (AssertableInertia $page) {
         $page
-            ->component('Org/Warehouse/Location')
-            ->has('title')
-            ->has('breadcrumbs', 3)
+            ->component("Org/Warehouse/Location")
+            ->has("title")
+            ->has("breadcrumbs", 3)
             ->has(
-                'pageHead',
-                fn (AssertableInertia $page) => $page
-                        ->where('title', $this->location->slug)
-                        ->etc()
+                "pageHead",
+                fn (AssertableInertia $page) => $page->where("title", $this->location->slug)->etc()
             )
-            ->has('navigation')
-            ->has('tabs');
+            ->has("navigation")
+            ->has("tabs");
+    });
+});
+
+test("UI Show location (showcase tab)", function () {
+    $response = get(
+        route("grp.org.warehouses.show.infrastructure.locations.show", [
+            $this->organisation->slug,
+            $this->warehouse->slug,
+            $this->location->slug,
+            "tabs" => LocationTabsEnum::SHOWCASE->value,
+        ])
+    );
+    $response->assertInertia(function (AssertableInertia $page) {
+        $page
+            ->component("Org/Warehouse/Location")
+            ->has("title")
+            ->has("breadcrumbs", 3)
+            ->has(
+                "pageHead",
+                fn (AssertableInertia $page) => $page->where("title", $this->location->slug)->etc()
+            )
+            ->has("navigation")
+            ->has("tabs")
+            ->has(LocationTabsEnum::SHOWCASE->value);
     });
 });
