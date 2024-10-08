94e9ee1b90d80795fd90411396ca636cf64a46b5
dandi <dandiferrari75@gmail.com>
2024-10-08 11:14:35 +0800

    delete file basket.json

--- resources/js/Components/Websites/Fields/PanelProperties.vue
+++ resources/js/Components/Websites/Fields/PanelProperties.vue
@@ -27,37 +27,28 @@ watch(compModel, () => {
         <!-- <pre>{{ model?.border }}</pre> -->
     </div>
 
-
     <div v-if="model?.background" class="border-t border-gray-300 bg-gray-100 pb-3">
         <div class="w-full text-center py-1 font-semibold select-none">{{ trans('Background') }}</div>
-        
-        <BackgroundProperty
-            v-model="model.background"
-        />
+
+        <BackgroundProperty v-model="model.background" />
     </div>
 
     <div v-if="model?.border" class="border-t border-gray-300 bg-gray-100">
         <div class="w-full text-center py-1 font-semibold select-none">{{ trans('Border') }}</div>
-        
-        <BorderProperty
-            v-model="model.border"
-        />
+
+        <BorderProperty v-model="model.border" />
     </div>
 
     <div v-if="model?.padding" class="border-t border-gray-300 bg-gray-100">
         <div class="w-full text-center py-1 font-semibold select-none">{{ trans('Padding') }}</div>
-        
-        <PaddingMarginProperty
-            v-model="model.padding"
-        />
+
+        <PaddingMarginProperty v-model="model.padding" />
     </div>
 
     <div v-if="model?.margin" class="border-t border-gray-300 bg-gray-100">
         <div class="w-full text-center py-1 font-semibold select-none">{{ trans('Margin') }}</div>
-        
-        <PaddingMarginProperty
-            v-model="model.margin"
-        />
+
+        <PaddingMarginProperty v-model="model.margin" />
     </div>
 
 </template>
