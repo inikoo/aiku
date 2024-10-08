490e75fbf5cfe72d8d177a188b5f5af630e28b87
KirinZero0 <aryapermana02@gmail.com>
2024-10-08 12:17:23 +0800

    support for visibility in fetch webblock

--- app/Actions/Web/Webpage/FetchWebpageWebBlocks.php
+++ app/Actions/Web/Webpage/FetchWebpageWebBlocks.php
@@ -23,110 +23,123 @@ class FetchWebpageWebBlocks extends OrgAction
     public function handle(Webpage $webpage): Webpage
     {
 
-        foreach (Arr::get($webpage->migration_data, "blocks", []) as $auroraBlock) {
+        foreach (Arr::get($webpage->migration_data['both'], "blocks", []) as $auroraBlock) {
             $migrationData = md5(json_encode($auroraBlock));
-
-            if ($auroraBlock['type'] == "text") {
-                $webBlockType = WebBlockType::where("slug", "text")->first();
-                $block        = $webBlockType->toArray();
-                data_set($block, "data.fieldValue.value", $auroraBlock['text_blocks'][0]['text']);
-
-            }
-
-            if ($auroraBlock['type'] == 'images') {
-
-                $webBlockType = WebBlockType::where("slug", "gallery")->first();
-                $block = $webBlockType->toArray();
-
-
-                foreach ($auroraBlock['images'] as $image) {
-                    $imagesArray[] = [
-                        'aurora_source' => $image['src']
-
-                    ];
-                }
-                $fieldValue['value'] = $imagesArray;
-                data_set($block, "data.fieldValue.value", $fieldValue['value']);
-            }
-
-            data_set($block, "data.properties.padding.unit", "px");
-            data_set($block, "data.properties.padding.left.value", 20);
-            data_set($block, "data.properties.padding.right.value", 20);
-            data_set($block, "data.properties.padding.bottom.value", 20);
-            data_set($block, "data.properties.padding.top.value", 20);
-            $webBlock = StoreWebBlock::make()->action(
-                $webBlockType,
-                [
-                    'layout'             => $block,
-                    'migration_checksum' => $migrationData
-                ],
-                strict: false
-            );
-
-            if ($webBlock->webBlockType->name == 'Gallery') {
-                $imageSources = [];
-                foreach ($webBlock->layout['data']['fieldValue']['value'] as $imageRawData) {
-                    if (!isset($imageRawData['aurora_source'])) {
-                        break;
-                    }
-                    $auroraImage = $imageRawData['aurora_source'];
-
-                    $urlToFile = 'https://www.'.$webpage->website->domain.$auroraImage;
-                    $content = file_get_contents($urlToFile);
-                    $tempPath = tempnam(sys_get_temp_dir(), 'img_');
-
-                    $headers = get_headers($urlToFile, 1);
-                    $mimeType = $headers['Content-Type'];
-
-                    if ($mimeType == 'image/jpeg') {
-                        $extension = '.jpg';
-                    } elseif ($mimeType == 'image/png') {
-                        $extension = '.png';
-                    } else {
-                        $extension = '.jpg';
-                    }
-
-                    $tempFile = $tempPath . $extension;
-
-                    file_put_contents($tempFile, $content);
-
-                    $media = SaveModelImages::run($webBlock, [
-                        'path' => $tempFile,
-                        'originalName' => 'aurora_image'
-                    ]);
-
-                    $image = $media->getImage();
-                    $imageSource = GetPictureSources::run($image);
-                    $imageSources[] = ["image" => ["source" => $imageSource]];
-                }
-
-                data_set($block, "data.fieldValue.value", $imageSources);
-                $webBlock->update([
-                    'layout'             => $block,
-                ]);
-            }
-
-            $webpage->modelHasWebBlocks()->create(
-                [
-                    'group_id'           => $webpage->group_id,
-                    'organisation_id'    => $webpage->organisation_id,
-                    'shop_id'            => $webpage->shop_id,
-                    'website_id'         => $webpage->website_id,
-                    'webpage_id'         => $webpage->id,
-                    'position'           => 1,
-                    'model_id'           => $webpage->id,
-                    'model_type'         => class_basename(Webpage::class),
-                    'web_block_id'       => $webBlock->id,
-                    'migration_checksum' => $migrationData
-                ]
-            );
-            UpdateWebpageContent::run($webpage->refresh());
-
-            BroadcastPreviewWebpage::dispatch($webpage);
+            $this->processData($webpage, $auroraBlock, $migrationData);
+        }
+        foreach (Arr::get($webpage->migration_data['loggedIn'], "blocks", []) as $auroraBlock) {
+            $migrationData = md5(json_encode($auroraBlock));
+            $this->processData($webpage, $auroraBlock, $migrationData, ['loggedIn' => true, 'loggedOut' => false]);
+        }
+        foreach (Arr::get($webpage->migration_data['loggedOut'], "blocks", []) as $auroraBlock) {
+            $migrationData = md5(json_encode($auroraBlock));
+            $this->processData($webpage, $auroraBlock, $migrationData, ['loggedIn' => false, 'loggedOut' => true]);
         }
 
 
         return $webpage;
+    }
+
+    private function processData(Webpage $webpage, $auroraBlock, $migrationData, $visibilty = ['loggedIn' => true, 'loggedOut' => true])
+    {
+        if ($auroraBlock['type'] == "text") {
+            $webBlockType = WebBlockType::where("slug", "text")->first();
+            $block        = $webBlockType->toArray();
+            data_set($block, "data.fieldValue.value", $auroraBlock['text_blocks'][0]['text']);
+
+        }
+
+        if ($auroraBlock['type'] == 'images') {
+
+            $webBlockType = WebBlockType::where("slug", "gallery")->first();
+            $block = $webBlockType->toArray();
+
+
+            foreach ($auroraBlock['images'] as $image) {
+                $imagesArray[] = [
+                    'aurora_source' => $image['src']
+
+                ];
+            }
+            $fieldValue['value'] = $imagesArray;
+            data_set($block, "data.fieldValue.value", $fieldValue['value']);
+        }
+
+        data_set($block, "data.properties.padding.unit", "px");
+        data_set($block, "data.properties.padding.left.value", 20);
+        data_set($block, "data.properties.padding.right.value", 20);
+        data_set($block, "data.properties.padding.bottom.value", 20);
+        data_set($block, "data.properties.padding.top.value", 20);
+        data_set($block, "data.visibility", $visibilty);
+        $webBlock = StoreWebBlock::make()->action(
+            $webBlockType,
+            [
+                'layout'             => $block,
+                'migration_checksum' => $migrationData
+            ],
+            strict: false
+        );
+
+        if ($webBlock->webBlockType->name == 'Gallery') {
+            $imageSources = [];
+            foreach ($webBlock->layout['data']['fieldValue']['value'] as $imageRawData) {
+                if (!isset($imageRawData['aurora_source'])) {
+                    break;
+                }
+                $auroraImage = $imageRawData['aurora_source'];
+
+                $urlToFile = 'https://www.'.$webpage->website->domain.$auroraImage;
+                $content = file_get_contents($urlToFile);
+                $tempPath = tempnam(sys_get_temp_dir(), 'img_');
+
+                $headers = get_headers($urlToFile, 1);
+                $mimeType = $headers['Content-Type'];
+
+                if ($mimeType == 'image/jpeg') {
+                    $extension = '.jpg';
+                } elseif ($mimeType == 'image/png') {
+                    $extension = '.png';
+                } else {
+                    $extension = '.jpg';
+                }
+
+                $tempFile = $tempPath . $extension;
+
+                file_put_contents($tempFile, $content);
+
+                $media = SaveModelImages::run($webBlock, [
+                    'path' => $tempFile,
+                    'originalName' => 'aurora_image'
+                ]);
+
+                $image = $media->getImage();
+                $imageSource = GetPictureSources::run($image);
+                $imageSources[] = ["image" => ["source" => $imageSource]];
+            }
+
+            data_set($block, "data.fieldValue.value", $imageSources);
+            $webBlock->update([
+                'layout'             => $block,
+            ]);
+        }
+
+        $webpage->modelHasWebBlocks()->create(
+            [
+                'group_id'           => $webpage->group_id,
+                'organisation_id'    => $webpage->organisation_id,
+                'shop_id'            => $webpage->shop_id,
+                'website_id'         => $webpage->website_id,
+                'webpage_id'         => $webpage->id,
+                'position'           => 1,
+                'model_id'           => $webpage->id,
+                'model_type'         => class_basename(Webpage::class),
+                'web_block_id'       => $webBlock->id,
+                'migration_checksum' => $migrationData
+            ]
+        );
+        UpdateWebpageContent::run($webpage->refresh());
+
+        BroadcastPreviewWebpage::dispatch($webpage);
     }
 
     public function action(Webpage $webpage): Webpage
