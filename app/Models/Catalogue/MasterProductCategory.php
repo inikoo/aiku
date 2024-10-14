90e71cd5c5093fb8d67c58413c6203bf1a824132
KirinZero0 <aryapermana02@gmail.com>
2024-10-14 13:35:51 +0800

    #1004: Migration and model

---
+++ app/Models/Catalogue/MasterProductCategory.php
@@ -0,0 +1,136 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 14-10-2024-13h-28m
+ * github: https://github.com/KirinZero0
+ * copyright 2024
+*/
+
+
+namespace App\Models\Catalogue;
+
+use App\Enums\Catalogue\ProductCategory\ProductCategoryStateEnum;
+use App\Enums\Catalogue\ProductCategory\ProductCategoryTypeEnum;
+use App\Models\CRM\Favourite;
+use App\Models\Helpers\UniversalSearch;
+use App\Models\SysAdmin\Group;
+use App\Models\SysAdmin\Organisation;
+use App\Models\Traits\HasHistory;
+use App\Models\Traits\HasImage;
+use App\Models\Traits\HasUniversalSearch;
+use App\Models\Traits\InShop;
+use App\Models\Web\Webpage;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection as LaravelCollection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class MasterProductCategory extends Model implements Auditable, HasMedia
+{
+    use HasSlug;
+    use SoftDeletes;
+    use HasUniversalSearch;
+    use HasHistory;
+    use HasImage;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'data'             => 'array',
+        'state'            => ProductCategoryStateEnum::class,
+        'type'             => ProductCategoryTypeEnum::class,
+        'activated_at'     => 'datetime',
+        'discontinuing_at' => 'datetime',
+        'discontinued_at'  => 'datetime',
+    ];
+
+    protected $attributes = [
+        'data' => '{}',
+    ];
+
+    public function generateTags(): array
+    {
+        return [
+            'catalogue',
+        ];
+    }
+
+    protected array $auditInclude = [
+        'code',
+        'name',
+        'description',
+    ];
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom(function () {
+                return $this->code.'-'.$this->group->code;
+            })
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate()
+            ->slugsShouldBeNoLongerThan(64);
+    }
+
+    public function group(): BelongsTo
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(MasterProductCategory::class, 'master_department_id');
+    }
+
+    public function subDepartment(): BelongsTo
+    {
+        return $this->belongsTo(MasterProductCategory::class, 'master_sub_department_id');
+    }
+
+    public function subDepartments(): HasMany
+    {
+        return $this->hasMany(MasterProductCategory::class, 'master_department_id');
+    }
+
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(MasterProductCategory::class, 'master_parent_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(MasterProductCategory::class, 'master_parent_id');
+    }
+
+    public function families(): LaravelCollection
+    {
+        return $this->children()->where('type', ProductCategoryTypeEnum::FAMILY)->get();
+    }
+
+
+    public function products(): HasMany|null
+    {
+        return match ($this->type) {
+            ProductCategoryTypeEnum::DEPARTMENT => $this->hasMany(MasterProduct::class, 'master_department_id'),
+            ProductCategoryTypeEnum::FAMILY     => $this->hasMany(MasterProduct::class, 'master_family_id'),
+            default                             => null
+        };
+    }
+}
