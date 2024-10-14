698997c9beb1d25a3cb557cb9420621d0ff34256
Raul Perusquia <raul@inikoo.com>
2024-10-14 11:12:34 +0800

    #944 review fetch uploads ☝🏽

--- app/Actions/Transfers/Aurora/FetchAuroraUploads.php
+++ app/Actions/Transfers/Aurora/FetchAuroraUploads.php
@@ -11,9 +11,11 @@ use App\Actions\Helpers\Upload\StoreUpload;
 use App\Actions\Helpers\Upload\UpdateUpload;
 use App\Models\Helpers\Upload;
 use App\Transfers\SourceOrganisationService;
+use Exception;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class FetchAuroraUploads extends FetchAuroraAction
 {
@@ -23,32 +25,44 @@ class FetchAuroraUploads extends FetchAuroraAction
     {
         if ($uploadData = $organisationSource->fetchUpload($organisationSourceId)) {
             if ($upload = Upload::where('source_id', $uploadData['upload']['source_id'])->first()) {
-                $upload = UpdateUpload::make()->action(
-                    upload: $upload,
-                    modelData: $uploadData['upload'],
-                    hydratorsDelay: 60,
-                    strict: false,
-                    audit: false
-                );
+                try {
+                    $upload = UpdateUpload::make()->action(
+                        upload: $upload,
+                        modelData: $uploadData['upload'],
+                        hydratorsDelay: 60,
+                        strict: false,
+                        audit: false
+                    );
+                } catch (Exception $e) {
+                    $this->recordError($organisationSource, $e, $uploadData['upload'], 'Upload', 'update');
+
+                    return null;
+                }
             } else {
-                $upload = StoreUpload::make()->action(
-                    parent: $uploadData['parent'],
-                    modelData: $uploadData['upload'],
-                    hydratorsDelay: 60,
-                    strict: false,
-                    audit: false
-                );
+                try {
+                    $upload = StoreUpload::make()->action(
+                        parent: $uploadData['parent'],
+                        modelData: $uploadData['upload'],
+                        hydratorsDelay: 60,
+                        strict: false,
+                        audit: false
+                    );
 
-                Upload::enableAuditing();
-                $this->saveMigrationHistory(
-                    $upload,
-                    Arr::except($uploadData['upload'], ['fetched_at', 'last_fetched_at'])
-                );
+                    Upload::enableAuditing();
+                    $this->saveMigrationHistory(
+                        $upload,
+                        Arr::except($uploadData['upload'], ['fetched_at', 'last_fetched_at','source_id'])
+                    );
 
-                $sourceData = explode(':', $upload->source_id);
-                DB::connection('aurora')->table('Upload Dimension')
-                    ->where('Upload Key', $sourceData[1])
-                    ->update(['aiku_id' => $upload->id]);
+                    $sourceData = explode(':', $upload->source_id);
+                    DB::connection('aurora')->table('Upload Dimension')
+                        ->where('Upload Key', $sourceData[1])
+                        ->update(['aiku_id' => $upload->id]);
+                } catch (Exception|Throwable $e) {
+                    $this->recordError($organisationSource, $e, $uploadData['upload'], 'Upload', 'store');
+
+                    return null;
+                }
             }
 
             return $upload;
