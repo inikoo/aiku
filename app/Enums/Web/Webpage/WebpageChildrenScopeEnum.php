2cf2d037b2cb248094cb95f79a773f564efc0ffa
Ganes556 <gustiganes@gmail.com>
2024-10-15 16:54:04 +0800

    #1012 migrate database

---
+++ app/Enums/Web/Webpage/WebpageChildrenScopeEnum.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 15-10-2024, Bali, Indonesia
+ * Github: https://github.com/aqordeon
+ * Copyright: 2024
+ *
+*/
+
+namespace App\Enums\Web\Webpage;
+
+use App\Enums\EnumHelperTrait;
+
+enum WebpageChildrenScopeEnum: string
+{
+    use EnumHelperTrait;
+
+    case DEPARTMENT  = 'department';
+    case FAMILY  = 'family';
+
+}
