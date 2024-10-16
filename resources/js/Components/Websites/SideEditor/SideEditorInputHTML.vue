d871cb6a4b098f224c00dcc8dee919c4780ae437
aqordeon <aqordivika@yahoo.co.id>
2024-10-16 11:21:43 +0800

    feat: Component: SideEditorInputHTML & SideEditorText (Website Workshop)

---
+++ resources/js/Components/Websites/SideEditor/SideEditorInputHTML.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+import PureEditor from '@/Components/Pure/PureEditor.vue'
+const props = defineProps<{
+    
+}>()
+
+const model = defineModel<{ text: string }>()
+
+</script>
+
+<template>
+    <PureEditor v-model="model" />
+</template>
