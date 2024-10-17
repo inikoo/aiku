95b0a342a2f9c807757022f9048e835c1aae4a84
Raul Perusquia <raul@inikoo.com>
2024-10-17 12:48:21 +0800

    #1039 parse linkedWebpage

--- app/Actions/Transfers/Aurora/FetchAuroraWebBlockLink.php
+++ app/Actions/Transfers/Aurora/FetchAuroraWebBlockLink.php
@@ -11,31 +11,75 @@ namespace App\Actions\Transfers\Aurora;
 
 use App\Actions\OrgAction;
 use App\Models\Web\Website;
-use App\Transfers\Aurora\WithAuroraImages;
+use App\Transfers\Aurora\WithAuroraParsers;
+use Illuminate\Support\Facades\DB;
 
 class FetchAuroraWebBlockLink extends OrgAction
 {
-    // use WithAuroraImages;
 
-    public function handle(Website $website, $auroraLink)
+    use WithAuroraParsers;
+
+    public function handle(Website $website, $auroraLink): array
     {
+        print $auroraLink."\n";
 
-        return $this->isInternal($website, $auroraLink);
+        if (!$this->isInternalLink($website, $auroraLink)) {
+            $linkData = [
+                'type' => 'external',
+                'link' => $auroraLink
+            ];
+        } else {
+            $linkData = [
+                'type'       => 'internal',
+                'webpage_id' => null
+            ];
+
+            $auroraLink        = $this->cleanUrl($auroraLink);
+            $auroraLink        = str_replace($website->domain, '', $auroraLink);
+            $auroraLink        = preg_replace('/^\/+/', "", $auroraLink);
+            $dataSource        = explode(':', $website->source_id);
+            $auroraWebpageData = DB::connection('aurora')
+                ->table('Page Store Dimension')
+                ->select('Page Key as source_id')
+                ->where('Webpage Website Key', $dataSource[1])
+                ->where('Webpage Code', $auroraLink)
+                ->orWhere('Webpage Canonical Code', $auroraLink)
+                ->first();
+
+
+            $linkedWebpage = $this->parseWebpage($website->organisation_id.':'.$auroraWebpageData->source_id);
+
+            if ($linkedWebpage) {
+                data_set($linkData, 'webpage_id', $linkedWebpage->id);
+            }
+        }
+
+
+        return $linkData;
     }
 
-    public function isInternal($website, $auroraLink)
+    public function isInternalLink($website, $auroraLink): bool
     {
         if (!str_starts_with($auroraLink, "http")) {
             return true;
         }
-        $domain = $website->domain;
-        $auroraLink = preg_replace('/^https?:\/\//', "", $auroraLink);
-        $auroraLink = preg_replace('/^www\./', "", $auroraLink);
+        $domain       = $website->domain;
+        $auroraLink   = $this->cleanUrl($auroraLink);
         $auroraDomain = preg_replace('/\/.*$/', "", $auroraLink);
+
         return $domain == $auroraDomain;
     }
 
+    public function cleanUrl($auroraLink): string
+    {
+        $auroraLink = preg_replace('/^https?:\/\//', "", $auroraLink);
+        $auroraLink = preg_replace('/^www\./', "", $auroraLink);
 
+        $auroraLink = preg_replace('/^\/+/', "", $auroraLink);
+        $auroraLink = preg_replace('/\/+$/', "", $auroraLink);
+
+        return trim($auroraLink);
+    }
 
 
 }
