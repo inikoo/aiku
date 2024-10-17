2611f7f32360d071c42e822f1a7cae3105480aa6
KirinZero0 <aryapermana02@gmail.com>
2024-10-17 14:43:32 +0800

    Download Button

---
+++ app/Actions/Helpers/Media/UI/DownloadAttachment.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 17-10-2024-14h-32m
+ * github: https://github.com/KirinZero0
+ * copyright 2024
+*/
+
+namespace App\Actions\Helpers\Media\UI;
+
+use App\Models\Helpers\Media;
+use Lorisleiva\Actions\ActionRequest;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class DownloadAttachment 
+{
+    use AsAction;
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function handle(Media $media)
+    {
+        return response()->download($media->getPath(), $media->file_name);
+    }
+
+
+}
