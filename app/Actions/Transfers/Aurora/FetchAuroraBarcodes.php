9e1ef2cb4a6c94b80ed0144787cd528f119ab2eb
Raul Perusquia <raul@inikoo.com>
2024-10-17 20:09:39 +0800

    #86 review Fetch Barcodes V oct24 🦓

--- app/Actions/Transfers/Aurora/FetchAuroraBarcodes.php
+++ app/Actions/Transfers/Aurora/FetchAuroraBarcodes.php
@@ -11,8 +11,11 @@ use App\Actions\Helpers\Barcode\StoreBarcode;
 use App\Actions\Helpers\Barcode\UpdateBarcode;
 use App\Models\Helpers\Barcode;
 use App\Transfers\SourceOrganisationService;
+use Exception;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class FetchAuroraBarcodes extends FetchAuroraAction
 {
@@ -21,19 +24,47 @@ class FetchAuroraBarcodes extends FetchAuroraAction
     public function handle(SourceOrganisationService $organisationSource, int $organisationSourceId): ?Barcode
     {
         if ($barcodeData = $organisationSource->fetchBarcode($organisationSourceId)) {
-
             if ($barcode = Barcode::where('source_id', $barcodeData['barcode']['source_id'])->first()) {
-                $barcode = UpdateBarcode::make()->action(
-                    barcode: $barcode,
-                    modelData: $barcodeData['barcode'],
-                    audit: false
-                );
-            } else {
-                $barcode = StoreBarcode::make()->action(
-                    group: $organisationSource->getOrganisation()->group,
-                    modelData: $barcodeData['barcode'],
-                );
+                try {
+                    $barcode = UpdateBarcode::make()->action(
+                        barcode: $barcode,
+                        modelData: $barcodeData['barcode'],
+                        hydratorsDelay: 60,
+                        strict: false,
+                        audit: false
+                    );
+                    $this->recordChange($organisationSource, $barcode->wasChanged());
+                } catch (Exception $e) {
+                    $this->recordError($organisationSource, $e, $barcodeData['barcode'], 'Barcode', 'update');
 
+                    return null;
+                }
+            } else {
+                try {
+                    $barcode = StoreBarcode::make()->action(
+                        group: $organisationSource->getOrganisation()->group,
+                        modelData: $barcodeData['barcode'],
+                        hydratorsDelay: 60,
+                        strict: false,
+                        audit: false
+                    );
+                    Barcode::enableAuditing();
+                    $this->saveMigrationHistory(
+                        $barcode,
+                        Arr::except($barcodeData['barcode'], ['fetched_at', 'last_fetched_at', 'source_id'])
+                    );
+
+                    $this->recordNew($organisationSource);
+
+                    $sourceData = explode(':', $barcode->source_id);
+                    DB::connection('aurora')->table('Barcode Dimension')
+                        ->where('Barcode Key', $sourceData[1])
+                        ->update(['aiku_id' => $barcode->id]);
+                } catch (Exception|Throwable $e) {
+                    $this->recordError($organisationSource, $e, $barcodeData['barcode'], 'Barcode', 'store');
+
+                    return null;
+                }
             }
 
             return $barcode;
