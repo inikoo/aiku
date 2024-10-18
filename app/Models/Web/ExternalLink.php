947aeb12358bd29d8f97c511b48aca8e0197582a
KirinZero0 <aryapermana02@gmail.com>
2024-10-18 16:35:02 +0800

    #1066: Migration and model

---
+++ app/Models/Web/ExternalLink.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 18-10-2024-16h-33m
+ * github: https://github.com/KirinZero0
+ * copyright 2024
+*/
+
+namespace App\Models\Web;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class ExternalLink extends Model
+{
+    protected $casts = [
+    ];
+
+    protected $attributes = [
+    ];
+
+    protected $guarded = [];
+
+}
