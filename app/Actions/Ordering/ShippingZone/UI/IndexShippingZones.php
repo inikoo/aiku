0e7e65e9f1eba42220396ce70423080a7a9a4e97
KirinZero0 <aryapermana02@gmail.com>
2024-10-18 14:54:16 +0800

    #1063: Index Shipping Zones

---
+++ app/Actions/Ordering/ShippingZone/UI/IndexShippingZones.php
@@ -0,0 +1,134 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 18-10-2024-14h-15m
+ * github: https://github.com/KirinZero0
+ * copyright 2024
+*/
+
+namespace App\Actions\Ordering\ShippingZone\UI;
+
+use App\Actions\Catalogue\Shop\UI\ShowShop;
+use App\Actions\Catalogue\WithCollectionSubNavigation;
+use App\Actions\Catalogue\WithDepartmentSubNavigation;
+use App\Actions\Catalogue\WithFamilySubNavigation;
+use App\Actions\Ordering\ShippingZoneSchema\WithShippingZoneSchemaSubNavigation;
+use App\Actions\OrgAction;
+use App\Actions\Traits\Authorisations\HasCatalogueAuthorisation;
+use App\Enums\UI\Catalogue\ShippingTabsEnum;
+use App\Http\Resources\Catalogue\ProductsResource;
+use App\Http\Resources\Catalogue\ShippingZoneSchemasResource;
+use App\Http\Resources\Catalogue\ShippingZonesResource;
+use App\InertiaTable\InertiaTable;
+use App\Models\Catalogue\Shop;
+use App\Models\Ordering\ShippingZone;
+use App\Models\Ordering\ShippingZoneSchema;
+use App\Models\SysAdmin\Organisation;
+use App\Services\QueryBuilder;
+use Closure;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Inertia\Inertia;
+use Inertia\Response;
+use Lorisleiva\Actions\ActionRequest;
+use Spatie\QueryBuilder\AllowedFilter;
+
+class IndexShippingZones extends OrgAction
+{
+    use HasCatalogueAuthorisation;
+
+    private ShippingZoneSchema $parent;
+
+    public function handle(ShippingZoneSchema $parent, $prefix = null): LengthAwarePaginator
+    {
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                $query->whereAnyWordStartWith('shipping_zones.code', $value)
+                    ->orWhereStartWith('shipping_zones.name', $value);
+            });
+        });
+
+        if ($prefix) {
+            InertiaTable::updateQueryBuilderParameters($prefix);
+        }
+
+        $queryBuilder = QueryBuilder::for(ShippingZone::class);
+
+        if ($parent instanceof ShippingZoneSchema) {
+            $queryBuilder->where('shipping_zones.shipping_zone_schema_id', $parent->id);
+        } else {
+            abort(419);
+        }
+
+        $queryBuilder->leftjoin('shipping_zone_stats', 'shipping_zone_stats.shipping_zone_id', '=', 'shipping_zones.id');
+        $queryBuilder->leftjoin('currencies', 'shipping_zones.currency_id', '=', 'currencies.id');
+
+        $queryBuilder
+            ->defaultSort('shipping_zones.name')
+            ->select([
+                'shipping_zones.id',
+                'shipping_zones.slug',
+                'shipping_zones.code',
+                'shipping_zones.name',
+                'shipping_zones.price',
+                'shipping_zones.territories',
+                'shipping_zones.position',
+                'shipping_zones.created_at',
+            ]);
+
+        return $queryBuilder->allowedSorts(['name', 'status'])
+            ->allowedFilters([$globalSearch])
+            ->withPaginator($prefix)
+            ->withQueryString();
+    }
+
+    public function tableStructure(ShippingZoneSchema $parent, ?array $modelOperations = null, $prefix = null, $canEdit = false): Closure
+    {
+        return function (InertiaTable $table) use ($parent, $modelOperations, $prefix, $canEdit) {
+            if ($prefix) {
+                $table
+                    ->name($prefix)
+                    ->pageName($prefix.'Page');
+            }
+
+            $table
+                ->withGlobalSearch()
+                ->withModelOperations($modelOperations)
+                ->withEmptyState(
+                    match (class_basename($parent)) {
+                        'ShippingZoneSchema' => [
+                            'title' => __("No shipping zones found"),
+                        ],
+                        default => null
+                    }
+
+                    /*
+                    [
+                        'title'       => __('no products'),
+                        'description' => $canEdit ? __('Get started by creating a new product.') : null,
+                        'count'       => $this->organisation->stats->number_products,
+                        'action'      => $canEdit ? [
+                            'type'    => 'button',
+                            'style'   => 'create',
+                            'tooltip' => __('new product'),
+                            'label'   => __('product'),
+                            'route'   => [
+                                'name'       => 'shops.products.create',
+                                'parameters' => array_values($request->route()->originalParameters())
+                            ]
+                        ] : null
+                    ]*/
+                );
+            $table->column(key: 'code', label: __('code'), canBeHidden: false, sortable: true, searchable: true);
+            $table->column(key: 'name', label: __('name'), canBeHidden: false, sortable: true, searchable: true);
+            $table->column(key: 'position', label: __('position'), canBeHidden: false, sortable: true, searchable: true);
+            $table->column(key: 'territories', label: __('territories'), canBeHidden: false, sortable: false, searchable: false);
+            $table->column(key: 'price', label: __('price'), canBeHidden: false, sortable: false, searchable: false);
+        };
+    }
+
+    public function jsonResponse(LengthAwarePaginator $shippingZones): AnonymousResourceCollection
+    {
+        return ShippingZonesResource::collection($shippingZones);
+    }
+}
---
+++ resources/js/Components/Tables/Grp/Org/Catalogue/TableShippingZones.vue
@@ -0,0 +1,48 @@
+<!--
+  - Author: Raul Perusquia <raul@inikoo.com>
+  - Created: Mon, 20 Mar 2023 23:18:59 Malaysia Time, Kuala Lumpur, Malaysia
+  - Copyright (c) 2023, Raul A Perusquia Flores
+  -->
+
+  <script setup lang="ts">
+  import {Link} from "@inertiajs/vue3";
+  import Table from "@/Components/Table/Table.vue";
+  import type {Links, Meta} from "@/types/Table";
+  
+  defineProps<{
+    data: {
+      data: {}
+      links: Links
+      meta: Meta
+    },
+    tab?: string
+  }>();
+  
+  
+  // function shopRoute(schema: {}) {
+  //   console.log(route().current())
+  //   switch (route().current()) {
+  //     case "grp.org.shops.show.assets.shipping.index":
+  //       return route(
+  //           "grp.org.shops.show.assets.shipping.show",
+  //           [route().params["organisation"],route().params["shop"], schema.slug]);
+  //     default:
+  //       return null;
+  //   }
+  // }
+  
+  
+  </script>
+  
+  <template>
+    <Table :resource="data" :name="tab" class="mt-5">
+      <!-- <template #cell(name)="{ item: schema }">
+        <Link :href="shopRoute(schema)" class="primaryLink">
+        {{ schema["name"] }}
+      </Link>
+      </template> -->
+    </Table>
+  </template>
+  
+  
+  
