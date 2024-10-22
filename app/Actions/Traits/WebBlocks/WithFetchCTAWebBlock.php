16a60a0a9fe38a658c673b5c89c212d41d47f631
Ganes556 <gustiganes@gmail.com>
2024-10-22 09:52:31 +0800

    #1088 cta aurora 1

---
+++ app/Actions/Traits/WebBlocks/WithFetchCTAWebBlock.php
@@ -0,0 +1,34 @@
+<?php
+/*
+* author Arya Permana - Kirin
+* created on 09-10-2024-11h-39m
+* github: https://github.com/KirinZero0
+* copyright 2024
+*/
+
+namespace App\Actions\Traits\WebBlocks;
+
+use App\Models\Web\WebBlockType;
+use Illuminate\Support\Arr;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+trait WithFetchCTAWebBlock
+{
+    use AsAction;
+    public function processCTAData(WebBlockType $webBlockType, $auroraBlock): array
+    {
+        data_set($layout, "data.fieldValue", Arr::get($webBlockType, "data.fieldValue"));
+        data_set($layout, "data.fieldValue.title", Arr::get($auroraBlock, "title"));
+        data_set($layout, "data.fieldValue.text", Arr::get($auroraBlock, "text"));
+        data_set($layout, "data.fieldValue.button.text", Arr::get($auroraBlock, "button_label"));
+        data_set($layout, "data.fieldValue.button.container.properties.background.color", Arr::get($auroraBlock, "bg_color"));
+        data_set($layout, "data.fieldValue.button.container.properties.text.color", Arr::get($auroraBlock, "text_color"));
+        data_set($layout, "data.fieldValue.button.link", Arr::get($auroraBlock, "link"));
+        data_set(
+            $layout,
+            "data.fieldValue.button.container.properties.background.image.original",
+            ["source" =>  Arr::get($auroraBlock, "bg_image")]
+        );
+        return $layout;
+    }
+}
