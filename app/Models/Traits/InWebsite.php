0f44a39a2427e86166187262bd6be6d2b5528ffc
Raul Perusquia <raul@inikoo.com>
2024-10-23 00:32:23 +0800

    fix Redirect

---
+++ app/Models/Traits/InWebsite.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 23 Oct 2024 00:28:18 Central Indonesia Time, Office, Kuala Lumpur, Malaysia
+ * Copyright (c) 2024, Raul A Perusquia Flores
+ */
+
+namespace App\Models\Traits;
+
+use App\Models\Catalogue\Shop;
+use App\Models\SysAdmin\Group;
+use App\Models\SysAdmin\Organisation;
+use App\Models\Web\Website;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+trait InWebsite
+{
+    public function group(): BelongsTo
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function organisation(): BelongsTo
+    {
+        return $this->belongsTo(Organisation::class);
+    }
+
+    public function shop(): BelongsTo
+    {
+        return $this->belongsTo(Shop::class);
+    }
+
+    public function website(): BelongsTo
+    {
+        return $this->belongsTo(Website::class);
+    }
+}
