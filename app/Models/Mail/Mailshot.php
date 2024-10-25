e30bbb6735e4e5be61d79fc49ff8901aab1d450a
Raul Perusquia <raul@inikoo.com>
2024-10-25 16:56:08 +0800

    #1125 Mailshot model 📩

--- app/Models/Mail/Mailshot.php
+++ app/Models/Mail/Mailshot.php
@@ -7,8 +7,11 @@
 
 namespace App\Models\Mail;
 
+use App\Actions\Utils\Abbreviate;
 use App\Enums\Mail\Mailshot\MailshotStateEnum;
+use App\Enums\Mail\Mailshot\MailshotTypeEnum;
 use App\Models\Catalogue\Shop;
+use App\Models\Traits\InShop;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
@@ -17,6 +20,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Sluggable\SlugOptions;
 
@@ -24,17 +28,38 @@ use Spatie\Sluggable\SlugOptions;
  * App\Models\Mail\Mailshot
  *
  * @property int $id
+ * @property int $group_id
+ * @property int $organisation_id
  * @property int|null $shop_id
+ * @property string $slug
+ * @property string $subject
  * @property int|null $outbox_id
  * @property int|null $email_template_id
  * @property MailshotStateEnum $state
+ * @property \Illuminate\Support\Carbon $date
+ * @property \Illuminate\Support\Carbon|null $ready_at
+ * @property \Illuminate\Support\Carbon|null $start_sending_at
+ * @property \Illuminate\Support\Carbon|null $sent_at
+ * @property \Illuminate\Support\Carbon|null $cancelled_at
+ * @property \Illuminate\Support\Carbon|null $stopped_at
+ * @property array $layout
+ * @property array $recipients_recipe
+ * @property int|null $publisher_id org user
+ * @property string $parent_type
+ * @property int $parent_id
  * @property array $data
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property string|null $delete_comment
  * @property string|null $source_id
+ * @property MailshotTypeEnum $type
  * @property-read Collection<int, \App\Models\Mail\DispatchedEmail> $dispatchedEmails
+ * @property-read \App\Models\SysAdmin\Group $group
+ * @property-read \App\Models\SysAdmin\Organisation $organisation
  * @property-read \App\Models\Mail\Outbox|null $outbox
+ * @property-read Model|\Eloquent $parent
+ * @property-read Collection<int, \App\Models\Mail\MailshotRecipient> $recipients
  * @property-read Shop|null $shop
  * @property-read \App\Models\Mail\MailshotStats|null $stats
  * @method static \Database\Factories\Mail\MailshotFactory factory($count = null, $state = [])
@@ -50,14 +75,27 @@ class Mailshot extends Model
 {
     use SoftDeletes;
     use HasFactory;
+    use InShop;
 
     protected $casts = [
-        'data'  => 'array',
-        'state' => MailshotStateEnum::class
+        'recipients_recipe' => 'array',
+        'layout'            => 'array',
+        'data'              => 'array',
+        'type'              => MailshotTypeEnum::class,
+        'state'             => MailshotStateEnum::class,
+        'date'              => 'datetime',
+        'sent_at'           => 'datetime',
+        'schedule_at'       => 'datetime',
+        'ready_at'          => 'datetime',
+        'cancelled_at'      => 'datetime',
+        'stopped_at'        => 'datetime',
+        'start_sending_at'  => 'datetime',
     ];
 
     protected $attributes = [
-        'data' => '{}',
+        'layout'            => '{}',
+        'data'              => '{}',
+        'recipients_recipe' => '{}',
     ];
 
 
@@ -66,14 +104,22 @@ class Mailshot extends Model
     public function getSlugOptions(): SlugOptions
     {
         return SlugOptions::create()
-            ->generateSlugsFrom('code')
+            ->generateSlugsFrom(function () {
+                return Abbreviate::run(string: $this->type, maximumLength: 6).' '.$this->date->format('Y-m-d');
+            })
             ->doNotGenerateSlugsOnUpdate()
-            ->saveSlugsTo('slug')->slugsShouldBeNoLongerThan(64);
+            ->saveSlugsTo('slug')
+            ->slugsShouldBeNoLongerThan(64);
     }
 
-    public function shop(): BelongsTo
+    public function parent(): MorphTo
     {
-        return $this->belongsTo(Shop::class);
+        return $this->morphTo();
+    }
+
+    public function recipients(): HasMany
+    {
+        return $this->hasMany(MailshotRecipient::class);
     }
 
     public function outbox(): BelongsTo
@@ -91,8 +137,5 @@ class Mailshot extends Model
         return $this->hasMany(DispatchedEmail::class);
     }
 
-    public function getRouteKeyName(): string
-    {
-        return 'id';
-    }
+
 }
