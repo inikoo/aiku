368708a7b47ac534f6887ab035cee09a1fa57b87
Raul Perusquia <raul@inikoo.com>
2024-10-27 22:03:56 +0800

    StoreOrganisation wrap in transaction

--- app/Actions/SysAdmin/Organisation/StoreOrganisation.php
+++ app/Actions/SysAdmin/Organisation/StoreOrganisation.php
@@ -29,12 +29,14 @@ use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Lorisleiva\Actions\Concerns\WithAttributes;
+use Throwable;
 
 class StoreOrganisation
 {
@@ -42,6 +44,9 @@ class StoreOrganisation
     use WithAttributes;
     use WithModelAddressActions;
 
+    /**
+     * @throws \Throwable
+     */
     public function handle(Group $group, array $modelData): Organisation
     {
         $addressData = Arr::get($modelData, 'address');
@@ -50,80 +55,83 @@ class StoreOrganisation
         data_set($modelData, 'ulid', Str::ulid());
         data_set($modelData, 'settings.ui.name', Arr::get($modelData, 'name'));
 
-        /** @var Organisation $organisation */
-        $organisation = $group->organisations()->create($modelData);
-        $organisation->refresh();
+
+        return DB::transaction(function () use ($group, $modelData, $addressData) {
+            /** @var Organisation $organisation */
+            $organisation = $group->organisations()->create($modelData);
+            $organisation->refresh();
 
 
-        SetOrganisationLogo::run($organisation);
-        SeedOrganisationPermissions::run($organisation);
-        SeedJobPositions::run($organisation);
+            SetOrganisationLogo::run($organisation);
+            SeedOrganisationPermissions::run($organisation);
+            SeedJobPositions::run($organisation);
 
 
-        $organisation = $this->addAddressToModel($organisation, $addressData);
+            $organisation = $this->addAddressToModel($organisation, $addressData);
 
 
-        $superAdmins = $group->users()->with('roles')->get()->filter(
-            fn ($user) => $user->roles->where('name', 'group-admin')->toArray()
-        );
-
-        foreach ($superAdmins as $superAdmin) {
-            UserAddRoles::run($superAdmin, [
-                Role::where(
-                    'name',
-                    RolesEnum::getRoleName(
-                        'org-admin',
-                        $organisation
-                    )
-                )->where('scope_id', $organisation->id)->first()
-            ]);
-        }
-
-
-        $organisation->refresh();
-
-        SetCurrencyHistoricFields::run($organisation->currency, $organisation->created_at);
-
-        $organisation->stats()->create();
-        $organisation->humanResourcesStats()->create();
-        $organisation->procurementStats()->create();
-        $organisation->inventoryStats()->create();
-        $organisation->accountingStats()->create();
-        $organisation->dropshippingStats()->create();
-        $organisation->webStats()->create();
-        $organisation->mailStats()->create();
-
-        if ($organisation->type == OrganisationTypeEnum::SHOP) {
-            $organisation->crmStats()->create();
-            $organisation->salesStats()->create();
-            $organisation->salesIntervals()->create();
-            $organisation->ordersIntervals()->create();
-            $organisation->mailshotsIntervals()->create();
-            $organisation->fulfilmentStats()->create();
-            $organisation->catalogueStats()->create();
-            $organisation->manufactureStats()->create();
-            $organisation->discountsStats()->create();
-
-            $paymentServiceProvider = PaymentServiceProvider::where('type', PaymentServiceProviderTypeEnum::ACCOUNT)->first();
-
-            StoreOrgPaymentServiceProvider::make()->action(
-                $paymentServiceProvider,
-                organisation: $organisation,
-                modelData: [
-                    'code' => 'account-'.$organisation->code,
-                ]
+            $superAdmins = $group->users()->with('roles')->get()->filter(
+                fn($user) => $user->roles->where('name', 'group-admin')->toArray()
             );
 
-            $otherOrganisations = Organisation::where('type', OrganisationTypeEnum::SHOP)->where('group_id', $group->id)->where('id', '!=', $organisation->id)->get();
-            foreach ($otherOrganisations as $otherOrganisation) {
-                StoreOrgPartner::make()->action($otherOrganisation, $organisation);
-                StoreOrgPartner::make()->action($organisation, $otherOrganisation);
+            foreach ($superAdmins as $superAdmin) {
+                UserAddRoles::run($superAdmin, [
+                    Role::where(
+                        'name',
+                        RolesEnum::getRoleName(
+                            'org-admin',
+                            $organisation
+                        )
+                    )->where('scope_id', $organisation->id)->first()
+                ]);
             }
-        }
 
-        SeedOrganisationOutboxes::run($organisation);
 
-        return $organisation;
+            $organisation->refresh();
+
+            SetCurrencyHistoricFields::run($organisation->currency, $organisation->created_at);
+
+            $organisation->stats()->create();
+            $organisation->humanResourcesStats()->create();
+            $organisation->procurementStats()->create();
+            $organisation->inventoryStats()->create();
+            $organisation->accountingStats()->create();
+            $organisation->dropshippingStats()->create();
+            $organisation->webStats()->create();
+            $organisation->mailStats()->create();
+
+            if ($organisation->type == OrganisationTypeEnum::SHOP) {
+                $organisation->crmStats()->create();
+                $organisation->salesStats()->create();
+                $organisation->salesIntervals()->create();
+                $organisation->ordersIntervals()->create();
+                $organisation->mailshotsIntervals()->create();
+                $organisation->fulfilmentStats()->create();
+                $organisation->catalogueStats()->create();
+                $organisation->manufactureStats()->create();
+                $organisation->discountsStats()->create();
+
+                $paymentServiceProvider = PaymentServiceProvider::where('type', PaymentServiceProviderTypeEnum::ACCOUNT)->first();
+
+                StoreOrgPaymentServiceProvider::make()->action(
+                    $paymentServiceProvider,
+                    organisation: $organisation,
+                    modelData: [
+                        'code' => 'account-'.$organisation->code,
+                    ]
+                );
+
+                $otherOrganisations = Organisation::where('type', OrganisationTypeEnum::SHOP)->where('group_id', $group->id)->where('id', '!=', $organisation->id)->get();
+                foreach ($otherOrganisations as $otherOrganisation) {
+                    StoreOrgPartner::make()->action($otherOrganisation, $organisation);
+                    StoreOrgPartner::make()->action($organisation, $otherOrganisation);
+                }
+            }
+
+            SeedOrganisationOutboxes::run($organisation);
+
+            return $organisation;
+        });
     }
 
 
@@ -146,6 +154,9 @@ class StoreOrganisation
         ];
     }
 
+    /**
+     * @throws \Throwable
+     */
     public function asController(Group $group, ActionRequest $request): Organisation
     {
         $this->setRawAttributes($request->all());
@@ -159,6 +170,9 @@ class StoreOrganisation
         return Redirect::route('grp.org.dashboard.show', $organisation->slug);
     }
 
+    /**
+     * @throws \Throwable
+     */
     public function action(Group $group, $modelData): Organisation
     {
         $this->setRawAttributes($modelData);
@@ -176,6 +190,7 @@ class StoreOrganisation
     public function asCommand(Command $command): int
     {
         try {
+            /** @var Group $group */
             $group = Group::where('slug', $command->argument('group'))->firstOrFail();
         } catch (Exception $e) {
             $command->error($e->getMessage());
@@ -185,6 +200,7 @@ class StoreOrganisation
 
         setPermissionsTeamId($group->id);
         try {
+            /** @var Country $country */
             $country = Country::where('code', $command->argument('country_code'))->firstOrFail();
         } catch (Exception $e) {
             $command->error($e->getMessage());
@@ -193,6 +209,7 @@ class StoreOrganisation
         }
 
         try {
+            /** @var Currency $currency */
             $currency = Currency::where('code', $command->argument('currency_code'))->firstOrFail();
         } catch (Exception $e) {
             $command->error($e->getMessage());
@@ -202,6 +219,7 @@ class StoreOrganisation
 
         if ($command->option('language_code')) {
             try {
+                /** @var Language $language */
                 $language = Language::where('code', $command->option('language_code'))->firstOrFail();
             } catch (Exception $e) {
                 $command->error($e->getMessage());
@@ -214,6 +232,7 @@ class StoreOrganisation
 
         if ($command->option('timezone')) {
             try {
+                /** @var Timezone $timezone */
                 $timezone = Timezone::where('name', $command->option('timezone'))->firstOrFail();
             } catch (Exception $e) {
                 $command->error($e->getMessage());
@@ -275,9 +294,14 @@ class StoreOrganisation
             return 1;
         }
 
-        $organisation = $this->handle($group, $validatedData);
+        try {
+            $organisation = $this->handle($group, $validatedData);
+            $command->info("Organisation $organisation->slug created successfully 🎉");
+        } catch (Exception|Throwable $e) {
+            $command->error($e->getMessage());
 
-        $command->info("Organisation $organisation->slug created successfully 🎉");
+            return 1;
+        }
 
         return 0;
     }
