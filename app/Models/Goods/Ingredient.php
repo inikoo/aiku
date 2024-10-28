e518b2df6576eefb5f7cfb1e6440bc601896c1e5
KirinZero0 <aryapermana02@gmail.com>
2024-10-28 10:24:44 +0800

    #1158: Model and Migrations

---
+++ app/Models/Goods/Ingredient.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 28-10-2024-10h-17m
+ * github: https://github.com/KirinZero0
+ * copyright 2024
+*/
+
+
+namespace App\Models\Goods;
+
+use App\Models\SysAdmin\Group;
+use App\Models\Traits\HasHistory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use OwenIt\Auditing\Contracts\Auditable;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Ingredient extends Model implements Auditable
+{
+    use HasSlug;
+    use HasHistory;
+
+    protected $guarded = [];
+
+    public function generateTags(): array
+    {
+        return [
+            'goods'
+        ];
+    }
+
+    protected array $auditInclude = [
+        'name',
+        'number_trade_units',
+    ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate();
+    }
+
+    public function group(): BelongsTo
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function tradeUnits(): BelongsToMany
+    {
+        return $this->belongsToMany(TradeUnit::class, 'trade_unit_has_ingredients');
+    }
+}
