e249cea301642e10c469e763d0c157c50678103e
dandiAW <dandiferrari75@gmail.com>
2024-10-28 14:10:59 +0800

    @1127 update order vuew

--- resources/js/Pages/Grp/Org/Ordering/Order.vue
+++ resources/js/Pages/Grp/Org/Ordering/Order.vue
@@ -309,6 +309,8 @@ const onSubmitNote = async (closePopup: Function) => {
 		})
     }
 }
+console.log('jhahahaha',props);
+
 </script>
 
 <template>
@@ -585,7 +587,7 @@ const onSubmitNote = async (closePopup: Function) => {
 
 
             <div v-if="delivery_note" class="mt-1 flex items-center w-full flex-none justify-between">
-                <Link :href="route(routes.delivery_note.name,routes.delivery_note.parameters)" class="flex items-center gap-3 gap-x-1.5 primaryLink cursor-pointer">
+                <Link :href="route(routes.delivery_note.deliveryNoteRoute.name,routes.delivery_note.deliveryNoteRoute.parameters)" class="flex items-center gap-3 gap-x-1.5 primaryLink cursor-pointer">
                     <dt class="flex-none">
                         <FontAwesomeIcon icon='fal fa-truck' fixed-width aria-hidden='true' class="text-gray-500" />
                     </dt>
@@ -593,13 +595,13 @@ const onSubmitNote = async (closePopup: Function) => {
                         {{ delivery_note?.reference }}
                     </dd>
                 </Link>
-                   <!--  <Link href="/your-route" class="flex items-center"> -->
+                    <Link :href="route(routes.delivery_note.deliveryNotePdfRoute.name,routes.delivery_note.deliveryNotePdfRoute.parameters)" class="flex items-center">
                         <button class="flex items-center">
                             <dt class="flex-none">
                                 <FontAwesomeIcon :icon="faFilePdf" fixed-width aria-hidden="true" class="text-gray-500 hover:text-indigo-500 transition-colors duration-200" />
                             </dt>
                         </button>
-                <!--     </Link> -->
+                    </Link>
             </div>
 
         </BoxStatPallet>
