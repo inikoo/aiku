50675530faf4ccb0788f4b2ae9454d06f46a561f
YudhistiraA <aryarajasa0@gmail.com>
2024-10-29 14:38:09 +0800

    header-1

--- resources/js/Components/Websites/Header/HeaderTemplates/Header1/Header1.vue
+++ resources/js/Components/Websites/Header/HeaderTemplates/Header1/Header1.vue
@@ -78,10 +78,10 @@ const toggle = (event) => {
 
                 <!-- Gold Member Button -->
                 <div class="justify-self-end w-fit">
-                    <button :style="getStyles(modelValue.gold_member.properties)"
+                    <button :style="getStyles(modelValue.button_1.properties)"
                         class="flex items-center justify-center px-4 py-2 bg-gold-500 text-white rounded-md shadow-md hover:bg-gold-600 transition duration-300 w-fit"
                         v-if="loginMode">
-                        <Editor v-model="modelValue.gold_member.text" :editable="false" />
+                        <Editor v-model="modelValue.button_1.text" :editable="false" />
                     </button>
                 </div>
 
