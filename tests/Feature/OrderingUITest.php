e966dc3ee4e207915def291d20d715e1ccf4f2cb
Ganes556 <gustiganes@gmail.com>
2024-10-30 14:46:19 +0800

    #1172 ordering: ui index aset shipping

---
+++ tests/Feature/OrderingUITest.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 30-10-2024, Bali, Indonesia
+ * Github: https://github.com/Ganes556
+ * Copyright: 2024
+ *
+*/
+
+use App\Actions\Ordering\ShippingZoneSchema\StoreShippingZoneSchema;
+use App\Models\Ordering\ShippingZoneSchema;
+use Inertia\Testing\AssertableInertia;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+uses()->group('ui');
+
+beforeAll(function () {
+    loadDB();
+});
+
+
+beforeEach(function () {
+    list(
+        $this->organisation,
+        $this->user,
+        $this->shop
+    ) = createShop();
+
+    $this->adminGuest   = createAdminGuest($this->organisation->group);
+
+    $shippingZoneSchema = ShippingZoneSchema::first();
+    if (!$shippingZoneSchema) {
+        $shippingZoneSchema = StoreShippingZoneSchema::make()->action($this->shop, ShippingZoneSchema::factory()->definition());
+    }
+    $this -> shippingZoneSchema = $shippingZoneSchema;
+
+    Config::set(
+        'inertia.testing.page_paths',
+        [resource_path('js/Pages/Grp')]
+    );
+    actingAs($this->adminGuest->getUser());
+});
+
+
+
+test('UI index asset shipping', function () {
+    $this->withoutExceptionHandling();
+    $response = get(route('grp.org.shops.show.assets.shipping.index', [$this->organisation, $this->shop]));
+    $response->assertInertia(function (AssertableInertia $page) {
+        $page
+            ->component('Org/Catalogue/Shippings')
+            ->where('title', 'shipping')
+            ->has('breadcrumbs', 3)
+            ->has(
+                'pageHead',
+                fn (AssertableInertia $page) => $page
+                        ->where('title', 'Shipping')
+                        ->etc()
+            )
+            ->has('data');
+    });
+});
