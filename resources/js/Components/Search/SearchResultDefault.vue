a879278f9b35758d2d023e6e98db824abe1c17f0
Ganes556 <gustiganes@gmail.com>
2024-11-15 10:35:13 +0800

    #1203 fix meta icon

--- resources/js/Components/Search/SearchResultDefault.vue
+++ resources/js/Components/Search/SearchResultDefault.vue
@@ -14,55 +14,55 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { useTruncate } from "@/Composables/useTruncate"
 
 const props = defineProps<{
-	data?: {
-		tooltip?: []
-		route?: routeType
-		container?: {
-			key?: string
-			icon: string | string[]
-			label: string
-			href?: routeType
-			tooltip: string
-		}
-		noCapitalise?: boolean
-		icon: {
-			icon: string | string[]
-			title: string
-			tooltip?: string
-		}
-		state_icon?: {
-			tooltip?: string
-			icon: string
-			class: string
-			color: string
-		}
-		image?: any
-		model: string
-		description: {
-			label:string
-		}
-		code?: {
-			label: string
-			Tooltip?: string
-		}
-		meta?: {
-			key: string
-			type?: string // 'date', 'amount'
-			code?: string // 'GBP',
-			amount?: number // 30.40, 85
-			label?: string | string[]
-			number?: number | string
-			icon?: Icon
-			leftIcon?: Icon
-			href?: routeType
-			tooltip?: string
-		}[]
-	}
-	modelType: string
+    data?: {
+        tooltip?: []
+        route?: routeType
+        container?: {
+            key?: string
+            icon: string | string[]
+            label: string
+            href?: routeType
+            tooltip: string
+        }
+        noCapitalise?: boolean
+        icon: {
+            icon: string | string[]
+            title: string
+            tooltip?: string
+        }
+        state_icon?: {
+            tooltip?: string
+            icon: string
+            class: string
+            color: string
+        }
+        image?: any
+        model: string
+        description: {
+            label: string
+        }
+        code?: {
+            label: string
+            Tooltip?: string
+        }
+        meta?: {
+            key: string
+            type?: string // 'date', 'amount'
+            code?: string // 'GBP',
+            amount?: number // 30.40, 85
+            label?: string | string[]
+            number?: number | string
+            icon?: Icon
+            leftIcon?: Icon
+            href?: routeType
+            tooltip?: string
+        }[]
+    }
+    modelType: string
 }>()
 
 const emits = defineEmits<{
-	(e: "finishVisit", value: false): void
+    (e: "finishVisit", value: false): void
 }>()
 
 const locale = inject("locale", aikuLocaleStructure)
@@ -71,105 +71,82 @@ const isLoading = ref(false)
 </script>
 
 <template>
-	<component
-		:is="data?.route?.name ? Link : 'div'"
-		as="a"
-		:href="data?.route?.name ? route(data?.route.name, data?.route.parameters) : ''"
-		class="flex gap-x-2 items-center"
-		@start="() => (isLoading = true)"
-		@finish="() => emits('finishVisit', false)">
-		<div
-			v-if="isLoading"
-			class="fixed inset-0 bottom-0 bg-black/50 flex flex-col justify-center items-center text-white cursor-default">
-			<LoadingIcon class="text-6xl" />
-			<LoadingText />
-		</div>
+    <component :is="data?.route?.name ? Link : 'div'" as="a"
+        :href="data?.route?.name ? route(data?.route.name, data?.route.parameters) : ''"
+        class="flex gap-x-2 items-center" @start="() => (isLoading = true)" @finish="() => emits('finishVisit', false)">
+        <div v-if="isLoading"
+            class="fixed inset-0 bottom-0 bg-black/50 flex flex-col justify-center items-center text-white cursor-default">
+            <LoadingIcon class="text-6xl" />
+            <LoadingText />
+        </div>
 
-		<div class="w-full flex leading-none justify-between tracking-tight">
-			<div class="">
+        <div class="w-full flex leading-none justify-between tracking-tight">
+            <div class="">
 
-				<div class="flex py-1 flex-row items-center gap-y-1.5">
-					<Icon :data="data?.icon" class="text-gray-400 mr-1" />
+                <div class="flex py-1 flex-row items-center gap-y-1.5">
+                    <Icon :data="data?.icon" class="text-gray-400 mr-1" />
 
-					<h2
-						:class="data?.noCapitalise ? '' : 'capitalize'"
-						class="leading-none text-base mr-2">
-						<span
-							v-if="data?.model"
-							class="leading-none text-gray-400 mr-2 block sm:inline">
-							{{ data?.model }}
-						</span>
-						<span class="leading-none inline-block text-sm whitespace-nowrap ">
-							{{ useTruncate(data?.code.label || "", 30) }}
-						</span>
-					</h2>
+                    <h2 :class="data?.noCapitalise ? '' : 'capitalize'" class="leading-none text-base mr-2">
+                        <span v-if="data?.model" class="leading-none text-gray-400 mr-2 block sm:inline">
+                            {{ data?.model }}
+                        </span>
+                        <span class="leading-none inline-block text-sm whitespace-nowrap ">
+                            {{ useTruncate(data?.code.label || "", 30) }}
+                        </span>
+                    </h2>
 
-					<Icon :data="data?.state_icon" size="2xs" class="" />
+                    <Icon :data="data?.state_icon" size="2xs" class="" />
 
-					<!-- Section: After Title -->
-					<div class="flex gap-x-2 items-center">
-						<div
-							v-if="data?.description"
-							class="text-gray-400 font-normal text-base leading-none">
-							{{ data?.description.label }}
-						</div>
-					</div>
+                    <!-- Section: After Title -->
+                    <div class="flex gap-x-2 items-center">
+                        <div v-if="data?.description" class="text-gray-400 font-normal text-base leading-none">
+                            {{ data?.description.label }}
+                        </div>
+                    </div>
 
-					<div
-						class="border-[2px] hidden border-gray-400 text-gray-500 rounded-md h-10 aspect-square flex items-center justify-center">
-						<Image :src="data?.image" />
-					</div>
-				</div>
+                    <div
+                        class="border-[2px] hidden border-gray-400 text-gray-500 rounded-md h-10 aspect-square flex items-center justify-center">
+                        <Image :src="data?.image" />
+                    </div>
+                </div>
 
-				<!-- Section: mini Tabs -->
-				<div
-					v-if="data?.meta?.length"
-					class="flex items-center pr-4 sm:pr-0 flex-wrap sm:gap-y-0.5 text-sm">
-					<template v-for="meta in data?.meta">
-						<div
-							v-if="meta.label"
-							v-tooltip="meta.tooltip"
-							class="flex items-center gap-x-1 text-gray-400 text-xs">
-							<Icon :data="meta?.icon" size="2xs" class="" />
+                <!-- Section: mini Tabs -->
+                <div v-if="data?.meta?.length" class="flex items-center pr-4 sm:pr-0 flex-wrap sm:gap-y-0.5 text-sm">
+                    <template v-for="meta in data?.meta">
+                        <div v-if="meta.label" v-tooltip="meta.tooltip"
+                            class="flex items-center gap-x-1 text-gray-400 text-xs">
+                            <Icon v-if='meta?.icon' :data="meta.icon" size="sm" />
 
-						<!-- 	<FontAwesomeIcon
-								v-if="meta.icon"
-								:icon="meta.icon"
-								:class="meta.class"
-								size="sm"
-								fixed-width
-								aria-hidden="true" /> -->
-							<template v-if="meta.type === 'date'">
-								{{ useFormatTime(meta.label) }}
-							</template>
+                            <template v-if="meta.type === 'date'">
+                                {{ useFormatTime(meta.label) }}
+                            </template>
 
-							<template v-else-if="meta.type === 'currency'">
-								{{ meta.label }}
-								{{ locale.currencyFormat(meta.code || "usd", meta.amount) }}
-							</template>
+                            <template v-else-if="meta.type === 'currency'">
+                                {{ meta.label }}
+                                {{ locale.currencyFormat(meta.code || "usd", meta.amount) }}
+                            </template>
 
-							<template v-else-if="meta.type === 'number'">
-								{{ meta.label }} {{ locale.number(meta.number) }}
-								<span v-if="meta.afterLabel">
-									{{ meta.afterLabel }}
-								</span>
-							</template>
-							<template v-else-if="meta.type === 'address'">
-								<AddressLocation :data="meta.label" />
-							</template>
+                            <template v-else-if="meta.type === 'number'">
+                                {{ meta.label }} {{ locale.number(meta.number) }}
+                                <span v-if="meta.afterLabel">
+                                    {{ meta.afterLabel }}
+                                </span>
+                            </template>
+                            <template v-else-if="meta.type === 'address'">
+                                <AddressLocation :data="meta.label" />
+                            </template>
 
-							<template v-else>{{ meta.label }}</template>
-						</div>
+                            <template v-else>{{ meta.label }}</template>
+                        </div>
 
-						<div v-if="meta.label" class="last:hidden px-2">•</div>
-					</template>
-				</div>
-			</div>
-			<div
-				v-if="data?.image"
-				class="border-[2px] border-gray-400 text-gray-500 rounded-md h-10 aspect-square flex items-center justify-center">
-				<Image :src="data?.image" />
-			</div>
-		</div>
-	</component>
+                        <div v-if="meta.label" class="last:hidden px-2">•</div>
+                    </template>
+                </div>
+            </div>
+            <div v-if="data?.image"
+                class="border-[2px] border-gray-400 text-gray-500 rounded-md h-10 aspect-square flex items-center justify-center">
+                <Image :src="data?.image" />
+            </div>
+        </div>
+    </component>
 </template>
