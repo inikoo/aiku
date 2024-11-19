51e3dade08b8714a36e63854fcd39b54ea4aa313
KirinZero0 <aryapermana02@gmail.com>
2024-11-19 09:25:54 +0800

    Delete pull_staging_db.sh

--- pull_stagging_db.sh
+++
@@ -1,5 +0,0 @@
-ssh staging pg_dump -Fc -x -f "paso/aiku-staging.dump" aiku_staging
-scp staging:paso/aiku-staging.dump devops/devel/snapshots/aiku-staging.dump
-echo "loading ..";
-pg_restore -x --no-owner -j 15  -U "${USER}" -c -d aiku  "devops/devel/snapshots/aiku-staging.dump"
-echo "🕺 done aiku staging loaded"
