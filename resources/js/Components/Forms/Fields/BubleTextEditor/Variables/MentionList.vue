1f85e63621a12bdbe10695fdbe0fbcfba7995ca0
YudhistiraA <aryarajasa0@gmail.com>
2024-11-22 09:06:00 +0800

    Editor: variables #1217

---
+++ resources/js/Components/Forms/Fields/BubleTextEditor/Variables/MentionList.vue
@@ -0,0 +1,128 @@
+<script setup lang="ts">
+import { watch, ref, defineExpose } from 'vue';
+
+const props = withDefaults(defineProps<{
+  items: {
+      type: Array<any>,
+      required: true,
+    },
+
+    command: {
+      type: Function,
+      required: true,
+    },
+}>(), {})
+
+const selectedIndex =  ref(0)
+
+  const onKeyDown = ({ event }) => {
+      if (event.key === 'ArrowUp') {
+        upHandler()
+        return true
+      }
+
+      if (event.key === 'ArrowDown') {
+        downHandler()
+        return true
+      }
+
+      if (event.key === 'Enter') {
+        enterHandler()
+        return true
+      }
+
+      return false
+    }
+
+    const upHandler = () => {
+      selectedIndex.value  = ((selectedIndex.value  + props.items.length) - 1) % props.items.length
+    }
+
+    const downHandler = () => {
+      selectedIndex.value  = (selectedIndex.value  + 1) % props.items.length
+    }
+
+    const enterHandler = () => {
+      selectItem(selectedIndex.value)
+    }
+
+    const selectItem = (index) => {
+      const item = props.items[index]
+
+      if (item) {
+        props.command({ id: item })
+      }
+    }
+
+    watch(()=>props.items , (newValue)=>{
+      selectedIndex.value = 0
+    })
+
+    defineExpose({
+      onKeyDown,
+      upHandler,
+      downHandler,
+      selectItem,
+      enterHandler
+});
+
+</script>
+
+<template>
+  <div class="dropdown-menu">
+    <template v-if="items.length">
+      <button
+        :class="{ 'is-selected': index === selectedIndex }"
+        v-for="(item, index) in items"
+        :key="index"
+        @click="selectItem(index)"
+      >
+        {{ item }}
+      </button>
+    </template>
+    <div class="item" v-else>
+      No result
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="scss">
+/* Dropdown menu */
+.dropdown-menu {
+  background: #fff;
+  border: 1px solid var(--gray-1);
+  border-radius: 0.7rem;
+  box-shadow: var(--shadow);
+  display: flex;
+  flex-direction: column;
+  gap: 0.1rem;
+  overflow: auto;
+  padding: 0.5rem 0.5rem;
+  position: relative;
+
+  button {
+    align-items: center;
+    background-color: transparent;
+    display: flex;
+    gap: 0.25rem;
+    text-align: left;
+    width: 100%;
+    padding: 0.1rem 0.3rem;
+
+    &:hover,
+    &:hover.is-selected {
+      background-color: #F6F2FF;
+      color: #6A00F5;
+      padding: 0.1rem 0.3rem;
+    }
+
+    &.is-selected {
+      background-color: #F6F2FF;
+      color: #6A00F5;
+      padding: 0.1rem 0.3rem;
+    }
+  }
+}
+</style>
---
+++ resources/js/Components/Forms/Fields/BubleTextEditor/Variables/Variables.js
@@ -0,0 +1,70 @@
+import Mention  from "@tiptap/extension-mention"
+import { Plugin } from "prosemirror-state"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { PluginKey } from '@tiptap/pm/state'
+import { mergeAttributes, Node } from '@tiptap/core'
+
+export const MentionPluginKey = new PluginKey('mention')
+
+const CustomLink = Mention.extend({
+    addOptions() {
+        return {
+          HTMLAttributes: {},
+          renderText({ options, node }) {
+            return `${options.suggestion.char}${node.attrs.label ?? node.attrs.id}`
+          },
+          deleteTriggerWithBackspace: false,
+          renderHTML({ options, node }) {
+            return [
+              'span',
+              mergeAttributes(this.HTMLAttributes, options.HTMLAttributes),
+              `${options.suggestion.char}  ${ node.attrs.label ?? node.attrs.id + " }}"}`,
+            ]
+          },
+          suggestion: {
+            char: '{{',
+            pluginKey: MentionPluginKey,
+            command: ({ editor, range, props }) => {
+              // increase range.to by one when the next node is of type "text"
+              // and starts with a space character
+              const nodeAfter = editor.view.state.selection.$to.nodeAfter
+              const overrideSpace = nodeAfter?.text?.startsWith(' ')
+    
+              if (overrideSpace) {
+                range.to += 1
+              }
+    
+              editor
+                .chain()
+                .focus()
+                .insertContentAt(range, [
+                  {
+                    type: this.name,
+                    attrs: props,
+                  },
+                  {
+                    type: 'text',
+                    text: ' ',
+                  },
+                ])
+                .run()
+    
+              // get reference to `window` object from editor element, to support cross-frame JS usage
+              editor.view.dom.ownerDocument.defaultView?.getSelection()?.collapseToEnd()
+            },
+            allow: ({ state, range }) => {
+              const $from = state.doc.resolve(range.from)
+              const type = state.schema.nodes[this.name]
+              const allow = !!$from.parent.type.contentMatch.matchType(type)
+    
+              return allow
+            },
+          },
+        }
+      },
+})
+
+
+
+
+export default CustomLink
