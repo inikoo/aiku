25553d347ce4a9884a4e3f39522279ff094b6aac
YudhistiraA <aryarajasa0@gmail.com>
2024-12-09 09:14:07 +0800

    prospect tags

--- resources/js/Components/Tables/Grp/Org/CRM/TableProspects.vue
+++ resources/js/Components/Tables/Grp/Org/CRM/TableProspects.vue
@@ -8,12 +8,23 @@
 import {Link} from '@inertiajs/vue3';
 import Table from '@/Components/Table/Table.vue';
 import {Prospect} from "@/types/prospect";
+import Multiselect from '@vueform/multiselect'
+import Tag from '@/Components/Tag.vue';
+import { ref } from 'vue'
+import Icon from '@/Components/Icon.vue';
+
+import { faThumbsDown, faChair, faLaugh } from '@fal';
+import { library } from "@fortawesome/fontawesome-svg-core"
+
+library.add(faThumbsDown, faChair, faLaugh)
 
 const props = defineProps<{
     data: object,
     tab?: string
 }>()
 
+const tagsListTemp: Ref<tag[]> = ref([])
+const maxId = ref(Math.max(...tagsListTemp.value.map(item => item.id)))
 
 function prospectRoute(prospect: Prospect) {
     switch (route().current()) {
@@ -28,14 +39,58 @@ function prospectRoute(prospect: Prospect) {
     }
 }
 
+
+
 </script>
 
 <template>
     <Table :resource="data" :name="tab" class="mt-5">
+        <template #cell(state)="{ item: prospect }">
+            <Icon :data="prospect.state_icon"></Icon>
+        </template>
         <template #cell(name)="{ item: prospect }">
             <!-- <Link :href="prospectRoute(prospect)"> -->
                 {{ prospect['name'] }}
+<<<<<<< Updated upstream
             <!-- </Link> -->
+=======
+         <!--    </Link> -->
+        </template>
+
+        <template #cell(tags)="item">
+            <div class="min-w-[200px]">
+                <Multiselect 
+                    v-model="item.tags"
+                    :key="item.slug"
+                    mode="tags"
+                    placeholder="Select the tag"
+                    valueProp="name"
+                    trackBy="name"
+                    label="name"
+                    @change="(idTag) => (updateTagItemTable(idTag, item.id))"
+                    :close-on-select="false"
+                    :searchable="true"
+                    :create-option="true"
+                    :caret="false"
+                    :options="tagsListTemp"
+                    noResultsText="No one left. Type to add new one."
+                    appendNewTag
+                >
+                    <template #tag="{ option, handleTagRemove, disabled }: {option: tag, handleTagRemove: Function, disabled: boolean}">
+                        <div class="px-0.5 py-[3px]">
+                            <Tag
+                                :theme="option.id ?? maxId"
+                                :label="option.name"
+                                :closeButton="true"
+                                :stringToColor="true"
+                                size="sm"
+                                @onClose="(event) => handleTagRemove(option, event)"
+                            />
+                        </div>
+                    </template>
+                </Multiselect>
+            </div>
+>>>>>>> Stashed changes
         </template>
     </Table>
 </template>
