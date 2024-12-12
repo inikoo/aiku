045f528d0da406e2938f1922dd17c3807b9f4725
KirinZero0 <aryapermana02@gmail.com>
2024-12-12 10:27:47 +0800

    Fix Outbox SHowcase data

--- app/Actions/Comms/Outbox/UI/GetOutboxShowcase.php
+++ app/Actions/Comms/Outbox/UI/GetOutboxShowcase.php
@@ -23,25 +23,23 @@ class GetOutboxShowcase
                 'state' => $outbox->state,
 
                 'stats' => [
-                    'subscribers'               => $stats->number_subscribers,
-                    'unsubscribed'              => $stats->number_unsubscribed,
-                    'mailshots'                 => $stats->number_mailshots,
-                    'email_bulk_runs'           => $stats->number_email_bulk_runs,
-                    'email_ongoing_runs'        => $stats->number_email_ongoing_runs,
-                    'dispatched_emails'         => [
-                            'total'                 => $stats->number_dispatched_emails,
-                            'ready'                 => $stats->number_dispatched_emails_state_ready,
-                            'sent_to_provider'      => $stats->number_dispatched_emails_state_sent_to_provider,
-                            'rejected_by_provider'  => $stats->number_dispatched_emails_state_rejected_by_provider,
-                            'sent'                  => $stats->number_dispatched_emails_state_sent,
-                            'delivered'             => $stats->number_dispatched_emails_state_delivered,
-                            'hard_bounce'           => $stats->number_dispatched_emails_state_hard_bounce,
-                            'soft_bounce'           => $stats->number_dispatched_emails_state_soft_bounce,
-                            'opened'                => $stats->number_dispatched_emails_state_opened,
-                            'clicked'               => $stats->number_dispatched_emails_state_clicked,
-                            'spam'                  => $stats->number_dispatched_emails_state_spam,
-                            'unsubscribed'          => $stats->number_dispatched_emails_state_unsubscribed,
-                    ]
+                    'subscribers'                                   => $stats->number_subscribers,
+                    'unsubscribed'                                  => $stats->number_unsubscribed,
+                    'mailshots'                                     => $stats->number_mailshots,
+                    'email_bulk_runs'                               => $stats->number_email_bulk_runs,
+                    'email_ongoing_runs'                            => $stats->number_email_ongoing_runs,
+                    'dispatched_emails'                             => $stats->number_dispatched_emails,
+                    'dispatched_emails_state_ready'                 => $stats->number_dispatched_emails_state_ready,
+                    'dispatched_emails_state_sent_to_provider'      => $stats->number_dispatched_emails_state_sent_to_provider,
+                    'dispatched_emails_state_rejected_by_provider'  => $stats->number_dispatched_emails_state_rejected_by_provider,
+                    'dispatched_emails_state_sent'                  => $stats->number_dispatched_emails_state_sent,
+                    'dispatched_emails_state_delivered'             => $stats->number_dispatched_emails_state_delivered,
+                    'dispatched_emails_state_hard_bounce'           => $stats->number_dispatched_emails_state_hard_bounce,
+                    'dispatched_emails_state_soft_bounce'           => $stats->number_dispatched_emails_state_soft_bounce,
+                    'dispatched_emails_state_opened'                => $stats->number_dispatched_emails_state_opened,
+                    'dispatched_emails_state_clicked'               => $stats->number_dispatched_emails_state_clicked,
+                    'dispatched_emails_state_spam'                  => $stats->number_dispatched_emails_state_spam,
+                    'dispatched_emails_state_unsubscribed'          => $stats->number_dispatched_emails_state_unsubscribed,
                 ]
             ]
         ];
