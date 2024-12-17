e0a54b5ee563384281ac5445f79c96b71911a092
KirinZero0 <aryapermana02@gmail.com>
2024-12-17 14:01:27 +0800

    Organisation Dahsboard data

---
+++ app/Models/SysAdmin/OrganisationOrderingIntervals.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * author Arya Permana - Kirin
+ * created on 17-12-2024-13h-39m
+ * github: https://github.com/KirinZero0
+ * copyright 2024
+*/
+
+namespace App\Models\SysAdmin;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OrganisationOrderingIntervals extends Model
+{
+    protected $table = 'organisation_ordering_intervals';
+
+    protected $guarded = [];
+
+    public function organisation(): BelongsTo
+    {
+        return $this->belongsTo(Organisation::class);
+    }
+}
