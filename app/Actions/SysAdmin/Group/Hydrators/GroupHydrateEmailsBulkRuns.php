62d4dd7f40381cc55d588d4e0c7b6e2670688d10
Ganes556 <gustiganes@gmail.com>
2024-12-20 12:56:49 +0800

    #1274 group hydator for overview

---
+++ app/Actions/SysAdmin/Group/Hydrators/GroupHydrateEmailsBulkRuns.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 20-12-2024, Bali, Indonesia
+ * Github: https://github.com/Ganes556
+ * Copyright: 2024
+ *
+*/
+
+namespace App\Actions\SysAdmin\Group\Hydrators;
+
+use App\Actions\Traits\WithEnumStats;
+use App\Models\SysAdmin\Group;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class GroupHydrateEmailsBulkRuns
+{
+    use AsAction;
+    use WithEnumStats;
+
+    private Group $group;
+
+    public function __construct(Group $group)
+    {
+        $this->group = $group;
+    }
+
+    public function getJobMiddleware(): array
+    {
+        return [(new WithoutOverlapping($this->group->id))->dontRelease()];
+    }
+
+    public function handle(Group $group): void
+    {
+        $stats = [
+            'number_email_bulk_runs' => $group->emailBulkRuns()->count(),
+        ];
+
+        $group->commsStats()->update($stats);
+    }
+    public string $commandSignature = 'hydrate:group-email-bulk-runs';
+
+    public function asCommand($command): void
+    {
+        $group = Group::first();
+        $this->handle($group);
+    }
+
+
+}
