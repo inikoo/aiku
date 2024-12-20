e3bb1f928326cbcd2a116942386ba42651fd3459
Raul Perusquia <raul@inikoo.com>
2024-12-21 04:07:24 +0800

    Add missing stats fields for overview (customers with balances)

--- app/Http/Resources/Accounting/CustomerBalancesResource.php
+++ app/Http/Resources/Accounting/CustomerBalancesResource.php
@@ -15,24 +15,29 @@ use Illuminate\Http\Resources\Json\JsonResource;
  * @property string $slug
  * @property string $name
  * @property string $code
- * @property mixed  $balance
+ * @property mixed $balance
+ * @property mixed $shop_slug
+ * @property mixed $shop_type
+ * @property mixed $fulfilment_slug
+ * @property mixed $organisation_name
+ * @property mixed $organisation_slug
+ * @property mixed $shop_name
  */
 class CustomerBalancesResource extends JsonResource
 {
     public function toArray($request): array
     {
         return [
-            'id'                            => $this->id,
-            'slug'                          => $this->slug,
-            'name'                          => $this->name,
-            'balance'                       => $this->balance,
-            'shop_slug'                     => $this->shop_slug,
-            'shop_type'                     => $this->shop_type,
-            'fulfilment_slug'               => $this->fulfilment_slug,
+            'id'                => $this->id,
+            'slug'              => $this->slug,
+            'name'              => $this->name,
+            'balance'           => $this->balance,
+            'shop_slug'         => $this->shop_slug,
+            'shop_type'         => $this->shop_type,
+            'fulfilment_slug'   => $this->fulfilment_slug,
             'organisation_name' => $this->organisation_name,
             'organisation_slug' => $this->organisation_slug,
             'shop_name'         => $this->shop_name,
-            'shop_slug'         => $this->shop_slug,
         ];
     }
 }
