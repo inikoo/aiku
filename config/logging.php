0fe5e2059f2f00f054f8b07e50bfb22c16e9a945
Raul Perusquia <raul@inikoo.com>
2025-01-01 04:34:51 +0800

    log maintenance

--- config/logging.php
+++ config/logging.php
@@ -32,7 +32,7 @@ return [
 
     'deprecations' => [
         'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
-        'trace'   => false,
+        'trace'   => env('LOG_DEPRECATIONS_TRACES', false),
     ],
 
     /*
