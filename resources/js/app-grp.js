fd6ee0b42985a293f107e0f0ce9a10d1841168eb
Raul Perusquia <raul@inikoo.com>
2025-01-04 12:55:26 +0800

    sentry Performance Monitoring

--- resources/js/app-grp.js
+++ resources/js/app-grp.js
@@ -1,94 +1,137 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import {createApp, h} from 'vue';
-import {createInertiaApp} from '@inertiajs/vue3';
-import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
-import {i18nVue} from 'laravel-vue-i18n';
-import Notifications from '@kyvg/vue3-notification';
-import {createPinia} from 'pinia';
-import * as Sentry from '@sentry/vue';
-import FloatingVue from 'floating-vue'
-import 'floating-vue/dist/style.css'
-import Layout from '@/Layouts/Grp.vue'
-import { capitalize } from '@/Composables/capitalize.ts'
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-import { definePreset } from '@primevue/themes';
-import ConfirmationService from 'primevue/confirmationservice';
+import { createApp, h } from "vue";
+import { createInertiaApp, router } from "@inertiajs/vue3";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+import { i18nVue } from "laravel-vue-i18n";
+import Notifications from "@kyvg/vue3-notification";
+import { createPinia } from "pinia";
+import * as Sentry from "@sentry/vue";
+import FloatingVue from "floating-vue";
+import "floating-vue/dist/style.css";
+import Layout from "@/Layouts/Grp.vue";
+import { capitalize } from "@/Composables/capitalize.ts";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import { definePreset } from "@primevue/themes";
+import ConfirmationService from "primevue/confirmationservice";
 
-const appName = capitalize(window.document.getElementsByTagName('title')[0]?.innerText) ||
-    'Aiku';
+const appName = capitalize(
+    window.document.getElementsByTagName("title")[0]?.innerText) ||
+  "Aiku";
 
 const MyPreset = definePreset(Aura, {
   semantic: {
-      primary: {
-          50: '{gray.50}',
-          100: '{gray.100}',
-          200: '{gray.200}',
-          300: '{gray.300}',
-          400: '{gray.400}',
-          500: '{gray.500}',
-          600: '{gray.600}',
-          700: '{gray.700}',
-          800: '{gray.800}',
-          900: '{gray.900}',
-          950: '{gray.950}'
-      }
+    primary: {
+      50 : "{gray.50}",
+      100: "{gray.100}",
+      200: "{gray.200}",
+      300: "{gray.300}",
+      400: "{gray.400}",
+      500: "{gray.500}",
+      600: "{gray.600}",
+      700: "{gray.700}",
+      800: "{gray.800}",
+      900: "{gray.900}",
+      950: "{gray.950}"
+    }
   }
 });
 
 
 createInertiaApp(
-    {
-      title  : (title) => `${title} - ${appName}`,
-        resolve: name => {
-            const pages = import.meta.glob('./Pages/Grp/**/*.vue', { eager: true })
-            let page = pages[`./Pages/Grp/${name}.vue`]
-            if(!page) console.error(`File './Pages/Grp/${name}.vue' is not exist`)
-            page.default.layout = page.default.layout || Layout
-            return page
-        },
-      setup({el, App, props, plugin}) {
-        const app = createApp({render: () => h(App, props)});
-        if (import.meta.env.VITE_SENTRY_CUST_DSN) {
-          Sentry.init({
-                        app,
-                        dsn                     : import.meta.env.VITE_SENTRY_CUST_DSN,
-                        environment             : import.meta.env.VITE_APP_ENV,
-                          release: import.meta.env.VITE_RELEASE,
-                        replaysSessionSampleRate: 0.1,
-                        replaysOnErrorSampleRate: 1.0,
-                        integrations: [new Sentry.Replay()]
-                      });
+  {
+    title  : (title) => `${title} - ${appName}`,
+    resolve: name => {
+      const pages = import.meta.glob("./Pages/Grp/**/*.vue", { eager: true });
+      let page = pages[`./Pages/Grp/${name}.vue`];
+      if (!page) console.error(`File './Pages/Grp/${name}.vue' is not exist`);
+      page.default.layout = page.default.layout || Layout;
+      return page;
+    },
+    setup({ el, App, props, plugin }) {
+      const app = createApp({ render: () => h(App, props) });
+      if (import.meta.env.VITE_SENTRY_CUST_DSN) {
+        Sentry.init({
+                      app,
+                      dsn                     : import.meta.env.VITE_SENTRY_CUST_DSN,
+                      environment             : import.meta.env.VITE_APP_ENV,
+                      release                 : import.meta.env.VITE_RELEASE,
+                      replaysSessionSampleRate: 0.1,
+                      replaysOnErrorSampleRate: 1.0,
+                      integrations            : [
+                        Sentry.browserTracingIntegration({
+                                                           // disable automatic span creation
+                                                           // https://github.com/getsentry/sentry-javascript/discussions/8528
+                                                           instrumentNavigation: false,
+                                                           instrumentPageLoad  : false
+                                                         }),
+                        new Sentry.Replay()
+                      ]
+                    });
+      }
+
+      app.use(plugin).
+        use(createPinia()).
+        use(ZiggyVue, Ziggy).
+        use(Notifications).
+        use(FloatingVue).
+        use(ConfirmationService).
+        use(PrimeVue, {
+          theme: {
+            preset : MyPreset,
+            options: {
+              darkModeSelector: ".my-app-dark"  // dark mode of Primevue
+                                                 // depends .my-add-dark in
+                                                 // <html>
+            }
+          }
+        }).
+        use(i18nVue, {
+          resolve: async (lang) => {
+            const languages = import.meta.glob(
+              "../../lang/*.json");
+            return await languages[`../../lang/${lang}.json`]();
+          }
+        }).
+        mount(el);
+
+    },
+    progress: {
+      color: "#4B5563"
+    }
+  });
+
+router.on("before", (route) => {
+  const client = Sentry.getClient();
+
+  const newName = route().current();
+
+  if (newName !== name) {
+    name = newName;
+
+    Sentry.startBrowserTracingNavigationSpan(
+      client,{
+        op: "navigation",
+        name,
+        attributes: {
+          [Sentry.SEMANTIC_ATTRIBUTE_SENTRY_SOURCE]: "route",
         }
+      }
+    );
 
+  }
 
-        app.use(plugin)
-            .use(createPinia())
-            .use(ZiggyVue, Ziggy)
-            .use(Notifications)
-            .use(FloatingVue)
-            .use(ConfirmationService)
-            .use(PrimeVue, {
-              theme: {
-                preset: MyPreset,
-                options: {
-                  darkModeSelector: '.my-app-dark',  // dark mode of Primevue depends .my-add-dark in <html>
-                }
-              }
-            })
-            .use(i18nVue, {
-              resolve: async (lang) => {
-                const languages = import.meta.glob(
-                    '../../lang/*.json');
-                return await languages[`../../lang/${lang}.json`]();
-              },
-            }).
-            mount(el);
+});
 
-      },
-      progress: {
-        color: '#4B5563',
-      },
-    });
+router.on('finish', () => {
+  name = route().current();
+  // always make sure we are using the correct route name
+  const span = Sentry.getActiveSpan();
+  const op = span && Sentry.spanToJSON(span).op;
+  if (op === 'pageload' || op === 'navigation') {
+    span.setName(name);
+  }
+});
+
