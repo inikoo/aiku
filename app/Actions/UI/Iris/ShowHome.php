42378f8401e560048def001348395c342db0d731
Ganes556 <gustiganes@gmail.com>
2025-01-08 15:08:04 +0800

    dynamic route in iris home

--- app/Actions/UI/Iris/ShowHome.php
+++ app/Actions/UI/Iris/ShowHome.php
@@ -11,23 +11,30 @@ namespace App\Actions\UI\Iris;
 use App\Models\Web\Website;
 use Inertia\Inertia;
 use Inertia\Response;
+use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsController;
 
 class ShowHome
 {
     use AsController;
 
-    public function handle(): Response
+    public function handle(ActionRequest $request, $path = null): Response
     {
         /** @var Website $website */
         $website   = request()->get('website');
-        $home      = $website->storefront;
+        if ($path) {
+            $webpage    = $website->webpages()->where('url', $path)->first();
+            if (!$webpage & $path !== '') {
+                abort(404, 'Webpage not found');
+            }
+        }
 
+        $home      = $website->storefront;
         return Inertia::render(
             'Home',
             [
-                'blocks' => $home->published_layout,
-                'data' => $home->data,
+                'blocks' => $webpage?->published_layout ?? $home->published_layout,
+                'data' => $webpage?->data ?? $home->data,
             ]
         );
     }
