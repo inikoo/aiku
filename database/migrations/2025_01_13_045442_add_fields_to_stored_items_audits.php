10fcb5ee7d7c03a9a89bcd846211113dd9d4db6a
Raul Perusquia <raul@inikoo.com>
2025-01-13 22:49:24 +0800

    stored item audits new DB fields

--- database/migrations/2025_01_13_045442_add_fields_to_stored_items_audits.php
+++ database/migrations/2025_01_13_045442_add_fields_to_stored_items_audits.php
@@ -24,11 +24,16 @@ return new class () extends Migration {
             $table->decimal('total_amount', 16)->nullable();
 
 
-            $table->unsignedSmallInteger('number_pallets')->default(0);
-            $table->unsignedSmallInteger('number_stored_items')->default(0);
-            $table->unsignedSmallInteger('number_added_stored_items')->default(0);
-            $table->unsignedSmallInteger('number_edited_stored_items')->default(0);
-            $table->unsignedSmallInteger('number_removed_stored_items')->default(0);
+            $table->unsignedSmallInteger('number_audited_pallets')->default(0);
+            $table->unsignedSmallInteger('number_audited_stored_items')->default(0);
+            $table->unsignedSmallInteger('number_audited_stored_items_with_additions')->default(0)->comment('Number of stored items with stock additions (found stock)');
+            $table->unsignedSmallInteger('number_audited_stored_items_with_with_subtractions')->default(0)->comment('Number of stored items with stock subtractions (lost stock)');
+            $table->unsignedSmallInteger('number_audited_stored_items_with_with_stock_checked')->default(0)->comment('Number of stored items with stock checked (stock was correct)');
+
+            $table->unsignedSmallInteger('number_associated_stored_items')->default(0)->comment('Number of stored items associated to the pallet during the audit');
+            $table->unsignedSmallInteger('number_created_stored_items')->default(0)->comment('Number of stored items created and associated to the pallet during the audit');
+
+
         });
     }
 
@@ -42,11 +47,15 @@ return new class () extends Migration {
             $table->dropColumn('org_net_amount');
             $table->dropColumn('tax_category_id');
             $table->dropColumn('tax_amount');
-            $table->dropColumn('number_pallets');
-            $table->dropColumn('number_stored_items');
-            $table->dropColumn('number_added_stored_items');
-            $table->dropColumn('number_edited_stored_items');
-            $table->dropColumn('number_removed_stored_items');
+            $table->dropColumn('total_amount');
+            $table->dropColumn('number_audited_pallets');
+            $table->dropColumn('number_audited_stored_items');
+            $table->dropColumn('number_audited_stored_items_with_additions');
+            $table->dropColumn('number_audited_stored_items_with_with_subtractions');
+            $table->dropColumn('number_audited_stored_items_with_with_stock_checked');
+            $table->dropColumn('number_associated_stored_items');
+            $table->dropColumn('number_created_stored_items');
+
 
 
         });
