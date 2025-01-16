c4ad00fd81528e472755e75605db33cd596ba8e5
Raul Perusquia <raul@inikoo.com>
2025-01-16 15:01:59 +0800

    work on invoice PDF

--- app/Actions/Retina/Accounting/PdfRetinaInvoice.php
+++ app/Actions/Retina/Accounting/PdfRetinaInvoice.php
@@ -11,12 +11,13 @@ namespace App\Actions\Retina\Accounting;
 use App\Actions\RetinaAction;
 use App\Actions\Traits\WithExportData;
 use App\Models\Accounting\Invoice;
+use Exception;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Lorisleiva\Actions\Concerns\WithAttributes;
 use Mccarlosen\LaravelMpdf\Facades\LaravelMpdf as PDF;
 use Symfony\Component\HttpFoundation\Response;
 
-class RetinaPdfInvoice extends RetinaAction
+class PdfRetinaInvoice extends RetinaAction
 {
     use AsAction;
     use WithAttributes;
@@ -25,23 +26,23 @@ class RetinaPdfInvoice extends RetinaAction
     public function handle(Invoice $invoice): Response
     {
         try {
-            $totalItemsNet = (int) $invoice->total_amount;
-            $totalShipping = (int) $invoice->order?->shipping_amount ?? 0;
+            $totalItemsNet = (int)$invoice->total_amount;
+            $totalShipping = (int)$invoice->order?->shipping_amount ?? 0;
 
             $totalNet = $totalItemsNet + $totalShipping;
 
-            $filename = $invoice->slug . '-' . now()->format('Y-m-d');
+            $filename = $invoice->slug.'-'.now()->format('Y-m-d');
             $pdf      = PDF::loadView('invoices.templates.pdf.invoice', [
-                'shop'          => $invoice->shop,
-                'invoice'       => $invoice,
-                'transactions'  => $invoice->invoiceTransactions,
-                'totalNet'      => $totalNet
+                'shop'         => $invoice->shop,
+                'invoice'      => $invoice,
+                'transactions' => $invoice->invoiceTransactions,
+                'totalNet'     => $totalNet
             ]);
 
             return response($pdf->output(), 200)
                 ->header('Content-Type', 'application/pdf')
-                ->header('Content-Disposition', 'inline; filename="' . $filename . '.pdf"');
-        } catch (\Exception $e) {
+                ->header('Content-Disposition', 'inline; filename="'.$filename.'.pdf"');
+        } catch (Exception ) {
             return response()->json(['error' => 'Failed to generate PDF'], 500);
         }
     }
