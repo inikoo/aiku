3e4cb9b4ba1b6bbbd3924cda1f0f10d8cdf3a025
Ganes556 <gustiganes@gmail.com>
2025-01-17 09:59:46 +0800

    hydrator stats in stored item audit

--- app/Actions/Fulfilment/StoredItemAudit/Hydrators/StoredItemAuditHydrateDeltas.php
+++ app/Actions/Fulfilment/StoredItemAudit/Hydrators/StoredItemAuditHydrateDeltas.php
@@ -11,10 +11,8 @@
 namespace App\Actions\Fulfilment\StoredItemAudit\Hydrators;
 
 use App\Actions\Traits\WithEnumStats;
-use App\Enums\Fulfilment\StoredItem\StoredItemStateEnum;
 use App\Enums\Fulfilment\StoredItemAuditDelta\StoredItemAuditDeltaStateEnum;
 use App\Enums\Fulfilment\StoredItemAuditDelta\StoredItemAuditDeltaTypeEnum;
-use App\Models\Fulfilment\PalletReturn;
 use App\Models\Fulfilment\StoredItemAudit;
 use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Lorisleiva\Actions\Concerns\AsAction;
@@ -24,15 +22,15 @@ class StoredItemAuditHydrateDeltas
     use AsAction;
     use WithEnumStats;
 
-    private PalletReturn $palletReturn;
-    public function __construct(PalletReturn $palletReturn)
+    private StoredItemAudit $StoredItemAudit;
+    public function __construct(StoredItemAudit $StoredItemAudit)
     {
-        $this->palletReturn = $palletReturn;
+        $this->StoredItemAudit = $StoredItemAudit;
     }
 
     public function getJobMiddleware(): array
     {
-        return [(new WithoutOverlapping($this->palletReturn->id))->dontRelease()];
+        return [(new WithoutOverlapping($this->StoredItemAudit->id))->dontRelease()];
     }
 
     public function handle(StoredItemAudit $storedItemAudit): void
@@ -40,16 +38,19 @@ class StoredItemAuditHydrateDeltas
 
         $deltas = $storedItemAudit->deltas;
 
+        $inProcessDeltas = $deltas->where('state', StoredItemAuditDeltaStateEnum::IN_PROCESS);
+        $additionDeltas = $deltas->where('audit_type', StoredItemAuditDeltaTypeEnum::ADDITION);
+        $subtractionDeltas = $deltas->where('audit_type', StoredItemAuditDeltaTypeEnum::SUBTRACTION);
+        $checkDeltas = $deltas->where('audit_type', StoredItemAuditDeltaTypeEnum::CHECK);
+
         $stats = [
             'number_audited_pallets' => $deltas->pluck('pallet_id')->unique()->count(),
             'number_audited_stored_items' => $deltas->pluck('stored_item_id')->unique()->count(),
-            'number_audited_stored_items_with_additions' => $deltas->where('audit_type', StoredItemAuditDeltaTypeEnum::ADDITION)->pluck('stored_item_id')->unique()->count(),
-            'number_audited_stored_items_with_with_subtractions' => $deltas->where('audit_type', StoredItemAuditDeltaTypeEnum::SUBTRACTION)->pluck('stored_item_id')->unique()->count(),
-            'number_audited_stored_items_with_with_stock_checked' => $deltas->where('audit_type', StoredItemAuditDeltaTypeEnum::CHECK)->pluck('stored_item_id')->unique()->count(),
-            'number_associated_stored_items' => $deltas->where('state', StoredItemAuditDeltaStateEnum::IN_PROCESS)->pluck('pallet_id')->unique()->count(),
-            'number_created_stored_items' => $deltas->where('state', StoredItemAuditDeltaStateEnum::IN_PROCESS)->filter(function ($delta) {
-                return $delta->storedItem->state === StoredItemStateEnum::IN_PROCESS;
-            })->pluck('pallet_id')->unique()->count(),
+            'number_audited_stored_items_with_additions' => $additionDeltas->pluck('stored_item_id')->unique()->count(),
+            'number_audited_stored_items_with_with_subtractions' => $subtractionDeltas->pluck('stored_item_id')->unique()->count(),
+            'number_audited_stored_items_with_with_stock_checked' => $checkDeltas->pluck('stored_item_id')->unique()->count(),
+            'number_associated_stored_items' => $inProcessDeltas->pluck('stored_items')->unique()->count(),
+            'number_created_stored_items' => $inProcessDeltas->where('is_new_stored_item', true)->where('is_stored_item_new_in_pallet', true)->pluck('stored_items')->unique()->count(),
         ];
 
         $storedItemAudit->update($stats);
@@ -60,8 +61,10 @@ class StoredItemAuditHydrateDeltas
 
     public function asCommand($command)
     {
-        $test = StoredItemAudit::find(4);
+        $storedItemAudits = StoredItemAudit::all();
 
-        $this->handle($test);
+        foreach ($storedItemAudits as $storedItemAudit) {
+            $this->handle($storedItemAudit);
+        }
     }
 }
