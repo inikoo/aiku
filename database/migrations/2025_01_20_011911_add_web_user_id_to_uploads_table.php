1df62de42755bca5222992e3aa0d3d3b14442119
Raul Perusquia <raul@inikoo.com>
2025-01-20 12:07:11 +0800

    review dn change

--- database/migrations/2025_01_20_011911_add_web_user_id_to_uploads_table.php
+++ database/migrations/2025_01_20_011911_add_web_user_id_to_uploads_table.php
@@ -1,29 +1,26 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 20 Jan 2025 12:01:51 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+
+    public function up(): void
     {
         Schema::table('uploads', function (Blueprint $table) {
             $table->unsignedInteger('web_user_id')->nullable();
-            $table->foreign('web_user_id')->references('id')->on('web_users');
+            $table->foreign('web_user_id')->references('id')->on('web_users')->nullOnDelete();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('uploads', function (Blueprint $table) {
             $table->dropColumn('web_user_id');
