b4f3ddb8f299e74d0bc10aaca88b4e546d45e7b5
YudhistiraA <aryarajasa0@gmail.com>
2025-01-20 14:45:09 +0800

    hide header search

--- resources/js/Components/CMS/Website/Headers/Header1/Header1Workshop.vue
+++ resources/js/Components/CMS/Website/Headers/Header1/Header1Workshop.vue
@@ -48,10 +48,10 @@ const toggle = (event) => {
 
                 <!-- Search Bar -->
                 <div class="relative justify-self-center w-full max-w-md">
-                    <input type="text" placeholder="Search Products"
+                    <!-- <input type="text" placeholder="Search Products"
                         class="border border-gray-300 py-2 px-4 rounded-md text-sm w-full shadow-inner focus:outline-none focus:border-gray-500">
                     <FontAwesomeIcon icon="fas fa-search"
-                        class="absolute top-1/2 -translate-y-1/2 right-4 text-gray-500" fixed-width />
+                        class="absolute top-1/2 -translate-y-1/2 right-4 text-gray-500" fixed-width /> -->
                 </div>
 
                 <!-- Gold Member Button -->
