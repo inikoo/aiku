566990bec5625731c6dc4f44a28bdfa71023ac50
Raul Perusquia <raul@inikoo.com>
2025-01-22 14:03:09 +0800

    customer in uploads

--- database/migrations/2025_01_22_055508_add_customer_id_to_uploads_table.php
+++ database/migrations/2025_01_22_055508_add_customer_id_to_uploads_table.php
@@ -1,29 +1,26 @@
 <?php
 
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 22 Jan 2025 14:00:35 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::table('uploads', function (Blueprint $table) {
             $table->unsignedInteger('customer_id')->index()->nullable();
-            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('customer_id')->references('id')->on('customers')->nullOnDelete();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('uploads', function (Blueprint $table) {
             $table->dropColumn('customer_id');
