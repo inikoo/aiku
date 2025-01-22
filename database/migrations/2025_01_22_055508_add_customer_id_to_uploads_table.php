4f4bc2b54240ab5fc86314ae0162f90d6d617bd8
Artha <manager.arthachan@gmail.com>
2025-01-22 13:57:34 +0800

    add: migrations uploads customer id

---
+++ database/migrations/2025_01_22_055508_add_customer_id_to_uploads_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('uploads', function (Blueprint $table) {
+            $table->unsignedInteger('customer_id')->index()->nullable();
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('uploads', function (Blueprint $table) {
+            $table->dropColumn('customer_id');
+        });
+    }
+};
