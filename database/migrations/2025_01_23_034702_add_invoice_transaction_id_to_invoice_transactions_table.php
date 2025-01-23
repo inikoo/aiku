7eb025a32a548b5f3641669afc7f3ceddd5bbed8
Raul Perusquia <raul@inikoo.com>
2025-01-23 12:16:42 +0800

    #1383 Refunds, review db migrations

--- database/migrations/2025_01_23_034702_add_invoice_transaction_id_to_invoice_transactions_table.php
+++ database/migrations/2025_01_23_034702_add_invoice_transaction_id_to_invoice_transactions_table.php
@@ -1,29 +1,26 @@
 <?php
 
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 23 Jan 2025 11:51:04 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::table('invoice_transactions', function (Blueprint $table) {
-            $table->unsignedInteger('invoice_transaction_id')->nullable()->index();
+            $table->unsignedInteger('invoice_transaction_id')->nullable()->index()->comment('For refunds link to original invoice transaction');
             $table->foreign('invoice_transaction_id')->references('id')->on('invoice_transactions')->nullOnDelete();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('invoice_transactions', function (Blueprint $table) {
             $table->dropColumn('invoice_transaction_id');
--- database/migrations/2025_01_23_035328_add_in_process_to_invoice_transactions_table.php
+++ database/migrations/2025_01_23_035328_add_in_process_to_invoice_transactions_table.php
@@ -1,28 +1,25 @@
 <?php
 
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 23 Jan 2025 12:13:20 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::table('invoice_transactions', function (Blueprint $table) {
-            $table->boolean('in_process')->default(false);
+            $table->boolean('in_process')->default(false)->comment('Used for refunds only');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('invoice_transactions', function (Blueprint $table) {
             $table->dropColumn('in_process');
