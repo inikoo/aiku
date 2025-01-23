da99939e61b4adc63730d826881b67fec3ceace1
YudhistiraA <aryarajasa0@gmail.com>
2025-01-23 14:17:21 +0800

    fix button can click in iris

--- resources/js/Layouts/Iris.vue
+++ resources/js/Layouts/Iris.vue
@@ -103,13 +103,18 @@ const setFirstVisitToFalse = () => {
         </template>
     </notifications>
 
+    <div class="fixed bottom-0 right-0 z-50" style="width: 300px; height: 700px;">
     <iframe 
         id="superchat-widget" 
-        class="fixed bottom-0 right-0 z-[10000] placement-right" 
-        style="width: 100%; height: 100%;" 
+        class="rounded-lg shadow-lg" 
+        style="width: 100%; height: 100%; border: none;" 
         src="https://widget.superchat.de/v2?applicationKey=WCNK7nqXPQlrVGq895A2obLRVa">
     </iframe>
+</div>
 
+
+
+ 
 </template>
 
 <style lang="scss">
