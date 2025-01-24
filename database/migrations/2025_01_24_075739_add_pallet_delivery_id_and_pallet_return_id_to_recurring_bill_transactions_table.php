8008437d80655cd1cfbe6957721e9ca42fded53a
Raul Perusquia <raul@inikoo.com>
2025-01-24 16:07:26 +0800

    review recurring bill relationship w PR and PD

--- database/migrations/2025_01_24_075739_add_pallet_delivery_id_and_pallet_return_id_to_recurring_bill_transactions_table.php
+++ database/migrations/2025_01_24_075739_add_pallet_delivery_id_and_pallet_return_id_to_recurring_bill_transactions_table.php
@@ -1,31 +1,28 @@
 <?php
 
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Fri, 24 Jan 2025 16:03:38 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::table('recurring_bill_transactions', function (Blueprint $table) {
             $table->unsignedInteger('pallet_delivery_id')->index()->nullable();
-            $table->foreign('pallet_delivery_id')->references('id')->on('pallet_deliveries');
+            $table->foreign('pallet_delivery_id')->references('id')->on('pallet_deliveries')->cascadeOnDelete();
             $table->unsignedInteger('pallet_return_id')->index()->nullable();
-            $table->foreign('pallet_return_id')->references('id')->on('pallet_returns');
+            $table->foreign('pallet_return_id')->references('id')->on('pallet_returns')->cascadeOnDelete();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('recurring_bill_transactions', function (Blueprint $table) {
             $table->dropColumn('pallet_delivery_id');
