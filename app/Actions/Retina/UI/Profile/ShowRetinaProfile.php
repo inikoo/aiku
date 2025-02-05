5df67c23b47f3fb86eccb02a57b37b7b290cdf16
KirinZero0 <aryapermana02@gmail.com>
2025-02-05 09:46:45 +0800

    Retina Profile Wrong image id fix

--- app/Actions/Retina/UI/Profile/ShowRetinaProfile.php
+++ app/Actions/Retina/UI/Profile/ShowRetinaProfile.php
@@ -30,7 +30,6 @@ class ShowRetinaProfile extends RetinaAction
     {
         $customer = $webUser->customer;
 
-
         $personalInformationFields = [
             'contact_name' => [
                 'type'  => 'input',
@@ -50,7 +49,7 @@ class ShowRetinaProfile extends RetinaAction
             'image'        => [
                 'type'  => 'image_crop_square',
                 'label' => __('avatar'),
-                'value' => !blank($customer->image_id)
+                'value' => !blank($webUser->image_id)
                     ? $customer->imageSources(320, 320)
                     : [
                         'original' => '/retina-default-user.svg'
