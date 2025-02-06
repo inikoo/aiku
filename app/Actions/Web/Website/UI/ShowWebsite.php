19d425e5a8078c8866976313f7d5d07c15372b41
Artha <manager.arthachan@gmail.com>
2025-02-06 14:19:59 +0800

    fix: send email to customer view link

--- app/Actions/Web/Website/UI/ShowWebsite.php
+++ app/Actions/Web/Website/UI/ShowWebsite.php
@@ -20,7 +20,6 @@ use App\Actions\Web\HasWorkshopAction;
 use App\Actions\Web\Website\GetWebsiteCloudflareAnalytics;
 use App\Actions\Web\Website\GetWebsiteWorkshopLayout;
 use App\Enums\UI\Web\WebsiteTabsEnum;
-use App\Enums\UI\Web\WebsiteWorkshopTabsEnum;
 use App\Enums\Web\Website\WebsiteStateEnum;
 use App\Http\Resources\CRM\WebUsersResource;
 use App\Http\Resources\History\HistoryResource;
@@ -118,7 +117,7 @@ class ShowWebsite extends OrgAction
                         ),
 
                 ],
-               /*  "layout" =>  GetWebsiteWorkshopLayout::run($this->scope, $website), */
+               "layout" =>  GetWebsiteWorkshopLayout::run($this->scope, $website),
                 'tabs'        => [
                     'current'    => $this->tab,
                     'navigation' => WebsiteTabsEnum::navigation()
