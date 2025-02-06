0e413a0fc4ba9560c5f2ce4ba0a3cd0e4c8e175f
KirinZero0 <aryapermana02@gmail.com>
2025-02-06 11:24:50 +0800

    migrations for audit

---
+++ database/migrations/2025_02_06_024420_set_foreign_key_for_pallet_return_items_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::table('pallet_return_items', function (Blueprint $table) {
+            $table->dropForeign(['pallet_stored_item_id']);
+            $table->foreign('pallet_stored_item_id')->references('id')->on('pallet_stored_items')->nullOnDelete();
+        });
+    }
+
+
+    public function down(): void
+    {
+        
+    }
+};
