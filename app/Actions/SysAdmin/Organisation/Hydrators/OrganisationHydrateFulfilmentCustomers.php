ef979678dcf68e6a199eb008e6c2bd13298a8238
Ganes556 <gustiganes@gmail.com>
2025-02-06 14:32:21 +0800

    add number_customers_status_pending_approval, hydrator & show in ui

--- app/Actions/SysAdmin/Organisation/Hydrators/OrganisationHydrateFulfilmentCustomers.php
+++ app/Actions/SysAdmin/Organisation/Hydrators/OrganisationHydrateFulfilmentCustomers.php
@@ -8,6 +8,7 @@
 
 namespace App\Actions\SysAdmin\Organisation\Hydrators;
 
+use App\Enums\CRM\Customer\CustomerStatusEnum;
 use App\Models\SysAdmin\Organisation;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -19,8 +20,14 @@ class OrganisationHydrateFulfilmentCustomers
     public function handle(Organisation $organisation): void
     {
         $stats = [
+            'number_customers_interest_pallets_storage' => $organisation->fulfilmentCustomers()->where('pallets_storage', true)->count(),
+            'number_customers_interest_items_storage'   => $organisation->fulfilmentCustomers()->where('items_storage', true)->count(),
+            'number_customers_interest_dropshipping'    => $organisation->fulfilmentCustomers()->where('dropshipping', true)->count(),
             'number_customers_with_stored_items' => $organisation->fulfilmentCustomers()->where('number_stored_items', '>', 0)->count(),
             'number_customers_with_pallets'      => $organisation->fulfilmentCustomers()->where('number_pallets', '>', 0)->count(),
+            'number_customers_status_pending_approval' => $organisation->fulfilmentCustomers->filter(function ($fulfilmentCustomer) {
+                return $fulfilmentCustomer->customer->status == CustomerStatusEnum::PENDING_APPROVAL;
+            })->count()
         ];
 
         $organisation->fulfilmentStats()->update($stats);
