21a51c971116dee115bc42da423d43d2a3289881
dandiAW <dandiferrari75@gmail.com>
2025-02-07 14:58:18 +0800

    table tree setup

---
+++ resources/js/Pages/Grp/Org/Web/Structure.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { Head } from "@inertiajs/vue3"
+import PageHeading from "@/Components/Headings/PageHeading.vue"
+import TableStructure from "@/Components/Tables/Grp/Org/Web/TableStructure.vue"
+import { capitalize } from "@/Composables/capitalize"
+import {
+	faShapes,
+	faSortAmountDownAlt,
+	faBrowser,
+	faSortAmountDown,
+	faHome,
+	faSignInAlt,
+	faBooks,
+	faColumns,
+	faInfoCircle,
+	faNewspaper,
+} from "@fal"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { PageHeading as TSPageHeading } from "@/types/PageHeading"
+library.add(
+	faShapes,
+	faSortAmountDownAlt,
+	faBrowser,
+	faSortAmountDown,
+	faHome,
+	faSignInAlt,
+	faBooks,
+	faColumns,
+	faInfoCircle,
+	faNewspaper
+)
+
+const props = defineProps<{
+	pageHead: TSPageHeading
+	title: string
+	data: {}
+}>()
+</script>
+
+<template>
+      <Head :title="capitalize(title)" />
+      <PageHeading :data="pageHead" />
+      <TableStructure  />
+</template>
