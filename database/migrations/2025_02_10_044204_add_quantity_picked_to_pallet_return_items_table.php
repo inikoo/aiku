d81893d214c16ccfa6c8015aad0db2c7ea82bb4b
Raul Perusquia <raul@inikoo.com>
2025-02-10 12:46:02 +0800

    review migration

--- database/migrations/2025_02_10_044204_add_quantity_picked_to_pallet_return_items_table.php
+++ database/migrations/2025_02_10_044204_add_quantity_picked_to_pallet_return_items_table.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 10 Feb 2025 12:44:20 Central Indonesia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -6,24 +11,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+
+    public function up(): void
     {
         Schema::table('pallet_return_items', function (Blueprint $table) {
-            $table->integer('quantity_picked')->default(0);
+            $table->decimal('quantity_picked', 16, 3)->default(0);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('pallet_return_items', function (Blueprint $table) {
             $table->dropColumn('quantity_picked');
