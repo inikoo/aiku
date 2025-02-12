2e2968fada1e98f864be4baf9bae8480df8a7433
Raul Perusquia <raul@inikoo.com>
2025-02-12 11:19:31 +0800

    review migrations

--- database/migrations/2025_02_12_030452_add_registered_at_to_customers_table.php
+++ database/migrations/2025_02_12_030452_add_registered_at_to_customers_table.php
@@ -1,31 +1,28 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Wed, 12 Feb 2025 11:15:47 Central Indonesia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+
+    public function up(): void
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->dateTimeTz('registered_at')->nullable();
+            $table->dateTimeTz('registered_at')->nullable()->index();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->dropColumn('registered_at ');
+            $table->dropColumn('registered_at');
         });
 
     }
