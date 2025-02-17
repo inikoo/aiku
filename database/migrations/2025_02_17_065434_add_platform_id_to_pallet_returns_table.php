55693cb98123bc97e870bba52e6df4452f55fc6c
Raul Perusquia <raul@inikoo.com>
2025-02-17 18:05:23 +0800

    add mirror type to payment_account_shop

--- database/migrations/2025_02_17_065434_add_platform_id_to_pallet_returns_table.php
+++ database/migrations/2025_02_17_065434_add_platform_id_to_pallet_returns_table.php
@@ -1,29 +1,26 @@
 <?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Mon, 17 Feb 2025 18:04:32 Central Indonesia Time, Sanur, Bali, Indonesia
+ * Copyright (c) 2025, Raul A Perusquia Flores
+ */
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+
+    public function up(): void
     {
         Schema::table('pallet_returns', function (Blueprint $table) {
             $table->unsignedSmallInteger('platform_id')->index()->nullable();
-            $table->foreign('platform_id')->references('id')->on('platforms')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('platform_id')->references('id')->on('platforms')->nullOnDelete();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+
+    public function down(): void
     {
         Schema::table('pallet_returns', function (Blueprint $table) {
             $table->dropColumn(['platform_id']);
