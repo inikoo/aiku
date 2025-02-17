7641a0ca2e828d25999d7ff3fff2c96015109db7
aqordeon <aqordivika@yahoo.co.id>
2025-02-17 16:43:09 +0800

    fix: PR Stored Item: zero value on picked

--- resources/js/Components/Tables/Grp/Org/Fulfilment/TableStoredItemReturnStoredItems.vue
+++ resources/js/Components/Tables/Grp/Org/Fulfilment/TableStoredItemReturnStoredItems.vue
@@ -327,7 +327,7 @@ const generateLinkPalletLocation = (pallet: any) => {
                                     </NumberWithButtonSave>
                                 </template>
     
-                                <div v-else class="flex flex-nowrap gap-x-1 items-center">
+                                <div v-else class="flex flex-nowrap gap-x-1 items-center tabular-nums">
                                     <ButtonWithLink
                                         v-if="palletReturn.state == 'picking' && pallet_stored_item.state == 'picked'"
                                         icon="fal fa-undo-alt"
@@ -338,7 +338,7 @@ const generateLinkPalletLocation = (pallet: any) => {
                                         class="py-0 mr-1"
                                         :routeTarget="pallet_stored_item.undoRoute"
                                     />
-                                    {{ pallet_stored_item.picked_quantity }}
+                                    {{ pallet_stored_item.picked_quantity }}/{{ pallet_stored_item.selected_quantity }}
                                     <FontAwesomeIcon v-if="pallet_stored_item.state == 'picked'" v-tooltip="trans('Picked')" icon='fal fa-check' class='text-green-500' fixed-width aria-hidden='true' />
                                 </div>
     
