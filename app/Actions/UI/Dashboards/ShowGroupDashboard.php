5f5c30f8fc9fda4695c972e5b02737069e6e1a85
Ganes556 <gustiganes@gmail.com>
2025-02-18 16:34:08 +0800

    passing data persentage refunds

--- app/Actions/UI/Dashboards/ShowGroupDashboard.php
+++ app/Actions/UI/Dashboards/ShowGroupDashboard.php
@@ -112,6 +112,7 @@ class ShowGroupDashboard extends OrgAction
             'total_invoices'             => 0,
             'total_invoices_percentages' => 0,
             'total_refunds'              => 0,
+            'total_refunds_percentages'  => 0,
         ];
 
         if ($this->tabDashboardInterval == GroupDashboardIntervalTabsEnum::SALES->value) {
@@ -189,6 +190,7 @@ class ShowGroupDashboard extends OrgAction
                     $selectedInterval,
                 );
                 $total['total_invoices_percentages']              += $responseData['interval_percentages']['invoices']['percentage'] ?? 0;
+                $total['total_refunds_percentages']              += $responseData['interval_percentages']['refunds']['percentage'] ?? 0;
 
                 $total['total_invoices'] += $responseData['interval_percentages']['invoices']['amount'];
                 $total['total_refunds']  += $responseData['interval_percentages']['refunds']['amount'];
@@ -365,6 +367,8 @@ class ShowGroupDashboard extends OrgAction
                 $total['total_invoices']             += $responseData['interval_percentages']['invoices']['amount'];
 
                 $total['total_refunds'] += $responseData['interval_percentages']['refunds']['amount'];
+                $total['total_refunds_percentages']              += $responseData['interval_percentages']['refunds']['percentage'] ?? 0;
+
 
                 // visual data
                 $visualData['invoices_data']['labels'][]              = $shop->code;
