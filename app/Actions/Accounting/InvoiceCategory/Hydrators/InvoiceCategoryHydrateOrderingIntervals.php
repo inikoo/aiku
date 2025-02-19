f3ea3e2122d3656b302dddac507de44d36049b67
Ganes556 <gustiganes@gmail.com>
2025-02-19 08:19:51 +0800

    hydrator invoice category

---
+++ app/Actions/Accounting/InvoiceCategory/Hydrators/InvoiceCategoryHydrateOrderingIntervals.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 19-02-2025, Bali, Indonesia
+ * Github: https://github.com/Ganes556
+ * Copyright: 2025
+ *
+*/
+
+namespace App\Actions\Accounting\InvoiceCategory\Hydrators;
+
+use App\Actions\Traits\WithIntervalsAggregators;
+use App\Enums\Accounting\Invoice\InvoiceTypeEnum;
+use App\Models\Accounting\Invoice;
+use App\Models\Accounting\InvoiceCategory;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class InvoiceCategoryHydrateOrderingIntervals
+{
+    use AsAction;
+    use WithIntervalsAggregators;
+
+    public string $jobQueue = 'orders';
+
+    private InvoiceCategory $invoicecategory;
+
+    public function __construct(InvoiceCategory $invoicecategory)
+    {
+        $this->invoicecategory = $invoicecategory;
+    }
+
+    public function getJobMiddleware(): array
+    {
+        return [(new WithoutOverlapping($this->invoicecategory->id))->dontRelease()];
+    }
+
+    public function handle(InvoiceCategory $invoiceCategory): void
+    {
+
+        $stats = [];
+
+        $queryBase = Invoice::where('invoice_category_id', $invoiceCategory->id)->where('type', InvoiceTypeEnum::INVOICE)->selectRaw('count(*) as  sum_aggregate');
+        $stats     = $this->getIntervalsData(
+            stats: $stats,
+            queryBase: $queryBase,
+            statField: 'invoices_'
+        );
+
+        $queryBase = Invoice::where('invoice_category_id', $invoiceCategory->id)->where('type', InvoiceTypeEnum::REFUND)->selectRaw(' count(*) as  sum_aggregate');
+        $stats     = $this->getIntervalsData(
+            stats: $stats,
+            queryBase: $queryBase,
+            statField: 'refunds_'
+        );
+
+        $invoiceCategory->salesIntervals->update($stats);
+    }
+
+    public string $commandSignature = 'invoicecategory:hydrate_ordering_intervals';
+
+    public function asCommand($command)
+    {
+        $f = InvoiceCategory::find(1);
+        $this->handle($f);
+    }
+
+}
---
+++ app/Actions/Accounting/InvoiceCategory/Hydrators/InvoiceCategoryHydrateSalesIntervals.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Author: Ganes <gustiganes@gmail.com>
+ * Created on: 18-02-2025, Bali, Indonesia
+ * Github: https://github.com/Ganes556
+ * Copyright: 2025
+ *
+*/
+
+namespace App\Actions\Accounting\InvoiceCategory\Hydrators;
+
+use App\Actions\Traits\WithIntervalsAggregators;
+use App\Enums\Accounting\Invoice\InvoiceTypeEnum;
+use App\Models\Accounting\Invoice;
+use App\Models\Accounting\InvoiceCategory;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class InvoiceCategoryHydrateSalesIntervals
+{
+    use AsAction;
+    use WithIntervalsAggregators;
+
+    public string $jobQueue = 'sales';
+
+    private InvoiceCategory $invoicecategory;
+
+    public function __construct(InvoiceCategory $invoicecategory)
+    {
+        $this->invoicecategory = $invoicecategory;
+    }
+
+    public function getJobMiddleware(): array
+    {
+        return [(new WithoutOverlapping($this->invoicecategory->id))->dontRelease()];
+    }
+
+    public function handle(InvoiceCategory $invoiceCategory): void
+    {
+
+        $stats = [];
+
+        $queryBase = Invoice::where('invoice_category_id', $invoiceCategory->id)->where('type', InvoiceTypeEnum::INVOICE)->selectRaw('sum(net_amount) as  sum_aggregate');
+        $stats     = $this->getIntervalsData(
+            stats: $stats,
+            queryBase: $queryBase,
+            statField: 'sales_'
+        );
+
+        $queryBase = Invoice::where('invoice_category_id', $invoiceCategory->id)->where('type', InvoiceTypeEnum::INVOICE)->selectRaw('sum(grp_net_amount) as  sum_aggregate');
+        $stats     = $this->getIntervalsData(
+            stats: $stats,
+            queryBase: $queryBase,
+            statField: 'sales_grp_currency_'
+        );
+
+        $queryBase = Invoice::where('invoice_category_id', $invoiceCategory->id)->where('type', InvoiceTypeEnum::INVOICE)->selectRaw('sum(org_net_amount) as  sum_aggregate');
+        $stats     = $this->getIntervalsData(
+            stats: $stats,
+            queryBase: $queryBase,
+            statField: 'sales_org_currency_'
+        );
+
+
+        $invoiceCategory->salesIntervals->update($stats);
+    }
+
+    public string $commandSignature = 'invoicecategory:hydrate_sales_intervals';
+
+    public function asCommand($command)
+    {
+        $f = InvoiceCategory::find(1);
+        $this->handle($f);
+    }
+
+}
