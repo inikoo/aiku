0101d6b1980347cda57386c4525a5e0dc1565e19
Artha <manager.arthachan@gmail.com>
2025-02-19 14:47:45 +0800

    [Afix: approved customer

--- app/Actions/CRM/Customer/ApproveCustomer.php
+++ app/Actions/CRM/Customer/ApproveCustomer.php
@@ -9,43 +9,41 @@
 namespace App\Actions\CRM\Customer;
 
 use App\Actions\Catalogue\Shop\Hydrators\ShopHydrateCrmStats;
+use App\Actions\Comms\Email\SendCustomerApprovedEmail;
 use App\Actions\OrgAction;
 use App\Actions\Traits\WithActionUpdate;
 use App\Enums\CRM\Customer\CustomerStatusEnum;
 use App\Models\CRM\Customer;
-use Illuminate\Validation\Rule;
 use Lorisleiva\Actions\ActionRequest;
 
 class ApproveCustomer extends OrgAction
 {
     use WithActionUpdate;
 
-    public function handle(Customer $customer, array $modelData): Customer
+    public function handle(Customer $customer): Customer
     {
-        $customer = $this->update($customer, $modelData);
+        $customer = $this->update($customer, [
+            'status' => CustomerStatusEnum::APPROVED,
+            'approved_at' => now()
+        ]);
+
+        SendCustomerApprovedEmail::run($customer);
+
         ShopHydrateCrmStats::run($customer->shop);
         return $customer;
     }
-
-    public function rules(): array
-    {
-        return [
-            'status' => ['required', 'string', Rule::enum(CustomerStatusEnum::class)],
-        ];
-    }
-
 
     public function asController(Customer $customer, ActionRequest $request): Customer
     {
         $this->initialisation($customer->organisation, $request);
 
-        return $this->handle($customer, $this->validatedData);
+        return $this->handle($customer);
     }
 
     public function action(Customer $customer, array $modelData): Customer
     {
         $this->initialisation($customer->organisation, $modelData);
 
-        return $this->handle($customer, $this->validatedData);
+        return $this->handle($customer);
     }
 }
