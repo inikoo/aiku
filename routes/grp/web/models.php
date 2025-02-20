068877bd8a3aca9fd8900614cb2b4f5eee8f03f8
Raul Perusquia <raul@inikoo.com>
2025-02-20 15:24:17 +0800

    rename action

--- routes/grp/web/models.php
+++ routes/grp/web/models.php
@@ -101,7 +101,7 @@ use App\Actions\Fulfilment\PalletReturn\UpdatePalletReturn;
 use App\Actions\Fulfilment\PalletReturn\UpdatePalletReturnDeliveryAddress;
 use App\Actions\Fulfilment\PalletReturnItem\NotPickedPalletFromReturn;
 use App\Actions\Fulfilment\PalletReturnItem\PickNewPalletReturnItem;
-use App\Actions\Fulfilment\PalletReturnItem\PickPalletReturnItem;
+use App\Actions\Fulfilment\PalletReturnItem\PickPalletReturnItemInPalletReturnWithStoredItem;
 use App\Actions\Fulfilment\PalletReturnItem\SyncPalletReturnItem;
 use App\Actions\Fulfilment\PalletReturnItem\UndoPalletReturnItem;
 use App\Actions\Fulfilment\PalletReturnItem\UndoPickingPalletFromReturn;
@@ -454,7 +454,7 @@ Route::name('pallet.')->prefix('pallet/{pallet:id}')->group(function () {
 
 Route::name('pallet-return-item.')->prefix('pallet-return-item/{palletReturnItem}')->group(function () {
     Route::patch('', PickWholePalletInPalletReturn::class)->name('set_as_picked');
-    Route::patch('update', PickPalletReturnItem::class)->name('update');
+    Route::patch('update', PickPalletReturnItemInPalletReturnWithStoredItem::class)->name('update');
     Route::patch('undo-picking-stored-item', UndoStoredItemPick::class)->name('undo-picking-stored-item');
     Route::patch('not-picked', NotPickedPalletFromReturn::class)->name('not-picked');
     Route::patch('undo-picking', UndoPickingPalletFromReturn::class)->name('undo-picking');
