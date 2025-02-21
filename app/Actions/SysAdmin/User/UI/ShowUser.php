93571906122b8bee02395551cb3613502599c23f
Raul Perusquia <raul@inikoo.com>
2025-02-21 14:51:45 +0800

    work on employee-> user navigation

--- app/Actions/SysAdmin/User/UI/ShowUser.php
+++ app/Actions/SysAdmin/User/UI/ShowUser.php
@@ -9,26 +9,47 @@
 namespace App\Actions\SysAdmin\User\UI;
 
 use App\Actions\Analytics\UserRequest\UI\ShowUserRequestLogs;
-use App\Actions\GrpAction;
 use App\Actions\Helpers\History\UI\IndexHistory;
+use App\Actions\HumanResources\Employee\UI\ShowEmployee;
+use App\Actions\OrgAction;
 use App\Actions\SysAdmin\UI\ShowSysAdminDashboard;
 use App\Actions\SysAdmin\User\WithUserSubNavigation;
 use App\Enums\UI\SysAdmin\UserTabsEnum;
 use App\Http\Resources\History\HistoryResource;
 use App\Http\Resources\SysAdmin\UserResource;
 use App\Http\Resources\SysAdmin\UserShowcaseResource;
+use App\Models\HumanResources\Employee;
+use App\Models\SysAdmin\Group;
+use App\Models\SysAdmin\Organisation;
 use App\Models\SysAdmin\User;
+use Illuminate\Support\Arr;
 use Inertia\Inertia;
 use Inertia\Response;
 use Lorisleiva\Actions\ActionRequest;
 
-class ShowUser extends GrpAction
+class ShowUser extends OrgAction
 {
     use WithUserSubNavigation;
 
+    /**
+     * @var \App\Models\SysAdmin\Organisation|\group|\Illuminate\Foundation\Application|mixed|object
+     */
+    private mixed $auth_scope;
+
     public function asController(User $user, ActionRequest $request): User
     {
-        $this->initialisation(app('group'), $request)->withTab(UserTabsEnum::values());
+        $group            = app('group');
+        $this->auth_scope = $group;
+        $this->initialisationFromGroup($group, $request)->withTab(UserTabsEnum::values());
+
+        return $user;
+    }
+
+    public function inEmployee(Organisation $organisation, Employee $employee, User $user, ActionRequest $request): User
+    {
+        $this->auth_scope = $organisation;
+        $this->initialisation($organisation, $request)->withTab(UserTabsEnum::values());
+
         return $user;
     }
 
@@ -39,8 +60,15 @@ class ShowUser extends GrpAction
 
     public function authorize(ActionRequest $request): bool
     {
-        $this->canEdit = $request->user()->authTo('sysadmin.edit');
-        return $request->user()->authTo("sysadmin.view");
+        if ($this->auth_scope instanceof Group) {
+            $this->canEdit = $request->user()->authTo('sysadmin.edit');
+
+            return $request->user()->authTo("sysadmin.view");
+        } else {
+            $this->canEdit = $request->user()->authTo("human-resources.{$this->organisation->id}.view");
+
+            return $request->user()->authTo("human-resources.{$this->organisation->id}.view");
+        }
     }
 
     public function htmlResponse(User $user, ActionRequest $request): Response
@@ -53,20 +81,21 @@ class ShowUser extends GrpAction
                     $request->route()->getName(),
                     $request->route()->originalParameters()
                 ),
-                'navigation' => [
-                    'previous' => $this->getPrevious($user, $request),
-                    'next'     => $this->getNext($user, $request),
-                ],
-                'pageHead' => [
-                    'model'   => __('user'),
-                    'icon'    =>
+                'navigation'  => $this->auth_scope instanceof Group
+                    ? [
+                        'previous' => $this->getPrevious($user, $request),
+                        'next'     => $this->getNext($user, $request),
+                    ] : null,
+                'pageHead'    => [
+                    'model'         => __('user'),
+                    'icon'          =>
                         [
                             'icon'  => ['fal', 'fa-user-circle'],
                             'title' => __('user')
                         ],
                     'subNavigation' => $this->getUserNavigation($user, $request),
-                    'title'   => $user->username,
-                    'actions' => [
+                    'title'         => $user->username,
+                    'actions'       => [
                         $this->canEdit ? [
                             'type'  => 'button',
                             'style' => 'edit',
@@ -77,7 +106,7 @@ class ShowUser extends GrpAction
                         ] : false,
                     ]
                 ],
-                'tabs' => [
+                'tabs'        => [
                     'current'    => $this->tab,
                     'navigation' => UserTabsEnum::navigation()
                 ],
@@ -109,7 +138,6 @@ class ShowUser extends GrpAction
 
     public function getBreadcrumbs(string $routeName, array $routeParameters, string $suffix = ''): array
     {
-
         $headCrumb = function (User $user, array $routeParameters, string $suffix) {
             return [
                 [
@@ -126,13 +154,18 @@ class ShowUser extends GrpAction
                         ],
 
                     ],
-                    'suffix' => $suffix
+                    'suffix'         => $suffix
 
                 ],
             ];
         };
 
-        $user = User::where('username', $routeParameters['user'])->first();
+        $employee = null;
+        $user     = User::where('username', $routeParameters['user'])->first();
+        if (Arr::has($routeParameters, 'employee')) {
+            $employee = Employee::where('slug', $routeParameters['employee'])->first();
+        }
+
 
         return match ($routeName) {
             'grp.sysadmin.users.show',
@@ -155,23 +188,40 @@ class ShowUser extends GrpAction
                     $suffix
                 ),
             ),
+            'grp.org.hr.employees.show.users.show' =>
+            array_merge(
+                ShowEmployee::make()->getBreadcrumbs($employee, $routeParameters),
+                [
+                    [
+                        'type'   => 'simple',
+                        'simple' => [
+                            'route' => [
+                                'name'       => $routeName,
+                                'parameters' => $routeParameters
+                            ],
+                            'label' => $user->username,
+                            'icon'  => 'fal fa-user-circle',
+                        ]
+                    ],
 
+                ],
+            ),
 
             default => []
         };
-
     }
 
     public function getPrevious(User $user, ActionRequest $request): ?array
     {
         $previous = User::where('username', '<', $user->username)->orderBy('username', 'desc')->first();
-        return $this->getNavigation($previous, $request->route()->getName());
 
+        return $this->getNavigation($previous, $request->route()->getName());
     }
 
     public function getNext(User $user, ActionRequest $request): ?array
     {
         $next = User::where('username', '>', $user->username)->orderBy('username')->first();
+
         return $this->getNavigation($next, $request->route()->getName());
     }
 
@@ -180,6 +230,7 @@ class ShowUser extends GrpAction
         if (!$user) {
             return null;
         }
+
         return match ($routeName) {
             'grp.sysadmin.users.show' => [
                 'label' => $user->username,
