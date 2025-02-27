c8850eaece1380793f61623cfeb7d78710e0b39b
Raul Perusquia <raul@inikoo.com>
2025-02-27 12:50:32 +0800

    fix DeleteRefundInProcessInvoiceTransaction

--- app/Actions/Accounting/InvoiceTransaction/DeleteRefundInProcessInvoiceTransaction.php
+++ app/Actions/Accounting/InvoiceTransaction/DeleteRefundInProcessInvoiceTransaction.php
@@ -11,26 +11,34 @@ namespace App\Actions\Accounting\InvoiceTransaction;
 
 use App\Actions\OrgAction;
 use App\Actions\Traits\WithActionUpdate;
+use App\Enums\Accounting\Invoice\InvoiceTypeEnum;
 use App\Models\Accounting\InvoiceTransaction;
+use Illuminate\Validation\Validator;
 use Lorisleiva\Actions\ActionRequest;
 
-class DeleteInvoiceTransaction extends OrgAction
+class DeleteRefundInProcessInvoiceTransaction extends OrgAction
 {
     use WithActionUpdate;
 
     public function handle(InvoiceTransaction $invoiceTransaction): void
     {
         $invoiceTransaction->delete();
-
     }
 
-    //todo authorisation
-    public function fromRetina(InvoiceTransaction $invoiceTransaction, ActionRequest $request): void
+
+    public function afterValidator(Validator $validator, ActionRequest $request): void
     {
-        $this->initialisationFromShop($invoiceTransaction->shop, $request);
+        $invoiceTransaction = $request->route()->parameter('invoiceTransaction');
 
-        $this->handle($invoiceTransaction);
+        if ($invoiceTransaction->invoice->type != InvoiceTypeEnum::REFUND) {
+            $validator->errors()->add('invoiceTransaction', 'Transaction is not a refund');
+        }
+
+        if (!$invoiceTransaction->invoice->in_process) {
+            $validator->errors()->add('invoiceTransaction', 'Refund is not in process');
+        }
     }
+
 
     public function asController(InvoiceTransaction $invoiceTransaction, ActionRequest $actionRequest): void
     {
