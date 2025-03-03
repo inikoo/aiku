845f0921d86ca992a3187fe1abe165d8d6f59bfa
Artha <manager.arthachan@gmail.com>
2025-03-03 16:03:55 +0800

    fix: update sender email

--- resources/js/Pages/Grp/EditModel.vue
+++ resources/js/Pages/Grp/EditModel.vue
@@ -18,13 +18,13 @@ import { routeType } from '@/types/route'
 import PageHeading from '@/Components/Headings/PageHeading.vue';
 import { inject } from 'vue'
 
-import { faUserLock, faShoePrints, faShoppingBag, faBell, faCopyright, faUserCircle, faMobileAndroidAlt, faKey, faClone, faPaintBrush, faMoonStars, faLightbulbOn, faCheck, faPhone, faIdCard, faFingerprint, faLanguage, faAddressBook, faTrashAlt, faSlidersH, faCog, faFlagCheckered } from '@fal'
+import { faUserLock, faEnvelope, faShoePrints, faShoppingBag, faBell, faCopyright, faUserCircle, faMobileAndroidAlt, faKey, faClone, faPaintBrush, faMoonStars, faLightbulbOn, faCheck, faPhone, faIdCard, faFingerprint, faLanguage, faAddressBook, faTrashAlt, faSlidersH, faCog, faFlagCheckered } from '@fal'
 import { faBrowser,faPowerOff } from '@fal'
 import { faBan } from '@far'
 import { Head, usePage } from '@inertiajs/vue3'
 import axios from "axios";
 
-library.add(faBan, faPowerOff, faShoePrints, faShoppingBag, faBrowser, faUserLock,faBell,faCopyright,faUserCircle, faMobileAndroidAlt, faKey, faClone, faPaintBrush, faMoonStars, faLightbulbOn, faCheck, faPhone, faIdCard, faFingerprint,faLanguage,faAddressBook,faTrashAlt, faSlidersH, faCog, faGoogle, faFlagCheckered)
+library.add(faBan, faEnvelope, faPowerOff, faShoePrints, faShoppingBag, faBrowser, faUserLock,faBell,faCopyright,faUserCircle, faMobileAndroidAlt, faKey, faClone, faPaintBrush, faMoonStars, faLightbulbOn, faCheck, faPhone, faIdCard, faFingerprint,faLanguage,faAddressBook,faTrashAlt, faSlidersH, faCog, faGoogle, faFlagCheckered)
 
 
 const props = defineProps<{
