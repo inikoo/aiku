fe2db0e1c7d486e4a71535d65e7fa32279970da4
Ganes556 <gustiganes@gmail.com>
2025-03-06 14:00:40 +0800

    return response

--- app/Actions/Comms/OutboxHasSubscribers/StoreManyOutboxHasSubscriber.php
+++ app/Actions/Comms/OutboxHasSubscribers/StoreManyOutboxHasSubscriber.php
@@ -12,15 +12,18 @@ namespace App\Actions\Comms\OutboxHasSubscribers;
 
 use App\Actions\OrgAction;
 use App\Actions\Traits\Rules\WithNoStrictRules;
+use App\Http\Resources\Mail\OutboxHasSubscribersResource;
 use App\Models\Comms\Outbox;
 use App\Models\Fulfilment\Fulfilment;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Lorisleiva\Actions\ActionRequest;
 
 class StoreManyOutboxHasSubscriber extends OrgAction
 {
     use WithNoStrictRules;
 
-    public function handle(Outbox $outbox, array $modelData): void
+    public function handle(Outbox $outbox, array $modelData): Collection
     {
         $externalEmails = $modelData['external_emails'] ?? [];
         $usersId = $modelData['users_id'] ?? [];
@@ -30,25 +33,37 @@ class StoreManyOutboxHasSubscriber extends OrgAction
             array_map(fn ($userId) => ['user_id' => $userId], $usersId)
         );
 
+        $subscribers = new Collection();
+
         foreach ($subscribersData as $data) {
-            StoreOutboxHasSubscriber::make()->action($outbox, $data);
+            $subscribers->push(OutboxHasSubscribersResource::make(StoreOutboxHasSubscriber::make()->action($outbox, $data)));
         }
+
+        return $subscribers;
+    }
+
+    public function jsonResponse(Collection $subscribers): AnonymousResourceCollection
+    {
+        return OutboxHasSubscribersResource::collection($subscribers);
     }
 
     public function rules(): array
     {
         $rules = [
             'users_id' => [
+                'required_if:external_emails,null',
                 'array',
             ],
 
             'external_emails' => [
+                'required_if:users_id,null',
                 'array',
             ],
         ];
 
         return $rules;
     }
+
 
     public function inFulfilment(Fulfilment $fulfilment, Outbox $outbox, ActionRequest $request)
     {
