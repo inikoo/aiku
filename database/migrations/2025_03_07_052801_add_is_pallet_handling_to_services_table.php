55325a5980f4e2e323584ea65f123922ab883c77
KirinZero0 <aryapermana02@gmail.com>
2025-03-07 13:29:34 +0800

    #1482: Migration for is_pallet_handling

---
+++ database/migrations/2025_03_07_052801_add_is_pallet_handling_to_services_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->boolean('is_pallet_handling')->default(false)->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->dropColumn('is_pallet_handling');
+        });
+    }
+};
