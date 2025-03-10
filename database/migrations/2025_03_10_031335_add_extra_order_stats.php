c9364b67fca4a5101af2e22ec7bb4f745d2b1d30
Ganes556 <gustiganes@gmail.com>
2025-03-10 12:06:10 +0800

    orders migration

---
+++ database/migrations/2025_03_10_031335_add_extra_order_stats.php
@@ -0,0 +1,87 @@
+<?php
+
+use App\Enums\Ordering\Order\OrderStateEnum;
+use App\Enums\Ordering\Order\OrderStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::table('group_ordering_stats', function (Blueprint $table) {
+            $this->orderingFields($table);
+        });
+
+        Schema::table('organisation_ordering_stats', function (Blueprint $table) {
+            $this->orderingFields($table);
+        });
+
+        Schema::table('shop_ordering_stats', function (Blueprint $table) {
+            $this->orderingFields($table);
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::table('group_ordering_stats', function (Blueprint $table) {
+            $this->rollBackOrderingFields($table);
+        });
+
+        Schema::table('organisation_ordering_stats', function (Blueprint $table) {
+            $this->rollBackOrderingFields($table);
+        });
+
+        Schema::table('shop_ordering_stats', function (Blueprint $table) {
+            $this->rollBackOrderingFields($table);
+        });
+    }
+
+
+    public function orderingFields(Blueprint $table): Blueprint
+    {
+
+        $table->unsignedInteger('number_orders_stat_submited_paid')->default(0);
+        $table->unsignedInteger('number_orders_stat_submited_unpaid')->default(0);
+
+
+        foreach (OrderStateEnum::cases() as $case) {
+            $table->decimal('orders_net_org_amount_state_'.$case->snake(), 16)->nullable();
+            $table->decimal('orders_net_grp_amount_state_'.$case->snake(), 16)->nullable();
+            $table->decimal('orders_net_amount_state_'.$case->snake(), 16)->nullable();
+        }
+
+        $table->unsignedInteger('number_orders_state_dispatched_today')->default(0);
+
+        foreach (OrderStatusEnum::cases() as $case) {
+            $table->decimal('orders_net_org_amount_status_'.$case->snake(), 16)->nullable();
+            $table->decimal('orders_net_grp_amount_status_'.$case->snake(), 16)->nullable();
+            $table->decimal('orders_net_amount_status_'.$case->snake(), 16)->nullable();
+        }
+
+        return $table;
+    }
+
+    public function rollBackOrderingFields(Blueprint $table): Blueprint
+    {
+        $table->dropColumn('number_orders_stat_submited_paid');
+        $table->dropColumn('number_orders_stat_submited_unpaid');
+
+        foreach (OrderStateEnum::cases() as $case) {
+            $table->dropColumn('orders_net_org_amount_state_'.$case->snake());
+            $table->dropColumn('orders_net_grp_amount_state_'.$case->snake());
+            $table->dropColumn('orders_net_amount_state_'.$case->snake());
+        }
+
+        $table->dropColumn('number_orders_state_dispatched_today');
+
+        foreach (OrderStatusEnum::cases() as $case) {
+            $table->dropColumn('orders_net_org_amount_status_'.$case->snake());
+            $table->dropColumn('orders_net_grp_amount_status_'.$case->snake());
+            $table->dropColumn('orders_net_amount_status_'.$case->snake());
+        }
+
+        return $table;
+    }
+};
