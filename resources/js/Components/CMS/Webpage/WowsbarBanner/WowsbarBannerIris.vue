5b480424581c3d40fb1c4600b3884527792bbcdc
aqordeon <aqordivika@yahoo.co.id>
2025-03-13 08:36:50 +0800

    fix: SSR: fallback if banner not setup

--- resources/js/Components/CMS/Webpage/WowsbarBanner/WowsbarBannerIris.vue
+++ resources/js/Components/CMS/Webpage/WowsbarBanner/WowsbarBannerIris.vue
@@ -2,6 +2,7 @@
 import SliderLandscape from "@/Components/Banners/Slider/SliderLandscape.vue";
 import SliderSquare from "@/Components/Banners/Slider/SliderSquare.vue";
 import { getStyles } from "@/Composables/styles";
+import { trans } from "laravel-vue-i18n"
 
 
 const props = defineProps<{ 
@@ -17,8 +18,11 @@ const props = defineProps<{
 
 <template>
     <div :style="getStyles(fieldValue?.container?.properties)">
-        <SliderLandscape v-if="fieldValue.compiled_layout.type === 'landscape'" :data="fieldValue.compiled_layout"
+        <SliderLandscape v-if="fieldValue?.compiled_layout?.type === 'landscape'" :data="fieldValue.compiled_layout"
             :production="true" />
-        <SliderSquare v-else :data="fieldValue.compiled_layout" :production="true" />
+        <SliderSquare v-else-if="fieldValue?.compiled_layout?.type === 'square'" :data="fieldValue.compiled_layout" :production="true" />
+        <div v-else class="py-4 w-full bg-gray-100 text-center text-gray-400 italic">
+            {{ trans("Banner is empty") }}
+        </div>
     </div>
 </template>
