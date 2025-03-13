809a0beb2c70e8763e81033ecbc52bc907d9d9a2
dandiAW <dandiferrari75@gmail.com>
2025-03-13 13:58:47 +0800

    Bug uuidv4

--- resources/js/Components/Workshop/SideEditor/ChildFieldSideEditor.vue
+++ resources/js/Components/Workshop/SideEditor/ChildFieldSideEditor.vue
@@ -26,13 +26,13 @@ const emits = defineEmits<{
             <div v-if="form.type != 'hidden'">
                 <div v-if="get(form, 'label', '')" class="my-2 text-xs font-semibold">{{ get(form, 'label', '') }}</div>
                 <ParentFieldSideEditor :blueprint="form" :modelValue="modelValue" :uploadImageRoute="uploadImageRoute" 
-                    @update:modelValue="newValue => emits('update:modelValue', newValue)" :index="index" :key="uuidv4()"/>
+                    @update:modelValue="newValue => emits('update:modelValue', newValue)" :index="index" />
             </div>
         </Accordion>
         
         <section v-else class="">
             <ParentFieldSideEditor :blueprint="form" :modelValue="modelValue" :uploadImageRoute="uploadImageRoute" 
-                @update:modelValue="newValue => emits('update:modelValue', newValue)" :index="index" :key="uuidv4()"/>
+                @update:modelValue="newValue => emits('update:modelValue', newValue)" :index="index" />
         </section>
 
     </div>
