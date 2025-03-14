fd2e93de5c863b777a2f9bb73a1d3df073e662be
KirinZero0 <aryapermana02@gmail.com>
2025-03-14 08:26:58 +0800

    is pallet handling data in index and show

--- app/Actions/Fulfilment/UI/Catalogue/Services/GetFulfilmentServiceShowcase.php
+++ app/Actions/Fulfilment/UI/Catalogue/Services/GetFulfilmentServiceShowcase.php
@@ -29,6 +29,8 @@ class GetFulfilmentServiceShowcase
             'status'      => $service->status,
             'state'       => $service->state,
             'created_at'  => $service->created_at,
+            'created_at'  => $service->created_at,
+            'is_pallet_handling' => $service->is_pallet_handling
         ];
     }
 }
